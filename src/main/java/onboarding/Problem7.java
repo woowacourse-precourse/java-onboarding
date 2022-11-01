b4a2661dcc67596d9b00a8e9b289184acea1e156
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-01 22:37:03 +0900

    [Problem 7] Complete main stream

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,68 +1,73 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Map;
-class Main{
-    public static void main(String[] args){
-        Problem7 prob = new Problem7();
-        String user = "mrko";
-        List<List<String>> friends = List.of(
-                List.of("donut", "andole"),
-                List.of("donut", "jun"),
-                List.of("donut", "mrko"),
-                List.of("shakevan", "andole"),
-                List.of("shakevan", "jun"),
-                List.of("shakevan", "mrko")
-        );
-        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
-        List<String> result = List.of("andole", "jun", "bedi");
-        System.out.println(prob.solution(user, friends, visitors));
-    }
-}
+import java.util.stream.Collectors;
+import java.util.Comparator;
+
 public class Problem7 {
+    static final int CONNECTED_SCORE = 10;
+    static final int VISITED_SCORE = 1;
+    static Set<String> directFriends = new HashSet<>(); // 1촌 친구 리스트
+    static Map<String, Integer> rank = new HashMap<>(); // 모르는 친구 : 점수 
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-
-        // 1촌 친구 리스트
-        List<String> directFriends = new ArrayList<>();
-
-        Map<String, Integer> rank = new HashMap<>();
-
         // 1. 1촌 친구 리스트 업
-        for ( int i = 0 ; i < friends.size() ; i ++ ){
-            String friendA = friends.get(i).get(0);
-            String friendB = friends.get(i).get(1);
-            if( friendA.equals(user) ) directFriends.add(friendB);
-            if( friendB.equals(user) ) directFriends.add(friendA);
-        }
-
+        getFriendsList(user, friends);
         // 2. friends를 돌며 1촌 친구들의 자신이 아닌 다른 친구들을 체크 -> HashMap
+        giveConnectedFriendScore(user, friends);
+        // 3. visitor를 순회하며 +1 씩
+        giveVisitedFriendScore(user, friends, visitors);
+        // 4, 5. Comparator 사용하여 HashMap 정렬 / 정렬된 HashMap의 key 값 5개 추출
+        return getTop5List();
+    }
+    static void getFriendsList(String user, List<List<String>> friends){
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                if (friend.get(0).equals(user))
+                    directFriends.add(friend.get(1));
+                else
+                    directFriends.add(friend.get(0));
+            }
+        }
+    }
+    static void giveConnectedFriendScore(String user, List<List<String>> friends ){
         for ( int i = 0 ; i < friends.size() ; i ++ ) {
             String friendA = friends.get(i).get(0);
             String friendB = friends.get(i).get(1);
             for ( int j = 0 ; j < directFriends.size() ; j ++ ){
-                if ( directFriends.get(j).equals(friendA) && ! friendB.equals(user)) 
-                    rank.put(friendB, rank.getOrDefault(friendB, 0) + 10 );
-                if ( directFriends.get(j).equals(friendB) && ! friendA.equals(user) ) 
-                    rank.put(friendA, rank.getOrDefault(friendA, 0) + 10 );
+                if ( directFriends.contains(friendA) && ! friendB.equals(user)) 
+                    rank.put(friendB, rank.getOrDefault(friendB, 0) + CONNECTED_SCORE );
+                if ( directFriends.contains(friendB) && ! friendA.equals(user) ) 
+                    rank.put(friendA, rank.getOrDefault(friendA, 0) + CONNECTED_SCORE );
             }
         }
-
-        // 3. visitor를 순회하며 +1 씩
+    }
+    static void giveVisitedFriendScore(String user, List<List<String>> friends, List<String> visitors){
         for ( int i = 0 ; i < visitors.size() ; i ++ )
             if (! directFriends.contains(visitors.get(i)))
                 rank.put(visitors.get(i), rank.getOrDefault(visitors.get(i), 0) + 1);
-        
-        for( String key : rank.keySet() ){
-            System.out.println(key + " " + rank.get(key));
-        }            
-        /*
-        4. Comparator 사용하여 HashMap 정렬
-        5. 정렬된 HashMap의 key 값 5개 추출
-        */
-        return answer;
+
+        // for ( int i = 0 ; i < friends.size() ; i ++ ) {
+        //     String friendA = friends.get(i).get(0);
+        //     String friendB = friends.get(i).get(1);
+        //     for ( int j = 0 ; j < directFriends.size() ; j ++ ){
+        //         if ( directFriends.get(j).equals(friendA) && ! friendB.equals(user)) 
+        //             rank.put(friendB, rank.getOrDefault(friendB, 0) + CONNECTED_SCORE );
+        //         if ( directFriends.get(j).equals(friendB) && ! friendA.equals(user) ) 
+        //             rank.put(friendA, rank.getOrDefault(friendA, 0) + CONNECTED_SCORE );
+        //     }
+        // }
+    }
+    static List<String> getTop5List() {
+        return rank.entrySet().stream()
+                .filter(v -> v.getValue() > 0 && !directFriends.contains(v.getKey()))
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
