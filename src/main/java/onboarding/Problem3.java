f82523b94547801cc196cbda526a2dcb55daa476
이규승 <rpg8988@naver.com>
2022-11-01 19:22:54 +0900

    update: solution에서 체크한 3 6 9의 갯수를 answer에 넣어서 리턴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -13,6 +13,9 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += solution1(i);
+        }
         return answer;
     }
 }
