5886036a8c993d7dc0fe72d815c9249311ecd68f
geonho <soo_1992@naver.com>
2022-11-01 17:48:29 +0900

    Problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder(1000);
+
+        for (int i = 0; i < word.length(); i++) {
+            char character = word.charAt(i);
+            if ('A' <= character && character <= 'Z') {
+                answer.append((char) ('A' + 'Z' - character));
+            } else if ('a' <= character && character <= 'z') {
+                answer.append((char) ('a' + 'z' - character));
+            } else {
+                answer.append(character);
+            }
+        }
+        return answer.toString();
     }
 }
