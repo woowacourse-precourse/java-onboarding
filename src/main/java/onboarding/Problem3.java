292dc3c75a214e7333b87bcea3ce498537dc97b4
tgoddessana <twicegoddessana1229@gmail.com>
2022-11-01 19:11:19 +0900

    refactor : Problem3 풀이 개선

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,43 +1,26 @@
 package onboarding;
 
+/**
+ * 숫자 number가 매개변수로 주어질 때,
+ * 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 return 하도록 solution 메서드를 완성하라.
+ * */
 public class Problem3 {
-
-    public static int countChar(String str, char ch) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) {
-                count++;
-            }
-        }
-        return count;
-    }
-
     public static int solution(int number) {
         int clapCount = 0;
-        for (Integer i = 0; i <= number; i++) {
-            String numtoString = i.toString();
-            if (numtoString.contains("3") && numtoString.contains("6") && numtoString.contains("9")) {
-                System.out.printf("%d 일 때 +3\n", i);
-                clapCount = clapCount + 3;
-            } else if ((numtoString.contains("3") && numtoString.contains("6")) || (numtoString.contains("6") && numtoString.contains("9")) || (numtoString.contains("3") && numtoString.contains("9"))) {
-                System.out.printf("%d 일 때 +2\n", i);
-                clapCount = clapCount + 2;
-            } else if (numtoString.contains("3") || numtoString.contains("6") || numtoString.contains("9")) {
-                if ((countChar(numtoString, '3') == 1)||(countChar(numtoString, '6') == 1)||(countChar(numtoString, '9') == 1)){
-                    System.out.printf("%d 일 때 +1\n", i);
-                    clapCount = clapCount + 1;
-                } else if ((countChar(numtoString, '3') == 2)||(countChar(numtoString, '6') == 2)||(countChar(numtoString, '9') == 2)) {
-                    System.out.printf("%d 일 때 +2\n", i);
-                    clapCount = clapCount + 2;
-                } else if ((countChar(numtoString, '3') == 3)||(countChar(numtoString, '6') == 3)||(countChar(numtoString, '9') == 3)) {
-                    System.out.printf("%d 일 때 +3\n", i);
-                    clapCount = clapCount + 3;
+        String numToString;
+
+        // 숫자에 대해서 모두 순회
+        for (int i = 1; i <= number; i++) {
+            numToString = i + "";
+
+            // 숫자 하나에 대해서 문자별로 순회
+            for (int j = 0; j < numToString.length(); j++) {
+                if (numToString.charAt(j) == '3' || numToString.charAt(j) == '6' || numToString.charAt(j) == '9') {
+                    clapCount++;
                 }
             }
-
         }
-
-
         return clapCount;
     }
 }
+
