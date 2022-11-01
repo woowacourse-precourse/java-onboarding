1b395e50a03b3c563c810b29ae12ce43648b2c69
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:32:01 +0900

    Problem3 javadocs
    Method and class description

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,17 @@
 package onboarding;
 
+/**
+ * Problem 3
+ * @author Davin An
+ * @version 1.0
+ */
 public class Problem3 {
+
+    /**
+     * Counts the occurrences of 3, 6, and 9
+     * @param number Counts within the range of 1 through given number
+     * @return Number of occurrences
+     */
     public static int solution(int number) {
         int count = 0;
         String temp;
