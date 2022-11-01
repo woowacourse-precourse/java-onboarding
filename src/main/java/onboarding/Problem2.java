106f658c5f4f221e44071f25654df914c778a142
ahinkim <ahin98@naver.com>
2022-11-01 10:34:00 +0900

    refactor(problem2.md): Add function to repeat until there are no duplicate characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,20 +3,32 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        int N = cryptogram.length();
-        // 1. 연속하는 중복 문자를 삭제한다.
-        char prev = cryptogram.charAt(0);
-        String res = cryptogram.substring(0, 1);
-        for (int i = 1; i < N; i++){
-            char now = cryptogram.charAt(i);
-            if(prev != now){
-                res += now;
+        // 2. 중복 문자가 없을 때까지 반복한다.
+        while(true) {
+            int N = cryptogram.length();
+            int f = 0;
+            if(N==0){
+                break;
             }
-            else{
-                res = res.substring(0, res.length()-1);
+            // 1. 연속하는 중복 문자를 삭제한다.
+            char prev = cryptogram.charAt(0);
+            String res = cryptogram.substring(0, 1);
+            for (int i = 1; i < N; i++) {
+                char now = cryptogram.charAt(i);
+                if (prev != now) {
+                    res += now;
+                } else {
+                    res = res.substring(0, res.length() - 1);
+                    f = 1;
+                }
+                prev = now;
             }
-            prev = now;
+            cryptogram = res;
+            // 중복이 없는 상태
+            if (f!=1){
+                break;
+            }
         }
-        return answer;
+        return cryptogram;
     }
 }
