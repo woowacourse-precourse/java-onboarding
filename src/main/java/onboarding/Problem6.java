9a95c0db4720fc8d7718e5136d9eed00f485bea3
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-01 23:41:48 +0900

    feat: Add Problem6 solution

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem6 {
+
     //email 유효성 검사
     public static boolean isValidEmail(String email) {
         String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@email.com";
