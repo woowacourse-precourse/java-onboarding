20050ec1967d9551068c78bfbc9323ae726306d0
hwanee__ <fkfkthth@naver.com>
2022-11-01 16:06:24 +0900

    4번문제_구현_완료_

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,29 +5,24 @@ import java.util.HashMap;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        HashMap<Character,Character> up_map = new HashMap<>();
-        HashMap<Character,Character> low_map = new HashMap<>();
 
-        for(int i=0; i < 26; i++ ){
-            int key_alpha = 65 + i;
-            int val_alpha = 90 - i;
-            char key = (char)key_alpha;
-            char val = (char)val_alpha;
-            up_map.put(key,val);
+        for(int i=0;i<word.length();i++){
+            if(word.charAt(i)>='A' && word.charAt(i)<='Z'){
+                answer += up_change_word(word.charAt(i));
+            }else if(word.charAt(i)>='a' && word.charAt(i)<='z') {
+                answer += low_change_word(word.charAt(i));
+            }else answer += ' ';
         }
-
-        for(int i=0; i < 26; i++ ){
-            int key_alpha = 97 + i;
-            int val_alpha = 122 - i;
-            char key = (char)key_alpha;
-            char val = (char)val_alpha;
-            low_map.put(key,val);
-        }
-
-
-
 
 
         return answer;
+    }
+
+    static char up_change_word(char ch) {
+        return (char)('Z' - ch + 'A');
+    }
+
+    static char low_change_word(char ch){
+        return (char)('z' - ch + 'a');
     }
 }
