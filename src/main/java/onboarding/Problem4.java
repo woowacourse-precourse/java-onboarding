00245072916b95f9b8ab2e7e90d810162cc13834
brotherstone97 <brotherstone97@gmail.com>
2022-11-01 20:28:34 +0900

    Refactor: Problem4 코드 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,21 +5,26 @@ public class Problem4 {
         int asciiCode = c;
         if (65 <= asciiCode && asciiCode <= 90)
             return (char) (155 - asciiCode);
-
         return (char) (219 - asciiCode);
     }
 
-    public static String solution(String word) {
-        String answer = "";
+    static String concatResult(String word) {
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             if (Character.isLetter(c)) {
                 char res = changeCharacter(c);
-                answer += res;
+                result.append(res);
                 continue;
             }
-            answer += c;
+            result.append(c);
         }
+        return result.toString();
+    }
+
+    public static String solution(String word) {
+        String answer = "";
+        answer = concatResult(word);
         return answer;
     }
 }
