2ba8068a7d4580efe0f5b88ee7096c69bfa2adb4
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 13:52:56 +0900

    [2번] feat: 두 문자가 같은지 체크하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,10 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    // 7. 두 문자가 같은 지 체크하기
+    public static Boolean checkIfNotDuplicatedAlphabet(char a, char b)
+    {
+        return (a == b) ? Boolean.FALSE : Boolean.TRUE;
+    }
 }
