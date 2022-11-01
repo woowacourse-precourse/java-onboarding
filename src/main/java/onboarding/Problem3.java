186c0530df4f49a6cb53e77800940a3ab20957b2
hum02 <ehdgma02@gmail.com>
2022-11-01 10:12:56 +0900

    refactor:3번 369일치 한 if문으로 통합

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,14 +4,14 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        for(int i=1; i<=number; i++){
+        for (int i=1; i<=number; i++) {
             Integer num = i;
             char[] digits = num.toString().toCharArray();
 
-            for( char digit :digits){
-                if(digit == '3'){ ++answer;}
-                if(digit == '6'){ ++answer;}
-                if(digit == '9'){ ++answer;}
+            for (char digit:digits) {
+                if ((digit == '3') || (digit == '6') || (digit == '9')) {
+                    ++answer;
+                }
             }
         }
 
