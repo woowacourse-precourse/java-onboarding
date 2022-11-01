18743784547f1c3556e94c187fee24a98491182b
yenawee <yaena0319@naver.com>
2022-11-01 16:04:05 +0900

    [FEAT] 4. 문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,4 +15,16 @@ public class Problem4 {
             return true;
         return false;
     }
+    private static char translateChar(char c){
+        boolean flag = Character.isLowerCase(c);
+        if (flag)
+            c -= 32;
+        if (c < 'N')
+            c = (char)('Z' - (c - 'A'));
+        else
+            c = (char)('A' + ('Z' - c));
+        if (flag)
+            c += 32;
+        return c;
+    }
 }
