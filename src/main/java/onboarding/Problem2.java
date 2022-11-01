2157355f1f0b901a3fbe5400da38b2aee547ba5e
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-01 16:23:12 +0900

    test2 - 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        return answer;
+    }
+
+    // 중복 문자 제거
+    public static String removeStr(String str) {
+        String answer = "";
+
+        for(int i=0; i<str.length()-1; i++) {
+            if(str.charAt(i)==str.charAt(i+1))
+                i++;
+            else
+                answer += str.charAt(i);
+        }
+
+        answer += str.charAt(str.length()-1);
+
         return answer;
     }
 }
