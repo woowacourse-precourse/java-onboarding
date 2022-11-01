be6edfa0c602d2b57d3e9f4b58199bded8551acc
seungjun <seungjun123>
2022-11-01 12:25:35 +0900

    페이지 번호의 각 자리 숫자를 모두 더하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,13 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static int add(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n%10;
+            n /= 10;
+        }
+        return sum;
+    }
 }
