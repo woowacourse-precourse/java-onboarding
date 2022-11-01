55a1ca47d435cff27e68c0640593bb0ce47b8f79
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:20:27 +0900

    문제4 : 알파벳 List를 반환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
+    }
+
+    /**
+     * 
+     * @param isUpper 대문자 여부
+     * @return {@code} ArrayList<String>} 알파벳의 역순 목록
+     */
+    private static ArrayList<String> getAlphabetList(boolean isUpper) {
+        ArrayList<String> res = new ArrayList<>();
+
+        int lastCharASCII = 122;
+
+        if (isUpper) {
+            lastCharASCII = 90;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            String alphabet = String.valueOf((char) (lastCharASCII - i));
+            res.add(alphabet);
+        }
+
+        return res;
     }
 }
