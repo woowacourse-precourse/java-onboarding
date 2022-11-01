a258393eee67ad2ec5ce0650709c60fe0b70ad31
이지영 <wldudwldhd@naver.com>
2022-11-01 13:44:25 +0900

    docs:기능구현,예외처리포함

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /*기능구현
+    예외처리 : money의 범위가 1보다 작거나, 1000000보다 클 때, 예외
+    1. 단위가 큰 금액부터 money를 나누고 그 나머지를 다음으로 큰 금액이 나눈다.
+    money != 0일때까지 반복하는 함수를 구현.
+    * */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
