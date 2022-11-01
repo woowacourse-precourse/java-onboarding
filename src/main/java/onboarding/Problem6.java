7ab8e3efdccc1166bc291a8f40421b8bd4e908e1
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-11-01 20:09:24 +0900

    Docs(Problem6): 추가 기능 목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/*
+기능 목록
+1. 중복 닉네임 검출
+2. 중복 닉네임 오름차순 중복제거 출력
+3. 이메일 입력 형식 예외 처리
+4. 닉네임 입력 형식 예외 처리
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
