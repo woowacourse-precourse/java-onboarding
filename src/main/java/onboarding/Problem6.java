ab6502bfe54d6ef53b2712bc604bdc9be6c83a36
aran <aran0317@naver.com>
2022-11-01 21:41:07 +0900

    문제 6번 기능요구사항

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,14 @@ package onboarding;
 
 import java.util.List;
 
+/*기능 요구 사항*/
+/* 1. 닉네임 중 같은 글자가 연속적으로 포함될 경우 제한
+ * 2. 같은 글자가 연속적으로 포함되는 지원자의 이메일 목록을 return
+ <solution>
+ *  - 모두의 이름을 배열 name 안에 넣은 후,
+ *  - 연속되는 두글자가 자신이 아닌 name에 포함되는지 확인
+ *  - 포함되면, answer에 이메일을 추가
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
