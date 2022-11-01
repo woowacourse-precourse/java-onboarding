f1cdff62f2577307d9e0bf7f1ffd34411e58e558
suhwan <popomarin28@gmail.com>
2022-11-01 16:25:38 +0900

    [problem6] 닉네임을 2마디씩 자르면서 그에 중복되면 email을 list에 추가하는 map 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<List<String>> getValidList(List<List<String>> forms){
@@ -19,7 +19,21 @@ public class Problem6 {
         return forms;
     }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+        List<String> answer = new ArrayList<String>();
+        List<List<String>> validForms = getValidList(forms);
+        if(forms.size() < 1 || forms.size() > 10000)
+            return answer;
+        //닉네임을 2개식 분리 하면서 중복된 이메일 add
+        Map<String, List<String>> listMap = new HashMap<>();
+        for (List<String> validForm : validForms) {
+            for (int j = 0; j < validForm.get(1).length() - 1; j++) {
+                if (listMap.containsKey(validForm.get(1).substring(j, j + 2)))
+                    listMap.get(validForm.get(1).substring(j, j + 2)).add(validForm.get(0));
+                else {
+                    List<String> emailList = new ArrayList<String>();
+                    emailList.add(validForm.get(0));
+                    listMap.put(validForm.get(1).substring(j, 2), emailList);
+                }
+            }
+        }
 }
