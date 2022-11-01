dce8a404e9d9fb4528e3539ae96878c502c5fb5d
younghoon211@naver.com <younghoon211@naver.com>
2022-11-01 20:54:34 +0900

    feat(Problem1): 예외처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,4 +10,30 @@ class Problem1 {
 
         return compareNum(maxSum(pobi), maxSum(crong));
     }
+
+    private static boolean exceptionCase(List<Integer> pages) {
+
+        final int START_PAGE = 1;
+        final int END_PAGE = 400;
+
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        // 페이지에 첫번째 또는 마지막 페이지가 포함된 경우
+        if (pages.contains(START_PAGE) || pages.contains(END_PAGE)) {
+            return true;
+        }
+
+        // 왼쪽 페이지가 홀수, 오른쪽 페이지가 짝수가 아닌 경우
+        if (leftPage % 2 != 1 || rightPage % 2 != 0) {
+            return true;
+        }
+
+        // 페이지의 범위가 1미만 400초과일 경우
+        if (leftPage < START_PAGE || rightPage > END_PAGE) {
+            return true;
+        }
+
+        return false;
+    }
 }
