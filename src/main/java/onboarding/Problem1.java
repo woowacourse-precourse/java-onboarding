578189a5a16f74f1bd2841f009ceffbafc391798
geonho <soo_1992@naver.com>
2022-11-01 17:42:06 +0900

    isValid() 메소드 구현 : 입력 페이지 값의 유효성을 확인한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,22 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!isValid(pobi) || !isValid(crong)) {
+            answer = -1;
+        }
+
         return answer;
     }
+
+    private static boolean isValid(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if (leftPage <= 1 || rightPage >= 400 || leftPage + 1 != rightPage) {
+            return false;
+        }
+        return true;
+    }
 }
+
