5a2e0f61d03e46f353378fc333ff86f0544cf344
devEdok13 <dev.edok13@gmail.com>
2022-11-01 16:25:14 +0900

    feat: P06-staticVariable

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    //  Static-Variable
+    private static final String domain = "email.com";
+    private static final int MIN_EMAIL_LENGTH = 11;
+    private static final int MAX_EMAIL_LENGTH = 20;
+    private static final int MIN_ID_LENGTH = 1;
+    private static final int MAX_ID_LENGTH = 20;
+    private static final String ID_PATTERN = "[ㄱ-ㅎㅏ-ㅣ가-힣]";
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
