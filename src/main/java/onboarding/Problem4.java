15ffba4b64fafd993f6f54fb45ba5f739a3662cf
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:30:47 +0900

    Feat: Add 문자 변환 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,17 @@ public class Problem4 {
         return answer;
     }
 
+
+    private static String convertWord(String word) {
+        String result = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            result += convertToFrogDictionary(word.charAt(i));
+        }
+
+        return result;
+    }
+
     
     private static char convertToFrogDictionary(char word) {
         if (65 <= (byte)word && (byte)word <= 90) {
