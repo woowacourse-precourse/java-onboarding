ef6bf65df7fa56c11d53281c2df0efdb7f62d751
신채원 <cw121@naver.com>
2022-11-01 18:00:05 +0900

    feat(problem3): 그 숫자에 손뼉을 몇 번 쳐야 하는지 횟수 세는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    static int countClap(int number){
+        int count=0;
+        while(number!=0){
+            if(number % 10 == 3 || number % 10 == 6 || number % 10 == 9)
+                count++;
+            number /= 10;
+        }
+        return count;
     }
 }
