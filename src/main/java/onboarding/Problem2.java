e0b0018b27d130d222a7ce0a2b45679a83c7af5a
gmu1026 <44140480+gmu1026@users.noreply.github.com>
2022-11-01 20:51:33 +0900

    Problem2 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,43 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int length = 0;
+        do {
+            length = cryptogram.length();
+            cryptogram = deleteDuplicate(cryptogram);
+        } while (length != cryptogram.length());
+
+        return cryptogram;
+    }
+
+    private static String deleteDuplicate(String cryptogram) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int length = cryptogram.length();
+        for (int i = 0; i < length; i++) {
+            char currentCharacter = cryptogram.charAt(i);
+
+            if (i == 0 && currentCharacter != cryptogram.charAt(i + 1)) {
+                stringBuilder.append(currentCharacter);
+                continue;
+            } else if (i == 0 && currentCharacter == cryptogram.charAt(i + 1)) {
+                continue;
+            }
+
+            if (i == length - 1 && currentCharacter != cryptogram.charAt(i - 1)) {
+                stringBuilder.append(currentCharacter);
+                continue;
+            } else if (i == length - 1 && currentCharacter == cryptogram.charAt(i - 1)) {
+                continue;
+            }
+
+            char previousCharacter = cryptogram.charAt(i - 1);
+            char nextCharacter = cryptogram.charAt(i + 1);
+            if (currentCharacter != nextCharacter
+                    && previousCharacter != currentCharacter) {
+                stringBuilder.append(currentCharacter);
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
