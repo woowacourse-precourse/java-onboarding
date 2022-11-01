cbc091309f78b5202528c13142bcb5ac35de82c1
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-01 19:43:31 +0900

    feat: Problem6 닉네임을 두 글자씩 변환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        List<String> nameList = separateNames(forms);
+        List<String> reduNameList = reduplicatedNames(nameList);
+        List<String> emailList = findEmails(reduNameList, forms);
+
+        return emailList;
+    }
+
+    /**
+     * 닉네임을 두 글자씩 변환
+     * @param forms
+     * @return List<String>
+     */
+    private static List<String> separateNames(List<List<String>> forms) {
+        List<String> nameList = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(1);
+            for (int j = 0; j < name.length() - 1; j++) {
+                nameList.add(name.substring(j, j + 2));
+            }
+        }
+        return nameList;
     }
 }
