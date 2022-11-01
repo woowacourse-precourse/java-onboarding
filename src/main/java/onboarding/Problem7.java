27364ba7e737c501d97cd05d81a1db31189df760
tenta <eunoia3802@gmail.com>
2022-11-01 13:07:04 +0900

    feat: 추천 점수에 따른 친구 목록 출력 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,76 +1,106 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
-    static HashMap<String, Integer> members;
+    static Set<String> members;
+    static List<String> recommendFriends;
+    static Set<String> userFriends;
+    static List<String> memberList;
+    static int[] memberPoint;
+    static int max;
+    static List memberPointList;
+
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         createFriendsList(user, friends, visitors);
         recommendPoint(user, friends, visitors);
-        List<String> answer = Collections.emptyList();
+        List<String> answer = recommendList();
         return answer;
     }
 
-    public static HashMap createFriendsList(String user, List<List<String>> friends, List<String> visitors) {
-        members = new HashMap<String, Integer>();
+    public static List<String> createFriendsList(String user, List<List<String>> friends, List<String> visitors) {
+        members = new HashSet<>();
+        userFriends = new HashSet<>();
         for (int i = 0; i < friends.size(); i++) {
             for (int j = 0; j < 2; j++) {
                 String name = friends.get(i).get(j);
-                members.put(name, 0);
+                if (j == 0 && name == user) {
+                    userFriends.add(friends.get(i).get(1));
+                }
+                if (j == 1 && name == user) {
+                    userFriends.add(friends.get(i).get(0));
+                }
+                members.add(name);
             }
         }
         for (int i = 0; i < visitors.size(); i++) {
             String name = visitors.get(i);
-            members.put(visitors.get(i), 0);
+            members.add(name);
         }
         members.remove(user);
-        return members;
+        for (String name : userFriends) {
+            members.remove(name);
+        }
+        memberList = new ArrayList<>(members);
+        return memberList;
     }
 
-    public static void recommendPoint(String user, List<List<String>> friends, List<String> visitors) {
+    public static List<Integer> recommendPoint(String user, List<List<String>> friends, List<String> visitors) {
+        memberPoint = new int[memberList.size()];
         for (int i = 0; i < friends.size(); i++) {
-            String userFriendL = friends.get(i).get(0);
-            String userFriendR = friends.get(i).get(1);
+            if (friends.get(i).contains(user)) {
+                continue;
+            }
+            for (String name : userFriends) {
+                if (friends.get(i).contains(name)) {
+                    if (friends.get(i).get(0) == name) {
+                        int index = memberList.indexOf(friends.get(i).get(1));
+                        memberPoint[index] += 10;
+                    }
+                    if (friends.get(i).get(1) == name) {
+                        int index = memberList.indexOf(friends.get(i).get(0));
+                        memberPoint[index] += 10;
+                    }
+                }
+            }
+        }
+        for (String visitor : visitors) {
+            if (memberList.contains(visitor)) {
+                int index = memberList.indexOf(visitor);
+                memberPoint[index]++;
+            }
+        }
+        for (int i = 0; i < memberPoint.length; i++) {
+        }
+        memberPointList = Arrays.stream(memberPoint).boxed().collect(Collectors.toList());
 
-            if (userFriendL.equals(user)) {
-                for (int j = 0; j < friends.size(); j++) {
-                    String friendOfFriendL = friends.get(j).get(0);
-                    String friendOfFriendR = friends.get(j).get(1);
-                    if (friendOfFriendL.equals(userFriendR) && !friendOfFriendR.equals(user)) {
-                        int oldValue = members.get(friendOfFriendR);
-                        int newValue = oldValue + 10;
-                        members.replace(friendOfFriendR, newValue);
-                    }
-                    if (friendOfFriendR.equals(userFriendR) && !friendOfFriendL.equals(user)) {
-                        int oldValue = members.get(friendOfFriendR);
-                        int newValue = oldValue + 10;
-                        members.replace(friendOfFriendL, newValue);
-                    }
+        return memberPointList;
+    }
+
+    public static List<String> recommendList() {
+        recommendFriends = new ArrayList<>();
+        for (int i = 0; i < memberPointList.size(); i++) {
+            max = (int) memberPointList.get(i);
+            for (int j = 0; j < memberPointList.size(); j++) {
+                if ((int) memberPointList.get(j) > max) {
+                    max = (int) memberPointList.get(j); //20
                 }
             }
-            if (userFriendR.equals(user)) {
-                for (int j = 0; j < friends.size(); j++) {
-                    String friendOfFriendL = friends.get(j).get(0);
-                    String friendOfFriendR = friends.get(j).get(1);
-                    if (friendOfFriendL.equals(userFriendL) && !friendOfFriendR.equals(user)) {
-                        int oldValue = members.get(friendOfFriendR);
-                        int newValue = oldValue + 10;
-                        members.replace(friendOfFriendR, newValue);
-                    }
-                    if (friendOfFriendR.equals(userFriendL) && !friendOfFriendL.equals(user)) {
-                        int oldValue = members.get(friendOfFriendR);
-                        int newValue = oldValue + 10;
-                        members.replace(friendOfFriendL, newValue);
+            if (max == 0) {
+                break;
+            }
+            for (int j = 0; j < memberPointList.size(); j++) {
+                if ((int) memberPointList.get(j) == max) {
+                    recommendFriends.add(memberList.get(j));
+                    memberPointList.set(j, 0);
+                    if (j == memberPointList.size()) {
+                        Collections.sort(recommendList(), Collections.reverseOrder());
                     }
                 }
             }
         }
-        for (int i = 0; i < visitors.size(); i++) {
-            String visitor = visitors.get(i);
-            int oldValue = members.get(visitor);
-            int newValue = oldValue + 1;
-            members.replace(visitor, newValue);
-        }
+        return recommendFriends;
     }
 }
