c40ad1f5572f2397b00c26c4653bcaea79eebcc7
cocomozila <joseyun94@naver.com>
2022-11-01 14:37:37 +0900

    [문제 2] 문자열 암호해독 메서드 틀 만들기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem2 {
+    // 전체 기능 호출
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decryption(cryptogram);
+    }
+    // 문자열 암호해독 메서드
+    private static String decryption(String cryptogram) {
+        StringBuilder answer = new StringBuilder();
+        char[] splitCryptogram = cryptogram.toCharArray();
+        boolean[] isReduplication = new boolean[splitCryptogram.length];
+        int reduplicationCount = 0;
+
+        return "";
     }
 }
