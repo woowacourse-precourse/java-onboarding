29db2405417f2c13e355f499e864cd4f45dab494
youswim <youswim96@gmail.com>
2022-11-01 22:05:07 +0900

    Feat: user와 친구인 사람들의 이름 List 반환하는 서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,6 +8,23 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+
+    // user와 친구인 사람들의 이름 List반환
+    private static List<String> getUserFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+        List<String> tmp;
+
+        for (int i = 0; i < friends.size(); i++) {
+            tmp = friends.get(i);
+            for (int j = 0; j < tmp.size(); j++) {
+                if (tmp.get(j).equals(user)) {
+                    userFriends.add(tmp.get(1 - j));
+                }
+            }
+        }
+        return userFriends;
     }
 
     // 추천 친구와 점수를 기록하기 위한 클래스
