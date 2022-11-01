66320030a72c23387baa2cad77da4d085b3df28d
RadSparkle <wwwartg1@naver.com>
2022-11-01 20:32:18 +0900

    FEAT : 도메인에 email.com이 포함되어있는지 확인하는 메소드 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        boolean checkDomain = checkDomain(forms);
+        if(!checkDomain) {
+            List<String> falseDomain = new ArrayList<>();
+            falseDomain.add("false");
+            return falseDomain;
+        }
+
+
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static boolean checkDomain(List<List<String>> forms) {
+        for(int i = 0, j=0; i<forms.size(); i++) {
+            String checkForms = forms.get(i).get(j);
+
+            if(!checkForms.contains("email.com")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
