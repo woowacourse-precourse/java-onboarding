192d9b358b696aec9358819a591130db54a6f1fc
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:26:29 +0900

    [CREATE] 청개구리 사전을 통해 단어 변환할 수 있는 기능 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        HashMap<Character,Character> map = makeHashMap();
+
         return answer;
     }
+
+    public static HashMap<Character,Character> makeHashMap(){
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        char upperBackCh = 'Z';
+        char lowerBackCh = 'z';
+
+        for(char upperFrontCh = 'A'; upperFrontCh <= 'Z'; upperFrontCh++){
+            map.put(upperFrontCh, upperBackCh);
+            upperBackCh = (char) (upperBackCh-1);
+        }
+
+        for(char lowerFrontCh = 'a'; lowerFrontCh < 'z'; lowerFrontCh++){
+            map.put(lowerFrontCh, lowerBackCh);
+            lowerBackCh = (char) (lowerBackCh-1);
+        }
+        return map;
+    }
+
 }
