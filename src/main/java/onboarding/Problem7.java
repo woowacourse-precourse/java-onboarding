16793123afd9beeb8736eea4adeb264e6f8b99b7
kimdoha <kkhhjj3393@gmail.com>
2022-11-01 14:14:00 +0900

    feat: 유저의 친구 리스트를 얻는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,4 +14,17 @@ public class Problem7 {
                pairs.get(0);
     }
 
+    private static List<String> getfriendListOfUser(String user, List<List<String>> friends) {
+        ArrayList<String> friendList = new ArrayList<>();
+
+        for(List<String> pairs : friends) {
+            if(pairs.contains(user)) {
+                String addFriend = findFriendInPairs(user, pairs);
+                friendList.add(addFriend);
+            }
+        }
+
+        return friendList;
+    }
+
 }
