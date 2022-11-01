bc4ac6bb45092d802ee5a88c6dc4ca71e132f2dc
김형기 <khk211113@naver.com>
2022-11-01 23:21:09 +0900

    [FEAT] Problem2 - 중복된 부분 제거하여 반환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int idx;
+        while ((idx = getDuplicateIdx(cryptogram)) != -1) {
+            String prefix = idx == 0 ? "" : cryptogram.substring(0, idx);
+            String suffix = cryptogram.substring(idx + 2);
+            cryptogram = prefix + suffix;
+        }
+
+        return cryptogram;
     }
 
     // 중복되는 문자의 위치 정보를 반환 i와 i+1이 중복됨, 없으면 -1
