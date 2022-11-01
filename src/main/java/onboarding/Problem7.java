a65dbfc2cbf2f2c010a6975a6795d6b40e841a3c
Lee-SungMin <saevers26@gmail.com>
2022-11-01 21:14:01 +0900

    Add: 7번 문제 기능 구현 - user와 친구인 사람을 저장하는 기능 구현 - 이름과 점수를 함께 저장하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // 이미 user 와 친구인 사람을 저장할 리스트
+        List<String> user_friend_list = new ArrayList<>();
+
+        // 이름과 점수를 함께 저장
+        Map<String, Integer> map = new TreeMap<>();
+
         return answer;
     }
 }
