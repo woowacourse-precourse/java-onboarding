1fdfb675aec4357a99542a98e5ef730916e26aa2
ye1ne <yewon7036@naver.com>
2022-11-01 16:48:28 +0900

    docs : 문제4번에 대한 풀이 및 구현 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+/**
+ * 문제 풀이 방식 & 기능 목록서는
+ * docs/PROBLEM4.md 의 하단에 기술되어 있습니다
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+
 }
