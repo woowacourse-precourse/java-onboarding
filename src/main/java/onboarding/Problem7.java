aa5b9b395ec474e693fe1d8af9f984d282a591bb
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-01 21:17:54 +0900

    feat : 문제7 친구 목록과 친구추천 목록 분리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -21,6 +21,19 @@ public class Problem7 {
         for(String name : visitors) userlist.add(name);
         userlist.remove(user);
 
+        //2) 친구 목록(HashSet)과 친구 추천 가능 회원 목록(HashMap) 분리
+        for(String name : userlist) recommendlist.put(name,0);
+        for(int i=0; i<friends.size(); i++) {
+            String name1 = friends.get(i).get(0);
+            String name2 = friends.get(i).get(1);
+            if (name1 == user) {
+                friendlist.add(name2);
+                recommendlist.remove(name2);
+            } else if (name2 == user) {
+                friendlist.add(name1);
+                recommendlist.remove(name1);
+            }
+        }
 
         return answer;
     }
