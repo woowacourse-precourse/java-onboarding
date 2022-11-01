98b6c9b91db1643a41fa98dda9845db440edb8e3
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:39:47 +0900

    범위 설정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,5 +3,13 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) { // 중복된 문자를 찾아서 자신을 두번 제거하는 방식으로
         StringBuilder duplicate = new StringBuilder(cryptogram); // 조작이 불가능한 String을 대신해서 사용 deleteCharAt 메서드 사용하기 위해서
+        int cryptoLen = cryptogram.length();
+        int i;
+
+        for (i = 0; i < cryptoLen; i++) // 전체 문자열을 돌면서 인접한 문자와 중복한 위치 찾기
+        {
+
+        }
+        return duplicate.toString();
     }
 }
