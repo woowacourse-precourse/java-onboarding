38634d56b8874fb2cf2053dd469f64a4c7b62b3c
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-01 15:58:41 +0900

    refactor(Problem5): 띄어쓰기 및 import문 리팩터링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,13 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
     public static final int[] MONEY = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        for (int i=0; i<MONEY.length; i++) {
+        for (int i = 0; i < MONEY.length; i++) {
             answer.add(money / MONEY[i]);
             money %= MONEY[i];
         }
