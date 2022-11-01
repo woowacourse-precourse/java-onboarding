bfbb16cd4485e873ce5d0ae4948803cc171b8bb0
sa46lll <9812smh@naver.com>
2022-11-01 19:36:02 +0900

    feat: 단어가 중복되는 이메일 출력
    중복 제거, 문자열 오름차순 정렬

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,26 +1,42 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
 
-        List<String> words = getEmailsByWord(forms);
+        Map<String, List<String>> words = getEmailsByWord(forms);
+        Set<String> answer = new HashSet<>();
 
-        return answer;
-    }
-
-    private static List<String> getEmailsByWord(List<List<String>> forms) {
-
-        List<String> list = new ArrayList<>();
-        for (List<String> form : forms) {
-            for (int i = 0; i < form.get(1).length() - 1; i++) {
-                String word = form.get(1).substring(i, i + 2);
-                list.add(word);
+        for (List<String> emails : words.values()) {
+            if (emails.size() >= 2) {
+                answer.addAll(emails);
             }
         }
-        return list;
+
+        return answer.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private static Map<String, List<String>> getEmailsByWord(List<List<String>> forms) {
+
+        Map<String, List<String>> words = new HashMap<>();
+        for (List<String> form : forms) {
+            for (int i = 0; i < form.get(1).length() - 1; i++) {
+                String email = form.get(0);
+                String word = form.get(1).substring(i, i + 2);
+
+                words.putIfAbsent(word, new ArrayList<>());
+                words.get(word).add(email);
+            }
+        }
+        return words;
     }
 }
