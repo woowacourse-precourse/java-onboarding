8452bca04249d8947b48202673406d1663bdc5f6
jhsseonn <hosu0125@gmail.com>
2022-11-01 22:11:18 +0900

    feat: (Problem6) 닉네임 받아 배열로 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        List<String> duplicateNick=new ArrayList<>();
+        char[] nickname=new char[0];
+
+        for (List<String> member: forms) {
+            //닉네임 받아 배열로 만들기
+            char[] nickString = (member.get(1)).toCharArray();
+        }
+
         return answer;
     }
+
 }
