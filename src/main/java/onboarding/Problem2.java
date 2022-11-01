f5d70214e79c1bd320ec421d158d201588ef8822
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 16:14:02 +0900

    comment(Problem2): 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. cryptogram을 하나씩 잘라 List로 변환하는 기능
+ * 2. 해독되었는지 확인하는 기능
+ * 3. 연속적으로 중복된 문자를 모두 삭제하는 기능
+ * 4. 중복된 문자를 삭제하는 기능
+ * 5. answer에 잘라뒀던 문자열을 붙이는 기능
+ */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
