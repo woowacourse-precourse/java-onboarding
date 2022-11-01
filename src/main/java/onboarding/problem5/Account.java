63e5da97ab573abbd4d554351485df75d3e44ba9
This2sho <zx00018@naver.com>
2022-11-01 15:29:18 +0900

    feat: 출금 기능 구현

--- src/main/java/onboarding/problem5/Account.java
+++ src/main/java/onboarding/problem5/Account.java
@@ -1,10 +1,22 @@
 package onboarding.problem5;
 
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Account {
 	private int budget;
 
 	public Account(int budget) {
 		this.budget = budget;
+	}
+
+	public List<Integer> withdraw() {
+		MoneyUnit[] moneyUnits = MoneyUnit.values();
+		return Arrays.stream(moneyUnits)
+			.map(this::withdraw)
+			.collect(Collectors.toList());
 	}
 
 	public int withdraw(MoneyUnit moneyUnit) {
--- src/test/java/onboarding/problem5/AccountTest.java
+++ src/test/java/onboarding/problem5/AccountTest.java
@@ -2,6 +2,8 @@ package onboarding.problem5;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
@@ -14,5 +16,20 @@ public class AccountTest {
 		int count = account.withdraw(MoneyUnit.FIFTY_THOUSAND);
 	    //then
 		assertThat(6).isEqualTo(count);
+	}
+
+	@Test
+	public void 출금_테스트() {
+		//given
+		int budget = 50237;
+		int budget2 = 15000;
+		Account account = new Account(budget);
+		Account account2 = new Account(budget2);
+		//when
+		List<Integer> result = account.withdraw();
+		List<Integer> result2 = account2.withdraw();
+		//then
+		assertThat(Arrays.asList(1, 0, 0, 0, 0, 2, 0, 3, 7)).isEqualTo(result);
+		assertThat(Arrays.asList(0, 1, 1, 0, 0, 0, 0, 0, 0)).isEqualTo(result2);
 	}
 }
