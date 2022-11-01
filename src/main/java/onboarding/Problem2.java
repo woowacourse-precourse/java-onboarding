1136bb8eaab246275e14e313f87518d6e046b64b
masiljangajji <hjk172262@gmail.com>
2022-11-01 20:40:00 +0900

    feat: Problem2_stack자료구조를 이용한 문자열 중복제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
-
+import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        String ss = "";
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!stack.empty()) {
+                if (cryptogram.charAt(stack.peek()) != cryptogram.charAt(i)) {
+                    stack.push(i);
+                }
+                else
+                {
+                    stack.pop();
+                }
+            }
+            else {
+                stack.push(i);
+            }
+        }
+
+
         return answer;
     }
 }
