c53eede244cf55cbaa7c4c6b2779f399d732f789
jemin <jemin03120111@gmail.com>
2022-11-01 22:34:17 +0900

    feat: #1 포비가 펼친 페이지에서 가장 큰 수를 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,31 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiMaxNum = 0;
+        int crongMaxNum = 0;
+
+        // 포비의 경우의 수 체크
+        for (Integer integer : pobi) {
+
+            int addNum = 0;
+            int multipleNum = 1;
+            int tempAdd = integer;
+            int tempMulti = integer;
+
+            while(tempAdd > 0){
+                addNum += tempAdd%10;
+                tempAdd/=10;
+            }
+
+            while(tempMulti > 0){
+                multipleNum *= tempMulti%10;
+                tempMulti/=10;
+            }
+
+            pobiMaxNum = Math.max(addNum,multipleNum);
+        }
+
         return answer;
     }
 }
