f745bedefd59af17e80a00e78ea10eb28bd72bd5
anso33 <hyeon131812@gmail.com>
2022-11-01 16:27:57 +0900

    [FEAT] add func that find number of coins

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static int coin[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList();
+
+        findNumberOfCoin(money , answer);
+//        for (int i = 0; i < 9; i++){
+//            System.out.println(answer.get(i));
+//        }
         return answer;
+    }
+
+    private static void findNumberOfCoin(int money, List<Integer> answer) {
+        int count = 0;
+        int n = 0;
+
+        while (n < 9) {
+            answer.add(money / coin[n]);
+            money = money % coin[n];
+            n++;
+        }
     }
 }
