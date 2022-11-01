a01d06d84bf71e541e9b57b6dbcb8ae3ed082c75
June1010 <tjrwns1010@naver.com>
2022-11-01 23:30:11 +0900

    chore: 기능 목록 커밋 (코드 내 주석 참고)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,14 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    // 토큰들을 저장할 해시맵 선언 (문자열, 리스트)
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        // forms를 순회하면서 토큰화
+        // 값들을 해시맵에 저장
+        // 해시맵에 겹치는게 있다면 리스트에 추가
+        // 반환
+        return new ArrayList<>();
     }
 }
