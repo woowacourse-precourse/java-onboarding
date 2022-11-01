f8c37fac01613e56698d52c1a0f0f8b680c1b633
Yh0sun <hy770819@naver.com>
2022-11-01 15:53:40 +0900

    problem5:func list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] unit={50000,10000,5000,1000,500,100,50,10,1};     //화폐 단위 배열
+
+        //unit의 값으로 나눈 값을 차례대로 answer에 저장
+
         return answer;
     }
 }
