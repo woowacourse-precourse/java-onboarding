81ab37abdbb5730ca2ce248d4b2a19d11c5b8daa
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 13:45:44 +0900

    Init first branch

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
