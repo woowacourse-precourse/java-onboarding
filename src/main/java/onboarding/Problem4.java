ff9ea4e768bd88edd3360c26b1c21afaee8b6c50
YusunPark <solarpy.kor@gmail.com>
2022-11-01 21:42:58 +0900

    Feat : 문제 4 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == ' ') answer += " ";
+            else {
+                Integer num = (int) word.charAt(i);
+                if (num < 96) {
+                    answer += (char) (77 + (77 - num + 1));
+                } else {
+                    answer += (char) (109 + (109 - num + 1));
+                }
+            }
+        }
         return answer;
     }
 }
