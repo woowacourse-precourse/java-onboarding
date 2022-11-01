2283d74486c2a08d575d4840da14375d381aae98
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-01 22:45:39 +0900

    refactor:(problem6): 분리한 클래스를 바탕으로 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,9 @@
 package onboarding;
 
 
-import java.util.HashMap;
-import java.util.HashSet;
+import custom.problem6.DuplicateCheckResolver;
+import custom.problem6.Form;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,23 +17,21 @@ import java.util.stream.Collectors;
 public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
-        Map<String, String> wordMap = new HashMap<>();
-        Set<String> answer = new HashSet<>();
+        List<Form> formList = makeFormList(forms);
 
-        for (List<String> form : forms) {
-            String email = form.get(0);
-            String nickname = form.get(1);
-            for (int j = 0; j < nickname.length() - 1; ++j) {
-                String word = nickname.substring(j, j + 2);
-                if (!wordMap.containsKey(word)) {
-                    wordMap.put(word, email);
-                } else {
-                    answer.add(email);
-                    answer.add(wordMap.get(word));
-                    wordMap.put(word, email);
-                }
-            }
-        }
+        DuplicateCheckResolver duplicateCheckResolver = new DuplicateCheckResolver();
+        Set<String> duplicateNicknameSet = duplicateCheckResolver.findDuplicateForm(formList);
+
+        return convertAnswerFormat(duplicateNicknameSet);
+    }
+
+    private static List<Form> makeFormList(List<List<String>> forms) {
+        return forms.stream()
+            .map(Form::of)
+            .collect(Collectors.toList());
+    }
+
+    private static List<String> convertAnswerFormat(Set<String> answer) {
         return answer.stream()
             .sorted()
             .collect(Collectors.toList());
