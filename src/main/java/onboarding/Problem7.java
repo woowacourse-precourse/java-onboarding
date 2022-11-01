4cf8bf212cc7df078ce73be8ffdee0017eebc440
ckyeon <qsky8520@gmail.com>
2022-11-01 23:40:12 +0900

    Add Score enum to Problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private enum Score {
+        ACQUAINTANCE_WITH_USER(10),
+        VISIT_TIMELINE(1);
+
+        private final int value;
+
+        Score(int value) {
+            this.value = value;
+        }
     }
 }
