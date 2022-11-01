fcc209118a194042174b2694a730803abd6a29ac
gomljo <gomljo0307@naver.com>
2022-11-01 20:18:28 +0900

    feat(Problem7.java): isMyFriend 함수 구현
    사용자와 친구 관계인지 검사하는 isMyFriend 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,7 +2,17 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+class User{
+    String user;
+    int totalScore;
+    User(String userName){
+        user=userName;
+        totalScore=0;
+    }
+    boolean isMyFriend(List<String> relation){
+        return relation.contains(user);
+    }
+}
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
