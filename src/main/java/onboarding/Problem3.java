55dd0b62c72d5d2809b1e3341de7af1810e0397c
geonho <soo_1992@naver.com>
2022-11-01 17:47:29 +0900

    Problem3 : countClaps 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    private static int countClaps(int number) {
+        int claps = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                claps++;
+            }
+            number /= 10;
+        }
+        return claps;
     }
 }
