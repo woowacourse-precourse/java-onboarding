9b1a909ee99afa5e2a218316b7209929cdd17feb
= <kimje1109@gmail.com>
2022-11-01 21:00:22 +0900

    style: Correct spacing and change method name findNextIndex to getNextIndex

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,8 +11,9 @@ public class Problem2 {
         return false;
     }
 
-    public static int findNextIndex(String string, int startIndex) {
+    public static int getNextIndex(String string, int startIndex) {
         int nextIndex = startIndex;
+
         for(int index = startIndex + 1; index < string.length() ; index++) {
             if(string.charAt(startIndex) != string.charAt(index)) {
                 return index;
@@ -26,24 +27,24 @@ public class Problem2 {
         int index = 0;
 
         while(index < string.length()) {
-            if(0 ==findNextIndex(string, index)) {
+            if(0 == getNextIndex(string, index)) {
                 result.append("");
                 return result.toString();
             }
-            if(index + 1 ==findNextIndex(string, index)) {
+            if(index + 1 == getNextIndex(string, index)) {
                 result.append(string.charAt(index));
-                index =findNextIndex(string, index);
+                index = getNextIndex(string, index);
                 continue;
             }
-            if(string.length() - 1 ==findNextIndex(string, index)) {
-                index =findNextIndex(string, index);
+            if(string.length() - 1 == getNextIndex(string, index)) {
+                index = getNextIndex(string, index);
                 result.append(string.charAt(index));
                 return result.toString();
             }
-            if(index ==findNextIndex(string, index)) {
+            if(index == getNextIndex(string, index)) {
                 return result.toString();
             }
-            index =findNextIndex(string, index);
+            index = getNextIndex(string, index);
         }
 
         return result.toString();
