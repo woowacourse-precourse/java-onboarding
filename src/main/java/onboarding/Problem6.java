9aa9db0f5971be184f4fd5f643070f4295259f18
LJW25 <dbjwl@g.skku.edu>
2022-11-01 17:51:19 +0900

    p6. complete main

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,26 +8,64 @@ public class Problem6 {
     private static List<String> splitNickname(String nickname){
         List<String> splitedList = new ArrayList<>();
 
+
         return splitedList;
     }
 
 
-
-    private static boolean checkDuplicate(String names, List<String> splitedList){
+    private static boolean checkDuplicateList(String names, List<String> splitedList){
         boolean isDuplicate = false;
 
         return isDuplicate;
+    }
+
+    private static List<String> checkUnduplicateList(List<List<String>> undupList, List<String> splitedList){
+        List<String> duplicated = new ArrayList<>();
+
+        return duplicated;
+    }
+
+    private static List<String> sortList(List<String> emailList){
+
+        return emailList;
     }
 
 
 
 
     public static List<String> solution(List<List<String>> forms) {
+        List<String> answer = new ArrayList<>();
 
-        String names = "";
+        String dupNames = "";
+        List<List<String>> undupList = new ArrayList<>();
 
+        String nickname, email;
+        List<String> splitedList = new ArrayList<>();
+        List<List<String>> dupList = new ArrayList<>();
 
-        List<String> answer = List.of("answer");
+        for(List<String> f:forms){
+            email = f.get(0);
+            nickname = f.get(1);
+            splitedList = splitNickname(nickname);
+            if(checkDuplicate(names, splitedList){
+                answer.add(email);
+                dupNames = dupNames + "\n" + nickname;
+            }
+            else{
+                dupList = checkUnduplicateList(undupList, splitedList);
+                if(dupList.size()) > 0){
+                    for(List<String> d:dupList){
+                        answer.add(d.get(0));
+                        dupNames = dupNames + "\n" + d.get(1);
+                    }
+                }
+                else undupList.add(f);
+            }
+
+            splitedList.clear();
+            dupList.clear();
+        }
+
 
         return answer;
     }
