ae633b8cb4ff66e0e80f1923f93f5c3fcc679940
wwwndus <simock22@gmail.com>
2022-11-01 23:31:33 +0900

    feat: while문을 진행하며 이전문자와 같지않으면 리스트에 문자를 넣는 메소드 실행

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,10 +11,30 @@ public class Problem2 {
         List<String> charList = new LinkedList<>();
         boolean isDuple = true;
 
+        while (isDuple) {
+            charList.clear();
+            char prev = 0;
+
+            removeRepeat(chars, charList, prev);
+
+
+        }
 
 
         return answer;
     }
 
+    private static void removeRepeat(char[] chars, List<String> charList, char prev) {
+        for (int i = 0; i < chars.length; i++) {
+            if (prev != chars[i]) {
+                charList.add(String.valueOf(chars[i]));
+                prev = chars[i];
+            } else {
+                if (charList.size() >= 1) {
+                    charList.remove(charList.size() - 1);
+                }
+            }
+        }
+    }
 
 }
