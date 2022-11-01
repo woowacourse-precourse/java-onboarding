5013ee4b2dc0f981ec4f173fcd68434b735999df
chanminmaxscaler <jcm04@naver.com>
2022-11-01 22:43:40 +0900

    test(Problem1Test): Problem1 클래스의 테스트 코드를 리팩토링한다.
    assertThat(...).isEqualTo(true) 등의 메소드를 assertThat(...).isTrue()로 수정한다.
    @BeforeEach void setUp 메소드를 통해 테스트 코드 실행 전에 Problem1 객체를 생성한다.

--- src/test/java/onboarding/Problem1Test.java
+++ src/test/java/onboarding/Problem1Test.java
@@ -1,85 +1,87 @@
 package onboarding;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem1Test {
 
+    private Problem1 problem1;
+
+    @BeforeEach
+    void setUp() {
+        problem1 = new Problem1();
+    }
+
     @Test
     @DisplayName("페이지 번호 타당성 에러를 맞게 판단하는지")
     void checkPageNumberValidityExceptionTest() {
-        Problem1 problem1 = new Problem1();
-        boolean result = true;
-
         List<Integer> exceptionCase1 = List.of(99, 102);
-        assertThat(problem1.checkPageNumberValidityException(exceptionCase1)).isNotEqualTo(result);
+        assertThat(problem1.checkPageNumberValidityException(exceptionCase1)).isFalse();
 
         List<Integer> exceptionCase2 = List.of(100, 101);
-        assertThat(problem1.checkPageNumberValidityException(exceptionCase2)).isNotEqualTo(result);
+        assertThat(problem1.checkPageNumberValidityException(exceptionCase2)).isFalse();
 
         List<Integer> exceptionCase3 = List.of(101, 100);
-        assertThat(problem1.checkPageNumberValidityException(exceptionCase3)).isNotEqualTo(result);
+        assertThat(problem1.checkPageNumberValidityException(exceptionCase3)).isFalse();
 
         List<Integer> correctCase1 = List.of(99, 100);
-        assertThat(problem1.checkPageNumberValidityException(correctCase1)).isEqualTo(result);
+        assertThat(problem1.checkPageNumberValidityException(correctCase1)).isTrue();
 
         List<Integer> correctCase2 = List.of(101, 102);
-        assertThat(problem1.checkPageNumberValidityException(correctCase2)).isEqualTo(result);
+        assertThat(problem1.checkPageNumberValidityException(correctCase2)).isTrue();
     }
 
     @Test
     @DisplayName("페이지 번호 범위 에러를 맞게 판단하는지")
     void checkOutOfRangeExceptionTest() {
-        Problem1 problem1 = new Problem1();
-        boolean result = true;
 
-        List<Integer> exceptionCase1 = List.of(1,2);
-        assertThat(problem1.checkOutOfRangeException(exceptionCase1)).isNotEqualTo(result);
+        List<Integer> exceptionCase1 = List.of(1, 2);
+        assertThat(problem1.checkOutOfRangeException(exceptionCase1)).isFalse();
 
         List<Integer> exceptionCase2 = List.of(399, 400);
-        assertThat(problem1.checkOutOfRangeException(exceptionCase2)).isNotEqualTo(result);
+        assertThat(problem1.checkOutOfRangeException(exceptionCase2)).isFalse();
 
         List<Integer> exceptionCase3 = List.of(-1, 0);
-        assertThat(problem1.checkOutOfRangeException(exceptionCase3)).isNotEqualTo(result);
+        assertThat(problem1.checkOutOfRangeException(exceptionCase3)).isFalse();
 
         List<Integer> exceptionCase4 = List.of(401, 402);
-        assertThat(problem1.checkOutOfRangeException(exceptionCase4)).isNotEqualTo(result);
+        assertThat(problem1.checkOutOfRangeException(exceptionCase4)).isFalse();
 
         List<Integer> correctCase1 = List.of(3, 4);
-        assertThat(problem1.checkOutOfRangeException(correctCase1)).isEqualTo(result);
+        assertThat(problem1.checkOutOfRangeException(correctCase1)).isTrue();
     }
 
     @Test
     @DisplayName("전체 에러를 맞게 확인하는지")
     void checkExceptionTest() {
-        Problem1 problem1 = new Problem1();
-        boolean result = true;
 
         List<Integer> exceptionCase1 = List.of(99, 102);
-        assertThat(problem1.checkException(exceptionCase1)).isNotEqualTo(result);
+        assertThat(problem1.checkException(exceptionCase1)).isFalse();
 
         List<Integer> exceptionCase2 = List.of(100, 101);
-        assertThat(problem1.checkException(exceptionCase2)).isNotEqualTo(result);
+        assertThat(problem1.checkException(exceptionCase2)).isFalse();
 
         List<Integer> exceptionCase3 = List.of(399, 400);
-        assertThat(problem1.checkException(exceptionCase3)).isNotEqualTo(result);
+        assertThat(problem1.checkException(exceptionCase3)).isFalse();
 
         List<Integer> exceptionCase4 = List.of(-1, 0);
-        assertThat(problem1.checkException(exceptionCase4)).isNotEqualTo(result);
+        assertThat(problem1.checkException(exceptionCase4)).isFalse();
 
         List<Integer> exceptionCase5 = List.of(398, 399);
-        assertThat(problem1.checkException(exceptionCase5)).isNotEqualTo(result);
+        assertThat(problem1.checkException(exceptionCase5)).isFalse();
 
         List<Integer> correctCase1 = List.of(3, 4);
-        assertThat(problem1.checkException(correctCase1)).isEqualTo(result);
+        assertThat(problem1.checkException(correctCase1)).isTrue();
     }
 
     @Test
     @DisplayName("리스트 각 요소의 자릿수 덧셈을 잘 하는지")
     void addEachDigitOfPageNumber() {
-        Problem1 problem1 = new Problem1();
         List<Integer> list1 = List.of(99, 100);
         List<Integer> result1 = List.of(18, 1);
         List<Integer> list2 = List.of(101, 102);
@@ -92,7 +94,6 @@ class Problem1Test {
     @Test
     @DisplayName("리스트 각 요소의 자릿수 곱셈을 잘 하는지")
     void multiplyEachDigitOfPageNumberTest() {
-        Problem1 problem1 = new Problem1();
         List<Integer> list1 = List.of(99, 100);
         List<Integer> result1 = List.of(81, 0);
         List<Integer> list2 = List.of(101, 102);
@@ -105,7 +106,6 @@ class Problem1Test {
     @Test
     @DisplayName("리스트 각 요소의 자릿수 연산을 잘 하는지")
     void calculateEachDigitOfPageNumberTest() {
-        Problem1 problem1 = new Problem1();
         List<Integer> list1 = List.of(99, 100);
         List<Integer> result1 = List.of(18, 1, 81, 0);
         List<Integer> list2 = List.of(101, 102);
@@ -118,7 +118,6 @@ class Problem1Test {
     @Test
     @DisplayName("리스트의 최댓값을 반환하는지")
     void getMaxValueTest() {
-        Problem1 problem1 = new Problem1();
         List<Integer> list1 = List.of(18, 1, 81, 0);
         int result1 = 81;
 
@@ -128,7 +127,6 @@ class Problem1Test {
     @Test
     @DisplayName("올바른 승부 결과를 판단하는지")
     void findWinnerTest() {
-        Problem1 problem1 = new Problem1();
         List<Integer> pobiWinCase = List.of(7, 1);
         int pobiWin = 1;
         assertThat(problem1.findWinner(pobiWinCase)).isEqualTo(pobiWin);
