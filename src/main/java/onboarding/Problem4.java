b7b9a13c1c04bdac62057ba62d18b69018adfd3d
alivejuicy <kkddhh1998@gmail.com>
2022-11-01 23:18:24 +0900

    feat Problem4 solve

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(searchDictionary(word.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    private static Character searchDictionary(char c) {
+        if (Character.isUpperCase(c)) {
+            return (char) ('Z' - c + 'A');
+        }
+        if (Character.isLowerCase(c)) {
+            return (char) ('z' - c + 'a');
+        }
+        return c;
     }
 }
