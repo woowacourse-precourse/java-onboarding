558ce430df0c756dfc2da25a2396740d62caeb98
mihea1206 <mihea12@gmail.com>
2022-11-01 09:15:08 +0900

    problem3(parsing digits function)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+
+    private static List<String> parseDigits(int num) {
+        List<String> digits = new ArrayList<>();
+
+        for (int i = 1; i <= num; i++) {
+            String s = Integer.toString(i);
+            digits.addAll(Arrays.asList(s.split("")));
+        }
+
+        return digits;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
