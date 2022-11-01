70d1e07c0451800ef8dd5279c9a72d7ff1cf8cd7
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 19:08:31 +0900

    Solve Problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] answer = new int[9];
+        int [] coinArr = new int []{50000,10000,5000,1000,500,100,50,10,1};
+
+        for (int i = 0; i < 9;i++){
+            if(money/coinArr[i] > 0) {
+                int curr = money / coinArr[i];
+                answer[i]= curr;
+                System.out.print(curr);
+                money = money % coinArr[i];
+            }
+        }
+
+        List<Integer> result = Arrays.stream(answer).boxed().collect(Collectors.toList());
+        return result;
     }
 }
