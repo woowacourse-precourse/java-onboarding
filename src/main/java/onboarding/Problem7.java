ff561fbe2e05119cd7105eaa2bce53e7d5e1372d
Jinhui <jda0526@naver.com>
2022-11-01 23:45:57 +0900

    Feat(Problem7): [F-02] 아는 친구 점수와 방문 횟수 점수를 확인하여 추천 점수를 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,19 +3,29 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    static Map<String, Integer> scoreMap = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         System.out.println(friends);
-        List<String> userFriends = makeFriendsList(user, friends);
+        System.out.println(visitors);
+        List<String> userFriends = makeFriendsList(user, friends); // 사용자의 친구 목록
         System.out.println(userFriends);
-        List<List<String>> recommendFriends = makeRecoFriends(userFriends, friends);
+        List<List<String>> recommendFriends = makeRecoFriends(userFriends, friends); // 사용자의 친구의 친구 목록
         System.out.println(recommendFriends);
+
+//        Map<String, Integer> friendsScore = getFriendsScore(user, recommendFriends);
+        getFriendsScore(user, recommendFriends);
+        getVisitScore(visitors);
+        removeAlreadyFriends(userFriends);
+        System.out.println(scoreMap);
 
         List<String> answer = Collections.emptyList();
         return answer;
     }
 
     private static List<String> makeFriendsList(String user, List<List<String>> friends) {
-        Set<String> userFriends = new HashSet<>();
+        // userFriends의 친구와 recommendFriends의 추천 친구 인덱스를 똑같이하기 위해서 LinkedHashSet를 사용함
+        Set<String> userFriends = new LinkedHashSet<>();
 
         for (List<String> friend : friends) {
             if(friend.contains(user)){ // 친구 관계라면
@@ -36,6 +46,49 @@ public class Problem7 {
         return list;
     }
 
+    private static void getFriendsScore(String user, List<List<String>> recommendFriends) {
+        for(List<String> friend : recommendFriends){
+            if(friend.contains(user)){ // 친구 관계라면
+                int userIndex = friend.indexOf(user);
+                getScore(userIndex, friend);
+            }
+        }
+    }
+
+    private static void getScore(int userIndex, List<String> friend) {
+        for (int i = 0; i < friend.size(); i++) {
+            String friendsName = friend.get(i);
+
+            if(i != userIndex){
+                if(!scoreMap.containsKey(friendsName)){ // 추천 친구가 없으면 등록하고, 10점 부여
+                    scoreMap.put(friendsName, 10);
+                }
+                else { // 추천 친구로 있으면 10점 더해주기
+                    scoreMap.put(friendsName, scoreMap.get(friendsName) + 10);
+                }
+            }
+        }
+    }
+
+    private static void getVisitScore(List<String> visitors) {
+        for (int i = 0; i < visitors.size(); i++) {
+            String friendsName = visitors.get(i);
+
+            if(!scoreMap.containsKey(friendsName)){ // 추천 친구가 없으면 등록하고, 1점 부여
+                scoreMap.put(friendsName, 1);
+            }
+            else { // 추천 친구로 있으면 1점 더해주기
+                scoreMap.put(friendsName, scoreMap.get(friendsName) + 1);
+            }
+        }
+    }
+
+    private static void removeAlreadyFriends(List<String> userFriends) {
+        for(String friendName : userFriends){
+            if(scoreMap.containsKey(friendName))
+                scoreMap.remove(friendName);
+        }
+    }
 
     public static void main(String[] args) {
         String user = "mrko";
