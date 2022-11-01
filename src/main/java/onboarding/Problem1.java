71f938d6904fd212c1ca463d9d36d554ede13041
Suzie Lee <wnsuwls0707@gmail.com>
2022-11-01 17:20:02 +0900

    feat: problem1, func checkValidation 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,7 +13,16 @@ class Problem1 {
         return Math.max(hundreds+tens+units,hundreds*tens*units);
 
     }
-
+    public static boolean checkValidation(List<Integer> pages){
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        if (leftPage%2==0 || leftPage+1!=rightPage){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
