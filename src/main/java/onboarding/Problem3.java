00e6ba376e22eda08e4c73bb19695d775542d6d2
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-01 17:35:35 +0900

    test3 - 박수 횟수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,21 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // 박수 횟수 세기
+    public static int count(int num) {
+        int cnt = 0;
+        int temp = 0;
+
+        while(num!=0) {
+            temp = num%10;
+
+            if(temp==3 || temp==6 || temp==9)
+                cnt++;
+
+            num /= 10;
+        }
+
+        return cnt;
+    }
 }
