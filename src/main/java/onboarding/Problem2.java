872f3d45a217104d47fdae595a081720ffa1c50c
TeddyYeoni <po99027@naver.com>
2022-11-01 23:30:47 +0900

    PROBLEM2 기능1 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+    
+	public String sameWordDelete(String cryptogramText) {
+		String[] cryptogramArray = cryptogramText.split("");
+		List<String> cryptogramList = new ArrayList<String>(Arrays.asList(cryptogramArray));
+		String cryptogramCorrection = "";
+
+		for (int i = 0; i < cryptogramArray.length - 1; i++) {
+			if (cryptogramArray[i].equals(cryptogramArray[i + 1])) {
+				cryptogramList.remove(i + 1);
+				cryptogramList.remove(i);
+				cryptogramArray = cryptogramList.toArray(new String[cryptogramArray.length - 2]);
+			}
+			cryptogramCorrection = String.join("", cryptogramArray);
+		}
+		return cryptogramCorrection;
+	}
 }
