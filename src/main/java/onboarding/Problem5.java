63b9c401e366da6d671ff4c31f19bf6178c6cb62
yw824 <yongwoo25@gmail.com>
2022-11-01 10:19:21 +0900

    feat: [Problem5] solution 입력 데이터 확인 및 화폐 단위별 추출 구현
    - solution() 함수:
    	- 입력이 1 이상 1,000,000 이하이므로 해당 범위가 확인
    	- 분류 함수를 통해 화폐 단위별 환산량 배열 반환받아 반환
    - classifyMoney(int money):
    	- 최종 단위별 화폐량 반환 배열 ArrayList로 implement
    	- 화폐 단위 배열 생성( 길이 : 9 )
    	- 배열 길이만큼 순회하면서, 큰 단위 순으로 나눈 몫만큼
    	- 단위당 화폐량 설정 -> 최소량 구할 수 있다.
    	- 단위당 화폐량을 반환할 ArrayList에 추가하여
    	- for문 순회가 끝난 후 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,18 +8,31 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
+        // money는 int형으로 제공되므로 자료형에 대한 체크는 할 필요 없음
+        // 입력 범위만 체크
+        if(money < 1 || money > 1000000)
+            return answer;
+
         // answer 변수에 각 화폐마다 분류를 완료한 List를 받는다.
         // classifyMoney(money)
+        answer = classifyMoney(money);
         return answer;
     }
 
     static List<Integer> classifyMoney(int money) {
-        List<Integer> answer = Collections.emptyList();
+        // ArrayList로 구현( implement )
+        List<Integer> answer = new ArrayList<Integer>();
 
-        // implement
-        // 화폐 단위의 배열을 생성한 후 for문을 돌면서
-        // 각 화폐 단위마다 나눠서 몫을 answer 배열에 저장한다.
-        // 총 저장된 answer 배열을 반환한다.
+        int len = 9; // 화폐 단위 개수
+        // 화폐 단위 배열
+        int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        // 순회하면서
+        for(int i = 0; i < len; ++i)
+        {
+            // 가장 큰 화폐단위 순으로 나눈 몫 : 해당 단위로 가져갈 수 있는 화폐량
+            answer.add(money / bill[i]);
+            money %= bill[i];
+        }
         return answer;
     }
 }
