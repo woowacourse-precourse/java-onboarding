9fbdafd256530ab5d106dbae8070db10a869d6f4
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-01 14:31:17 +0900

    feat: Q5 : 검증로직 구현

--- src/main/java/onboarding/problem5/MoneyChanger.java
+++ src/main/java/onboarding/problem5/MoneyChanger.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class MoneyChanger {
     private static final int NUMBER_OF_CURRENCY_TYPE = 9;
+    private final InputNumberValidator inputNumberValidator = new InputNumberValidator();
 
     public List<Integer> change(int money) {
+        inputNumberValidator.validate(money);
         int[] moneyIntValues = getMoneyIntValues();
         Integer[] moneys = new Integer[NUMBER_OF_CURRENCY_TYPE];
 
