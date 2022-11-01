e123dfa76d4a8c87f01d0080ce4da7ea654784d0
hataerin <goil1113@likelion.org>
2022-11-01 19:58:01 +0900

    ADD: Problem3 손뼉 횟수 계산 기능 구현
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,19 @@ public class Problem3 {
 
         //손뼉 횟수 계산 기능
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String str = Integer.toString(i);
+            String[] arr = str.split("");
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j].equals("3") || arr[j].equals("6") || arr[j].equals("9")) {
+                    answer += 1;
+                }
+            }
+
+        }
+
         return answer;
     }
 }
