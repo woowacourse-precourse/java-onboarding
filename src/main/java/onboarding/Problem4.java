bcc1e875d9ffacc429d0e43cf10c05873ff8e075
KyungKyu Park <rudrb0505@gmail.com>
2022-11-01 14:20:27 +0900

    refactor(Problem4): 숫자로 나타낸 것을 상수로 선언해 사용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+
+	public static final int ASCII_A = 65;
+	public static final int ASCII_Z = 90;
+	public static final int ASCII_a = 97;
+	public static final int ASCII_z = 122;
+
 	public static String solution(String word) {
-		char [] wordArray = word.toCharArray();
+		char[] wordArray = word.toCharArray();
 		reverseWord(wordArray);
 		return new String(wordArray);
 	}
@@ -13,15 +19,15 @@ public class Problem4 {
 		int distanceOfWord;
 		for (int i = 0; i < wordArray.length; i++) {
 			asciiNum = (int)wordArray[i];
-			if (asciiNum >= 65 && asciiNum <= 90) {
-				distanceOfWord = asciiNum - 65;
-				reverseAsciiNum = 90 - distanceOfWord;
-				wordArray[i] = (char) reverseAsciiNum;
-			} 
-			if (asciiNum >= 97 && asciiNum <= 122) {
-				distanceOfWord = asciiNum - 97;
-				reverseAsciiNum = 122 - distanceOfWord;
-				wordArray[i] = (char) reverseAsciiNum;
+			if (asciiNum >= ASCII_A && asciiNum <= ASCII_Z) {
+				distanceOfWord = asciiNum - ASCII_A;
+				reverseAsciiNum = ASCII_Z - distanceOfWord;
+				wordArray[i] = (char)reverseAsciiNum;
+			}
+			if (asciiNum >= ASCII_a && asciiNum <= ASCII_z) {
+				distanceOfWord = asciiNum - ASCII_a;
+				reverseAsciiNum = ASCII_z - distanceOfWord;
+				wordArray[i] = (char)reverseAsciiNum;
 			}
 		}
 	}
