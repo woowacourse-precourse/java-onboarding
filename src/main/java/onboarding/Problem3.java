51540f341950539644d626a0f17f548622b3625b
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:24:26 +0900

    [CREATE] 각 자리수 별 3,6,9와 일치하는지 비교하는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,7 +12,33 @@ public class Problem3 {
                 .boxed()
                 .collect(Collectors.toList());
 
+        for(int data : stream){
+            answer += threeMultiple(data);
+        }
         return answer;
     }
 
+    private static int threeMultiple(int number) {
+        int thousands = number / 1000;
+        int hundreds = number % 1000 / 100;
+        int tens = number % 100 / 10;
+        int units = number % 10;
+        int result = 0;
+
+        result += check(thousands);
+        result += check(hundreds);
+        result += check(tens);
+        result += check(units);
+
+        return result;
+    }
+
+    private static int check(int number){
+        if(number == 3 || number == 6 || number == 9){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
 }
