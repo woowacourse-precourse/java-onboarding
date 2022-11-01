4ebb815699944db72bc9b9f4af9f42a091f8e51f
dingding-21 <phj8221@naver.com>
2022-11-01 22:41:37 +0900

    feat: Problem1 페이지 번호 게임의 최종 결과를 구하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,10 @@ import java.util.List;
 class Problem1 {
     private static final int maxPageNum = 400;
     private static final int minPageNum = 1;
+    
+    private static final int crongWin = 2;
+    private static final int pobiWin = 1;
+    private static final int draw = 0;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -35,5 +39,14 @@ class Problem1 {
         }
 
         return Math.max(sum, mul);
+    }
+
+    private static int getResult(List<Integer> pobi, List<Integer> crong) {
+        int maxPobiNum = Math.max(getMaxNum(pobi.get(0)), getMaxNum(pobi.get(1)));
+        int maxCrongNum = Math.max(getMaxNum(crong.get(0)), getMaxNum(crong.get(1)));
+
+        if(maxPobiNum < maxCrongNum) return crongWin;
+        if(maxPobiNum == maxCrongNum) return draw;
+        return pobiWin;
     }
 }
