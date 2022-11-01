21488c1cee545f716446ea8d598d0fe872a7501d
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:24:45 +0900

    [5번] feat: 돈 단위로 나눈 몫/나머지 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,14 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    // 2. (총 금액 / 돈 단위)의 몫 반환하기
+    public static int getShare(int money, int moneyUnit) {
+        return money / moneyUnit;
+    }
+
+    // 3. 현재 돈 단위를 제외한 나머지 금액 구하기
+    public static int getLeftMoney(int money, int moneyUnit) {
+        return money % moneyUnit;
+    }
 }
