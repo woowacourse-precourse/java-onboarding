f9ececf5468676498dbb63b9cf0708b182743c41
sschan99 <seungchan1219@gmail.com>
2022-11-01 21:47:41 +0900

    style: change to enhanced for loop

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,8 +8,7 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         int[] divisors = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for (int i = 0; i < divisors.length; i++) {
-            int divisor = divisors[i];
+        for (int divisor : divisors) {
             answer.add(money / divisor);
             money %= divisor;
         }
