f0e810c9e4864707bfa869673bf70575b7e5e078
wxxhyeong <wxxhyeong@gmail.com>
2022-11-01 20:02:53 +0900

    [Problem1] feat : getSum() 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,28 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static int getSum(int a) {
+
+        int ans = 0;
+
+        while (a > 0) {
+            ans += a%10;
+            a /= 10;
+        }
+
+        return ans;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        int pobiScore = 0;
+        int crongScore = 0;
+
+
+
+
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
