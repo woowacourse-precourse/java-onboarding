7d7439462d2a70fb809a5bb88f18e26b116e938c
bestinwoo <bestinwoo@gmail.com>
2022-11-01 15:09:26 +0900

    feat: 문제3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++) {
+            String num = String.valueOf(i);
+            for (char c : num.toCharArray()) {
+                if(c == '3' || c == '6' || c == '9') {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
