2420c2446c34917e8a973051ce566d7f8ef1b60b
WonSeok-dd <cluejws@gmail.com>
2022-11-01 13:50:25 +0900

    Problem2 | docs: 기능 정의

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,11 +3,6 @@ package onboarding;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
-/**
- * 기능 구현 사항
- * 1. 현재 문자열에서 Stack통해 중복문자 제거
- * 2. Stack -> 문자열 바꾸기
- */
 public class Problem2 {
     private static String charStackToString(Stack<Character> stack){
 
