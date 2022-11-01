adb038554781de6ccab982fb2c60419e6b534c6e
Konahyeon <koo@inha.edu>
2022-11-01 16:07:05 +0900

    money 단위 배열 초기화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] UNIT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    public static List<Integer> moneyCount(int money) {
+        List<Integer> answer = new ArrayList<>();
+        
+        return answer;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = moneyCount(money);
+
         return answer;
     }
 }
