f80e73e23d6b7058589d20603dca4c6169dece19
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:38:18 +0900

    문제7 : InnerClass 선언

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,11 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    /**
+     * 추천 친구 목록을 구하기 위한 기능들을 구현한 클래스
+     */
+    public static class Problem7_Functions {
+
+    }
 }
