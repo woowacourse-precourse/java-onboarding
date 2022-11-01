20b42fbbfb21558c9701cf9d674abeaf6684f59e
yunjeong <uu0078@naver.com>
2022-11-01 23:03:50 +0900

    Problem7 기능 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/* 기능 목록
+1. 친구관계 그래프 Map 생성
+2. 그래프에서 user 와 인접한 친구의 친구들에게 +10점
+3. visitor +1점
+4. 점수, 이름 순으로 정렬
+5. 최대 5인 추천 친구 선정
+*/
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
