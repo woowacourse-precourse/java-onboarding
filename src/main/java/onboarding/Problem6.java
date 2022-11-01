0cc497890413df21fc85dc81bcf4460c9cb91214
장한울 <swingjhw@naver.com>
2022-11-01 20:26:02 +0900

    feat(#문제6) : hashMap으로닉네임과 이메일 정리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,42 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        HashMap<String, List<String>> mapEmail = new HashMap<>();
+        List<String> tmp = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            String nickName = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+
+            // 순회하면서 nickName과 email 정리
+            makeEmail(nickName, email, mapEmail);
+        }
+
+ 
+        List<String> answer = new ArrayList<>();
+
         return answer;
+    }
+
+    public static void makeEmail(String nickName, String email,  HashMap<String, List<String>> mapEmail) {
+
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            String now = String.valueOf(nickName.charAt(i));
+            String next = String.valueOf(nickName.charAt(i + 1));
+
+            // mapEmail은 두 글자 조합을 key로 두고 생성
+            String both = now + next;
+            if (mapEmail.containsKey(both)) {
+                mapEmail.get(both).add(email);
+            } else {
+                mapEmail.put(both, new ArrayList<>());
+                mapEmail.get(both).add(email);
+            }
+
+        }
     }
 }
