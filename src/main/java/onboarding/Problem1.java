8941d2ba188d38549f0d26f5eb07c2c60743d2d2
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-01 18:29:42 +0900

    [P1] 왼쪽과 오른쪽 페이지의 쪽수 차이가 1일 때 각 자리 숫자를 모두 더한 값과 모두 곱한 값을 구함.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -18,6 +18,22 @@ class Problem1 {
         // 1. 왼쪽과 오른쪽 페이지의 쪽수 차이가 2 이상일 때 예외 사항으로 간주 (-1 반환)
         if ((Math.abs(pobi.get(0) - pobi.get(1)) > 1) || (Math.abs(crong.get(0) - crong.get(1)) > 1)) {
             answer = -1;
+        } else {
+            // 2. 왼쪽과 오른쪽 페이지의 쪽수 차이가 1일 때 각 자리 숫자를 모두 더한 값과 모두 곱한 값을 구함.
+            for (List<Integer> user : userArr) {
+                userScoreArr = new ArrayList<>();
+                for (Integer score : user) {
+                    while (score != 0) {
+                        scoreSum += score % 10;
+                        scoreMultiply *= score % 10;
+                        score /= 10;
+                    }
+                    maxScore = Math.max(scoreSum, scoreMultiply);
+                    userScoreArr.add(maxScore);
+                    scoreSum = 0;
+                    scoreMultiply = 1;
+                }
+            }
         }
         return answer;
     }
