1f3667f3e7b1f5c85156e1597e86b9eebb0d5427
JinWoo Park <jinwuui@gmail.com>
2022-11-01 10:30:24 +0900

    refactor: problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,18 +7,6 @@ import java.util.List;
 
 public class Problem5 {
 
-    private static final List<CashType> cashTypes = List.of(
-            CashType.MONEY_50000,
-            CashType.MONEY_10000,
-            CashType.MONEY_5000,
-            CashType.MONEY_1000,
-            CashType.MONEY_500,
-            CashType.MONEY_100,
-            CashType.MONEY_50,
-            CashType.MONEY_10,
-            CashType.MONEY_1
-    );
-
     public static List<Integer> solution(int money) {
 
         if (money < 1 || money > 1_000_000) {
@@ -27,7 +15,7 @@ public class Problem5 {
 
         List<Integer> answer = new ArrayList<>();
 
-        for (CashType cashType : cashTypes) {
+        for (CashType cashType : CashType.values()) {
             int amount = cashType.getAmount();
 
             answer.add(money / amount);
