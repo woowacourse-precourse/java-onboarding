20f2a04f8687a0b4a18199778314fa751cafe07a
YOON <seoung59@gmail.com>
2022-11-01 18:16:03 +0900

    Feat : Problem 5 - solution 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 지폐 리스트 생성
+ */
 public class Problem5 {
+    private static final int[] cashes = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for(int cash : cashes){
+            int cnt = money / cash;
+            money %= cash;
+            answer.add(cnt);
+        }
         return answer;
     }
 }
