f94d51bf5c983b175e47ca0b841839d905f5ab41
함궈 <heoja0123@naver.com>
2022-11-01 21:48:24 +0900

    [Problem7] 리팩토링 적용

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Problem7 {
 
-    static Map<String, User> map = new HashMap<>();
+    static Map<String, User> duplicationCheckingMap = new HashMap<>();
     static PriorityQueue<User> pq = new PriorityQueue<>();
     static User visitor;
-    static User root;
+    static User rootUser;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
@@ -20,33 +20,39 @@ public class Problem7 {
             user2.addFriend(user1);
         }
 
-        root = User.findUser(user);
-        List<User> friendList = root.getFriends();
+        rootUser = User.findUser(user);
+        Friends rootUserFriends = rootUser.getFriends();
+        List<User> friendsList = rootUserFriends.getFriendsList();
 
-        for(User friend : friendList){
-            User.calcFriendsScore(friend);
-        }
+        Friends.calcFriendsScore(rootUser);
+
+
 
 //        friends 리스트에 없는 경우를 대비
         for(String visitorName : visitors) {
             visitor = User.findUser(visitorName);
-            if(!friendList.contains(visitor)){
+
+            if(!friendsList.contains(visitor)){
                 User.calcVisitorsScore(visitor);
             }
         }
 
-        Iterator<String> keyIt = map.keySet().iterator();
+        Iterator<String> keyIt = duplicationCheckingMap.keySet().iterator();
+
         while(keyIt.hasNext()){
             String key = keyIt.next();
             User tmpUser = User.findUser(key);
-            if(tmpUser.getScore() > 0){
+
+            if(tmpUser.getScore() > 0){    // 0점 이상인 유저만 우선순위 큐에 add
                 pq.add(tmpUser);
             }
         }
 
+        // 우선순위 큐에 들어있는 User 인스턴스의 String 값만 answer에 넣어 반환
         while(!pq.isEmpty()){
             User nextUser = pq.poll();
-            answer.add(nextUser.getName());
+            String userName = nextUser.getName();
+            answer.add(userName);
         }
 
         return answer;
@@ -54,17 +60,25 @@ public class Problem7 {
 
 
     static class User implements Comparable<User>{
+        private static final int VISITOR_SCORE = 1;
+        private static final int MIN_USER_NAME_LENGTH = 1;
+        private static final int MAX_USER_NAME_LENGTH = 30;
 
         private String name;
         private int score = 0;
-        private List<User> friends = new ArrayList<>();
+//        private List<User> friends = new ArrayList<>();
+
+        private Friends friends = new Friends();
 
         public User(String name){
+            validateNameLength(name);
+            validateNameLowerCase(name);
+
             this.name = name;
         }
 
-        public boolean addFriend(User user){
-            return this.friends.add(user);
+        public void addFriend(User user){
+            this.friends.addFriend(user);
         }
 
         public void addScore(int score){
@@ -73,30 +87,19 @@ public class Problem7 {
 
         public static User findUser(String name){
 
-            if(!map.containsKey(name)){
+            if(!duplicationCheckingMap.containsKey(name)){
                 User newUser = new User(name);
-                map.put(name, newUser);
+                duplicationCheckingMap.put(name, newUser);
                 return newUser;
             }
 
-            return map.get(name);
+            return duplicationCheckingMap.get(name);
         }
 
-        // 사용자와 함께 아는
-        public static void calcFriendsScore(User userFriend){
-
-            List<User> friends = userFriend.getFriends();
-
-            for(User friend : friends){
-                if(!friend.equals(root)) {
-                    friend.addScore(10);
-                }
-            }
-        }
 
         // 타임라인에 방문한 경우의 점수 계산 
         public static void calcVisitorsScore(User user){
-            user.addScore(1);
+            user.addScore(VISITOR_SCORE);
         }
 
         @Override
@@ -117,8 +120,57 @@ public class Problem7 {
             return score;
         }
 
-        public List<User> getFriends() {
+        public Friends getFriends() {
             return friends;
+        }
+
+        void validateNameLength(String name){
+            if(MIN_USER_NAME_LENGTH > name.length() || name.length() > MAX_USER_NAME_LENGTH){
+                throw new IllegalArgumentException("길이가 " +
+                        MIN_USER_NAME_LENGTH + " 이상 " + MAX_USER_NAME_LENGTH + " 이하인 문자열이어야 한다.");
+            }
+        }
+
+        void validateNameLowerCase(String name){
+            for(int i = 0; i < name.length(); i++){
+                char c = name.charAt(i);
+                if(Character.isUpperCase(c)){
+                    throw new IllegalArgumentException("사용자 아이디는 알파벳 소문자로만 이루어져 있어야 한다.");
+                }
+            }
+        }
+    }
+
+    // 일급 컬렉션 적용
+    static class Friends{
+        private static final int FRIEND_SCORE = 10;
+
+        private List<User> friends = new ArrayList<>();
+
+        public List<User> getFriendsList() {
+            return friends;
+        }
+
+        public void addFriend(User friend){
+            this.friends.add(friend);
+        }
+
+
+        // 인자로 받은 유저의 직접적인 친구들의 친구들을 방문하며 점수계산한다.
+        public static void calcFriendsScore(User user){
+
+            Friends friendList = user.getFriends();
+
+            for(User friend : friendList.getFriendsList()){
+                Friends friendsOfFriend = friend.getFriends();
+
+                for(User friendOfFriend : friendsOfFriend.getFriendsList()){
+
+                    if(!friendOfFriend.equals(rootUser)) {
+                        friendOfFriend.addScore(FRIEND_SCORE);
+                    }
+                }
+            }
         }
     }
 }
