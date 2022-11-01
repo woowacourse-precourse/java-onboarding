3f64007e07f12cacd1101e42b5d85f97b39b4a70
brotherstone97 <brotherstone97@gmail.com>
2022-11-01 15:03:54 +0900

    Feat: user와 이미 친구인 사람을 찾는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    static List<String> findFriends(String user, <List<List<String>> friends) {
+        List<String> alreadyFriends = new ArrayList<>();
+        for (List<String> relationship : friends) {
+            if (!relationship.get(0).equals(user) && !relationship.get(1).equals(user))
+                continue;
+            if (relationship.get(0).equals(user)) {
+                alreadyFriends.add(relationship.get(1));
+                continue;
+            }
+            if (relationship.get(1).equals(user)) {
+                alreadyFriends.add(relationship.get(0));
+            }
+        }
+        return alreadyFriends;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
