b3a18757cdac09de73657b297987bbd2b0fc3d79
YUSEONJUN <seonjun0906@gmail.com>
2022-11-01 13:17:24 +0900

    [Problem 2] 예외 사항 조건 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,12 @@
 package onboarding;
 
 import java.util.Stack;
+import java.util.regex.Pattern;
+
 public class Problem2 {
 	public static String solution(String cryptogram) {
+		check1(cryptogram);
+		check2(cryptogram);
 		String answer = "answer";
 		Stack<Character> stack = new Stack<>();
 
@@ -21,4 +25,18 @@ public class Problem2 {
 		}
 		return sb.toString();
 	}
+	public static void check1(String cryptogram) throws IllegalArgumentException{
+		if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+			throw new IllegalArgumentException("글자수가 없거나 초과하였습니다.");
+		}
+
+	}
+
+	public static void check2(String cryptogram) throws IllegalArgumentException{
+		if (!cryptogram.matches("^[a-z]*$")) {
+			throw new IllegalArgumentException("알파벳 소문자가 아닙니다.");
+		}
+
+	}
+
 }
