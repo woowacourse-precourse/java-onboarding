c456b1916bed00867bf1b5fdabc38c36857491c1
bnbnac <bnbnac@naver.com>
2022-11-01 09:42:02 +0900

    feat: (P1)페이지별 각 자리 숫자 추출

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -7,12 +8,13 @@ class Problem1 {
     static int LAST_PAGE = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
         if (!validatePageList(pobi) || !validatePageList(crong)) {
             return -1;
         }
+        int answer = numberGame(pobi, crong);
         return answer;
     }
+
 
     private static boolean validatePageList(List<Integer> pageList) {
         if (!isConsecutive(pageList)) {
@@ -56,5 +58,45 @@ class Problem1 {
             return true;
         }
         return false;
+    }
+
+
+    private static int numberGame(List<Integer> leftPlayer, List<Integer> rightPlayer) {
+        int LEFT_WIN = 1;
+        int RIGHT_WIN = 2;
+        if (getScore(leftPlayer) < getScore(rightPlayer)) {
+            return RIGHT_WIN;
+        }
+        if (getScore(leftPlayer) > getScore(rightPlayer)) {
+            return LEFT_WIN;
+        }
+        return 0;
+    }
+
+    private static int getScore(List<Integer> pageList) {
+        List<Integer> leftPageDigits = getDigits(pageList.get(0));
+        List<Integer> rightPageDigits = getDigits(pageList.get(1));
+        int maxSum = getMaxSum(leftPageDigits, rightPageDigits);
+        int maxMul = getMaxMul(leftPageDigits, rightPageDigits);
+        return Math.max(maxSum, maxMul);
+    }
+
+    private static List<Integer> getDigits(int num) {
+        List<Integer> digits = new ArrayList<>();
+        while (num > 0) {
+            digits.add(num % 10);
+            num /= 10;
+        }
+        return digits;
+    }
+
+    private static int getMaxSum(List<Integer> left, List<Integer> right) {
+
+        return 0;
+    }
+
+    private static int getMaxMul(List<Integer> left, List<Integer> right) {
+
+        return 0;
     }
 }
