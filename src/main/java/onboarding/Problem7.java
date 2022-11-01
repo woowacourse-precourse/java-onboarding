9fe216239acdd1db344a57e1e783c18cb06fd84b
DavinAn <blueapple1030@gmail.com>
2022-11-01 23:02:59 +0900

    Problem7 solution method
    Add variables needed for method. To track each user's name and score we use a HashMap.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, Integer> users = new HashMap<>();
+        List<String> friendList = findFriends(user, friends);
+        String l0, l1;
     }
 
     public static List<String> findFriends(String user, List<List<String>> friends) {
