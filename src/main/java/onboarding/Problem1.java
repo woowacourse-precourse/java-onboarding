f2b0f8fe6521e4ad59d257d3da40b5da7f52b41e
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 13:19:57 +0900

    comment(Problem1): 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,17 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 예외 사항
+ *      1. 페이지가 연속적이지 않을 경우
+ *      2. 왼쪽 페이지가 홀수가 아닐 경우
+ *      3 .오른쪽 페이지가 짝수가 아닐 경우
+ * 2. 각 자리수를 더한 값을 반환하는 기능
+ * 3. 각 자리수를 곱한 값을 반환하는 기능
+ * 4. max 값을 도출하는 기능
+ * 5. pobi와 crong의 승부를 판정하는 기능
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
