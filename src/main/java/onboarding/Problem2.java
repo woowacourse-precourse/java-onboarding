d42b723372b49771d743e6cd2e93dbfc98e35880
KimSangHyeok <tkdgur0906@naver.com>
2022-11-01 15:47:34 +0900

    Feat(PROBLEM2): 스택에 문자를 넣는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> st = new Stack<Character>();
+        boolean isSame = false;
+
+        st.push(cryptogram.charAt(0));
+        for(int i = 1;i< cryptogram.length();i++){
+            st.push(cryptogram.charAt(i));
+        }
         return answer;
     }
 }
