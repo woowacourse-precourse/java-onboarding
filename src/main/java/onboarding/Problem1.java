5bd61a09a1d7858be103a7fa84b4d0891a4c0a76
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 11:40:26 +0900

    feat: 입력 페이지 리스트 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,6 +15,39 @@ class Problem1 {
     
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
+        if (isInvalidPage(pobi) || isInvalidPage(crong)) {
+            return EXCEPTION;
+        }
+        
         return answer;
+    }
+
+    private static boolean isInvalidPage(List<Integer> page) {
+        return isNotValidPageSize(page) ||
+                hasFirstOrLastPage(page) ||
+                isNotSequencePages(page) ||
+                isEven(page.get(0)) ||
+                isOdd(page.get(1));
+    }
+
+    private static boolean isNotValidPageSize(List<Integer> pages) {
+        return pages.size() != PAGE_LIST_SIZE;
+    }
+
+    private static boolean hasFirstOrLastPage(List<Integer> pages) {
+        return pages.stream().anyMatch(page -> page == START_PAGE_NUMBER | page == END_PAGE_NUMBER);
+    }
+
+    private static boolean isNotSequencePages(List<Integer> pages) {
+        return pages.get(0) + PAGE_GAP != pages.get(1);
+    }
+
+    private static boolean isOdd(int page) {
+        return page % 2 == 1;
+    }
+
+    private static boolean isEven(int page) {
+        return page % 2 == 0;
     }
 }
