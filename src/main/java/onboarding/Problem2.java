9a96b6aee0a70d680f365f6ff5dd2940d51df4b6
tunkcalb <wjdgusdndi@gmail.com>
2022-11-01 15:47:37 +0900

    problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        for (int i=0; i<cryptogram.length()-1 ; i++){
+
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)){
+                cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
+                i = 0;
+            }
+
+            if (cryptogram.length() == 2){
+                if (cryptogram.charAt(0) == cryptogram.charAt(1)){
+                    cryptogram = "";
+                }
+            }
+        }
+        answer = cryptogram;
         return answer;
     }
 }
