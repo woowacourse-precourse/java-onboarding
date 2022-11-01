97d7ad78bb0e465decb4ec743309b5750bb33a34
jamangstangs <jamangsong@gmail.com>
2022-11-01 10:56:45 +0900

    feat: problem5(진행), withdrawCurrencyFromATM() -> 지폐 액수를 받아 인출자가 받을 화폐 갯수를 반환하는 메소드 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+    public static List<Integer> solution(int account) {
+
+        List<Integer> currencyInATM = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        return withdrawCurrencyFromATM(currencyInATM, account);
+    }
+
+    private static List<Integer> withdrawCurrencyFromATM(List<Integer> currencyInATM, int account) {
+        return null;
     }
 }
