705cdc8304038c48476df923ee6ff8ca989d316b
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-01 16:16:03 +0900

    Refactor: Problem4 code refactoring - rename function & variable
    기존 solution 함수에 전부 구현해두었던 기능을
    changeChar2ReverseAlphabet 함수로 구분하였음.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,24 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-
         char[] str2list = word.toCharArray();
-        for (char ch : str2list){
-            if ((int)ch >= 65 && (int)ch <= 90)
-                answer += (char)(90-(int)ch+65);
-            else if ((int)ch >= 97 && (int)ch <= 122)
-                answer += (char)(122-(int)ch+97);
-            else
-                answer += ch;
 
-        }
+        for (char ch : str2list)
+            answer += changeChar2ReverseAlphabet(ch);
 
         return answer;
     }
+
+    public static char changeChar2ReverseAlphabet(char ch){
+
+        if ((int)ch >= 65 && (int)ch <= 90) // A(65) - Z(90)
+            return (char)(90-(int)ch+65);
+
+        else if ((int)ch >= 97 && (int)ch <= 122) // a(97) - z(122)
+            return  (char)(122-(int)ch+97);
+
+        else // another character
+            return ch;
+    }
 }
-/*
-* 65: A
-* 90: Z
-* 97: a
-* 122: z
-* */
