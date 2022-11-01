32619de832c20432c55e2b38833097c9fcb80f83
Yh0sun <hy770819@naver.com>
2022-11-01 15:12:41 +0900

    func2 add

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,6 +9,10 @@ public class Problem3 {
         //     숫자를 10으로 나눈 몫으로 계속 반복
         for(int i=1;i<=number;i++){
             int num=i;
+            while(num>0){
+                if(num%10==3||num%10==6||num%10==9)answer+=1;
+                num/=10;
+            }
         }
         return answer;
     }
