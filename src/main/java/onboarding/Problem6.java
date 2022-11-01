18b7d4018bf580a090cd27cb90c14ebcf8e45287
dvlpr0 <rladjswkd@naver.com>
2022-11-01 19:23:38 +0900

    added/Problem6: 모든 문자열에서 추출한 길이가 2인 부분 문자열을 키로, 출현 횟수를 값으로 하는 해시 테이블을 적절한 값으로 채우는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,37 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	private static HashSet<String> emails;
+	private static Hashtable<String, Integer> words;
+	private static void fillSetEach(List<String> form) {
+		String nickname = form.get(1);
+		String word;
+		int last;
+
+		last = nickname.length();
+		if (last == 1)
+			return;
+		last--;
+		for (int i = 0; i < last; i++) {
+			word = nickname.substring(i, i + 2);
+			words.put(word, words.getOrDefault(word, 0) + 1);
+		}
+	}
+	private static void fillSetAll(List<List<String>> forms) {
+		forms.forEach(Problem6::fillSetEach);
+	}
+
+	public static List<String> solution(List<List<String>> forms) {
+		emails = new HashSet<>();
+		words = new Hashtable<>();
+		List<String> answer;
+
+		fillSetAll(forms);
+//		findDuplicated(forms);
+//		answer = new ArrayList<>(emails);
+//		Collections.sort(answer);
+		return (answer);
+	}
 }
