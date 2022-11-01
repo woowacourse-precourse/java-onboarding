f86d963fe6c243fd31b41cbcd6416e3f81403c7a
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 18:02:03 +0900

    [Feat] Problem2 인자로 들어온 문자열을 하나씩 넣어가면서 answer의 마지막과 들어올 문자가 같으면 answer 문자 삭제 아니면 answer에 넣기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        for (int i = 0; i < cryptogram.length(); i++)
+        {
+            if (answer.length() != 0)
+                if (answer.charAt(answer.length() - 1) == cryptogram.charAt(i))
+                {
+                    answer = answer.substring(0, answer.length() - 1);
+                    continue;
+                }
+            answer += cryptogram.charAt(i);
+        }
         return answer;
     }
 }
