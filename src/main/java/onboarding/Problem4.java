e0265c67038ef6145eb23b832fc28c09200ef8fc
kksks3192 <kksks3192@gmail.com>
2022-11-01 11:14:58 +0900

    feat(problem4): 청개구리 사전을 만드는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    public static Map<Character, Character> makeDictionary(){
+        Map<Character, Character> dictionary = new HashMap<>();
+        for(int i =0; i <26 ; i++){
+            //a-z 넣기
+            int key_ascii = 65+i;
+            int val_ascii = 90-i;
+            char key = (char)key_ascii;
+            char value = (char)val_ascii;
+            dictionary.put(key, value);
+
+            //A-Z 넣기
+            key_ascii = 97+i;
+            val_ascii = 122-i;
+            key = (char)key_ascii;
+            value = (char)val_ascii;
+            dictionary.put(key, value);
+
+        }
+        return dictionary;
+    }
+
+
 }
