220a111497b549c4e7a694e3452a52c25ab9bc31
fefe2200 <dlcjf2200@naver.com>
2022-11-01 23:37:01 +0900

    problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int result = 0;
+
+        for(int num= 1; num <= number; num++){
+            result += countCheck(num);
+        }
+        return result;
+    }
+
+    public static int countCheck(int num){
+        int count = 0;
+
+        while(num!=0){
+            if(num % 10 == 3|| num % 10 == 6|| num % 10 == 9)
+                count++;
+            num /= 10;
+        }
+        return count;
     }
 }
