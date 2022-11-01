1ab8241b1b6f829945ab45dfe709e852e528b196
youngkyunIm <yee0230@naver.com>
2022-11-01 20:10:22 +0900

    fix(Problem2) : 기존 코드 백업

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,25 +1,55 @@
 package onboarding;
 
 public class Problem2 {
-    public static final int NO_DUPLICATE = -1;
     public static String solution(String cryptogram) {
         String answer = cryptogram;
         while (true) {
-            int duplicatesIndex = getDuplicatesIndex(answer);
-            if (duplicatesIndex == NO_DUPLICATE) return answer;
-            answer = removeDuplicates(answer, duplicatesIndex);
+            if(!hasReduplication(answer)){
+                return answer;
+            }
+            if(hasReduplication(answer)){
+                answer = removeReduplication(answer);
+            }
         }
     }
 
-    public static String removeDuplicates(String str, int index) {
-        String afterStr = str.substring(0, index) + str.substring(index + 2);
-        return afterStr;
+    private static String removeReduplication(String cryptogram) {
+        int startIndex = -1;
+        int lastIndex = -1;
+        String startString;
+        String lastString;
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (hasReduplication(cryptogram, i)) {
+                if (startIndex == -1) {
+                    startIndex = i;
+                }
+                lastIndex = i + 1;
+            }
+        }
+        if (startIndex == -1) {
+            return cryptogram;
+        } else {
+            startString = cryptogram.substring(0, startIndex);
+            // 중복 다음 값의 index 부터 시작이라 +1
+            lastString = cryptogram.substring(lastIndex + 1);
+            return startString + lastString;
+        }
     }
 
-    public static int getDuplicatesIndex(String str) {
+    public static boolean hasReduplication(String str, int index) {
+        if (str.charAt(index) == str.charAt(index + 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean hasReduplication(String str) {
         for (int index = 0; index < str.length() - 1; index++) {
-            if (str.charAt(index) == str.charAt(index + 1)) return index;
+            if (str.charAt(index) == str.charAt(index + 1)) {
+                return true;
+            }
         }
-        return NO_DUPLICATE;
+        return false;
     }
 }
