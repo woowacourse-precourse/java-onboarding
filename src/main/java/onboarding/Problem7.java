feba9ab715d480ab26445794323f61aef5146b5a
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 19:02:53 +0900

    트리구조

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,5 +4,8 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) { // MST, BFS 써보고 싶은데 내 개념이 모호해서..  6번과 유사하게 접근해보기.
-
+        Set<String> userFriend = new HashSet<>(); // user와 직접 친구, 간접친구 구분 후 부여받을 점수 고려
+        List<List<String>> knownFriend = new ArrayList<>();
+        Map<String, Integer> friendScore = new HashMap<>();
+    }
 }
