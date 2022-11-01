209aa39e4eeb721757679c910b6c40f06d0f0acb
2do1 <dowon3036@naver.com>
2022-11-01 17:22:42 +0900

    feat: 문제 4번 알파벳 외의 문자는 변환하지 않는 예외처리 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,13 @@ public class Problem4 {
             // 문자열의 각 문자에 접근 후 아스키 값으로 변환하는 기능
             char ch = word.charAt(i);
             int ascii = (int)ch;
+
+            // 알파벳 외의 문자는 변환하지 않는다
+            if (65 <= ascii && ascii <= 90) { // 문자가 대문자일 경우
+
+            } else if (97 <= ascii && ascii <= 122) { // 문자가 소문자일 경우
+
+            }
         }
         return answer;
     }
