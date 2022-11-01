1e8f6ff50dcceb86875a8e0416d12c1c08a45219
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-01 17:28:11 +0900

    feat: Problem 7, Add IndexMap

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
+    static Map<String, Integer> userIndexMappingMap = new HashMap<String, Integer>();
+    static int index = 0;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
+    public static void addMap(String userName){
+        if(!userIndexMappingMap.containsKey(userName)){
+            userIndexMappingMap.put(userName,index);
+            index++;
+        }
+    }
+
 }
