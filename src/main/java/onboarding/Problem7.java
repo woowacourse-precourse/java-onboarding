b31805a7f06bc88d2bdb5fe4e10b6f49d90bf92f
dener8 <8dener@gmail.com>
2022-11-01 20:14:30 +0900

    [Problem7] feat: 한 사람의 모든 친구 목록을 나타내는 map

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static List<String> recommendFriend(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, List<String>> relationship = new HashMap<>();
+        // 사람 목록 만들기
+        String name;
+        for (int i = 0; i < friends.size(); i++) {
+            for (int j = 0; j < 2; j++) {
+                name = (friends.get(i)).get(j);
+                if (!relationship.containsKey(name)) {
+                    relationship.put(name, new ArrayList<>());
+                }
+            }
+        }
+        // 사람마다 친구인 사람을 나타내는 map
+        for (int i = 0; i < friends.size(); i++) {
+            String firstName = friends.get(i).get(0);
+            String secondName = friends.get(i).get(1);
+            relationship.get(firstName).add(secondName);
+            relationship.get(secondName).add(firstName);
+        }
+
+
+    }
+
 }
