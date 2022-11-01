752d510566fa357cdab06cf0256d02ab2906f8e2
khj <kim37171@cuk.edu>
2022-11-01 21:45:23 +0900

    [Problem7] 방문자 점수 계산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    private static Map<String,Integer> recommenedFreinds = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new LinkedList<>();
+        calcVisitorsPoints(visitors);
         return answer;
+    }
+
+    private static void calcVisitorsPoints(List<String> visitors) {
+        for (String visitor : visitors) {
+            if(recommenedFreinds.containsKey(visitor)){
+                recommenedFreinds.put(visitor, recommenedFreinds.get(visitor) + 1);
+                continue;
+            }
+            recommenedFreinds.put(visitor, 1);
+        }
     }
 }
