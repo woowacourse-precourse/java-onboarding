1c5d2fadf8d1a29c27b58e53f656cb6c3ff86bfd
ygreenb <nhj7911@gmail.com>
2022-11-01 10:30:33 +0900

    refactor: Problem5. 돈의 단위로 환산하는 코드 함수로 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,19 +6,21 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-
         int currency[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        return moneyToCurrency(money, currency);
+    }
+    static List<Integer> moneyToCurrency(int money, int currency[]){
+        List<Integer> currencyCount = new ArrayList<>();
         for(int i=0;i<currency.length;i++){
             if(money < currency[i]) {
-                answer.add(0);
-            } else {
-                answer.add(money / currency[i]);
-                money -= answer.get(i) * currency[i];
+                currencyCount.add(0);
+            }
+            if(money >= currency[i]) {
+                currencyCount.add(money / currency[i]);
+                money -= currencyCount.get(i) * currency[i];
             }
         }
-
-        return answer;
+        return currencyCount;
     }
 }
