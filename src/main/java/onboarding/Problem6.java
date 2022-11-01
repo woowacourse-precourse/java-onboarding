3679fc4c789462992bcf8a730351065c99ea704d
khj <bbb6254177@gmail.com>
2022-11-01 14:22:43 +0900

    fix is_duplicate function

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,34 +3,41 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+    //크루의 닉네임에 존재하는 모든 두글자 문자열의 개수를 저장하는 hashmap
     private static HashMap<String, Integer> count = new HashMap<String, Integer>();
+
+    //주어진 nickname을 이용해 hashmap을 업데이트하는 함수
+    private static void _update(String nickname){
+        for(int j = 0; j<nickname.length()-1; j++){
+            String key = nickname.substring(j, j+2);
+
+            if (count.containsKey(key)) count.put(key, count.get(key) + 1);
+            else count.put(key, 1);
+        }
+    }
+    
+    //주어진 nickname이 중복된 nickname인지 판별하는 함수
+    private static int _is_duplicate(String nickname){
+        for(String str: count.keySet()){
+            if (count.get(str) >= 2){
+                if (nickname.contains(str)) return 1;
+            }
+        }
+        return 0;
+    }
 
     //크루의 닉네임이 중복되는지 판별해 배열로 반환해주는 함수
     private static void is_duplicate(List<List<String>> forms, int[] duplicated){
+        //닉네임이 중복되는지 판별하는 데 사용할 hashmap을 업데이트한다.
         for(int i = 0; i<forms.size(); i++){
             String nickname = forms.get(i).get(1);
-            for(int j = 0; j<nickname.length()-1; j++){
-                String key = nickname.substring(j, j+2);
-
-                if (count.containsKey(key)){
-                    count.put(key, count.get(key) + 1);
-                    duplicated[i] = 1;
-                    break;
-                }
-                else {
-                    count.put(key, 1);
-                }
-            }
+            _update(nickname);
         }
 
-        //첫 번재 요소의 경우 중복되더라도 표시되지 않으므로 다시 확인해준다.
-        String nickname = forms.get(0).get(1);
-        for(int i = 0; i<nickname.length()-1; i++){
-            String key = nickname.substring(i, i+2);
-            if (count.containsKey(key)){
-                duplicated[i] = 1;
-                break;
-            }
+        //닉네임이 중복되는지 판별한다.
+        for(int i = 0; i<forms.size(); i++){
+            String nickname = forms.get(i).get(1);
+            duplicated[i] = _is_duplicate(nickname);
         }
     }
 
@@ -48,7 +55,7 @@ public class Problem6 {
         return result;
     }
 
-    //
+    //중복되는 닉네임을 가지는 크루들의 이메일을 이메일 형식을 제외한 부분으로 정렬한 리스트를 반환하는 함수
     private static List<String> sort_email(List<String> lst){
         Collections.sort(lst, new Comparator<String>() {
             @Override
