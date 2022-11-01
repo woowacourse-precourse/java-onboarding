dfd0c0564815d931a7ba48546027583241807f02
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-01 23:48:10 +0900

    Problem 7 : 함께 아는 친구를 구하는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -20,6 +20,26 @@ public class Problem7 {
         return myfriends;
     }
 
+    // 기능 목록 2 : 함께 아는 친구를 구하는 함수
+    public static List<String> findFriendsOfFriends(List<List<String>> friends, List<String> myfriends, String user){
+        List<String> friendsOffriends = new ArrayList<>();
+
+        for(String myfriend:myfriends){
+            for(List<String> friend:friends){
+                if(friend.contains(myfriend)&&!friend.contains(user)){
+                    if(friend.get(0)==myfriend){
+                        friendsOffriends.add(friend.get(1));
+                    }
+                    if(friend.get(1)==myfriend){
+                        friendsOffriends.add(friend.get(0));
+                    }
+                }
+            }
+        }
+
+        return friendsOffriends;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
