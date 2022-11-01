c9ea0855cd5c913d22846074daac9f9b1d48a19a
simjaeseo <delpho@naver.com>
2022-11-01 14:46:31 +0900

    feat: 문제 3 기능 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 검증할 수를 char[]로 변환
+ * 2. 3,6,9 몇 개 포함됐는지 세는 함수
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
