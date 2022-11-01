2805abb573aba948fc5b38892d1efadf09144700
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-01 09:19:00 +0900

    feat: P5 화폐단위 상수 배열 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,8 +8,11 @@ import java.util.List;
  * 기능 1: 가장 큰 금액 화폐부터 뽑도록 하는 기능 구현
  */
 public class Problem5 {
+    private static final int[] MONEY = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
     }
 }
