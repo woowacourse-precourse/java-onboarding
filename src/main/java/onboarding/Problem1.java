a74c170099e8db336705ff96616fa2e298d660ad
juno-junho <ssmm0205@naver.com>
2022-11-01 19:17:52 +0900

    refactor(Problem1): refactor code with problem1 package
    problem1 package의 객체들을 이용해 게임 보드를 통해 게임을 진행한 결과 값을 바로 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,34 +1,15 @@
 package onboarding;
 
+import onboarding.problem1.GameBoard;
+import onboarding.problem1.PageValidator;
+import onboarding.problem1.ScoreCalculator;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.get(1) < pobi.get(0) || crong.get(1) < crong.get(0)) return -1;
-        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) return -1;
-        if (pobi.get(0) <= 1 || crong.get(1) >= 400) return -1;
-
-        int pobiMaxNumber = getGameScore(pobi);
-        int crongMaxNumber = getGameScore(crong);
-
-        if (pobiMaxNumber == crongMaxNumber) return 0;
-        return pobiMaxNumber > crongMaxNumber ? 1 : 2;
+        GameBoard gameBoard = new GameBoard(new ScoreCalculator(), new PageValidator());
+        gameBoard.playtheGame(pobi, crong);
+        return gameBoard.getResult();
     }
-
-    static int getGameScore(List<Integer> pages) {
-        return pages.stream().mapToInt(Problem1::findGreaterOfSumAndProduct).max().getAsInt();
-    }
-
-    static int findGreaterOfSumAndProduct(int number) {
-        int sum = 0;
-        int multiply = 1;
-        while (number != 0) {
-            int units = number % 10;
-            sum += units;
-            multiply *= units;
-            number /= 10;
-        }
-        return (sum >= multiply) ? sum : multiply;  // Math. max?
-    }
-
 }
