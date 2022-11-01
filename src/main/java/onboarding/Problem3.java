045ec7ee2f036650cf3ca4d4b539f199ba9a4cc6
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 21:32:26 +0900

    feat:Problem3 정규식으로 369 들어간 숫자 찾아내는 기능 judgeNumber()추가.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -15,14 +15,9 @@ public class Problem3 {
         }
         return answernumber;
     }
+
     public static int solution(int number) {
         int answer = 0;
-
-        for (int i = 1; i <= number; i++) {
-            String element = Integer.toString((i));
-            answer += judgeNumber(element);
-        }
-
         return answer;
     }
 }
