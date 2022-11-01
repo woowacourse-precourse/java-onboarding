743241abb3f99bf11d2db6ff7c8969b444a4d227
송민진 <m_6595@naver.com>
2022-11-01 22:32:31 +0900

    #6 refactor: replace List with HashMap for efficiency

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,63 +1,37 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        List<String> emails = new ArrayList<>();
-        List<String> nicknames = new ArrayList<>();
-        for (List<String> form : forms) {
-            emails.add(form.get(0));
-            nicknames.add(form.get(1));
+        // HashMap을 활용하여 중복 검사 + 검색 빠르게 수행
+        Map<String, Integer> strSlices = new HashMap<>();
+        Set<Integer> duplicatedIndexes = new HashSet<>();
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> slices = devideIntoTwo(forms.get(i).get(1));
+            for (String slice : slices) {
+                if (strSlices.containsKey(slice)) {
+                    duplicatedIndexes.add(strSlices.get(slice));
+                    duplicatedIndexes.add(i);
+                } else {
+                    strSlices.put(slice, i);
+                }
+            }
         }
-
-        List<Integer> duplicatedIndexes = checkNicknames(nicknames);
+        // 중복이 있는 인덱스의 이메일 반환하기
         for (int index : duplicatedIndexes) {
-            answer.add(emails.get(index));
+            answer.add(forms.get(index).get(0));
         }
-        
         Collections.sort(answer);
         return answer;
     }
 
-    private static List<Integer> checkNicknames(List<String> nicknames) {
-        List<Integer> indexes = new ArrayList<>();
-
-        for (int i = 0; i < nicknames.size()-1; i++) {
-
-            String checkingNickname = nicknames.get(i);     // 이번에 검사할 닉네임
-            List<String> checkingNicknameSlices = devideIntoTwo(checkingNickname);  // 검사할 닉네임을 2글자씩 조각으로 분리
-
-            List<String> others = new ArrayList<>(nicknames.subList(i+1,nicknames.size()));
-            for (String other : others) {
-                for (String nowChecking : checkingNicknameSlices) {
-                    if (isDuplicated(nowChecking, other)) {     // 중복일 경우
-                        if (!indexes.contains(i)) {
-                            indexes.add(i);         // 두글자로 검사한 닉네임의 인덱스 추가
-                        }
-                        if (!indexes.contains(nicknames.indexOf(other))){
-                            indexes.add(nicknames.indexOf(other));      // 검사 대상이 된 닉네임의 인덱스 추가
-                        }
-                    }
-                }
-            }
-        }
-        return indexes;
-    }
-
-    private static boolean isDuplicated(String nowChecking, String otherNickname) {
-        boolean tf = otherNickname.contains(nowChecking);
-        return tf;
-    }
-
     private static List<String> devideIntoTwo(String nickname) {
         List<String> twoLetters = new ArrayList<>();
-            for (int i = 0; i < nickname.length() - 2; i++) {
+            for (int i = 0; i < nickname.length() - 1; i++) {
                 String slicedStr = nickname.substring(i, i + 2);
                 if (!twoLetters.contains(slicedStr)) {
                     twoLetters.add(slicedStr);
