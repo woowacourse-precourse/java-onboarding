aa33f7891fc6db5cca80a5ddbb7a6aee9e619237
kim1387 <kevink0@naver.com>
2022-11-01 16:01:16 +0900

    feat: 해당 숫자의 쳐야하는 박수 수를 구하는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+  public static int solution(int number) {
+    return 0;
+  }
+
+  private static int getClapCount(int number) {
+    int count = 0;
+    while (number > 0) {
+      int digitNumber = number % 10;
+      if ((digitNumber == 3) || (digitNumber == 6) || (digitNumber == 9)) {
+        count++;
+      }
+      number /= 10;
     }
+    return count;
+  }
 }
