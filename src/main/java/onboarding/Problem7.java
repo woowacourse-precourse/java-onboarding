12ea4d252a2a049b9779abb073b3564e9f32e87e
hodomaroo <enghemdwkd@gmail.com>
2022-11-01 20:07:10 +0900

    [Problem7/FEAT] Problem7문제 풀이에 필요한 모든 도메인 클래스와 그 기능을 구현하고, Problem7 클래스의 solution 메서드에서 이를 호출, 결과를 반환하였습니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,13 @@
 package onboarding;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import org.junit.jupiter.api.Test;
+import onboarding.problem7.FriendRecommender;
 
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
-
-        return answer;
+        FriendRecommender recommender = new FriendRecommender(friends);
+        return recommender.recommendUser(user, visitors);
     }
 }
 
