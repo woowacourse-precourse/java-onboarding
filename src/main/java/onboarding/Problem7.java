c2f22b0b9cb5be9b4ca0e4fb5e4d4ef29083a878
seunghye <hsh990107@naver.com>
2022-11-01 22:49:41 +0900

    Refactoring 1101 10:49

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,21 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String, Integer> new_friend = new HashMap<>();
+        List<String> old_friend = Collections.emptyList();
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                friend.remove(user);
+                old_friend.add(friend.get(0));
+            }
+        }
         return answer;
     }
 }
