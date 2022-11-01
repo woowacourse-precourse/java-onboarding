236b010b4222556982879b399c4968c8b0d14451
Konahyeon <koo@inha.edu>
2022-11-01 23:57:55 +0900

    이름과 점수 담긴 리스트 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,21 +12,23 @@ class User {
         this.score = 0;
         this.friendList = new ArrayList<String>();
     }
-
     public String getName() {
         return this.name;
     }
-
-    public int scoreAdd(int score) {
-        this.score += score;
-        return score;
-    }
-
     public List<String> getFriendList() {
         return this.friendList;
     }
 
-    public void classFriend(String friendName) {//클래스에 넣기
+    public void scoreAdd(int score) {
+        this.score += score;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+
+    public void setFriendList(String friendName) {//클래스에 넣기
         this.friendList.add(friendName);
     }
 }
@@ -34,46 +36,54 @@ class User {
 public class Problem7 {
     public static final int FRIEND_SCORE = 10;
     public static final int VISITOR_SCORE = 1;
-    public static final HashMap<String, User> classList = new HashMap<>(); //이름과 그 이름에 해당하는 클래스 보관
-    public static void makeClass(String name) {
-        if (!classList.containsKey(name)) { //class hashmap에 없을 경우 클래스 먼저 만듦
+    public static final HashMap<String, User> hashMap = new HashMap<>(); //이름과 그 이름에 해당하는 클래스 보관
+
+    //1. 친구 맺기
+    public static void makeUserClass(String name) {
+        if (!hashMap.containsKey(name)) { //class hashmap에 없을 경우 클래스 먼저 만듦
             User user = new User(name);
-            classList.put(name, user);
+            hashMap.put(name, user);
         }
     }
 
-    public static User findClass(String name) {
-        return classList.get(name); //User 클래스 리턴
-    }
-
-    public static void makeFriend(List<List<String>> friendList) { //친구 맺기
-        for (int i = 0; i < friendList.size(); i++) {
+    public static void makeFriend(List<List<String>> friends) { //친구 맺기
+        for (int i = 0; i < friends.size(); i++) {
+            //i번째 node의 각 원소 이름 및 클래스 없을 경우 생성
+            String left = friends.get(i).get(0);
+            String right = friends.get(i).get(1);
+            makeUserClass(left);
+            makeUserClass(right);
 
             //i번째 node의 각 원소에 대한 클래스 가져오기
-            User first = findClass(friendList.get(i).get(0)); //i번째 node의 첫 번째 원소
-            User second = findClass(friendList.get(i).get(1)); //i번째 node의 두 번째 원소
+            User first = hashMap.get(left); //i번째 node의 첫 번째 원소
+            User second = hashMap.get(right); //i번째 node의 두 번째 원소
 
-            //User 멤버변수 classFriend에 없는 String(name)일 경우 추가하기
-            if (!first.getFriendList().contains(second.getName()))
-                first.classFriend(second.getName());
-            if (!second.getFriendList().contains(first.getName()))
-                second.classFriend(first.getName());
+            //User 멤버변수 friendList에 없는 String(name)일 경우 추가하기
+            if (!first.getFriendList().contains(right))
+                first.setFriendList(right);
+            if (!second.getFriendList().contains(left))
+                second.setFriendList(left);
         }
     }
 
     public static void friendScore(String name) {
-        User user = findClass(name); //user 클래스 가져오기
+        User user = hashMap.get(name); //user 클래스 가져오기
         for (int i = 0; i < user.getFriendList().size(); i++) {
-            User friendUser = classList.get(user.getFriendList().get(i));
+            User friendUser = hashMap.get(user.getFriendList().get(i));
             for (int j = 0; j < friendUser.getFriendList().size(); j++) {
-                if (!user.getFriendList().contains(friendUser.getFriendList().get(j))) //friendUser의 친구가 user 친구 목록에도 없는 친구라면
-                    classList.get(friendUser.getFriendList().get(j)).scoreAdd(FRIEND_SCORE);
+                String newFriendName = friendUser.getFriendList().get(j);
+
+                //friendUser의 친구가 user 친구 목록에도 없는 친구라면 점수 추가
+                if (!user.getFriendList().contains(newFriendName) && newFriendName != name) {
+                    User newFriend = hashMap.get(newFriendName);
+                    newFriend.scoreAdd(FRIEND_SCORE);
+                }
             }
         }
     }
     public static void visitorScore(List<String> visitors) {
         for (int i = 0; i < visitors.size(); i++) {
-            User user = classList.get(visitors.get(i));
+            User user = hashMap.get(visitors.get(i));
             user.scoreAdd(VISITOR_SCORE);
         }
     }
@@ -82,18 +92,26 @@ public class Problem7 {
         visitorScore(visitors);
     }
 
-    public static List<String> selectFriend() {
+    public static List<String> score() {
         List<String> answer = new ArrayList<>();
+        HashMap<Integer, String> scoreList = new HashMap<>();
 
-/*        int scoreArr[]; //점수 집어넣기
-        for (int i = 0; i < classList.size(); i++) {
-            classList.
-        }*/
+        for (Map.Entry<String, User> entry : hashMap.entrySet()) {
+            User user = entry.getValue();
+            scoreList.put(user.getScore(), user.getName());
+            System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
 
+        Object[] mapKey = scoreList.keySet().toArray();
+        Arrays.sort(mapKey);
 
+        for (int i = 0; i < 5; i++) {
+            
+        }
 
         return answer;
     }
+
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -105,7 +123,7 @@ public class Problem7 {
         scoreManage(user, visitors);
 
         //3. 추천 점수 상위 5명 담긴 리스트 리턴받기
-        selectFriend();
+        score();
 
         return answer;
     }
