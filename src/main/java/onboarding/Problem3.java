892d24fe070d5d4adc00db38a43be85c33a1d6fb
jsc1014 <wotlr5209@gmail.com>
2022-11-01 18:20:55 +0900

    자릿수를 나누어 확인 및 main문 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,15 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-
+        for(int i = 1; i <= number; i++){
+            String numStr = Integer.toString(i);
+            for(int j = 0; j < numStr.length(); j++){
+                int Strnum = Integer.parseInt(numStr.substring(j,j+1));
+                if(checkMultipleOfThree(Strnum)){
+                    answer++;
+                }
+            }
+        }
 
         return answer;
     }
@@ -17,5 +25,4 @@ public class Problem3 {
         else
             return false;
     }
-
 }
