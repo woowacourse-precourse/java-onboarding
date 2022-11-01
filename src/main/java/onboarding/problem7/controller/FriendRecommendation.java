d260f6ded8f8806c9d3972a1c1b866d6a29cd5ce
song-wooseok <sws981202@gmail.com>
2022-11-01 17:32:09 +0900

    feat: [PROBLEM7 - 기능3] 사용자와 함께 아는 친구의 점수를 계산하는 기능

--- src/main/java/onboarding/problem7/controller/FriendRecommendation.java
+++ src/main/java/onboarding/problem7/controller/FriendRecommendation.java
@@ -1,7 +1,9 @@
 package onboarding.problem7.controller;
 
 import onboarding.problem7.domain.User;
+import onboarding.problem7.utils.Constants;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,6 +13,7 @@ public class FriendRecommendation {
     private List<List<String>> friends;
     private List<String> visitors;
     private HashMap<String, User> userTable = new HashMap<>();
+    private HashMap<String, Integer> recommendPointTable = new HashMap<>();
 
     public FriendRecommendation(String targetUserName, List<List<String>> friends, List<String> visitors){
         this.targetUserName = targetUserName;
@@ -39,12 +42,50 @@ public class FriendRecommendation {
 
 
     private void putUser(String name){
-        if(!alreadyInHashMap(name)){
+        if(!alreadyInHashMap(userTable,name)){
             userTable.put(name, new User(name));
         }
     }
-    private boolean alreadyInHashMap(String name){
-        return userTable.containsKey(name);
+    private boolean alreadyInHashMap(HashMap hashMap, String name){
+        return hashMap.containsKey(name);
+    }
+
+    private void updateFriendOfFriendPoints(){
+        for(String friendName: targetUser.getFriendList()){
+            User friend = userTable.get(friendName);
+            giveFriendOfFriendPoints(friend.getFriendList());
+        }
+    }
+
+    private void giveFriendOfFriendPoints(ArrayList<String> friendList){
+        for(String name: friendList){
+            addPointsToRecommendPointTable(name, Constants.POINT_OF_FRIEND_OF_FRIEND);
+        }
+    }
+
+    private void addPointsToRecommendPointTable(String name, int point){
+        if(!(isMyFriend(name) || isMe(name))){
+            givePoint(name, point);
+        }
+    }
+
+    private boolean isMyFriend(String name){
+        return targetUser.getFriendList().contains(name);
+    }
+
+    private boolean isMe(String name){
+        return name.equals(targetUserName);
+    }
+
+    private void givePoint(String name, int point){
+        recommendPointTable.put(name, calculateRecommendPoint(name, point));
+    }
+
+    private int calculateRecommendPoint(String name, int point){
+        if(alreadyInHashMap(recommendPointTable, name)){
+            return recommendPointTable.get(name)+point;
+        }
+        return point;
     }
 
 
---
+++ src/main/java/onboarding/problem7/utils/Constants.java
@@ -0,0 +1,5 @@
+package onboarding.problem7.utils;
+
+public class Constants {
+    public static final int POINT_OF_FRIEND_OF_FRIEND = 10;
+}
