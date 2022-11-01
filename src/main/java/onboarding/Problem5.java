a67d8063a09641cb252eaba1c3916ecf04686228
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-01 16:08:31 +0900

    feat : P5 - 돈의 단위별 갯수 리스트 만들기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final List<Integer> typeOfMoney = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int unit : typeOfMoney) {
+            answer.add(countMoney(money, unit));
+            money = balanceMoney(money, unit);
+        }
         return answer;
     }
-
     private static int countMoney(int money, int won) {
         return money / won;
     }
