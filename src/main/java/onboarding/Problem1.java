9e56a0f0aad2163fd21a26bbf1553ce7a10c03ca
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:01:20 +0900

    문제1 예외사항 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pageChecks = 0;
+        pageChecks += pobi.get(1) - pobi.get(0);
+        pageChecks += crong.get(1) - crong.get(0);
+
+        if (pageChecks != 2) {
+            return -1;
+        }
+
+        return 0;
     }
 }
