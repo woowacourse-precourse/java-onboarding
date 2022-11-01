3c74b8e669653b2b88357775aa59e6a72a5214e3
Jaewoooong Kim <kimje1109@gmail.com>
2022-11-01 16:01:29 +0900

    refactor: Delete conversionCase

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -44,19 +44,9 @@ public class Problem4 {
         return alphabet;
     }
 
-    public static Character conversionCase(Character alphabet) {
-        if (alphabet >= 'a' && alphabet <= 'z') {
-            return (char)((int)alphabet - 32);
-        }
-        if (alphabet >= 'A' && alphabet <= 'Z') {
-            return (char)((int)alphabet + 32);
-        }
-
-        return alphabet;
-    }
-
     public static String solution(String word) {
         String answer = "";
+        answer = getWord(word);
         return answer;
     }
 }
