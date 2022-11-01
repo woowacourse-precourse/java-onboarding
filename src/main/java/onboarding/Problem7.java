56f6f45366f79c72d444a8a76d7a6b6c44b8eb92
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:40:32 +0900

    Feat: Add 친구 관계 Map 생성 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static HashMap<String, ArrayList<String>> createRelation(List<List<String>> friends) {
+        HashMap<String, ArrayList<String>> relationMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String userA = friend.get(0);
+            String userB = friend.get(1);
+
+            relationMap = putRelation(relationMap, userA, userB);
+            relationMap = putRelation(relationMap, userB, userA);
+        }
+
+        return relationMap;
+    }
+
+
+    private static HashMap<String, ArrayList<String>> putRelation(HashMap<String, ArrayList<String>> relationMap, String userA, String userB) {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        if (relationMap.containsKey(userA)) {
+            arrayList = relationMap.get(userA);
+        }
+        arrayList.add(userB);
+        relationMap.put(userA, arrayList);
+
+        return relationMap;
     }
 }
