236bc59110c5a25a075406773f8250dfa75814a6
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 14:10:30 +0900

    FEAT countClap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int countClap(int i) {
+        int count = 0;
+
+        while (i > 0) {
+            int mod = i % 10;
+            if (mod == 3 || mod == 6 || mod == 9) {
+                count++;
+            }
+            i = i / 10;
+        }
+
+        return count;
+    }
 }
