3a76a4899eb8c8d4365aebc1128fa97ed6770d65
Suzie Lee <wnsuwls0707@gmail.com>
2022-11-01 21:51:23 +0900

    feat:Problem4, func convertWord 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem4 {
+    public static String convertWord(String word){
+        String newWord="";
+        for(int i=0;i< word.length();i++){
+            int now = word.charAt(i);
+            int tmp;
+            if(now==32){
+                tmp=now;
+            }
+            else if(now<97){
+                //대문자
+                tmp = (25 - (now - 65))+65;
+            }else{
+                //소문자
+                tmp = (25 - (now - 97))+97;
+            }
+
+            newWord=newWord+(char)tmp;
+        }
+        return newWord;
+    }
     public static String solution(String word) {
-        String answer = "";
+
+        String answer = String.join("",convertWord(word));
         return answer;
     }
 }
