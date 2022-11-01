d9274d7c32ca5b0fc54a13323dac0ac1a54dfb47
Moveuk <v_donguk@naver.com>
2022-11-01 13:40:01 +0900

    feat: Problem1 페이지 번호의 각 자리 숫자를 더한 수를 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -31,4 +31,15 @@ class Problem1 {
 
         return true;
     }
+
+    public static int getSumOfEachDigit(int number) {
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
 }
