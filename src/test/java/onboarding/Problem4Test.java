f5addf69dcde15b122592dd9d83cd145eb436bf0
이소영 <th42500@naver.com>
2022-11-01 21:25:03 +0900

    [Test] word가 청개구리 규칙에 맞게 변환되는지 테스트 할 단어 추가

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -3,8 +3,6 @@ package onboarding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class Problem4Test {
@@ -23,5 +21,6 @@ class Problem4Test {
     void keepFrogRule() {
         problem4 = new Problem4();
         assertEquals("R olev blf", problem4.greenFlogRule("I love you"));
+        assertEquals("Dlldz Xlfihv", problem4.greenFlogRule("Woowa Course"));
     }
 }
