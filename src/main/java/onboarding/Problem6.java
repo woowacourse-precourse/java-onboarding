c00086868005e838a7a2d6c9d1c804704fa7cfdb
sa46lll <9812smh@naver.com>
2022-11-01 19:30:38 +0900

    feat: 두 글자씩 저장, 단어를 기준으로 이메일 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        List<String> words = getEmailsByWord(forms);
+
         return answer;
+    }
+
+    private static List<String> getEmailsByWord(List<List<String>> forms) {
+
+        List<String> list = new ArrayList<>();
+        for (List<String> form : forms) {
+            for (int i = 0; i < form.get(1).length() - 1; i++) {
+                String word = form.get(1).substring(i, i + 2);
+                list.add(word);
+            }
+        }
+        return list;
     }
 }
