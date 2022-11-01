ecae2f6afda207213c9d06c7b5d34aa96342cb0e
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 14:50:30 +0900

    feat : 3, 6, 9가 들어가는 숫자를 count 해주는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,14 @@ public class Problem3 {
     private static final int MIN = 1;
     private static final int MAX = 10000;
 
-
+    public static int count369(String num) {
+        int cnt = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) == '3' || num.charAt(i) == '6' || num.charAt(i) == '9') {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 
 }
