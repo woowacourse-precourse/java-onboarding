1d1e12df73a744d66c3a3155f17906ff19013322
Lee-Jiseung <ljss98@naver.com>
2022-11-01 16:48:07 +0900

    feat: problem7 함께 아는 친구 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,16 +1,18 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
 
+    private static final Map<String, Integer> scores = new HashMap<>();
     private static List<String> userFriends;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         initUserFriends(user, friends);
+
+        getCofriends(friends);
+
         return answer;
     }
 
@@ -21,6 +23,17 @@ public class Problem7 {
                 userFriends.add(friend.get(1));
             } else if (friend.get(1).equals(user)) {
                 userFriends.add(friend.get(0));
+            }
+        }
+    }
+
+    private static void getCofriends(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            if (userFriends.contains(friend.get(0))) {
+                scores.put(friend.get(1), scores.getOrDefault(friend.get(1), 0) + 10);
+            }
+            if (userFriends.contains(friend.get(1))) {
+                scores.put(friend.get(0), scores.getOrDefault(friend.get(0), 0) + 10);
             }
         }
     }
