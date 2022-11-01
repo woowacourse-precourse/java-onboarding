77e01e0c76ca47a4a621804c7d4ba78597eaaf3f
sun007021 <sun007021@gmail.com>
2022-11-01 16:29:03 +0900

    feat: money를 화폐 단위로 큰 순서부터 나누어 개수 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,8 +5,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-class Money{
-    private List<Integer> moneyUnits = Arrays.asList(50000, 10000, 1000, 500, 100, 50, 10, 1); // default 값 한국기준
+class CalculateMoney{
+    private List<Integer> moneyUnits = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1); // default 값 한국기준
+
+    public List<Integer> calculateMoneyByUnits(int money){
+        List<Integer> result = new ArrayList<>();
+        int share; // 몫 저장
+        for(Integer moneyUnit: moneyUnits){//각 돈 단위로 남은 금액을 나눔
+            share = money/moneyUnit;
+            result.add(share);
+            money = money%moneyUnit;
+        }
+        return result;
+    }
 
     public void setMoneyUnits(List<Integer> moneyUnits) {// 단위 변경
         this.moneyUnits = moneyUnits;
@@ -14,7 +25,8 @@ class Money{
 }
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        CalculateMoney calculateMoney = new CalculateMoney();
+        List<Integer> answer = calculateMoney.calculateMoneyByUnits(money);
         return answer;
     }
 }
