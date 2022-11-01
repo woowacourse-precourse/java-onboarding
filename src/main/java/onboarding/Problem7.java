5a742e46efce63a8e441ec5eba772b9008362603
ParkJoungWoo <jug78@naver.com>
2022-11-01 19:27:45 +0900

    문제 7) 기능 4 : 이름을 key로, 추천 점수를 value로 하는 map을 입력 받아 map의 value 기준 내림차순, 같은 value는 key 기준 사전순으로 정렬한 최대 5개의 이름 목록을 반환하는 기능 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,18 @@ import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Set<String> user_friends = findUserFriends(user, friends);
+        HashMap<String, Integer> not_user_friends = findNotUserFriends(user, friends, user_friends);
+        not_user_friends = addVisitor(not_user_friends, user_friends, visitors);
+        answer = sortFiveValueAndName(not_user_friends);
         return answer;
     }
     private static Set<String> findUserFriends(String user, List<List<String>> friends) {
@@ -45,5 +53,25 @@ public class Problem7 {
             notFriendMap.put(friend_key, notFriendMap.getOrDefault(friend_key, 0) + 1);
         }
         return notFriendMap;
+    }
+    public static List<String> sortFiveValueAndName(HashMap<String,Integer> map){
+        List<String> result = new ArrayList<>();
+        List<Map.Entry<String,Integer>> map_entryList = new ArrayList<Entry<String, Integer>>(map.entrySet());
+        Collections.sort(map_entryList, new Comparator<Entry<String,Integer>>(){
+            @Override
+            public int compare(Entry<String,Integer> ob1, Entry<String,Integer>ob2){
+                if(ob1.getValue() > ob2.getValue())
+                    return -1;
+                if(ob1.getValue() < ob2.getValue())
+                    return 1;
+                if(ob1.getKey().compareTo(ob2.getKey()) <= 0)
+                    return -1;
+                return 1;
+            }
+        });
+        for(int j = 0, i = 0; (j < 5)&&(i < map_entryList.size()); i++, j++){
+            result.add(map_entryList.get(i).getKey());
+        }
+        return result;
     }
 }
