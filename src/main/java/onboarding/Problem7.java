6d169dd1b60fe0039c4d1f2ef3a351e86e28571d
p <gustjr2332@naver.com>
2022-11-01 22:38:50 +0900

    feat(Problem7): findFriendsOfUserFriends() 현재 user친구의 친구 찾기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,17 +12,17 @@ public class Problem7 {
     }
 
     public static String findMyFriend( String myname, List<String> friends){
-        String userfriends = "";
+        String myfriend = "";
 
         if(friends.contains(myname)){
             if(friends.get(0).equals(myname)) {
-                userfriends= friends.get(1);
+                myfriend= friends.get(1);
             }
             else if(friends.get(1).equals(myname)){
-                userfriends = (friends.get(0));
+                myfriend = friends.get(0);
             }
         }
-        return userfriends;
+        return myfriend;
     }
 
     public static List<String> findUserFreinds(String user, List<List<String>> friends){
@@ -34,6 +34,21 @@ public class Problem7 {
             }
         }
         return userfriends;
+    }
+
+    public static List<String> findFriendsOfUserFriends(String user, List<String> userfriends, List<List<String>> friends){
+        List<String> freindoffriend = new ArrayList<>();
+        for (int i = 0; i < userfriends.size(); i++) {
+            for(int j=0; j<friends.size(); j++) {
+                if(!friends.get(j).contains(user)) {
+                    String myfreind = findMyFriend(userfriends.get(i), friends.get(j));
+                    if (!myfreind.isEmpty()) {
+                        freindoffriend.add(myfreind);
+                    }
+                }
+            }
+        }
+        return freindoffriend;
     }
 
 }
