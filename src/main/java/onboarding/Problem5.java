8000fbfddc64e1a3040b0540b7cf5816c27a0a38
hataerin <goil1113@likelion.org>
2022-11-01 20:07:32 +0900

    DOCS: Problem5 기능목록 작성
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+
+        //화폐 개수 계산 기능
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
