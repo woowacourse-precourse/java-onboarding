49bf8e9a525bb8bab666c01ba20925d7786bb687
Brian <bwook9908@gmail.com>
2022-11-01 15:19:42 +0900

    [문제5] 2차 리팩토링 완료, 문제1~5 수정사항 반영

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,7 +1,7 @@
 /*
  * Problem5
  *
- * v1.1
+ * v1.2
  *
  * 2022.11.01
  *
@@ -15,24 +15,24 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         ArrayList<Integer> answer = new ArrayList<Integer>();
-        int[] standardMoneyArray={50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] exchangeMoneyArray={50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         int remainMoney=money;
 
-        for (int standardMoney : standardMoneyArray) {
-            answer.add(quotientAndRemainder(remainMoney, standardMoney)[0]);
-            remainMoney = quotientAndRemainder(remainMoney, standardMoney)[1];
+        for (int exchangeMoney : exchangeMoneyArray) {
+            answer.add(quotientAndRemainder(remainMoney, exchangeMoney, 0));
+            remainMoney = quotientAndRemainder(remainMoney, exchangeMoney, 1);
         }
 
         return answer;
     }
 
-    static int[] quotientAndRemainder(int number, int numberToDivide){
+    static int quotientAndRemainder(int number, int numberToDivide, int index){
         int[] array= new int[2];
         int quotient = number / numberToDivide;
         int remainder= number % numberToDivide;
         array[0]= quotient;
         array[1]= remainder;
-        return array;
+        return array[index];
     }
 
 }
