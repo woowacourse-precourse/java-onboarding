467ec16173ffb995ff04dd91fa8b01a2e9226d47
jemin <jemin03120111@gmail.com>
2022-11-01 22:39:21 +0900

    feat: #1-4 예외 케이스 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,10 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
 
+
+        int answer = Integer.MAX_VALUE;
         int pobiMaxNum = 0;
         int crongMaxNum = 0;
+
+
+        // 만약 책을 폈을때 함께 나오는 두 페이지가 아닐 경우 예외 케이스로 -1을 반환한다.
+        if ((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1))
+        {
+            answer = -1;
+            return answer;
+        }
+
+
+        // 책을 시작면이나 마지막 면이 나오도록 펼치면 예외 케이스로 -1을 반환한다.
+        if(pobi.get(0) == 1 || pobi.get(1) == 400 || crong.get(0) == 1 || crong.get(1) == 400)
+        {
+            answer = -1;
+            return answer;
+        }
+
 
         // 포비의 경우의 수 체크
         for (Integer integer : pobi) {
@@ -29,6 +47,7 @@ class Problem1 {
 
             pobiMaxNum = Math.max(addNum,multipleNum);
         }
+
 
         // 크롱의 경우의 수 체크
         for (Integer integer : crong) {
