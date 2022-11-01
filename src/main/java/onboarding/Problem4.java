2413e85bdca24ae36f2489cbe3c1907387f6cf9f
sxxgxxnz <tmddms2292@gmail.com>
2022-11-01 13:39:47 +0900

    fix Problem4 refactoring

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,18 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
+    public static Map<Character,Character> initAlpha(){
         Map<Character,Character> map = new HashMap<>();
         int alphaLength=26;
         int upperA=65,upperZ=90;
         int lowerA=97,lowerZ=122;
-        //initAlpha
         for(int i=0;i<alphaLength;i++){
             map.put((char)(upperA + i),(char)(upperZ-i));
             map.put((char)(lowerA+i),(char)(lowerZ-i));
         }
-
+        return map;
+    }
+    public static String solution(String word) {
+        String answer = "";
+        Map<Character,Character> map = initAlpha();
         char[] wordArr = word.toCharArray();
         for(int i=0;i<wordArr.length;i++){
             if(!(wordArr[i] >= 'A' && wordArr[i] <= 'Z') && !(wordArr[i] >= 'a' && wordArr[i] <= 'z')){
