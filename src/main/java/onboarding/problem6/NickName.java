ae6834236a83a4c00066458b2a5869c49b47d7e8
This2sho <zx00018@naver.com>
2022-11-01 18:27:08 +0900

    feat: 닉네임 유효성 체크 구현

---
+++ src/main/java/onboarding/problem6/NickName.java
@@ -0,0 +1,22 @@
+package onboarding.problem6;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NickName {
+	private static final String NICK_NAME_PATTERN = "^[가-힣]{1,19}";
+	private final String nickName;
+
+	public NickName(String inputNickName) {
+		if (!isValidateNickName(inputNickName)) {
+			throw new IllegalArgumentException("닉네임은 한글로만 1자 이상 20자 미만이어야합니다.");
+		}
+		this.nickName = inputNickName;
+	}
+
+	boolean isValidateNickName(String inputNickName) {
+		Pattern compile = Pattern.compile(NICK_NAME_PATTERN);
+		Matcher matcher = compile.matcher(inputNickName);
+		return matcher.matches();
+	}
+}
