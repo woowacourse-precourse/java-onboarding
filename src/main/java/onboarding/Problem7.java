f2c6f6ab9ce04858ac650536ebc098b5a5875abe
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 17:41:18 +0900

    {PROBLEM 7] user의 친구 리스트에 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        // user의 친구 리스트
+        List<String> userFriendsList = new ArrayList<>();
+        for(int i=0; i<friends.size(); i++) {
+            if(friends.get(i).get(0).equals(user)) {
+                userFriendsList.add(friends.get(i).get(1));
+            } else if(friends.get(i).get(1).equals(user)) {
+                userFriendsList.add(friends.get(i).get(0));
+            }
+        }
+
         return answer;
     }
 }
