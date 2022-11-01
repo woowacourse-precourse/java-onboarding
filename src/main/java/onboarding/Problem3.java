390c29c589b119aa285b210d066aaef276bde0f8
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:41:12 +0900

    feat(problem3): 숫자 범위 검증

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        validate(number);
+        return 0;
     }
+
+    private static void validate(int number) {
+        if (number < 1 || number > 10000) {
+            throw new IllegalArgumentException("number는 1 이상 10,000 이하인 자연수이어야 합니다.");
+        }
+    }
+
 }
