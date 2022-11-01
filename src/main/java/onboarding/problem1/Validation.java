f80598983256bf77c047fc889714b2f7340360f3
kimminsu <xjfcnfw3@naver.com>
2022-11-01 13:08:26 +0900

    refactor(문제1): page 검증 함수 기능 분리

--- src/main/java/onboarding/problem1/Validation.java
+++ src/main/java/onboarding/problem1/Validation.java
@@ -3,30 +3,37 @@ package onboarding.problem1;
 import java.util.List;
 
 public class Validation {
+    private static final int SEQUENCE_CHECK_NUMBER = 1;
     private static final int START_PAGE = 1;
     private static final int END_PAGE = 400;
 
-    public static boolean validate(List<Integer> pobi, List<Integer> crong){
+    public static boolean validate(List<Integer> pobi, List<Integer> crong) {
         return validateUser(pobi) && validateUser(crong);
     }
-    private static boolean validateUser(List<Integer> user){
+
+    private static boolean validateUser(List<Integer> user) {
         return validatePages(user.get(0), user.get(1));
     }
-    private static boolean validatePages(int leftPage, int rightPage){
-        if (leftPage % 2 == 0 || rightPage % 2 != 0){
-            return false;
-        }
 
-        if (rightPage - leftPage != 1){
+    private static boolean validatePages(int leftPage, int rightPage) {
+        if (!validateLeftRightPage(leftPage, rightPage)) {
             return false;
         }
-
-        if (validateNotInRange(leftPage) || validateNotInRange(rightPage)){
+        if (!validatePageSequence(leftPage, rightPage)) {
             return false;
         }
-        return true;
+        return validateInRange(leftPage) && validateInRange(rightPage);
     }
-    private static boolean validateNotInRange(int page){
-        return page <= START_PAGE || page >= END_PAGE;
+
+    private static boolean validateInRange(int page) {
+        return (page > START_PAGE) && (page < END_PAGE);
+    }
+
+    private static boolean validateLeftRightPage(int leftPage, int rightPage) {
+        return (leftPage % 2 != 0) && (rightPage % 2 == 0);
+    }
+
+    private static boolean validatePageSequence(int leftPage, int rightPage) {
+        return rightPage - leftPage == SEQUENCE_CHECK_NUMBER;
     }
 }
