3fc37a91f0cc7645aa84831f918954ff9a0209b8
SHEOMM <esh0821@snu.ac.kr>
2022-11-01 20:06:43 +0900

    리팩토링 : 문제 3번
    input 유효성 검사 버그 체크
    369 판별 함수 기능 세분화
    - 각 자릿수가 3, 6, 9인지 판별 함수
    - 그 수의 3, 6, 9 개수 카운트

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,10 @@ public class Problem3 {
     private static final List<Integer> listOf369 = new ArrayList<>(Arrays.asList(3, 6 ,9));
 
     public static int solution(int number) {
+        if(isInvalidInput(number)){
+            return -1;
+        }
+
         int result = 0;
         for(int i = 1; i <= number; i++){
             result += get369CountEachNum(i);
@@ -15,12 +19,21 @@ public class Problem3 {
         return result;
     }
 
+    private static boolean isInvalidInput(int num){
+        return !(1 <= num && num <= 10000);
+    }
+
     private static int get369CountEachNum(int num){
         int result = 0;
         for(;num > 0; num /= 10){
-            if(listOf369.contains(num % 10)){
-                result++;
-            }
+            result += is369Digit(num % 10);
+        }
+        return result;
+    }
+    private static int is369Digit(int num){
+        int result = 0;
+        if(listOf369.contains(num)){
+            result++;
         }
         return result;
     }
