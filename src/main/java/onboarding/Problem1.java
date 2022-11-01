b184c24a30ee59cd16de6b133aa56c619e79ea94
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-01 15:29:07 +0900

    test1 - 페이지 예외사항처리 (처음이거나 마지막페이지 추가)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,7 +7,7 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
 
         // 페이지 예외사항 처리
-        if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1)
+        if(exception(pobi.get(0), pobi.get(1)) || exception(crong.get(0), crong.get(1)))
             return answer = -1;
 
         // 승 패 무승부 처리
@@ -55,5 +55,19 @@ class Problem1 {
         int big = Math.max(left, right);
 
         return big;
+    }
+
+    // 페이지 예외사항 처리
+    // 페이지 오류
+    // 처음이거나 끝페이지
+    public static boolean exception(int num1, int num2) {
+        boolean except = false;
+
+        if(num2 - num1 != 1)
+            except = true;
+        else if(num1==1 || num2==400)
+            except = true;
+
+        return except;
     }
 }
