3881fbf0dbe0332545629e4676a9c012d3d47844
SEONMl <sm000117@naver.com>
2022-11-01 14:38:03 +0900

    refactor: Problem1 필드 숫자 상수화

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -75,19 +75,23 @@ class Problem1 {
     }
 
     static class Page {
+        private final int NECESSARY_INPUT_PAGE_SIZE =2;
+        private final int MIN_PAGE_INPUT_VALUE = 1;
+        private final int MAX_PAGE_INPUT_VALUE =400;
+
         private List<Integer> page;
 
         public Page(List<Integer> page) {
-            if(page.size()!=2) {
+            if(page.size() != NECESSARY_INPUT_PAGE_SIZE) {
                 throw new RuntimeException("페이지는 두 개의 값을 입력해야 합니다.");
             }
-            if(page.get(0)<=1||page.get(1)>=400) {
+            if(page.get(0)<=MIN_PAGE_INPUT_VALUE || page.get(1)>=MAX_PAGE_INPUT_VALUE) {
                 throw new RuntimeException("입력 가능한 페이지 범위를 초과하였습니다.");
             }
-            if(page.get(1)-page.get(0)!=1) {
+            if(page.get(1)-page.get(0) != 1) {
                 throw new RuntimeException("잘못된 페이지 값을 입력하였습니다.");
             }
-            if(page.get(0)%2==0||page.get(1)%2==1) {
+            if(page.get(0)%2==0 || page.get(1)%2==1) {
                 throw new RuntimeException("잘못된 페이지 값을 입력하였습니다.");
             }
             this.page = page;
