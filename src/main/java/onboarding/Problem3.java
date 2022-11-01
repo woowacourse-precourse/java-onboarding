964902616ea3c0446b88ccae309ee2d1df411c00
Giho <poqw741@gmail.com>
2022-11-01 22:39:25 +0900

    add 369method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            String tmp = String.valueOf(i);
+            for (int j = 0; j < tmp.length(); j++) {
+                char chartmp = tmp.charAt(j);
+                if (chartmp == '3' || chartmp == '6' || chartmp == '9') {
+                    answer += 1;
+                }
+            }
+        }
+
+
         return answer;
     }
 }
