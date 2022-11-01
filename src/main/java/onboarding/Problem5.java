91e7648f4e0460076427278f10182ded5e116d5a
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:25:34 +0900

    [5번] feat: 총 금액이 각각의 돈 단위마다 몇 개씩 변환되는 지 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    static int moneyUnitList[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer =getResultArray(money);
+        return answer;
+    }
+
+    // 1. 총 금액이 각각의 돈 단위마다 몇 개씩 변환되는 지 구하기
+    public static List<Integer> getResultArray(int money) {
+        List<Integer> answer = new ArrayList<>(moneyUnitList.length);
+
+        int moneyLeft = money;
+        for (Integer moneyUnit :moneyUnitList) {
+            answer.add(getShare(moneyLeft, moneyUnit));
+            moneyLeft =getLeftMoney(moneyLeft, moneyUnit);
+        }
         return answer;
     }
 
