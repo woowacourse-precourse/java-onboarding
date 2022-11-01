f23dcf4cd4c321b7f4c2615ddbd3b3991af70e1b
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-01 22:22:45 +0900

    feat 청개구리 게임을 통해 문자열 알파벳의  배열의 역순을 구현한 문제
    문자열 알파벳의 번호의 이해도를 높일 수 있었다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static String reverseAlphabet(String str) {
+        int tmp;
+        StringBuilder reverse = new StringBuilder();
+        for (int i =0; i< str.length(); i++) {
+            tmp = str.charAt(i);
+            if((65 <= tmp)&&(tmp <=90)){
+                reverse.append((char) (155 - tmp));
+            }else if((97 <= tmp)&&(tmp<=122)){
+                reverse.append((char) (219 - tmp));
+            }else{
+                reverse.append((char) tmp);
+            }
+        }return reverse.toString();
+    }
     public static String solution(String word) {
+
         String answer = "";
+        answer = reverseAlphabet(word);
         return answer;
     }
 }
