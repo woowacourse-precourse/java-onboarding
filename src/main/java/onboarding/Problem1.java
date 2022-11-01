ce84e9f548f0ad8da2c4ffdffbb2f156d01be6c9
신채원 <cw121@naver.com>
2022-11-01 16:04:39 +0900

    feat(problem1): 게임결과를 반환하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiScore = getScore(pobi);
+        int crongScore = getScore(crong);
+        int answer = getResult(pobiScore,crongScore);
         return answer;
     }
 
@@ -30,5 +32,15 @@ class Problem1 {
         int leftScore=Math.max(addEachDigits(pages.get(0)),multiplyEachDigits(pages.get(0)));
         int rightScore=Math.max(addEachDigits(pages.get(1)),multiplyEachDigits(pages.get(1)));
         return Math.max(leftScore,rightScore);
+    }
+
+    private static int getResult(int pobiScore, int crongScore) {
+        if(pobiScore>crongScore) {
+            return 1;
+        }else if(pobiScore<crongScore){
+            return 2;
+        }else{
+            return 0;
+        }
     }
 }
