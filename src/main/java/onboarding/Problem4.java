439e2e1f230c3eab84c7b88342221704d57c451d
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-01 18:02:09 +0900

    feat(PROBLEM4): solution 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<word.length(); i++){
+                answer += reverseWord(word.charAt(i));
+        }
         return answer;
     }
 }
