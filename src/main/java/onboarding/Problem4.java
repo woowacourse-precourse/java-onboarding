93e75be203f762562c4eadcf9c6ac2d4665ad5c9
rhqudco <rhqudco1204@naver.com>
2022-11-01 16:00:02 +0900

    DOCS : Requirements analysis

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/*
+* 1. 제한사항 알파벳 외의 문자는 변환하지 않기 때문에 알파벳인지 판별하는 메소드 구현
+* 2. 대문자인지 소문자인지 판별하는 메소드 구현
+* 3. 대문자 -> 대문자, 소문자 -> 소문자 구현
+* 4. 최종 반환
+* */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
