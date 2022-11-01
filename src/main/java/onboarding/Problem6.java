433746715ae69d5d7f90c087f19c0ee7f7ff6028
dvlpr0 <rladjswkd@naver.com>
2022-11-01 19:25:43 +0900

    added/Problem6: 등장 횟수가 2 이상인 부분 문자열을 포함한 모든 닉네임에 대해, 이메일을 set에 담고 리스트로 변환해 정렬한 후 반환하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -22,16 +22,27 @@ public class Problem6 {
 	private static void fillSetAll(List<List<String>> forms) {
 		forms.forEach(Problem6::fillSetEach);
 	}
+	private static void addEmailIfContainKey(String email, boolean flag) {
+		if (flag)
+			emails.add(email);
+	}
+	private static void checkIfContainKey(String email, String nickname) {
+		Set<String> keySet = words.keySet();
 
+		keySet.forEach(x->addEmailIfContainKey(email, nickname.contains(x) && words.get(x) > 1));
+	}
+	private static void findDuplicated(List<List<String>> forms) {
+		forms.forEach(x->checkIfContainKey(x.get(0), x.get(1)));
+	}
 	public static List<String> solution(List<List<String>> forms) {
 		emails = new HashSet<>();
 		words = new Hashtable<>();
 		List<String> answer;
 
 		fillSetAll(forms);
-//		findDuplicated(forms);
-//		answer = new ArrayList<>(emails);
-//		Collections.sort(answer);
+		findDuplicated(forms);
+		answer = new ArrayList<>(emails);
+		Collections.sort(answer);
 		return (answer);
 	}
 }
