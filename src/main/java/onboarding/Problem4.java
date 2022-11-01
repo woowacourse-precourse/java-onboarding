9859a905dc5895fbbe8411b3d38beb1cf935c8b9
ingpyo <juninpyo72@gmail.com>
2022-11-01 11:44:06 +0900

    [problem4]매칭된 알파벳으로 바꿔주는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+
+
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            if ((int)word.charAt(i) >= 65 && (int)word.charAt(i) <= 90) {
+                answer += (char)(155 - (int)word.charAt(i));
+            } else if ((int)word.charAt(i) >= 97 && (int)word.charAt(i) <= 122) {
+                answer += (char)(219 - (int)word.charAt(i));
+            } else {
+                answer += word.charAt(i);
+            }
+        }
+
         return answer;
     }
 }
+
