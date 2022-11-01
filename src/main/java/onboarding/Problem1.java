e044cb6e288f7f6a3d8bd24065d73d88bbf66f5b
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-11-01 23:01:30 +0900

    style(Problem1): 코드 스타일 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int sumOfPageNumber(int page) {
         int result = 0;
         while (page != 0) {
@@ -41,22 +42,30 @@ class Problem1 {
 
         int result = Math.max(maxOfFirstPageList, maxOfSecondPageList);
 
-        if (maxOfFirstPageList == maxOfSecondPageList) return 0;
-        else if (result == maxOfFirstPageList) return 1;
-        else return 2;
+        if (maxOfFirstPageList == maxOfSecondPageList) {
+            return 0;
+        } else if (result == maxOfFirstPageList) {
+            return 1;
+        } else {
+            return 2;
+        }
 
     }
 
     public static boolean validatePageNumberInRange(List<Integer> pageList) {
         for (int page : pageList) {
-            if (page < 1 || page > 400) return false;
+            if (page < 1 || page > 400) {
+                return false;
+            }
         }
         return true;
     }
 
     public static boolean validatePageNumberSequence(List<Integer> pageList) {
-        for(int i=0; i<pageList.size() - 1; i++) {
-            if(pageList.get(i+1) != pageList.get(i) + 1) return false;
+        for (int i = 0; i < pageList.size() - 1; i++) {
+            if (pageList.get(i + 1) != pageList.get(i) + 1) {
+                return false;
+            }
         }
         return true;
     }
@@ -64,8 +73,12 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if (!validatePageNumberInRange(pobi) || !validatePageNumberInRange(crong)) return -1;
-        if (!validatePageNumberSequence(pobi) || !validatePageNumberSequence(crong)) return -1;
+        if (!validatePageNumberInRange(pobi) || !validatePageNumberInRange(crong)) {
+            return -1;
+        }
+        if (!validatePageNumberSequence(pobi) || !validatePageNumberSequence(crong)) {
+            return -1;
+        }
 
         answer = winnerOfGame(pobi, crong);
         return answer;
