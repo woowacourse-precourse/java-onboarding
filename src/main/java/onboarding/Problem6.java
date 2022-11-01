af4192b9196fe17784dfdf3bac1711faeaa405b0
sangeun-b <baek0007@algonquinlive.com>
2022-11-01 20:21:55 +0900

    feat(PROBLEM6): Add feature for saving nicknames

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
@@ -7,4 +8,13 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    // 닉네임들만 저장
+    public static List<String> getNicknameList(List<List<String>> forms){
+        List<String> nicknameList = new ArrayList<>();
+        for(int i=0; i<forms.size(); i++){
+            nicknameList.add(forms.get(i).get(1));
+        }
+        return nicknameList;
+    }
+
 }
