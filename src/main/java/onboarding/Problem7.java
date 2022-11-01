9702228b63a2554a9e2377532612b54122d89e29
hongjinwon <herenever@naver.com>
2022-11-01 20:52:51 +0900

    feat: Problem7 makeScoreMap, scoring, visitScoring, freindScoring 추가
    - Problem7 makeScoreMap, scoring, visitScoring, freindScoring 추가
    -
    makeScoreMap : freindsMap과 visitors를 참고하여 친구 추천 대상자들을 key 값으로 하고 점수를
    value로 하는 hashMap 생성
    - scoring : 함께 아는 친구 점수와 방문 점수를 실행 시킴
    -
    visitScoring : user의 방문 횟수를 참고하여 scroeMap에 점수를 업데이트 함
    - friendScroing:
    user의 친구의 친구들을 참고하여 scoreMap에 점수를 업데이트 함

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,6 +9,7 @@ public class Problem7 {
     }
 
     public static List<String> getUserFriends(String user, List<List<String>> friends){
+
         List<String> userFriends = new ArrayList<>();
         for (List<String> friend : friends) {
             if(friend.get(0).equals(user)){
@@ -21,6 +22,7 @@ public class Problem7 {
     }
 
     public static Map<String,List<String>> makeFriendsMap(String user, List<String> userFriends, List<List<String>> friends){
+
         Map<String,List<String>> friendsMap = new HashMap<>();
 
         for (List<String> friend : friends){
@@ -32,6 +34,22 @@ public class Problem7 {
             }
 
         return friendsMap;
+    }
+
+    public static Map<String,Integer> makeScoreMap(Map<String,List<String>> friendsMap,List<String> visitors){
+
+        Map<String,Integer> scoreMap = new HashMap<>();
+        Set<String> keySet = friendsMap.keySet();
+
+        for(String user : keySet){
+            scoreMap.put(user,0);
+        }
+
+        for(String user: visitors){
+            scoreMap.put(user,0);
+        }
+
+        return scoreMap;
     }
 
 
@@ -64,6 +82,36 @@ public class Problem7 {
 
         friends.add(friend.get(0));
         friendsMap.put(friend.get(1),friends);
+
+    }
+
+    public static void scoring(Map<String,Integer> scoreMap,Map<String,List<String>> friendsMap ,List<String> userFriends,List<String> visitors){
+
+        visitScoring(scoreMap,visitors);
+        friendScoring(scoreMap,userFriends,friendsMap);
+
+    }
+
+    public static void visitScoring(Map<String,Integer> scoreMap,List<String> visitors){
+
+        for (String visitor : visitors){
+            Integer score = scoreMap.get(visitor);
+            scoreMap.put(visitor,score+1);
+        }
+
+    }
+
+    public static void friendScoring(Map<String,Integer> scoreMap, List<String> userFriends, Map<String,List<String>> friendsMap){
+
+        List<String> scoreList = new ArrayList<>();
+        for(String userFriend : userFriends){
+            friendsMap.get(userFriend).forEach(i -> scoreList.add(i));
+        }
+
+        for(String user : scoreList){
+            Integer score = scoreMap.get(user);
+            scoreMap.put(user,score+10);
+        }
 
     }
 
