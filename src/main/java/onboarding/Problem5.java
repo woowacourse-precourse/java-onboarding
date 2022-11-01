74dc367b9b1efe196f2be3a9e3d1f9e52006183c
ye1ne <yewon7036@naver.com>
2022-11-01 17:07:03 +0900

    docs : 문제 5번에 대한 풀이 및 구현 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 문제 풀이 방식 & 기능 목록서는
+ * docs/PROBLEM5.md 의 하단에 기술되어 있습니다
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
