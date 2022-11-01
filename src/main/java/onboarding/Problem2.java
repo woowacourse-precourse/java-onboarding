201a2568fabe464ad45ad30f3263ebd8bdafcb4c
Yh0sun <hy770819@naver.com>
2022-11-01 14:43:50 +0900

    func2 add

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,7 +10,10 @@ public class Problem2 {
         //2. cryptogram 하나씩 읽어서
         //      - answer 마지막 글자랑 같으면 answer 마지막 글자 삭제
         //      - answer 마지막 글자랑 다르면 answer에 추가
-
+        for(int i=1;i<cryptogram.length();i++){
+            if(cryptogram.charAt(i)==answer.charAt(answer.length()-1))answer=answer.substring(0,answer.length()-1);
+            else answer+=Character.toString(cryptogram.charAt(i));
+        }
         return answer;
     }
 }
