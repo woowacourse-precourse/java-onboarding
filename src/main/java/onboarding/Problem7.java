fdf184fbd652906d7b43d5ec425872771bbc45bd
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 15:22:45 +0900

    [7번] feat: 아는 친구 점수 반영

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,6 +15,9 @@ public class Problem7 {
         // 2. 모든 친구/ 점수 리스트 초기화 (모든 친구 = 직접 친구 + 아는 친구)
         friendsScoreList = getFriendsScoreList(user, friends, visitors);
 
+        // 3. 아는 친구 점수 반영
+        friendsScoreList = scoreByKnownFriends(user, friends);
+
         return answer;
     }
 
@@ -42,6 +45,25 @@ public class Problem7 {
         for (String visitor : visitors) {
             if (!friendsScoreList.containsKey(visitors))friendsScoreList.put(visitor, 0);
         }
+        return friendsScoreList;
+    }
+
+    // 3. 아는 친구 점수 반영
+    static Map<String, Integer> scoreByKnownFriends(String user, List<List<String>> friends){
+
+        HashMap<String, Integer> knownFriends = new HashMap<>();
+
+        // 아는 친구들
+        for (List<String> friendRelation : friends) {
+            if (friendRelation.contains(user)) continue;
+            if (directFriends.contains(friendRelation.get(1))) knownFriends.put(friendRelation.get(0), 1);
+            if (directFriends.contains(friendRelation.get(0))) knownFriends.put(friendRelation.get(1), 1);
+        }
+
+        // 아는 친구면 10점 추가
+        knownFriends.forEach((key, value)->{
+            friendsScoreList.put(key, 10);
+        });
         return friendsScoreList;
     }
 }
