52423de6f1fa8ba6c98a1a3b9d67917f18a688db
sangeun-b <baek0007@algonquinlive.com>
2022-11-01 20:12:23 +0900

    feat(PROBLEM4): Use methods for working according to the rules

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String upperDictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String frogUpperDictionary = reverseString(upperDictionary);
+        String lowerDictionary = "abcdefghijklmnopqrstuvwxyz";
+        String frogLowerDictionary = reverseString(lowerDictionary);
+        for(int i=0; i<word.length(); i++) {
+            if (checkLetter(word.charAt(i)) == "A")
+                answer += translateUpperCase(word.charAt(i), upperDictionary, frogUpperDictionary);
+            if (checkLetter(word.charAt(i)) == "a")
+                answer += translateLowerCase(word.charAt(i), lowerDictionary, frogLowerDictionary);
+            if (checkLetter(word.charAt(i)) == "etc")
+                answer += word.charAt(i);
+
+        }
+
         return answer;
     }
     // 문자열 대문자,소문자,공백, 특수기호, 확인
