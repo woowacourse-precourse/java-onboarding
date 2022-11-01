8c8964980d0c6d15efcc66fbfad999121fcf8151
idoburnish <doyun7433@naver.com>
2022-11-01 16:10:14 +0900

    feat(PROBLEM2): 연속하는 중복 문자 제거 기능
    현재의 문자열에서 연속하는 중복 문자들을 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        return removeDuplicateString(cryptogram);
+    }
+
+    public static String removeDuplicateString(String cryptogram) {
+        StringBuilder decryption = new StringBuilder();
+        int length = cryptogram.length();
+
+        for (int i=1; i<length; i++) {
+            if (cryptogram.charAt(i-1) != cryptogram.charAt(i)) {
+                decryption.append(cryptogram.charAt(i-1));
+            }
+            if (cryptogram.charAt(i-1) == cryptogram.charAt(i)) i++;
+        }
+        if (cryptogram.charAt(length-2) != cryptogram.charAt(length-1))
+            decryption.append(cryptogram.charAt(length-1));
+
+        return decryption.toString();
     }
 }
