877afb199d2d1478352e002a4b7ce3debaad7e1d
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2022-11-01 10:43:12 +0900

    feat(Problem6): 중복된 닉네임 확인하은 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,23 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
 public class Problem6 {
+
+    private final static int EMAIL_INDEX = 0;
+    private final static int NICKNAME_INDEX = 1;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = checkNameDuplication(forms,addTwoLettersFromName(forms));
 
         return answer;
     }
-    private static HashMap<String,Integer> checkNicknameDuplication(List<List<String>> forms){
-        HashMap<String,Integer> nicknameDuplication = new HashMap<>();
+    private static HashMap<String,Integer> addTwoLettersFromName(List<List<String>> forms){
+        HashMap<String,Integer> twoLettersFromName = new HashMap<>();
         for(List<String> form: forms){
-            for(int i=0; i<form.get(1).length();i++ ){
-                String twoWords = form.get(1).substring(i,i+2);
-                nicknameDuplication.put(twoWords,
-                        nicknameDuplication.getOrDefault(twoWords,0)+1);
+            for(int i=0; i<form.get(1).length()-1;i++ ){
+                String twoLetters = form.get(NICKNAME_INDEX).substring(i,i+2);
+                twoLettersFromName.put(twoLetters,
+                        twoLettersFromName.getOrDefault(twoLetters,0)+1);
             }
         }
-        return nicknameDuplication;
+        return twoLettersFromName;
+    }
+
+    private static List<String> checkNameDuplication (List<List<String>> forms,
+                                                      HashMap<String,Integer> duplicatedTwoLettersFromName){
+    List<String> resultEmail = new ArrayList<>();
+    for(List<String> form : forms){
+        for(int i=0; i<form.get(NICKNAME_INDEX).length()-1;i++){
+            String twoLetters = form.get(NICKNAME_INDEX).substring(i,i+2);
+            if(duplicatedTwoLettersFromName.get(twoLetters)>1)
+                resultEmail.add(form.get(EMAIL_INDEX));
+        }
+    }
+    return resultEmail;
     }
 }
