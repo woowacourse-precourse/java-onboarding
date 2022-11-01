9154719fa9d40a3d6e7383ec1731f35ee47f47b1
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:26:47 +0900

    feat(problem2): 연속하는 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,7 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
         validate(cryptogram);
-        return "";
+        return decode(cryptogram);
     }
 
     private static void validate(String cryptogram) {
@@ -18,5 +18,36 @@ public class Problem2 {
                 throw new IllegalArgumentException("알파벳 소문자로만 이루어져 있어야 합니다.");
             }
         }
+    }
+
+    private static String decode(String cryptogram) {
+        while (isExistDuplicatedMessage(cryptogram)) {
+            cryptogram = removeDuplicatedMessages(cryptogram);
+        }
+        return cryptogram;
+    }
+
+    private static boolean isExistDuplicatedMessage(String cryptogram) {
+        return IntStream.range(1, cryptogram.length()).anyMatch(index -> isDuplicate(cryptogram, index));
+    }
+
+    private static String removeDuplicatedMessages(String cryptogram) {
+        StringBuilder message = new StringBuilder(cryptogram);
+        for (int index = 1; index < message.length(); index++) {
+            index = removeDuplicatedMessage(message, index);
+        }
+        return message.toString();
+    }
+
+    private static int removeDuplicatedMessage(StringBuilder message, int index) {
+        if (isDuplicate(message.toString(), index)) {
+            message.replace(index - 1, index + 1, "");
+            index++;
+        }
+        return index;
+    }
+
+    private static boolean isDuplicate(String message, int index) {
+        return message.charAt(index - 1) == message.charAt(index);
     }
 }
