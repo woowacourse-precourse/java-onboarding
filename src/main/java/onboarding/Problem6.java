4e9508773415658072d7eea36da08e52a1619bd3
Yh0sun <hy770819@naver.com>
2022-11-01 17:03:19 +0900

    problem6:func1 add

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,7 +12,13 @@ public class Problem6 {
         //1. 닉네임에서 연속된 2글자 추출
         //2. 다른 닉네임에 추출한 글자가 포함되면 ans에 이메일 넣음
         //3. 글자 포함된 닉네임이 있었다면 1.닉네임도 ans에 이메일 넣음
+        for(int i=0;i<forms.size();i++){
+            String nickName=forms.get(i).get(1);
+            for(int j=0;j<nickName.length()-1;j++){
+                String nameStr=nickName.substring(j,j+2);
 
+            }
+        }
         List<String> answer = new ArrayList<>(ans);
         return answer;
     }
