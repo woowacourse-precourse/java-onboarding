253f9003ada698047aeb12bb26a956d3acaeb2ef
liarreez <liarreez@gmail.com>
2022-11-01 16:29:02 +0900

    week1

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<Integer>();
+    	List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList<Integer>();
         
 		int [] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 		
