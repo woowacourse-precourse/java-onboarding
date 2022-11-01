38291e8aee47b11d260c03beda09f4cb5158a218
이건회 <rg970604@naver.com>
2022-11-01 21:11:28 +0900

    feat(Problem7.java) : 예외 검증 처리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem7 {
 
@@ -17,5 +18,40 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static void validateFriendId(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            for (String id : friend) {
+                if (!Pattern.matches(ENGLISH_PATTERN, id)) {
+                    throw new IllegalArgumentException("아이디는 영소문자로만 이루어져야 합니다");
+                }
+                if (id.length() < MIN_ID_SIZE || id.length() > MAX_ID_SIZE) {
+                    throw new IllegalArgumentException("아이디 길이는 1이상 30 이하여야 합니다");
+                }
+            }
+        }
+    }
+
+    private static void validateFriendRelationshipListSize(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            if (friend.size() != REQULAR_FRIEND_RELATIONSHIP_SIZE) {
+                throw new IllegalArgumentException("친구목록 리스트의 길이는 2여야 합니다");
+            }
+        }
+    }
+
+    private static void validateFriendsSize(List<List<String>> friends) {
+        if (friends.size() < MIN_FRIENDS_SIZE || friends.size() > MAX_FRIENDS_SIZE) {
+            throw new IllegalArgumentException("친구목록의 길이는 1 이상 10000 이하여야 합니다");
+        }
+    }
+
+    private static void validateUser(String user) {
+        if (user.length() < MIN_USER_SIZE || user.length() > MAX_USER_SIZE) {
+            throw new IllegalArgumentException("유저 이름 길이는 1 이상 30 이하여야 합니다");
+        }
+        if (!Pattern.matches(ENGLISH_PATTERN, user)) {
+            throw new IllegalArgumentException("유저이름은 영소문자로만 이루어져야 합니다");
+        }
     }
 }
