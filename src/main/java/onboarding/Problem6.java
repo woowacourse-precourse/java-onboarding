e7fd5b461cf9dfed4301cc3a1950ec256730e713
hwiyeon0831 <aab555586@gmail.com>
2022-11-01 23:33:05 +0900

    구현 기능 commit

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,106 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		validateForms(forms);
+
+		List<String> answer = new ArrayList<>();
+		List<String> duplicateTwoLetterList = getDuplicateTwoLetterList(getTwoLetterList(forms));
+
+		for(List<String> form : forms) {
+			validateEmail(form.get(0));
+			validateNickname(form.get(1));
+			if(hasDuplicateTwoLetter(form.get(1), duplicateTwoLetterList)) answer.add(form.get(0));
+		}
+
+		return answer.stream()
+				.distinct() // 중복 제거
+				.sorted(Comparator.naturalOrder())
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * list에 대한 유효성 검사
+	 * @param forms
+	 */
+	public static void validateForms(List<List<String>> forms) {
+		if(forms.size() < 1 || forms.size() > 10000) throw new IllegalArgumentException();
+	}
+
+	/**
+	 * email에 대한 유효성 검사
+	 * @param email
+	 */
+	public static void validateEmail(String email) {
+		if(!email.contains("@email.com")) throw new IllegalArgumentException();
+		if(email.length() < 11 || email.length() >= 20) throw new IllegalArgumentException();
+	}
+
+	/**
+	 * 닉네임에 대한 유효성 검사
+	 * @param nickname
+	 * @return
+	 */
+	public static boolean validateNickname(String nickname) {
+		if(!nickname.matches("^[가-힣]*$")) throw new IllegalArgumentException();
+		if(nickname.length() < 1 || nickname.length() >= 20) throw new IllegalArgumentException();
+		return true;
+	}
+
+	/**
+	 * 주어진 문자열을 2글자로 자른 문자열을 주어진 리스트에 담는다.
+	 * @param nickname
+	 * @param twoLetterList 문자열을 담는 리스트
+	 */
+	public static void addTwoLetter(String nickname, List<String> twoLetterList) {
+		String[] nicknameArray = nickname.split("");
+
+		for(int i = 0; i < nicknameArray.length - 1; i++){
+			twoLetterList.add(nicknameArray[i] + nicknameArray[i+1]);
+		}
+	}
+
+	/**
+	 * 모든 크루들의 닉네임을 2글자로 자른 문자열들을 담아 리스트로 반환한다.
+	 * @param forms
+	 * @return
+	 */
+	public static List<String> getTwoLetterList(List<List<String>> forms) {
+		List<String> twoLetterList = new ArrayList<>();
+
+		for (List<String> form : forms) {
+			addTwoLetter(form.get(1), twoLetterList);
+		}
+
+		return twoLetterList;
+	}
+
+	/**
+	 * 크루들의 닉네임을 2글자로 자른 문자열들을 담은 리스트 중 중복이 2번 이상 되는 문자열들을 새로운 리스트에 담아 반환한다.
+	 * @param twoLetterList 2글자 리스트 : 크루들의 닉네임을 2글자로 자른 문자열들을 담은 리스트
+	 * @return
+	 */
+	public static List<String> getDuplicateTwoLetterList(List<String> twoLetterList) {
+		return twoLetterList.stream()
+				.filter(x -> Collections.frequency(twoLetterList, x) > 1)
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * 주어진 nickname이 중복 2글자 리스트의 문자열들을 포함하는 지 판단한다.
+	 * @param nickname
+	 * @param duplicateTwoLetterList 중복 2글자 리스트 : 크루들의 닉네임을 2글자로 자른 후, 중복이 2번 이상 되는 문자열을 담은 리스트
+	 * @return
+	 */
+	public static boolean hasDuplicateTwoLetter(String nickname, List<String> duplicateTwoLetterList) {
+		for (String twoLetter : duplicateTwoLetterList) {
+			if (nickname.contains(twoLetter) == true) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,111 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		validateUser(user);
+		validateFriends(friends);
+		validateVisitors(visitors);
+		
+		// user와 친구인 리스트
+		List<String> usersFriends = getUsersFriends(user, friends);
+
+		// key : 이름 / value : 포인트 로 구성되는 HashMap
+		Map<String, Integer> pointMap = new HashMap<>();
+
+		// 친구 리스트 중 추천에 해당하는 사용자에게 포인트 10점을 부여
+		for(String name : getRecommendedFriends(user, usersFriends, friends)) {
+			addPoint(pointMap, name, 10);
+		}
+
+		// 방문객 리스트 중 추천에 해당하는 사용자에게 1점을 부여
+		for(String visitor : getRecommendedVisitors(visitors, usersFriends)) {
+			addPoint(pointMap, visitor, Collections.frequency(visitors, visitor));
+		}
+
+		return getSortedList(pointMap);
+	}
+
+	public static void validateUser(String user) {
+		if(user.length() > 30 || user.length() < 1) throw new IllegalArgumentException();
+	}
+
+	public static void validateFriends(List<List<String>> friends) {
+		if(friends.size() > 10000 || friends.size() < 1) throw new IllegalArgumentException();
+	}
+
+	public static void validateVisitors(List<String> visitors) {
+		if(visitors.size() > 10000 || visitors.size() < 0) throw new IllegalArgumentException();
+	}
+
+	/**
+	 * 주어진 사용자의 친구 리스트를 반환한다.
+	 * @param user
+	 * @param friends
+	 * @return
+	 */
+	public static List<String> getUsersFriends(String user, List<List<String>> friends) {
+		return friends.stream()
+				.filter(x->x.contains(user))
+				.flatMap(Collection::stream)
+				.filter(x->!x.equals(user))
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * 친구리스트 중 추천 친구에 해당하는 리스트를 반환한다.
+	 * @param user
+	 * @param usersFriends
+	 * @param friends
+	 * @return
+	 */
+	public static List<String> getRecommendedFriends(String user, List<String> usersFriends, List<List<String>> friends) {
+		List<String> recommenedFriends = new ArrayList<>();
+
+		for(String userFriend : usersFriends){
+			List<String> friendOfFriends = getUsersFriends(userFriend, friends);
+			friendOfFriends.remove(user);   // user는 제외
+			recommenedFriends.addAll(friendOfFriends);
+		}
+
+		return recommenedFriends;
+	}
+
+	/**
+	 * 방문리스트 중 추천 친구에 해당하는 리스트를 반환한다.
+	 * @param visitors
+	 * @param userFriends
+	 * @return
+	 */
+	public static List<String> getRecommendedVisitors(List<String> visitors, List<String> userFriends) {
+		return visitors.stream()
+				.distinct()
+				.filter(x-> !userFriends.contains(x)) // 방문리스트는 모두 추천 친구에 해당하지만, user와 먼저 친구일 경우 추천 리스트에서 뺀다.
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * 주어진 포인트만큼 name에 해당하는 사용자에게 포인트를 더한다.
+	 * @param pointMap
+	 * @param name
+	 * @param point
+	 */
+	public static void addPoint(Map<String, Integer> pointMap, String name, int point) {
+		pointMap.put(name, pointMap.getOrDefault(name, 0) + point);
+	}
+
+	/**
+	 * 주어진 포인트 map을 정렬 후, 최대 5명을 담은 list를 반환한다.
+	 * @param pointMap
+	 * @return
+	 */
+	public static List<String> getSortedList(Map<String, Integer> pointMap) {
+		List<String> keySet = new ArrayList<>(pointMap.keySet());
+
+		keySet.sort(Comparator.comparing(pointMap::get).reversed().thenComparing(String::valueOf));
+		
+		return keySet.size() > 5 ? keySet.subList(0, 5) : keySet.subList(0, keySet.size());
+	}
 }
