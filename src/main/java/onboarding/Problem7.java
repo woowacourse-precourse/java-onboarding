e8202c498384aba0f20237daf56c547db4951cf3
yw824 <yongwoo25@gmail.com>
2022-11-01 22:46:04 +0900

    feat: [Problem7] SNS 관리 클래스 생성 및 클래스 멤버변수 구성
    - 기능 1 : SNS 관리 클래스 생성 및 클래스 멤버변수 구성
    - 기능 2 : 지정된 사용자에게 방문한 타 사용자 저장 및 친구 설정
    - 기능 3 : 각 사용자마다 점수 부여
    - 기능 4 : 최고 점수 순으로 추천 아이디 리스트 확보
    - class SNS
    	- targetUser : solution()의 user가 지정된 사용자 아이디
    	- users : 모든 사용자 - 관계 및 방문 내역 순회해서 추가할 예정
    	- len : 모든 사용자의 수 : users 순회 후 설정할 예정
    	- scores : 각 사용자가 가질 수 있는 점수 저장하는 배열
    		- 기능 3에서  구현 예정
    	- visitors : 지정된 사용자의 SNS에 접근한 다른 사용자 리스트
    		- 기능 2에서 수행
    	- 지정된 사용자의 친구 목록
    		- users 순회하면서 친구 목록 생성할 예정
    		- 기능 2에서 수행

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // 기능 1 : SNS 관리 클래스 생성 및 클래스 멤버변수 구성
+        SNS sns = new SNS(user);
+
+        // 기능 2 : 지정된 사용자에게 방문한 타 사용자 저장 및 친구 설정
+
+        // 기능 3 : 각 사용자마다 점수 부여
+
+        // 기능 4 : 최고 점수 가진 리스트 확보d
+
         return answer;
+    }
+}
+
+class SNS
+{
+    private String targetUser; // 지정된 실행 결과
+    private ArrayList<String> users; // 모든 사용자
+    private int len; // 사용자 수
+    private int[] scores; // 각 사용자 별 점수
+    private ArrayList<String> visitors; // 지정된 사용자 SNS에 접근한 다른 사용자
+    private ArrayList<String> friends; // 지정된 사용자의 친구 목록
+    SNS(String name)
+    {
+        //implement
     }
 }
