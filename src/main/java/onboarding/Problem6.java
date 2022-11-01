30b23d952cbd43347af79d204b217ecc494b5d69
UngHeung <t01047377477@gmail.com>
2022-11-01 23:00:05 +0900

    feat : (problem 6) 목표 비교 닉네임 전송과 중복되는 이메일 저장 및 정렬 기능추가
    solution() 메소드 내 목표 닉네임과 비교 닉네임을
    중복을 확인하는 메소드로 보내주고 중복값에 따라 이메일을 넘겨주는 메소드 추가
    리턴하기 전 정렬 코드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
 		List<String> answer = List.of("answer");
+		Set<String> deleteDuplicate = new HashSet<>();
+
+		for(int targetIdx = 0; targetIdx < forms.size(); targetIdx++) {
+
+			for(int compareIdx = targetIdx + 1; compareIdx < forms.size(); compareIdx++) {
+
+				String target = forms.get(targetIdx).get(1);
+				String compare = forms.get(compareIdx).get(1);
+				String targetEmail = forms.get(targetIdx).get(0);
+				String compareEmail = forms.get(compareIdx).get(0);
+
+				if(dupleCheck(target, compare)) {
+					deleteDuplicate.add(targetEmail);
+					deleteDuplicate.add(compareEmail);
+					break;
+				}
+			}
+
+		}
+		answer = new ArrayList<>(deleteDuplicate);
+		Collections.sort(answer);
+
 		return answer;
 	}
 
@@ -19,4 +45,5 @@ public class Problem6 {
 
 		return false;
 	}
+
 }
