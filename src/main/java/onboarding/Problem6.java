18381dd0db010e771037ee93108f567681fb1fe5
pasly0920 <inwoo920621@gmail.com>
2022-11-01 17:45:00 +0900

    feat: 1 이메일 유효성 판단

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import org.assertj.core.util.Lists;
+import org.assertj.core.util.Sets;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
+
+    private static boolean isValidEmail(String email){
+        if(email.length() < 11 || email.length() > 19)
+            return false;
+        String[] emailArr = email.split("@");
+        String emailDomain = emailArr[1];
+        if(!emailDomain.equals("email.com"))
+            return false;
+
+        return true;
+    }
+
 }
