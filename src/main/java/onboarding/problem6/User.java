e3d9c17fee89b15ddc0ef92070fe9d1fd881874e
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 22:18:07 +0900

    feat(problem6): 이름 2자씩 분리

--- src/main/java/onboarding/problem6/User.java
+++ src/main/java/onboarding/problem6/User.java
@@ -13,6 +13,15 @@ public class User {
         this.name = new Name(name);
     }
 
+    public List<String> getWords() {
+        String username = name.getName();
+        List<String> userWords = new ArrayList<>();
+        for (int i = 0; i < username.length() - 1; i++) {
+            userWords.add(username.substring(i, i + 2));
+        }
+        return userWords;
+    }
+
     public String getEmail() {
         return email.getEmail();
     }
