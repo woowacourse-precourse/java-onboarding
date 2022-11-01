7daee75c1d9e4daf34d30dc17b66a5b41cc221c5
Gentlee <ekdma3259@gmail.com>
2022-11-01 19:35:04 +0900

    Problem6 - HashMap을 이용하여 Key 값 설정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+	public static List<String> solution(List<List<String>> forms) {
+    	if(forms.size()<=1 && forms.size()>=10000) return null;
+    	List<String> answer=new ArrayList<>();
+    	List<String> nick=new ArrayList<>();
+    	HashMap<String, String> map=new HashMap<>();
+    	
+    	for(int i=0;i<forms.size();i++) {
+    		map.put(forms.get(i).get(1),forms.get(i).get(0));
+    		nick.add(forms.get(i).get(1));
+    	}
     }
 }
