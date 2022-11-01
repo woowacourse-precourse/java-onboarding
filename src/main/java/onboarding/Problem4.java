dd1c1246235b827ebf0086f99b40df82148f72d1
lynne921 <dkfls921@gmail.com>
2022-11-01 18:54:37 +0900

    [feat][problem4] 값을 역으로 바꾸는 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+    private static char convertLetter(char alpha) {
+        if (alpha >= 'A' && alpha <= 'Z') {
+            return (char) ('A' + 'Z' - alpha);
+        }
+        if (alpha >= 'a' && alpha <= 'z') {
+            return (char) ('a' + 'z' - alpha);
+        }
+        return alpha;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
