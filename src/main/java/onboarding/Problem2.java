37fdfa25690a91338ddb4309387a0f59a676e67c
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2022-11-01 22:20:23 +0900

    Update Problem2.java

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    	
+    	int ln = cryptogram.length();
+
+		while(true){
+			cryptogram = cryptogram.replaceAll("(([a-z])\\2{1,})","");
+			//연속된 같은 문자 (2개이상)을 변환
+			
+			
+			//cryptogram의 길이가 변화되지 않으면 연속하는 알파벳이 없다고 판단
+			if(ln == cryptogram.length())
+				return cryptogram;
+			else 
+				ln = cryptogram.length();
+		}
     }
 }
