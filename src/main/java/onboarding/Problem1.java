2984a5a618040f350f58596acb45056babe0d680
김형기 <khk211113@naver.com>
2022-11-01 23:18:38 +0900

    [FEAT] Problem1 - 자릿수 연산 클래스 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,23 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    // 페이지 숫자를 이용하여 자릿수 연산 결과를 반환
+    static class DigitDecoder {
+        int product = 1;
+        int sum = 0;
+
+        DigitDecoder(int num) {
+            while (num > 1) {
+                int digit = num % 10;
+                product *= digit;
+                sum += digit;
+                num /= 10;
+            }
+        }
+
+        int getMax() {
+            return Math.max(this.product, this.sum);
+        }
+    }
 }
