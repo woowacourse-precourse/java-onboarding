d9f61354938abe278688477669f9400e69e72713
taehyun <didls1643@gmail.com>
2022-11-01 17:04:04 +0900

    초기화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,11 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-	public static List<Integer> solution(int money) {
-		List<String> answer = Collections.emptyList();
-
+    public static List<Integer> solution(int money) {
+        List<Integer> answer = Collections.emptyList();
         return answer;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,9 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.TreeMap;
 
 public class Problem6 {
-	public static List<String> solution(List<List<String>> forms) {
-		List<String> answer = Collections.emptyList();
-        
-        return answer; 
-}
-}
+    public static List<String> solution(List<List<String>> forms) {
+        List<String> answer = List.of("answer");
+        return answer;
+    }
