bc5ad81da073aaf17ba2ce8612ee3290941df33b
Ilchul Kim <huj0159@gmail.com>
2022-11-01 16:27:05 +0900

    refactor: simplified method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,29 +1,19 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>(9);
+        ArrayList<Integer> unitsOfMoney = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500,100,50,10,1));
 
-        answer.add(money / 50000);
-        money %= 50000;
-        answer.add(money / 10000);
-        money %= 10000;
-        answer.add(money / 5000);
-        money %= 5000;
-        answer.add(money / 1000);
-        money %= 1000;
-        answer.add(money / 500);
-        money %= 500;
-        answer.add(money / 100);
-        money %= 100;
-        answer.add(money / 50);
-        money %= 50;
-        answer.add(money / 10);
-        money %= 10;
-        answer.add(money / 1);
+        List<Integer> answer = new ArrayList<>(unitsOfMoney.size());
+
+        for(Integer unit : unitsOfMoney){
+            answer.add(money/unit);
+            money %= unit;
+        }
 
         return answer;
     }
