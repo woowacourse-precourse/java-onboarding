6b5179992365883d983bc8eec646397c95a2d442
yenawee <yaena0319@naver.com>
2022-11-01 16:03:27 +0900

    [FEAT] 4. word length 에러 처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,12 @@ public class Problem4 {
     public static String solution(String word) {
 
     }
+
+    private static boolean checkInput(String word){
+        if (word.length() < 1 || word.length() > 4000)
+            return false;
+        return true;
+    }
     private static boolean is_Alpha(char c){
         if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
             return true;
