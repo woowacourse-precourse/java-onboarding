d3beaf6b812ea6d62092f152ee52e2b8f0fef7ac
0417taehyun <0417taehyun@gmail.com>
2022-11-01 15:50:28 +0900

    feat: [문제4] 메서드 및 풀이 구조 구축

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem4 {
+    public static char reverseCharacter(int asciiValue, int startNumber, int endNumber) {
+
+    }
+
     public static String solution(String word) {
         String answer = "";
+
+        int lowerStart = 'a';
+        int lowerEnd = 'z';
+
+        int upperStart = 'A';
+        int upperEnd = 'Z';
+
+        for (char character: word.toCharArray()) {
+            int asciiValue = character;
+            String reversedCharacter;
+            if (asciiValue >= lowerStart && asciiValue <= lowerEnd) {
+                reversedCharacter = Character.toString(reverseCharacter(asciiValue, lowerStart, lowerEnd));
+            } else if (asciiValue >= upperStart && asciiValue <= upperEnd) {
+                reversedCharacter = Character.toString(reverseCharacter(asciiValue, upperStart, upperEnd));
+            } else {
+                reversedCharacter = Character.toString(character);
+            }
+
+            answer = answer.concat(reversedCharacter);
+        }
+
         return answer;
     }
 }
