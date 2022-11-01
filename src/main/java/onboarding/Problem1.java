e2f9669701230df5ccac3572ff7a091583d261c9
June1010 <tjrwns1010@naver.com>
2022-11-01 23:12:57 +0900

    chore: 기능 목록 커밋 (Mock 함수만 구현)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int LEFT_PAGE = 0;
+    private static final int RIGHT_PAGE = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        return 0;
+    }
+
+    private static boolean validateInput(List<Integer> pages) {
+        return false;
+    }
+
+    private static int getScore(List<Integer> pages) {
+        return 0;
+    }
+
+    private static int addEachDigit(int pageNum) {
+        return 0;
+    }
+
+    private static int multiplyEachDigit(int pageNum) {
+        return 0;
+    }
+
+    private static int getBiggerNumber(int numX, int numY) {
+        return 0;
     }
 }
