94fdbe446efd46a2d8a2ed5ecfee5bd07ce36948
Eungyu-code <Agyu625@gmail.com>
2022-11-01 13:11:15 +0900

    user와 이미 친구인 리스트를 생성하는 createUserFriend() 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        // user의 친구 리스트를 생성하는 함수 createUserFriend()
+        // 점수를 부여받은(0점이 아닌) 친구의 이름과 그의 점수를 갱신해주는 함수 putRecommendList()
+
+        List<String> userFriends = createUserFriend(user, friends);
+        Map<String, Integer> recommendList = new HashMap<>();
+
+        List<String> answer = new ArrayList<>();
         return answer;
     }
+
+    public static List<String> createUserFriend(String user, List<List<String>> friends) {
+
+        List<String> userFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            String friendA = friends.get(i).get(0);
+            String friendB = friends.get(i).get(1);
+            if (friendA.equals(user)) userFriends.add(friendB);
+            if (friendB.equals(user)) userFriends.add(friendA);
+        }
+
+        return userFriends;
+
+    }
+
 }
