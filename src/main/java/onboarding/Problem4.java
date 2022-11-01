0f3ee4ae68c4d9885e230e752260ad6a66951d03
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-01 14:51:16 +0900

    백엔드 4번 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,10 +3,48 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String difference = "";
 
-//        1. 알파벳이 순서대로 들어있는 배열 rightAlpha와 역순으로 들어있는 reverseAlpha를 생성
-//        2. 기본적으로 소문자를 취급하며 대문자의 경우에 한해 대문자로 취급
+        char[] rightAlpha = new char[26];
+        char[] rightCAlpha = new char[26];
+        char[] wordList = word.toCharArray();
+
+//        알파벳순
+        for(int i=0;i<rightAlpha.length;i++)
+        {
+            rightAlpha[i] = (char)((int)'a' + i);
+        }
+
+//        대문자 알파벳순
+        for(int i=0;i<rightAlpha.length;i++)
+        {
+            rightCAlpha[i] = (char)(65 + i);
+        }
+
+        for(int i = 0 ; i < wordList.length; i++){
+            difference = answer;
+            for(int j = 0; j < rightCAlpha.length; j++){
+                if(wordList[i] == rightAlpha[j]){
+                    wordList[i] = (char)(219 - (int)wordList[i]);
+                    answer += wordList[i];
+                    break;
+                }
+                else if(wordList[i] == rightCAlpha[j]){
+                    wordList[i] = (char)(155 - (int)wordList[i]);
+                    answer += wordList[i];
+                    break;
+                }
+            }
+            if (difference.equals(answer) == true){
+                answer += wordList[i];
+            }
+        }
 //        +알파벳을 수치화시켜서 챌린지해볼만 함.
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String word = "I love you";
+        System.out.println(solution(word));
     }
 }
