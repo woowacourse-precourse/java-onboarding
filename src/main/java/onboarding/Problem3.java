9586c269777f3ac5b0209c88a96223bd40bbed59
seungjun <seungjun123>
2022-11-01 13:34:41 +0900

    주어진 숫자에 손뼉을 몇 번 치는지 세는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,13 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int check(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            if (n%10 == 3 || n%10 == 6 || n%10 == 9) cnt += 1;
+            n /= 10;
+        }
+        return cnt;
+    }
 }
