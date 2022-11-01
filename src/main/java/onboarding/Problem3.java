9ccc4de6b1cce33abee594c0fb3cc976cd917fd2
Park Sungchul <try615@naver.com>
2022-11-01 21:18:11 +0900

    [feat] Problem3.java : 자리수 별 숫자 계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,8 +8,8 @@ public class Problem3 {
         for (int i = 1; i <= number; i++) {
             int pow = 0;
             while (i >= Math.pow(10, pow)) {
-                int n = i % (int)Math.pow(10, pow + 1) / (int)Math.pow(10, pow);
-                if (n == 3 || n == 6 || n == 9) {
+                int n = digitNumber(i, pow);
+                if (needCrap(n)) {
                     result += 1;
                 }
                 pow += 1;
@@ -18,6 +18,11 @@ public class Problem3 {
 
         return result;
     }
+
+    private static int digitNumber(int number, int pow) {
+        return number % (int)Math.pow(10, pow + 1) / (int)Math.pow(10, pow);
+    }
+
 
     private static boolean needCrap(int number) {
         for (int check : checkNumber) {
