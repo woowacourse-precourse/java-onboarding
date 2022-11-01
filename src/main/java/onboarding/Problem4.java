e579c45b8441ccd63c305a149d2dd8267ebd8b44
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 19:11:35 +0900

    문제4 / feat : 유효성 검증 코드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,11 @@
 package onboarding;
 
 import onboarding.problem4.Convertor;
+import onboarding.problem4.Validator;
 
 public class Problem4 {
     public static String solution(String word) {
+        if(!Validator.validate(word)){return "Error";}
         return Convertor.convertString(word);
     }
 }
