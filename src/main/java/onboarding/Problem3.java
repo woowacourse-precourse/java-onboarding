d2851ce2c9a1492c2648bf2edd65f6d2d57967d2
YoosunMoon <muzikue@naver.com>
2022-11-01 14:51:57 +0900

    style: 변수명 명확하게 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,24 +8,24 @@ public class Problem3 {
         String str = Integer.toString(number);
         numberSize = str.length();
 
-        for(int i = 0; i < numberSize; i++) {
-            int digits = (int)Math.pow(10, i);
-            int currDigitsNum;
+        for (int i = 0; i < numberSize; i++) {
+            int digit = (int) Math.pow(10, i);
+            int currDigitNum;
 
-            if( i == 0) {
-                currDigitsNum = number % (digits * 10);
-                answer += currDigitsNum / 3;
+            if (i == 0) {
+                currDigitNum = number % (digit * 10);
+                answer += currDigitNum / 3;
                 continue;
             }
 
-            currDigitsNum = (number / digits) % 10;
+            currDigitNum = (number / digit) % 10;
 
-            if(currDigitsNum != 0 && currDigitsNum % 3 == 0) {
-                answer += (number % digits) + 1;
+            if (currDigitNum != 0 && currDigitNum % 3 == 0) {
+                answer += (number % digit) + 1;
             }
 
-            answer += ((currDigitsNum - 1) / 3) * digits;
-            answer += (number / digits) * 3 * (digits / 10);
+            answer += ((currDigitNum - 1) / 3) * digit;
+            answer += (number / digit) * 3 * (digit / 10);
         }
         return answer;
     }
