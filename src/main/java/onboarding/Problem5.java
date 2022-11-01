5b5acb22e160d11dcd4522cdd4df26b96de9c334
Basak <chgy2131@naver.com>
2022-11-01 12:02:42 +0900

    [feat] 5번 - 화폐 단위(상수)를 저장하는 리스트 변수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> unit = List.of(50000,10000,5000,1000,500,100,50,10,1);  // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원
+
         return answer;
     }
 }
