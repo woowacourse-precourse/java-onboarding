f94e7e3b9bbfbc12cb456f4535ae171ccca866ef
kwx4957 <kdy4957@naver.com>
2022-11-01 15:51:52 +0900

    4번 문제 기능 구현 2번 및 문제

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.HashMap;
+
 /*
 기능 목록
 1. 알파벳외 문자 확인
@@ -10,8 +12,37 @@ public class Problem4 {
     public static boolean isAlpah(char c){
         return Character.isAlphabetic(c);
     }
+    public static HashMap<Character,Character> initWord(){
+        HashMap<Character,Character> alpah = new HashMap<Character,Character>();
+        int i = 97;
+        for(int c = 122; c>=97;c--){
+            alpah.put((char)c,(char)(i));
+            i++;
+        }
+        return alpah;
+    }
+
     public static String solution(String word) {
         String answer = "";
+        StringBuilder tmp = new StringBuilder();
+
+        HashMap<Character,Character> alpah = initWord();
+
+        for(char c:word.toCharArray()){
+            if(isAlpah(c)) {
+                if (Character.isUpperCase(c)) {
+                    c = Character.toLowerCase(c);
+                    c = alpah.get(c);
+                    c = Character.toUpperCase(c);
+                } else {
+                    c = alpah.get(c);
+                }
+            }
+
+            tmp.append(c);
+        }
+
+        answer = tmp.toString();
         return answer;
     }
 }
