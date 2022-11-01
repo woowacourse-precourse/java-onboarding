75c65058ffe2955ff4c1109645cb558a55f94a4e
masiljangajji <hjk172262@gmail.com>
2022-11-01 20:12:38 +0900

    feat: Problem6_최종 solution

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -34,31 +34,50 @@ public class Problem6 {
         }
     }
 
-        public static List<String> solution(List<List<String>> forms) {
 
-            String [] str= new String[10001];
-            String [] str2= new String[10001];
+    public static List<String> solution(List<List<String>> forms) {
 
-            //List<String> answer = List.of("answer");
-            List<String> answer= new ArrayList<String>();
+        String [] str= new String[10001];
+        String [] str2= new String[10001];
 
-            int check=0;
-            int check2=0;
-            for(int i=0;i<forms.size();i++)
-            {
-                for(int j=0;j<forms.get(i).size();j++) {
-                    if (j ==1) {
-                        str[check] = forms.get(i).get(j);
-                        check++;
-                    }
-                    else if(j==0)
-                    {
-                        str2[check2]=forms.get(i).get(j);
-                        check2++;
-                    }
+        //List<String> answer = List.of("answer");
+        List<String> answer= new ArrayList<String>();
+
+        int check=0;
+        int check2=0;
+        for(int i=0;i<forms.size();i++)
+        {
+            for(int j=0;j<forms.get(i).size();j++) {
+                if (j ==1) {
+                    str[check] = forms.get(i).get(j);
+                    check++;
+                }
+                else if(j==0)
+                {
+                    str2[check2]=forms.get(i).get(j);
+                    check2++;
                 }
             }
+        }
 
-            return answer;
+
+        for(int i=0;i<check;i++)
+        { // str에 모든 한글 이름
+            go(str,str[i],check,i);
+        }
+
+        for(int i=0;i<check;i++)
+        {
+            if(arr[i]==1)
+            {
+                String ppp="";
+                ppp+=str2[i];
+                answer.add(ppp);
+            }
+        }
+
+        Collections.sort(answer);
+
+        return answer;
     }
 }
