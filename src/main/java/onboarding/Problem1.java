7961880d7c00d40bcfc7bd5095fae6a10163b62f
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2022-11-01 15:42:59 +0900

    [Feat] 인접 중복 문자제거 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
+        //유효성 검사
         boolean validValuePobi = isValid(pobi);
         boolean validValueCrong = isValid(crong);
 
@@ -12,7 +15,15 @@ class Problem1 {
             return -1;
         }
 
-        return 0;
+        List<Integer> pobiCalculationResult = Arrays.asList(sum(pobi), mul(pobi));
+        List<Integer> crongCalculationResult = Arrays.asList(sum(crong), mul(crong));
+
+        int pobiFinalValue = Collections.max(pobiCalculationResult);
+        int crongFinalValue = Collections.max(crongCalculationResult);
+
+        int answer =  winnerResult(pobiFinalValue, crongFinalValue);
+
+        return answer;
     }
 
     /*
@@ -30,9 +41,60 @@ class Problem1 {
 
     }
 
-/*
-    int answer = Integer.MAX_VALUE;
-    return answer;
- */
+    //Integer형 List가 인자로 들어왔을 때, 합을 계산하여 큰 값을 리턴
+    static private int sum(List<Integer> sumArr) {
+
+        int firSumValue = 0;
+        int secSumValue = 0;
+
+        String[] temArray = String.valueOf(sumArr.get(0)).split("");
+        for(String splitNum : temArray) {
+            firSumValue += Integer.valueOf(splitNum);
+        }
+
+        temArray = String.valueOf(sumArr.get(1)).split("");
+        for(String splitNum : temArray) {
+            secSumValue += Integer.valueOf(splitNum);
+        }
+
+        return Math.max(firSumValue, secSumValue);
+
+    }
+
+    //Integer형 List가 인자로 들어왔을 때, 곱을 계산하여 큰 값을 리턴
+    static private int mul(List<Integer> mulArr) {
+
+        int firMulValue = 1;
+        int secMulValue = 1;
+
+        String[] temArray = String.valueOf(mulArr.get(0)).split("");
+        for(String splitNum : temArray) {
+            firMulValue *= Integer.valueOf(splitNum);
+        }
+
+        temArray = String.valueOf(mulArr.get(1)).split("");
+        for(String splitNum : temArray) {
+            secMulValue *= Integer.valueOf(splitNum);
+        }
+
+        return Math.max(firMulValue, secMulValue);
+
+    }
+
+    //해당 solution의 최종 결과를 계산하여 winner에 해당하는 리턴값 제시
+    static private int winnerResult(int participant1, int participant2) {
+        if(participant1 > participant2) {
+            return 1;
+        }
+        else if(participant1 < participant2) {
+            return 2;
+        }
+        else if(participant1 == participant2) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
 
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,45 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static String removeDuplicateCharacters(String testingString) {
+
+        if(testingString.length() == 0) {
+            return testingString;
+        }
+
+        char[] testingArray = testingString.toCharArray();
+        int index = 0;
+        int writingIndex = 0;
+
+        for(index=1; index<testingArray.length; index++)
+        {
+            //인접한 문자가 같은 시, 문자 제거
+            if(testingArray[index] == testingArray[index-1]) {
+                while (index<testingArray.length && testingArray[index-1] == testingArray[index]){
+                    index++;
+                }
+            }
+            //인접한 문자가 같지 않을 경우, testingArray 배열에 writingIndex 를 이용하여 문자 작성
+            else {
+                testingArray[writingIndex++] = testingArray[index-1];
+            }
+        }
+
+        String newString = new String(testingArray).substring(0,writingIndex);
+
+        //중복 제거 완료한 새로운 문자열을 인자로 전달하여 동일한 중복 제거 함수 시행
+        if(writingIndex != testingArray.length) {
+            return removeDuplicateCharacters(newString);
+        }
+
+        //문자열의 길이가 변하지 않았다는 것은 인접한 중복 문자를 모두 제거 완료했다는 의미
+        return newString;
+
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeDuplicateCharacters(cryptogram);
         return answer;
     }
 }
