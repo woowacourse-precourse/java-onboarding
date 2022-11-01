d95c17872af4e35d5a8af9a8ddf8f2194809623a
seheonCho <whtpgjs94@gmail.com>
2022-11-01 18:37:38 +0900

    feat: [문제2] 암호문 에서 정규식에 일치하는것을 모두 해독한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
+
+    private static final String REGEX_LOWER_ALPHABET = "([a-z])\\1";
+    private static final String SPACE = "";
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String decrypting = SPACE;
+        String beforeTryedDecrypting = SPACE;
+
+        decrypting = cryptogram.replaceAll(REGEX_LOWER_ALPHABET, SPACE);
+        return decrypting;
     }
 }
