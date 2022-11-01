747151afa37c8673f6b201718a74b53a6defbc22
2do1 <dowon3036@naver.com>
2022-11-01 22:34:47 +0900

    feat: 문제 7번 사용자와 친구인 아이디를 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        // 사용자와 친구인 아이디를 구하는 기능
+        List<String> userFriends = new ArrayList<>(); // 사용자의 친구들
+        for (int i = 0; i < friends.size(); i++) {
+            // 사용자에 관한 친구 정보일 경우
+            if (friends.get(i).contains(user)) {
+                String user1 = friends.get(i).get(0);
+                String user2 = friends.get(i).get(1);
+
+                if (user1.equals(user)) {
+                    userFriends.add(user2);
+                } else if (user2.equals(user)) {
+                    userFriends.add(user1);
+                }
+            }
+        }
+
         return answer;
     }
 }
