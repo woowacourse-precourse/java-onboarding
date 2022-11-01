a4662c05bfa6cb8ba920f4f70185dd05592fc67e
stk346 <stk346@gamil.com>
2022-11-01 12:35:42 +0900

    refactor: 멤버변수가 너무 많고 생성자가 불필요하다고 판단했음. baseWords 멤버변수로 놓고 이를 순회하며 조금 더 간결하게 dict가 만들어질 수 있도록 기능 수정.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,50 +2,23 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class Problem4 {
+
     public static HashMap<String, String> map = new HashMap<>();
     public static ArrayList<String> baseWords = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
-    public static ArrayList<String> keyList = new ArrayList<>();
-    public static ArrayList<String> valueList = new ArrayList<>();
-
-    Problem4() {
-        addWordsToValueList();
-        addWordsToKeyList();
-        generateDict();
-    }
-
-    private static void addWordsToValueList() {
-        for (String s: baseWords) {
-            valueList.add(s.toUpperCase());
-        }
-        for (String s: baseWords) {
-            valueList.add(s);
-        }
-        Collections.reverse(valueList);
-    }
-
-    private static void addWordsToKeyList() {
-        for (String s : baseWords) {
-            keyList.add(s);
-        }
-        for (String s : baseWords) {
-            keyList.add(s.toUpperCase());
-        }
-    }
-
-
 
     private static void generateDict() {
-        for (int i=0; i<keyList.size(); i++) {
-            map.put(keyList.get(i), valueList.get(i));
+        for(int i=0; i< baseWords.size(); i++) {
+            map.put(baseWords.get(i), baseWords.get(25-i));
+            map.put(baseWords.get(i).toUpperCase(), baseWords.get(25-i).toUpperCase());
         }
     }
 
     public static String solution(String word) {
-        Problem4 problem4 = new Problem4();
+        generateDict();
+
         String convertedString = "";
         for (int i=0; i< word.length(); i++) {
             String targetString = String.valueOf(word.charAt(i));
