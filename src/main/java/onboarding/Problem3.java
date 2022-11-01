4ea72e470f43dddc32e92769257f9d661eda3605
June1010 <tjrwns1010@naver.com>
2022-11-01 23:22:46 +0900

    feat: 3, 6, 9가 포함된 숫자인지를 판단하고 카운트를 증가시키는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,16 +3,30 @@ package onboarding;
 import java.util.List;
 
 public class Problem3 {
+    private static final List<Integer> targetNum  = List.of(3, 6, 9);
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += getTargetNumCount(i);
+        }
         return answer;
     }
 
     private static int increaseCount(int count, int index) {
-        return 0;
+        if (index >= 0) {
+            count++;
+        }
+        return count;
     }
 
     private static int getTargetNumCount(int number) {
-        return 0;
+        int count = 0;
+        int index = -1;
+        while (number != 0) {
+            index = targetNum.indexOf(number % 10);
+            count = increaseCount(count, index);
+            number /= 10;
+        }
+        return count;
     }
 }
