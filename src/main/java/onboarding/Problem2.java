a16cbf6389ec8a585975c7a893fd402c4916b07b
anneu <b.imnuj@gmail.com>
2022-11-01 18:26:27 +0900

    feat(problem2): 연속 중복이 끝나는 지점을 구하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,20 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    /**
+     * 연속 중복이 끝나는 인덱스를 반환합니다.
+     */
+    private static int findDuplicateEndIndex(String str, int start) {
+        int end = str.length() - 1;
+
+        for (int i = start; i < str.length(); i++) {
+            if (str.charAt(start) != str.charAt(i)) {
+                end = i - 1;
+                break;
+            }
+        }
+
+        return end;
+    }
 }
