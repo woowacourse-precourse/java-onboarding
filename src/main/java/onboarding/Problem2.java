b94d9152456d576c10a28caf3e7f665cd1e8850e
taehyun <didls1643@gmail.com>
2022-11-01 15:39:21 +0900

    2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
+	public static String solution(String cryptogram) {
         String answer = "answer";
+        String lastStr = "";
+		char temp = ' ';
+		while(!lastStr.equals(cryptogram)) {
+			char[] arr = cryptogram.toCharArray();
+			lastStr = cryptogram;
+			for(int i = 0; i < arr.length - 1; i++) {
+				if(arr[i] == arr[i+1]) {
+					temp = arr[i];
+					arr[i] = ' ';
+					arr[i+1] = ' ';
+				}
+				else if(arr[i] == ' ' && temp == arr[i+1]) {
+					arr[i+1] = ' ';
+				}
+				else {}
+			}
+			cryptogram = String.valueOf(arr);
+			cryptogram = cryptogram.replaceAll(" ", "");
+		}
+		
+		
+		answer = cryptogram;
+        
+        
         return answer;
     }
 }
