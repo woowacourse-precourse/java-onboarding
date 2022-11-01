31d0fb7681b4eb2926620bfcac644b15d6e9f17d
ho2n <jungho2n@gmail.com>
2022-11-01 17:11:17 +0900

    중복 문자 제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -24,4 +24,27 @@ public class Problem2 {
         }
         return isReturn;
     }
+
+    public static String removeRepetition(String cryptogram) {
+        String result = "";
+        int start = 0;
+        int end = cryptogram.length();
+        for (int i = 1; i < cryptogram.length(); ++i) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i - 1)) {
+                start = i - 1;
+                break;
+            }
+            for (int j = i + 1; j < cryptogram.length(); ++j) {
+                if (cryptogram.charAt(j - 1) != cryptogram.charAt(j)) {
+                    end = j;
+                    break;
+                }
+            }
+        }
+        result += cryptogram.substring(0, start);
+        if (end < cryptogram.length()) {
+            result += cryptogram.substring(end);
+        }
+        return result;
+    }
 }
