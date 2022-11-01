51e193b8f764b17f1a9a2de8230f6782920f29ad
bnbnac <bnbnac@naver.com>
2022-11-01 18:51:15 +0900

    feat: (P2)이전 문자와 비교하며 스택에 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,20 +4,55 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        int len = 0;
+        int prevSize = 0;
+        int reversed = -1;
         Stack<Character> stack = makeStack(cryptogram);
 
         return answer;
     }
 
     private static Stack<Character> makeStack(String cryptogram) {
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         char[] ch = cryptogram.toCharArray();
-        for (char c : ch) {
-            stack.push(c);
+        for (char cur : ch) {
+            stack.push(cur);
         }
         return stack;
     }
 
+    private static Stack<Character> removeDuplication(Stack<Character> stack) {
+        int i = 0;
+        int isPrevSame = 0;
+        Stack<Character> removed = new Stack<>();
+
+        while (!stack.empty()) {
+            char cur = stack.pop();
+            if (removed.empty()) {
+                removed.push(cur);
+                continue;
+            }
+            if (removed.peek() == cur) {
+                removed.pop();
+                isPrevSame = 1;
+                removed.push(cur);
+                continue;
+            }
+            if (isPrevSame == 1) {
+                removed.pop();
+            }
+            isPrevSame = 0;
+            removed.push(cur);
+        }
+        if (isPrevSame == 1) {
+            removed.pop();
+        }
+        return removed;
+    }
+
+    private static void checkPrev(Stack<Character> stack, Stack<Character> removed) {
+        char cur = stack.pop();
+        char prev = removed.peek();
+
+    }
 }
 
