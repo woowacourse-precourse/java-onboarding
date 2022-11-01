cc715c494974874aa384e1a8c2bc273844b51114
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-01 20:23:52 +0900

    [Problem6] 2) 연속된 중복문자 차출하는 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -17,4 +17,20 @@ public class Problem6 {
                 .collect(Collectors.toList());
     }
 
+    // 차출 나올 수 있는 연속문자 검색
+    public static String esNgram(String nickName, int n){
+        StringTokenizer stringTokenizer = new StringTokenizer(nickName.trim()," ");
+        List<String> strArr = new ArrayList<>();
+        while(stringTokenizer.hasMoreTokens()){
+            String token = stringTokenizer.nextToken();
+            for(int i=0; i<token.length(); i++){
+                if(i+n > token.length()){
+                    break;
+                }
+                strArr.add(token.substring(i,i+n));
+            }
+        }
+        return String.join(" ", strArr);
+    }
+
 }
