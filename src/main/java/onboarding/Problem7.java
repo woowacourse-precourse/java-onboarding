d6b2a7b779c4fcdeacadaf5f3ac0ddd83612cd1a
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 22:03:31 +0900

    feat(PROBLEM7): addHashMap() HashMap의 value(list)에 요소 추가함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+
+
+    public static void addHashMap(HashMap<String, List<String>> hashMap, String key, String value) {
+        if(hashMap.containsKey(key))
+            hashMap.get(key).add(value);
+        else
+            hashMap.put(key,new ArrayList<>(List.of(value)));
     }
 }
