7b581bd3090f39684a5fc76def6158e121e5fe16
youswim <youswim96@gmail.com>
2022-11-01 21:55:14 +0900

    Feat: 369규칙에 따라 tot를 증가시키는 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,11 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int plusTot(int tot, int tmp) {
+        if (tmp == 3 || tmp == 6 || tmp == 9) {
+            tot++;
+        }
+        return tot;
+    }
 }
