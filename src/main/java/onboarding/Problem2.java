e0b7395925967b93a7ec2f49cd06d9e98cc9e2a4
canbeyour <wuzucy@gmail.com>
2022-11-01 18:01:09 +0900

    refactor: merge functions get index into function remove characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,31 +11,16 @@ public class Problem2 {
         }
         return true;
     }
-    public static int getRepeatEndIndex(StringBuilder answer, int index) {
-        while (index > 0) {
-            if (answer.charAt(index) == answer.charAt(index - 1))
-                return index;
-            index --;
-        }
-        return -1;
-    }
-    public static int getRepeatStartIndex(StringBuilder answer, int repeatIndex) {
-        char repeatChar = answer.charAt(repeatIndex);
-        for (int index = repeatIndex - 1; index >= -1; index--) {
-            if (index == -1 || answer.charAt(index) != repeatChar)
-                return index + 1;
-        }
-        return repeatIndex;
-    }
     public static boolean removeRepeatChar(StringBuilder answer) {
         boolean repeat = false;
         for (int index = answer.length() - 1; index > 0; index--) {
-            int repeatEndIndex = getRepeatEndIndex(answer, index);
-            if (repeatEndIndex == -1)
+            if (answer.charAt(index) != answer.charAt(index - 1))
                 continue;
-            int repeatStartIndex = getRepeatStartIndex(answer, repeatEndIndex - 1);
-            answer.delete(repeatStartIndex, repeatEndIndex + 1);
-            index = repeatStartIndex;
+            char repeatChar = answer.charAt(index);
+            while (index >= 0 && answer.charAt(index) == repeatChar) {
+                answer.deleteCharAt(index);
+                index--;
+            }
             repeat = true;
         }
         return repeat;
