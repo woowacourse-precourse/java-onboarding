bf01813909d27170701c766fa158e3150f3b8f24
ouohoon <begreat@kakao.com>
2022-11-01 23:23:12 +0900

    수정: 문제2 오류 발생하는 케이스 제거
    acccabba일 경우이 나오도록 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,32 +11,34 @@ public class Problem2 {
     1. 중복 문자 제거 기능
      */
     public static String solution(String cryptogram) {
-        return decrypt(cryptogram);
+        String result = decrypt(cryptogram);
+        String prevResult = cryptogram;
+        while (!result.equals(prevResult) && !result.equals("")) {
+            prevResult = result;
+            result = decrypt(result);
+        }
+        return result;
     }
 
     // deque의 last에서 중복 문자를 제거해서 암호문을 해독한다.
     private static String decrypt(String cryptogram) {
         Deque<Character> deque = new ArrayDeque<>();
         deque.addLast(cryptogram.charAt(0));
-        int sequenceCount = 0;
+        boolean isSequence = false;
         for (int i = 1; i < cryptogram.length(); i++) {
             Character last = deque.peekLast();
             Character current = cryptogram.charAt(i);
             if (current.equals(last)) {
-                sequenceCount++;
+                isSequence = true;
                 continue;
             }
-            if (sequenceCount > 0) {
+            if (isSequence) {
                 deque.removeLast();
-                sequenceCount = 0;
-            }
-            if (current.equals(deque.peekLast())) {
-                sequenceCount++;
-                continue;
+                isSequence = false;
             }
             deque.addLast(current);
         }
-        if (sequenceCount > 0) {
+        if (isSequence) {
             deque.removeLast();
         }
         return deque.stream().map(String::valueOf)
