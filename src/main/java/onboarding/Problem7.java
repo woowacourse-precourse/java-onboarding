197f040ad55087f29df63504b49bb88c2bc09670
gomdol <oioifor@naver.com>
2022-11-01 23:15:23 +0900

    feat(Problem7): 친구 목록 저징 및 점수 계산 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,47 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> friendArr = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                String str = friend.get(0);
+                if (!str.equals(user)) {
+                    friendArr.add(str);
+                } else {
+                    friendArr.add(friend.get(1));
+                }
+            }
+        }
+
+        for (List<String> friend : friends) {
+            for (String userFriend : friendArr) {
+                if (!friend.contains(userFriend) || friend.contains(user)) continue;
+                String name = Objects.equals(friend.get(0), userFriend) ? friend.get(1) : friend.get(0);
+                map.put(name, map.containsKey(name) ? map.get(name) + 10 : 10);
+            }
+        }
+
+        for (String visitor : visitors) {
+            if (!friendArr.contains(visitor))
+                map.put(visitor, map.getOrDefault(visitor, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> entList = new LinkedList<>(map.entrySet());
+        entList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+
+        for (Map.Entry<String,Integer> entry : entList){
+            answer.add(entry.getKey());
+        }
         return answer;
     }
 }
