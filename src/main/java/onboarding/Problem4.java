aa8b52120ee366537c86d1159fd0e17e6720f443
yeeun <dpdms9621@naver.com>
2022-11-01 16:49:43 +0900

    [Problem4] 글자수 길이만큼 반복해서 한글자씩 아스키코드 int형으로 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,13 @@ public class Problem4 {
         //char배열에 한글자씩 넣는 기능
         char[] arr = word.toCharArray();
 
+        //글자수 길이만큼 반복해서 한글자씩 비교하는 기능
+        for(int i = 0 ; i < arr.length ; i++) {
+            //글자를 아스키코드 int형으로 변환
+            int num = (int)arr[i];
+
+        }
+
         String answer = sb.toString();
         return answer;
     }
