9b79e92bdfe24ebc54d1ea46aa090bdcf6dbbb9c
si hwan <pum005@naver.com>
2022-11-01 22:31:15 +0900

    feat (problem5) : 입력 숫자 예외 처리 하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import ExceptionValidation.P5ExceptionValidation;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        if(!(P5ExceptionValidation.p5ExceptionValidation(money))){
+            return Collections.emptyList();
+        }
+
     }
+
 }
