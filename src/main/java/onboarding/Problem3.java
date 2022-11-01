afc5aae97c9982555efc12f93a62646b5dd2af7f
herbssssst <zxx52539@kyonggi.ac.kr>
2022-11-01 17:53:28 +0900

    Changes

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,20 +1,15 @@
 package onboarding;
 
-import java.util.ArrayList;
-
 public class Problem3 {
     public static int solution(int number) {
         int count = 0;
-        ArrayList<Integer> numberList = new ArrayList<Integer>();
 
-        for(int i=1; i<=number; i++)
-        {
-            numberList.add(i);
-        }
+        if(number <=2)
+            return 0;
 
-        for(int i=0; i<number; i++)
+        for(int i=3; i<=number; i++)
         {
-            String numStr = numberList.get(i)+"";
+            String numStr = i+"";
             for(int j=0; j<numStr.length(); j++)
             {
                 if(numStr.charAt(j)=='3' || numStr.charAt(j)=='6' || numStr.charAt(j)=='9')
