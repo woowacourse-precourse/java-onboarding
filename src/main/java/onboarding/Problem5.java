5962eae1dca20e6154083c7c0c52be9fef7f66be
sa46lll <9812smh@naver.com>
2022-11-01 11:19:51 +0900

    feat: 화폐 큰 단위부터 나누어 몫 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new LinkedList<>();
+        int[] moneyUnit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int currentUnit : moneyUnit) {
+            answer.add(divideAB(money, currentUnit));
+            money %= currentUnit;
+        }
+
         return answer;
     }
+
+    private static int divideAB(int a, int b) {
+        return a / b;
+    }
+
 }
