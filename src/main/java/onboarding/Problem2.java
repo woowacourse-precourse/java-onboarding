038f97af938e4e6a9ef86fa6b95481452760c6d0
rt3310 <btac3310@gmail.com>
2022-11-01 23:12:46 +0900

    refactor(Problem2): Decryptor 객체 problem2 패키지로 이동

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import onboarding.problem2.Decryptor;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         Decryptor decryptor = new Decryptor(cryptogram);
--- src/main/java/onboarding/problem2/Decryptor.java
+++ src/main/java/onboarding/problem2/Decryptor.java
@@ -1,4 +1,4 @@
-package onboarding;
+package onboarding.problem2;
 
 public class Decryptor {
     private String cryptogram;
