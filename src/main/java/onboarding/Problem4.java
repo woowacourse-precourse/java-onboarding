533fc217b6077540fd86c1aeba2947624ba6c214
wodnjs7800 <wodnjs7800@naver.com>
2022-11-01 15:46:20 +0900

    청개구리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,6 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        
         for(int i=0;i<word.length();i++){
             int chr=(int)word.charAt(i);
             if(chr>=65 && chr<=90){
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,7 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         int[] arr={50000,10000,5000,1000,500,100,50,10,1};
         List<Integer> answer = new ArrayList<>();
+
         for(int i=0;i<9;i++){
             answer.add(money/arr[i]);
             money%=arr[i];
