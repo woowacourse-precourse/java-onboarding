a62d46155e8d77d351ab109033c2790b03b7e07d
mihea1206 <mihea12@gmail.com>
2022-11-01 09:44:11 +0900

    problem7(create AdjList function)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -23,8 +23,21 @@ public class Problem7 {
         keys.forEach(key -> updateMapValue(key, addScore, map));
     }
 
+    private static HashMap<String, List<String>> createAdjListGraph(List<List<String>> list) {
+        HashMap<String, List<String>> graph = new HashMap<>(); //인접리스트 그래프 생성
+
+        for (List<String> item : list) {
+            appendMapValue(item.get(0), item.get(1), graph);
+            appendMapValue(item.get(1), item.get(0), graph);
+        }
+        return graph;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends,
         List<String> visitors) {
+
+        HashMap<String, List<String>> friendsGraph = createAdjListGraph(friends);
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
