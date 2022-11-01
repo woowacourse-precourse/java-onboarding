04946aa726638c8a0730d5f5c6dcbcbbba2759b8
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:27:24 +0900

    1번 문제 풀이

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiMax = findMax(pobi); // 최대값을 찾는 함수를 만들어 포비, 크롱의 좌, 우, 덧셈, 곱셈 중 큰 값을 찾자
+        int crongMax = findMax(crong);
+
+        if (pobiMax == crongMax)
+            return (0);
+        return (pobiMax > crongMax ? 1 : 2);
     }
 }
