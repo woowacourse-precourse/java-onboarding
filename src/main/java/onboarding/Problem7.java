79096b101d70e2696ea28a004c0419748ef98cb9
ingpyo <juninpyo72@gmail.com>
2022-11-01 12:46:23 +0900

    [problem7]전체 코드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,7 +10,52 @@ import java.util.Map;
 public class Problem7 {
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		checkList(user, friends, visitors);
-
+		List<String> userfriends = new ArrayList<>();
+		Map<String, Integer> map = new HashMap<>();
+		List<String> answer = new ArrayList<>();
+		for (List<String> list : friends) {
+			String friend1 = list.get(0), friend2 = list.get(1);
+			if (friend1 == user) {
+				userfriends.add(friend2);
+			}
+			if (friend2 == user) {
+				userfriends.add(friend1);
+			}
+		}
+		for (List<String> list : friends) {
+			String friend1 = list.get(0), friend2 = list.get(1);
+			for (int i = 0; i < userfriends.size(); i++) {
+				if (friend2 != user && userfriends.get(i) == friend1) {
+					map.put(friend2, map.getOrDefault(friend2, 0) + 10);
+				} else if (friend1 != user && userfriends.get(i) == friend2) {
+					map.put(friend1, map.getOrDefault(friend1, 0) + 10);
+				}
+			}
+		}
+		for (String list : visitors) {
+			int count = 0;
+			for (int i = 0; i < userfriends.size(); i++) {
+				if (list != userfriends.get(i)) {
+					count++;
+				}
+			}
+			if (count == userfriends.size()) {
+				map.put(list, map.getOrDefault(list, 0) + 1);
+			}
+		}
+		List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
+		entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return o2.getValue() - o1.getValue();
+			}
+		});
+		for (String name : map.keySet()) {
+			if (answer.size() <= 5) {
+				answer.add(name);
+			}
+		}
+		return answer;
 	}
 
 	public static void checkList(String user, List<List<String>> friends, List<String> visitors) {
