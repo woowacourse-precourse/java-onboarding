77b31abba5494d84bbc6a8b45d26cdd16af0109a
yesjuhee <juhee0924@g.skku.edu>
2022-11-01 16:01:15 +0900

    [PROBLEM7] User의 친구 목록을 반환해주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,28 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        List<String> friendsOfUser = findFriendsOfUser(user, friends);
+
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> findFriendsOfUser(String user, List<List<String>> friends){
+
+        List<String> friendsOfUser = new ArrayList<>();
+
+        for(int i=0; i < friends.size() ; i++){
+            if(friends.get(i).contains(user)){
+                int userIndex = friends.get(i).indexOf(user);
+                friendsOfUser.add(friends.get(i).get(1-userIndex));
+            }
+        }
+        return friendsOfUser;
     }
 }
