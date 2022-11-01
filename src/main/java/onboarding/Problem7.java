3e0bd11ac98d4aff623f1c2ddf1a0c41daa459a4
이지영 <jioung93@naver.com>
2022-11-01 12:08:56 +0900

    comment: 기능사항 정리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    /*
+    1. 친구 관계를 전부 받은 후, 친구 목록 갱신
+    2. 공통 친구 수*10만큼 점수 갱신
+    3. 방문자 점수 갱신
+    3. 점수-이름 순 정렬
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    // user1의 친구 목록에 user2를 추가하는 함수 addFriend()
+
+    // user1과 user2 사이의 공통 친구 수를 알아내는 함수 countCommonFriend()
+
+    // A에게 점수를 n만큼 추가해주는 함수
+
+    // 사용자를 점수-이름 순으로 정렬해주는 함수 sortUserList()
 }
