3598a5384a7000e0b6459481f491a0a892b11541
metorg <metrog950@gmail.com>
2022-11-01 15:32:27 +0900

    feat: [Problem7] 유저와 친구인지 체크 메서드 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+
+    public static HashMap<String,Boolean> user_friend;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        user_friend = new HashMap<String,Boolean>();
         List<String> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    public static void userFriendCheck(String user,List<List<String>> friends){
+        for(int i=0;i<friends.size();i++){
+            if(friends.get(i).get(0)==user){
+                user_friend.put(friends.get(i).get(1),true);
+            }
+            else if(friends.get(i).get(1)==user){
+                user_friend.put(friends.get(i).get(0),true);
+            }
+            else continue;
+        }
     }
 }
