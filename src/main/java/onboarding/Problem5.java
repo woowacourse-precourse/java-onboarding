f40bf0e3c894d2f194f54830fe59662f388ca015
rlaalstn0107 <minsu980107@naver.com>
2022-11-01 21:40:30 +0900

    5번문제 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyUnits = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int currMoney = money;
+
+        for(int i = 0; i < moneyUnits.length; i++) {
+            int numOfCurrUnit = currMoney / moneyUnits[i];
+            answer.add(numOfCurrUnit);
+            currMoney -= numOfCurrUnit * moneyUnits[i];
+        }
         return answer;
     }
 }
