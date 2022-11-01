8ff48c864feac8a686545d721276292dadbff7ec
jmjmjames <chungjm0711@gmail.com>
2022-11-01 19:22:06 +0900

    refactor: add ArrayList initialCapacity #5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        final List<Integer> answer = new ArrayList<>();
-        final int[] currencies = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        final int[] CURRENCIES = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
 
-        for (int currency : currencies) {
+        final List<Integer> answer = new ArrayList<>(CURRENCIES.length);
+
+        for (int currency : CURRENCIES) {
             answer.add(money / currency);
             money %= currency;
         }
