9ad382b431dd1ae708910ed9d8aa6558972b52b1
khj <bbb6254177@gmail.com>
2022-11-01 13:43:33 +0900

    refactor

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,7 @@ public class Problem5 {
         int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> ans = new ArrayList<Integer>();
 
+        //큰 단위부터 작은 단위로의 순서대로 최대로 가능한만큼 받는다.
         for(Integer unit: units){
             ans.add(money/unit);
             money %= unit;
