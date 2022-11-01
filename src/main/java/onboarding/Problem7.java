6b9218e718ff4a49f9667f77f5cbc314bbd9e22d
minji <heunhanireum@gmail.com>
2022-11-01 15:30:11 +0900

    Problem7 : user랑 친구인 사람 목록 해시셋 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
+
+    private static HashSet<String> userFriendsList(String user, List<List<String>> friends) {
+        HashSet<String> userFriends = new HashSet<>();
+        for(List<String> friend : friends) {
+            if (friend.contains(user)) {
+                userFriends.add(friend.get(1-friend.indexOf(user)));
+            }
+        }
+
+        return userFriends;
+    }
+
 }
+
