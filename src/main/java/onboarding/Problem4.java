48975927abbd3881b1fb295d34163368ab36c790
youngdaragon <93703248+youngdaragon@users.noreply.github.com>
2022-11-01 23:06:34 +0900

    Add files via upload

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i)>=65 && word.charAt(i) <=90){
+                int dis = word.charAt(i) - 65;
+                answer += (char)  ('A' +25 -dis);
+
+            }else if (word.charAt(i)>=97 && word.charAt(i) <=122){
+                int dis = word.charAt(i) - 97;
+                answer += (char) ('a'+25 -dis);
+            }
+            else answer+=word.charAt(i);
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyValue =  Collections.emptyList();
+        answer = Arrays.asList(0,0,0,0,0,0,0,0,0);
+        moneyValue = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+
+        for(int i=0; i<9; i++){
+            answer.set(i,money/moneyValue.get(i));
+            money = money % moneyValue.get(i);
+        }
+
         return answer;
     }
 }
