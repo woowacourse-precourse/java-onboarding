00a10254aa61bc14e0b70e634a8744d444b37699
shpark30 <seohyeonpark30.gmail.com>
2022-11-01 20:34:36 +0900

    refact: Wrapping Integer and make object solve

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,26 +4,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Problem3 {
-    public static int solution(int number) {
-        /* 숫자마다 박수 카운트 누적 */
-        int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            answer += count369(i);
-        }
-        return answer;
+class ClapCount {
+    /* 박수 횟수 Integer Wrapping */
+    private int clapCount;
+
+    public ClapCount() {
+        this.clapCount = 0;
     }
 
-    static int count369(int number) {
-        /* 3, 6, 9와 매칭되는 자리수 개수를 반환 */
-        List<Integer> condition = new ArrayList<>(Arrays.asList(3, 6, 9));
-        int count = 0;
-        while (number > 0) {
-            if (condition.contains(number % 10)) {  // 끝 자리수 숫자가 3, 6, 9에 해당하면
-                count += 1;
-            }
-            number /= 10;   // 남은 자리수
+    public void clap() {
+        this.clapCount += 1;
+    }
+
+    public int getClapCount() {
+        return this.clapCount;
+    }
+}
+
+class Game369 {
+    private final List<Integer> condition = new ArrayList<>(Arrays.asList(3, 6, 9));
+    private ClapCount clapCount;
+
+    public int play(final int endNumber) {
+        /* endNumber까지 369 게임을 하면서 박수친 횟수 return */
+        this.clapCount = new ClapCount();
+        for (int n = 1; n <= endNumber; n++) {
+            count369(n);    // 숫자 n에서 3, 6, 9가 들어 있는 횟수 카운트
         }
-        return count;
+        return this.clapCount.getClapCount();
+    }
+
+    private void count369(int number) {
+        while (number > 0) {
+            count(number % 10);
+            number /= 10;
+        }
+    }
+
+    private void count(int number) {
+        if (this.condition.contains(number)) {
+            this.clapCount.clap();
+        }
+    }
+}
+
+public class Problem3 {
+    public static int solution(int number) {
+        Game369 game = new Game369();
+        return game.play(number);
     }
 }
