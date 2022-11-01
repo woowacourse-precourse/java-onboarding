6bbc62615be4812933c49ff55e47e30df8d715b3
MJ Choi <1621139@gmail.com>
2022-11-01 09:46:07 +0900

    [ADD] Problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int UPPER_LAST_ALPHABET = 90;
+    private static final int LOWER_LAST_ALPHABET = 90;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] alphabetArray = word.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < alphabetArray.length; i++) {
+            Character result;
+            if (Character.isAlphabetic(alphabetArray[i]) && Character.isUpperCase(alphabetArray[i])) {
+                result = (char) (UPPER_LAST_ALPHABET - alphabetArray[i] + 'A');
+            } else if (Character.isAlphabetic(alphabetArray[i]) && Character.isLowerCase(alphabetArray[i])) {
+                result = (char) (LOWER_LAST_ALPHABET - alphabetArray[i] + 'a');
+            } else {
+                result = alphabetArray[i];
+            }
+            stringBuilder.append(result);
+        }
+        return stringBuilder.toString();
     }
 }
