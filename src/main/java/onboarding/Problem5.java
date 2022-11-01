c76ea0093355aae18e5c15d00b54a327845dab9b
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:41:58 +0900

    [Refactor] 변수명 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,12 +7,12 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> coins = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        List<Integer> monetaryUnit = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
 
-        for (int i = 0; i < coins.size(); i++) {
-            if (money >= coins.get(i)) {
-                answer.add(i, money / coins.get(i));
-                money = money % coins.get(i);
+        for (int i = 0; i < monetaryUnit.size(); i++) {
+            if (money >= monetaryUnit.get(i)) {
+                answer.add(i, money / monetaryUnit.get(i));
+                money = money % monetaryUnit.get(i);
             } else {
                 answer.add(i, 0);
             }
