2b43fddd999e333663203c36f50aba25b89693cc
swandevson <shai4956@gmail.com>
2022-11-01 14:19:17 +0900

    style: P3 컨벤션 수정
    - 메소드 사이 빈줄 추가
    - while과 여는 소괄호 사이에 공백 삽입

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+
+
 public class Problem3 {
     public static int chckClap(int n) {
         int cnt = 0;
 
-        while(n > 0) {
+        while (n > 0) {
             if (n%10 != 0 && (n%10) % 3 == 0) {  // 3, 6, 9 모두 3의 배수임
                 cnt++;
             }
@@ -17,12 +19,13 @@ public class Problem3 {
     public static int getAnswer(int n) {
         int cnt = 0;
 
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             cnt += chckClap(i);
         }
 
         return cnt;
     }
+
 
     public static int solution(int number) {
         int answer = 0;
