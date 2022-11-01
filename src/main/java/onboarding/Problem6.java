af3898d6832e69101b83afbe2bc1580498988ad5
0417taehyun <0417taehyun@gmail.com>
2022-11-01 20:22:40 +0900

    feat: [문제6] 반복문을 통한 연속된 중복 문자열이 존재하는 사용자 이메일 반환 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -26,7 +26,27 @@ public class Problem6 {
 
 
     public static List<String> getDuplicatedMemberEmails(List<List<String>> forms) {
+        HashSet<String> emails = new HashSet<>();
 
+        for (int i = 0; i < forms.size() - 1; i++) {
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+
+            for (int j = i + 1; j < forms.size(); j++) {
+                String targetEmail = forms.get(j).get(0);
+                String targetNickname = forms.get(j).get(1);
+
+                if (isDuplicated(nickname, targetNickname)) {
+                    emails.add(email);
+                    emails.add(targetEmail);
+                }
+            }
+        }
+
+        List<String> sortedEmails = new ArrayList<>(emails);
+        Collections.sort(sortedEmails);
+
+        return sortedEmails;
     }
 
     public static List<String> solution(List<List<String>> forms) {
