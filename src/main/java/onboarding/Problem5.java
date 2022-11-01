c4de7e034d0e02f5009073f54d915b1b67863299
nayooniverse <kangna99@naver.com>
2022-11-01 16:13:30 +0900

    [feat] 화폐 변환 개수 리턴 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
+    /**
+     * 화폐 변환
+     * @param money
+     * @return 개수 리스트
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = calcMoney(money);
         return answer;
+    }
+
+    /**
+     * 화폐 변환 개수 함수
+     * @param money
+     * @return 화폐 변환 개수
+     */
+    public static List<Integer> calcMoney(int money) {
+        int[] bills = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> result = new ArrayList<>();
+        for(int bill : bills) {
+            result.add(money / bill);
+            money %= bill;
+        }
+        return result;
     }
 }
