316f95e99a14d4ad5bfa5cb618b0b9ed772e4558
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-01 15:28:22 +0900

    Feat(Problem7) : getPeopleSet 메서드 추가 모든 사람의 목록을 구하는 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Set<String> peopleSet = getPeopleSet(friends, visitors);
         return answer;
+    }
+
+    public static Set<String> getPeopleSet(List<List<String>> friends, List<String> visitors) {
+        Set<String> set = new HashSet<>();
+        for(List<String> l : friends) {
+            set.addAll(l);
+        }
+        set.addAll(visitors);
+        return set;
     }
 }
