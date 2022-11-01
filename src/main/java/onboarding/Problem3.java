0bca9bbc4c7b666ad5948fbf8c32e307ca817754
gardenee <snnjjang@naver.com>
2022-11-01 17:10:36 +0900

    refactor - 배열에 저장 안하고 답만 도출

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,19 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int[] samYookGoo = new int[number+1];
+        int answer = 0;
 
-        for (int i = 1; i < samYookGoo.length; i++) {
-            samYookGoo[i] = samYookGoo[i-1];
+        for (int i = 1; i < number+1; i++) {
             int num = i;
 
             while (num > 0) {
                 int mod = num % 10;
-                if (mod != 0 && mod % 3 == 0) samYookGoo[i] += 1;
+                if (mod != 0 && mod % 3 == 0) answer += 1;
                 num /= 10;
             }
         }
 
-        return samYookGoo[number];
+        return answer;
     }
 }
