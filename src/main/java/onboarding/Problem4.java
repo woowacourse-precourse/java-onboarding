a3ee52a7023a3fa425e5eb328d9dda5f1ab3d2e2
unknown <dragontaek98@naver.com>
2022-11-01 17:29:25 +0900

    feat: problem4 string을 문자 단위로 분리하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        //convert String to char Array
+        char[] ch = word.toCharArray();
         return answer;
     }
 }
