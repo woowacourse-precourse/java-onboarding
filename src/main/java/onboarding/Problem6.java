5433051315ad858764b7a3660ceff1e03d3f3150
iamminho <minho100227@kakao.com>
2022-11-01 18:49:43 +0900

    Problem6 refactor(getOverlapNamesQueue): 로직 변경을 통한 속도 개선

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,9 @@ import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
 public class Problem6 {
+    static PriorityQueue<String> overlapNamesQueue;
+    static boolean[] check;
+
     public static List<String> solution(List<List<String>> forms) {
         PriorityQueue<String> overlapNamesQueue = getOverlapNamesQueue(forms);
 
@@ -14,26 +17,64 @@ public class Problem6 {
 
     static PriorityQueue<String> getOverlapNamesQueue(List<List<String>> forms) {
         int size = forms.size();
-        PriorityQueue<String> overlapNamesQueue = new PriorityQueue<>();
+        overlapNamesQueue = new PriorityQueue<>();
+        check = new boolean[size];
 
         for (int i = 0; i < size; i++) {
-            setOverlapNamesQueue(forms, overlapNamesQueue, i);
+            if (!check[i]) {
+                setOverlapNamesQueue(forms, i);
+            }
         }
 
         return overlapNamesQueue;
     }
 
-    static void setOverlapNamesQueue(List<List<String>> forms, PriorityQueue<String> overlapNamesQueue, int i) {
-        for (int j = 0; j < forms.size(); j++) {
-            String currentEmail = forms.get(i).get(0);
-            String currentName = forms.get(i).get(1);
-            String targetName = forms.get(j).get(1);
+    static void setOverlapNamesQueue(List<List<String>> forms, int i) {
+        String formName = forms.get(i).get(1);
 
-            if (!isException(forms, i, j) && isOverlapName(currentName, targetName)) {
-                removeOverlapEmail(overlapNamesQueue, currentEmail);
-                break;
-            }
+        for (int j = 0; j < formName.length() -1; j++) {
+            String subStr = formName.substring(j, j+2);
+            findOverlapNames(subStr, forms, i);
         }
+    }
+
+    static void findOverlapNames(String subStr, List<List<String>> forms, int i) {
+        for (int k = 0; k < forms.size(); k++) {
+            findOverlapName(forms, subStr, i, k);
+        }
+    }
+
+    static void findOverlapName(List<List<String>> forms, String subStr, int i, int k) {
+        String currentEmail = forms.get(i).get(0);
+        String targetName = forms.get(k).get(1);
+        String targetEmail = forms.get(k).get(0);
+
+        if(!isException(forms, i, k) && targetName.contains(subStr)) {
+            inputOverlapName(currentEmail, targetEmail, k);
+        }
+    }
+
+    static void inputOverlapName(String currentEmail, String targetEmail, int k) {
+        check[k] = true;
+
+        inputOverlapNameEmail(currentEmail, targetEmail);
+    }
+
+    static void inputOverlapNameEmail(String currentEmail, String targetEmail) {
+        if (validateOverlapEmail(currentEmail)) {
+            overlapNamesQueue.add(currentEmail);
+        }
+        if (validateOverlapEmail(targetEmail)) {
+            overlapNamesQueue.add(targetEmail);
+        }
+    }
+
+    static boolean validateOverlapEmail(String email) {
+        if (!overlapNamesQueue.contains(email)) {
+            return true;
+        }
+
+        return false;
     }
 
     static boolean isException(List<List<String>> forms, int i, int j) {
@@ -43,7 +84,6 @@ public class Problem6 {
 
         return false;
     }
-
 
     static boolean isSameIdx(int i, int j) {
         if (i == j) {
@@ -64,29 +104,12 @@ public class Problem6 {
         return true;
     }
 
-    static boolean isOverlapName(String currentName, String targetName) {
-        for (int i = 0; i < currentName.length() -1; i++) {
-            String substrCurName = currentName.substring(i, i+2);
-
-            if (targetName.contains(substrCurName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static void removeOverlapEmail(PriorityQueue<String> overlapNamesQueue, String currentEmail) {
-        if (!overlapNamesQueue.contains(currentEmail)) {
-            overlapNamesQueue.add(currentEmail);
-        }
-    }
-
     static List<String> priorityQueueToList(PriorityQueue<String> queue) {
         List<String> result = new ArrayList<>();
 
         Stream.generate(queue::poll)
-            .limit(queue.size())
-            .forEach(value -> result.add(value));
+                .limit(queue.size())
+                .forEach(value -> result.add(value));
 
         return result;
     }
