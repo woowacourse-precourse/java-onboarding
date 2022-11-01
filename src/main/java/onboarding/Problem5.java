24af4f7f0d40ed3268f8355527ec09ed1f61ff6a
gun1kim <obkh31@naver.com>
2022-11-01 11:49:21 +0900

    feat: 5번 문제 풀이 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,20 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyLists = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i=0;i<moneyLists.length;i++){
+            int temp = money / moneyLists[i];
+            money %= moneyLists[i];
+            answer.add(temp);
+        }
+
         return answer;
     }
 }
