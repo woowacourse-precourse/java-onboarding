7b621f967805111ae736a444df17cb41c8660b66
2do1 <dowon3036@naver.com>
2022-11-01 17:35:40 +0900

    feat: 문제 5번 금액이 큰 화폐 순서대로 화폐를 받는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+// import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1}; // 화폐의 종류
+
+        // 금액이 큰 화폐 순서대로 화폐를 받는 기능
+        for (int cash: moneyArr) {
+
+        }
         return answer;
     }
 }
