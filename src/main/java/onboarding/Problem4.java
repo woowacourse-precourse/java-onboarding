4741d2a37d31a37943ff825af59159adcb85a834
hyee0715 <hyee0715@naver.com>
2022-11-01 16:51:44 +0900

    feat(Problem4.java): 청개구리 사전을 만드는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        HashMap<Character, Character> treeFrogDictionary = makeTreeFrogDictionary();
+
         return answer;
+    }
+
+    private static HashMap<Character, Character> makeTreeFrogDictionary() {
+        int alphabetCount = 26;
+        int halfOfTheAlphabetCount = alphabetCount / 2;
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < halfOfTheAlphabetCount; i++) {
+            map.put((char)('A' + i), (char)('Z' - i));
+            map.put((char)('Z' - i), (char)('A' + i));
+        }
+
+        return map;
     }
 }
