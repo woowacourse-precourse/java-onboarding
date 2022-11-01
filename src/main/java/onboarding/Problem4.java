57739ce56ca069b74dbcd48decde9dcf5a09ea46
Konahyeon <koo@inha.edu>
2022-11-01 14:21:09 +0900

    대문자, 소문자 및 알파벳 외 문자 구분

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
 public class Problem4 {
+    public static String reverse(String word) {
+        char sentence[] = word.toCharArray();
+
+        int ascii = 0;
+        String answer = "";
+
+        for (int i = 0; i < sentence.length; i++) {
+            if (Character.isUpperCase(sentence[i])) //대문자
+                ascii = 65;
+            else if (Character.isLowerCase(sentence[i])) //소문자
+                ascii = 97;
+            else {
+                answer += sentence[i];
+                continue;
+            }
+
+        }
+
+        return answer;
+    }
+
     public static String solution(String word) {
         String answer = "";
+
+        answer = reverse(word);
+
         return answer;
     }
 }
