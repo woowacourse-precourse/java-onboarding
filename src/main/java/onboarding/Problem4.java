bfae4508c181ce65f88b5a1f6c186279de6ddf82
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-01 13:16:39 +0900

    feat(Problem4): 반복문을 돌며 해당 문자에 해당하는 메소드 호출해 각각의 반환 값을 저장해 리턴(기능목록3)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                answer += changeUpperCase(word.charAt(i));
+            } else if(Character.isLowerCase(word.charAt(i))) {
+                answer += changeLowerCase(word.charAt(i));
+            } else {
+                answer += word.charAt(i);
+            }
+        }
         return answer;
     }
 
