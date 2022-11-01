f6e3751ad7d2611f081fc4b7bfd85ada95c94664
WonSeok-dd <cluejws@gmail.com>
2022-11-01 14:08:13 +0900

    Problem6 | docs: 기능 정의

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * 기능 구현 사항
- * 1. 겹치지 않았던 닉네임끼리만 비교
- * 2. 현재 닉네임에서 연속된 두 문자 추출해서 다른 닉네임 비교
- * 3. 겹쳤던 닉네임을 가진 크루의 이메일 오름차순 정렬
- */
 public class Problem6 {
 
     private static boolean compareName(String s1, String s2) {
