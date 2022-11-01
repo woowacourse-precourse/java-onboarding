965b89a57dca67dcd042c156447bdcf19a6df2a0
aran <aran0317@naver.com>
2022-11-01 21:42:42 +0900

    문제 6번 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /*기능 요구 사항*/
@@ -12,7 +14,29 @@ import java.util.List;
  */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer =  new ArrayList<String>();
+        List<String> name = new ArrayList<String>();
+
+        for(int i=0; i<forms.size(); i++){
+            name.add(forms.get(i).get(1));
+        }
+
+        for(int i=0; i<forms.size(); i++) { // 10000 * 20 * 10000
+            boolean usable = true;
+            for (int j = 0; j <= forms.get(i).get(1).length() - 2; j++) {
+                for(int k=0; k<name.size(); k++){
+                    if(i==k) continue;
+                    if (name.get(k).contains(forms.get(i).get(1).substring(j, j + 2))) { // 이름이 중복되면
+                        usable = false;
+                        answer.add(forms.get(i).get(0));
+                        break;
+                    }
+                }
+                if(!usable) break;
+            }
+        }
+
+        Collections.sort(answer);
         return answer;
     }
 }
