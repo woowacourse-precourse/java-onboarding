324f9d219381da5e5d1d9ee52bcde1841c5f4eb1
tdmdgh <tdmdgh@gmail.com>
2022-11-01 18:58:59 +0900

    problem4_1_대문자를 규칙에 맞게 변환하는 매소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+    private static char changeUpper(char x){
+        return (char) (x+25-(x-'A')*2);
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
