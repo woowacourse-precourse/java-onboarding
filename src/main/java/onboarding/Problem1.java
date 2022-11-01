79c1a424718e613e512d7680753f2661ba99e15a
alswn <alswn9938@gmail.com>
2022-11-01 10:54:56 +0900

    [Q1] refactor : add "상수" and refactor "isValidInput" 메소드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,69 +3,73 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int IS_NOT_VALID = -1;
+    public static int FIRST_PLAYER_WIN = 1;
+    public static int SECOND_PLAYER_WIN = 2;
+    public static int GAME_IN_A_TIE = 0;
+    public static int FIRST_PAGE = 1;
+    public static int LAST_PAGE = 400;
+    public static int LEFT_PAGE_INDEX = 0;
+    public static int RIGHT_PAGE_INDEX = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(isValidInputSet(pobi, crong)){
-            return -1;
+        if (isValidInputSet(pobi, crong)) {
+            return IS_NOT_VALID;
         }
 
-        int pobiMaxScore  = getPlayerMaxScore(pobi);
+        int pobiMaxScore = getPlayerMaxScore(pobi);
         int crongMaxScore = getPlayerMaxScore(crong);
 
-        if(pobiMaxScore > crongMaxScore){
-            return 1;
-        }
-        else if(crongMaxScore > pobiMaxScore){
-            return 2;
-        }
-        else{
-            return 0;
+        if (pobiMaxScore > crongMaxScore) {
+            return FIRST_PLAYER_WIN;
+        } else if (crongMaxScore > pobiMaxScore) {
+            return SECOND_PLAYER_WIN;
+        } else {
+            return GAME_IN_A_TIE;
         }
     }
 
-    public static boolean isValidInputSet(List<Integer> firstPlayer, List<Integer> secondPlayer){
+    public static boolean isValidInputSet(List<Integer> firstPlayer, List<Integer> secondPlayer) {
         return isValidInput(firstPlayer) && isValidInput(secondPlayer);
     }
 
-    public static boolean isValidInput(List<Integer> pages){
-        if(!isValidNumber(pages)){
-            return false;
-        }
-        if (!isContinuous(pages)) {
-            return false;
-        }
+    public static boolean isValidInput(List<Integer> pages) {
+        Integer leftPage = pages.get(LEFT_PAGE_INDEX);
+        Integer rightPage = pages.get(RIGHT_PAGE_INDEX);
 
-        Integer leftPage = pages.get(0);
-        Integer rightPage = pages.get(1);
-
-        if(!isOddNumber(leftPage) || !isEvenNumber(rightPage)){
+        if (!isValidNumber(leftPage, rightPage)) {
             return false;
         }
-        if(!isValidRange(leftPage) || !isValidRange(rightPage)){
+        if (!isContinuous(leftPage, rightPage)) {
+            return false;
+        }
+        if (!isOddNumber(leftPage) || !isEvenNumber(rightPage)) {
+            return false;
+        }
+        if (!isValidRange(leftPage) || !isValidRange(rightPage)) {
             return false;
         }
         return true;
     }
 
-    public static boolean isOddNumber(int page){
-        return page % 2==1;
+    public static boolean isOddNumber(int page) {
+        return page % 2 == 1;
     }
 
-    public static boolean isEvenNumber(int page){
+    public static boolean isEvenNumber(int page) {
         return page % 2 == 0;
     }
 
-    public static boolean isValidRange(int page){
-        return page >=1 && page <=400;
+    public static boolean isValidRange(int page) {
+        return page >= FIRST_PAGE && page <= LAST_PAGE;
     }
 
-    public static boolean isValidNumber(List<Integer> pages){
-        Integer leftPage = pages.get(0);
-        Integer rightPage = pages.get(1);
+    public static boolean isValidNumber(Integer leftPage, Integer rightPage) {
         return leftPage != null && rightPage != null;
     }
 
-    public static boolean isContinuous(List<Integer> pages){
-        return pages.get(1) - pages.get(0) == 1;
+    public static boolean isContinuous(int leftPage, int rightPage) {
+        return rightPage - leftPage == 1;
     }
 
     public static int sumDigit(int page) {
@@ -77,22 +81,22 @@ class Problem1 {
         return sum;
     }
 
-    public static int multiplyDigit(int page){
+    public static int multiplyDigit(int page) {
         int multi = 1;
-        while(page > 0){
+        while (page > 0) {
             multi *= page % 10;
             page /= 10;
         }
         return multi;
     }
 
-    public static int getMaxScoreOfMultiAndSum(int page){
+    public static int getMaxScoreOfMultiAndSum(int page) {
         int multiScore = multiplyDigit(page);
         int sumScore = sumDigit(page);
         return Math.max(multiScore, sumScore);
     }
 
-    public static int getPlayerMaxScore(List<Integer> pages){
+    public static int getPlayerMaxScore(List<Integer> pages) {
         int leftMaxScore = getMaxScoreOfMultiAndSum(pages.get(0));
         int rightMaxScore = getMaxScoreOfMultiAndSum(pages.get(1));
         return Math.max(leftMaxScore, rightMaxScore);
