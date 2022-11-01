9e0a3e72731605ca5046101c69ac68bddaa5c06a
ttaehee <ttaehee1105@gmail.com>
2022-11-01 11:10:41 +0900

    문제5) feat : 화폐단위 변환 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyTypes = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int moneyType : moneyTypes) {
+            answer.add(money / moneyType);
+            money = money % moneyType;
+        }
         return answer;
     }
+
 }
