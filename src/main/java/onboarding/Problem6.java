4757acdd2cf975f06870c4aa9fefb407a4ea2554
dingding-21 <phj8221@naver.com>
2022-11-01 23:05:16 +0900

    feat: Problem6 solution함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> crewEmail = getDuplicatedCrewEmail(forms);
+        return getSortedData(crewEmail);
     }
 
     private static Set<String> getDuplicatedCrewEmail(List<List<String>> forms) {
