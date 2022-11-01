369e868207fb33bc01276d4563b4c63a858bd61c
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 15:00:04 +0900

    feat:static value

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    private static StringBuilder sb=new StringBuilder();
+    private static String x="";
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        return reverseString(word);
     }
 }
