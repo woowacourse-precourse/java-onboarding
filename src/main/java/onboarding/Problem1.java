36024c78e03a99fca728c0ad0424f5c51e928995
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:22:53 +0900

    Feat: Add 입력 예외 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,39 @@ class Problem1 {
     }
 
 
+    private static boolean IsExceptionCase(List<Integer> pages) {
+
+        final int START_PAGE = 1;
+        final int END_PAGE = 400;
+
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+
+        // 왼쪽이 홀수, 오른쪽이 짝수가 아닌 경우
+        if (!(leftPage % 2 == 1 && rightPage % 2 == 0)) {
+            return true;
+        }
+
+        // 첫번째 또는 마지막 페이지가 포함되어 있는 경우
+        if (pages.contains(START_PAGE) || pages.contains(END_PAGE)) {
+            return true;
+        }
+
+        // 연속된 숫자가 아닐 경우
+        if (!(leftPage + 1 == rightPage)) {
+            return true;
+        }
+
+        // 페이지 범위를 넘어선 경우
+        if (leftPage < START_PAGE && rightPage > END_PAGE) {
+            return true;
+        }
+
+        return false;
+    }
+
+
     private static int pageAddition(int number) {
         int result = 0;
 
@@ -42,7 +75,7 @@ class Problem1 {
         return results.stream().max(Integer::compare).orElse(-1);
     }
 
-    
+
     private static int winner(int pobi, int crong) {
         if (pobi > crong) {
             return 1;
