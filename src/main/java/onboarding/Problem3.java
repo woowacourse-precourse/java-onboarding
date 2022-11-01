e940889300bfbad5e2568de0256c378df517cd63
hyunseo <goatsfish@gmail.com>
2022-11-01 14:27:18 +0900

    feat(Problem3) : 하나의 숫자를 통한 박수 횟수 계산 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+
+    private static final List<Integer> CLAP_VALUES = List.of(3, 6, 9);
+    private static final int CIPHER_OF_TEN = 10;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    }
+
+    private static int findClapCount(int number) {
+        int clapCount = 0;
+        while (number > 0) {
+            clapCount += detectAnyClapValue(number);
+            number /= CIPHER_OF_TEN;
+        }
+        return clapCount;
+    }
+
+    private static int detectAnyClapValue(int number) {
+        if (checkClapValue(number)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private static boolean checkClapValue(int number) {
+        int cipherNumber = findCipherNumber(number);
+        return CLAP_VALUES.contains(cipherNumber);
+    }
+
+    private static int findCipherNumber(int number) {
+        return number % CIPHER_OF_TEN;
     }
 }
