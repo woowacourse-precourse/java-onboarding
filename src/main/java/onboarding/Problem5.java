24dfe73a0dea68a274bfdb1c15d8e3135a91c818
taejeong <sontaewjd@nate.com>
2022-11-01 19:41:31 +0900

    [Feat] moneyUnit 배열 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] moneyUnit = {50000,10000,5000,1000,500,100,50,10,1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = Arrays.asList(0,0,0,0,0,0,0,0,0);
         return answer;
     }
 }
