6c540e16bca506bd44f4424f99f9f026f2d6f640
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:37:28 +0900

    Feat: Add substring 닉네임을 Key로 하는 Map 생성 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
@@ -7,6 +9,36 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+
+    private static HashMap makeNicknameHashMap(List<List<String>> forms) {
+        HashMap<String, ArrayList<Integer>> hashMap = new HashMap<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            groupByTwoLetter(forms, hashMap, i);
+        }
+
+        return hashMap;
+    }
+
+
+    private static void groupByTwoLetter(List<List<String>> forms, HashMap<String, ArrayList<Integer>> hashMap, int i) {
+        String email = forms.get(i).get(0);
+        String name = forms.get(i).get(1);
+
+        validationCheck(email, name);
+
+        for (int j = 0; j < name.length() - 1; j++) {
+            String word = name.substring(j, j + 2);
+            ArrayList<Integer> list = new ArrayList<>();
+            if (hashMap.containsKey(word)) {
+                list = hashMap.get(word);
+            }
+            list.add(i);
+            hashMap.put(word, list);
+        }
+    }
+
 
     private static void validationCheck(String email, String name) {
         if (!validateEmailCheck(email)) {
