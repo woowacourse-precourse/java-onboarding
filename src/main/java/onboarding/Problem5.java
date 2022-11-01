df622bc91731b928230b93b576fff7c01ed11d75
kwx4957 <kdy4957@naver.com>
2022-11-01 13:23:04 +0900

    5번 문제 기능 목록 1. 돈의 액수 초기화 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,9 +3,18 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+1. 돈의 액수 초기화 설정
+2. money 최소 최대값 기능
+ */
 public class Problem5 {
+    public static int[] initCoin(){
+        return new int[]{50000,10000,5000,1000,500,100,50,10,1};
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+
     }
 }
