eeb569d3fec3e882f8449a49a4fa89785dce96ba
junyong <jymoon1121@gmail.com>
2022-11-01 15:23:29 +0900

    [4번] ASCII코드로 변환후 리스트를 반환하는 함수 changeWord 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        int[] arr = changeWord(word); // 아스키 코드로 변환
+
         return answer;
+    }
+    public static int[] changeWord(String word) {
+        char[] arr = word.toCharArray();
+        int[] check_list = new int[arr.length]; // 0으로 초기화된 배열
+
+        for(int i = 0; i < arr.length; i++) {
+            check_list[i] = arr[i];
+        }
+        return check_list;
     }
 }
