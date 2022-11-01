648e91f57a96b6bdebb5d4a0f257e1bf2e8ef09f
dlgosla <dlgoslaek1@gmail.com>
2022-11-01 11:54:07 +0900

    feature: [Problem3] 특정 숫자에 대한 박수 횟수 계산 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+
+    }
+
+    public static int calculate_clap_count(int number){
+        int count = 0;
+
+        while (number != 0) {
+            int units = number % 10;
+
+            if (units == 3 || units == 6 || units == 9) {
+                count += 1;
+            }
+
+            number /= 10;
+        }
+
+        return count;
     }
 }
