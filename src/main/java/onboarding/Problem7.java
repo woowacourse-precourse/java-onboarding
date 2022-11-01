a93b2e57492c8adf2ca0a587d97a14a1ea7ff1ca
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-01 20:05:18 +0900

    feat: P7 기능 2 친구 추천 점수를 담을 맵 초기화 함수 작성 및 기능 주석 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,14 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 기능 1: 사용자의 친구 목록을 뽑는 함수 작성
- * 기능 2: 사용자의 친구 목록을 사용하여 그 친구를 같이 아는 사람들에게 점수를 주는 함수 작성
- * 기능 3: 방문자에게 점수를 주는 함수 작성
- * 기능 4-1: 점수를 첫번째 기준으로, 이름을 두번째 기준으로 하여 내림차순으로 정렬하는 함수 작성
- * 기능 4-2: 이미 친구인 경우와 0점인 경우를 제외하는 함수 작성
+ * 기능 2: 친구 추천 점수를 담을 맵 초기화 함수 작성
+ * 기능 3: 사용자의 친구 목록을 사용하여 그 친구를 같이 아는 사람들에게 점수를 주는 함수 작성
+ * 기능 4: 방문자에게 점수를 주는 함수 작성
+ * 기능 5: 점수를 첫번째 기준으로, 이름을 두번째 기준으로 하여 내림차순으로 정렬하는 함수 작성
+ * 기능 6: 이미 친구인 경우와 0점인 경우를 제외하는 함수 작성
  */
 public class Problem7 {
 
@@ -29,8 +28,29 @@ public class Problem7 {
         return userFriends;
     }
 
+    public static Map<String, Integer> initScoreMap(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, Integer> friendScoreMap = new HashMap<>();
+
+        for (List<String> friendList : friends) {
+            for (String name : friendList) {
+                if (user != name) {
+                    friendScoreMap.put(name, 0);
+                }
+            }
+        }
+
+        for (String visitor : visitors) {
+            friendScoreMap.put(visitor, 0);
+        }
+
+        return friendScoreMap;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, Integer> friendScoreMap = initScoreMap(user, friends, visitors);
+
         return answer;
     }
 
