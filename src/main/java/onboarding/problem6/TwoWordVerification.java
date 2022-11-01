b86ea0305405a1b18298d13db7b0ad403a7339d4
alstjr706 <alstjr706@naver.com>
2022-11-01 17:38:30 +0900

    refactor : (중복 닉네임 제거) 매개변수, 변수명 이름 수정

--- src/main/java/onboarding/problem6/TwoWordVerification.java
+++ src/main/java/onboarding/problem6/TwoWordVerification.java
@@ -4,38 +4,34 @@ import java.util.*;
 
 public class TwoWordVerification {
 
-    public Map<String, Integer> CountOfCase(Set<String> cases, List<CrewMember> crewMemberList) {
+    public Map<String, Integer> CountOfCase(Set<String> cases, List<Crew> crewList) {
 
         Map<String, Integer> resultMap = new HashMap<>();
 
         for (String word : cases) {
 
             int count = 0;
-            for (CrewMember crewMember : crewMemberList) {
-                if (crewMember.getNickname().contains(word)) {
+            for (Crew crew : crewList) {
+                if (crew.getNickname().contains(word)) {
                     count++;
                 }
             }
-
             resultMap.put(word, count);
         }
-
-
         return resultMap;
     }
+
 
     public List<String> DuplicatedCase(Map<String, Integer> CountCase) {
 
         ArrayList<String> resultList = new ArrayList<>();
 
         for (Map.Entry<String, Integer> entry : CountCase.entrySet()) {
+
             if (entry.getValue() >= 2) {
                 resultList.add(entry.getKey());
             }
         }
-
         return resultList;
     }
-
-
 }
