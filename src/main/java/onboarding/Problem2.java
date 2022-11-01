1dd56804b0dc08787571079328fe02623f1203c0
eunjiiiiii <47842737+eunjiiiiii@users.noreply.github.com>
2022-11-01 16:41:27 +0900

    add problem 1-5

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
 package onboarding;
+import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        StringBuffer str = new StringBuffer();
+        str.append(cryptogram);
+        // String to Array
+        char[] chararr = new char[cryptogram.length()];
+
+        for (int i = 0; i < str.length(); i++){
+            if(str.length()==0){
+                return "";
+            }
+            
+            if ((i+1) < str.length()){
+                if (str.charAt(i) == str.charAt(i+1)){
+                    str.deleteCharAt(i);
+                    str.deleteCharAt(i);
+                    //length -= 2;
+                    solution(str.toString());
+                    //chararr[i] = str.charAt(i);
+                    //i+=2;
+                    //solution(chararr.toString());
+                }
+                /*
+                else{
+                    chararr[i] = str.charAt(i);
+                    i++;
+                }
+                 */
+            }
+
+        }
+
+        
         return answer;
     }
 }
