eb83fc56faade682ecbb6323f25425da920d2881
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-11-01 21:57:37 +0900

    [FEAT] : problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,10 +4,24 @@ import java.util.regex.Pattern;
 
 public class Problem4 {
     public static String solution(String word) {
-    }
-
-    /** 알파벳인지 확인하는 정규표현식 */
-    private static boolean checkStr(String str){
-        return Pattern.matches("^[a-zA-Z]*$", str);
+        String result ="";
+        char[] wordtemp = word.toCharArray();
+        int reverse, abstmp, acsii = 0;
+        for(int index = 0; index < wordtemp.length; index++){
+            if(Character.isUpperCase(wordtemp[index])){
+                acsii = 65;
+            }
+            if(Character.isLowerCase(wordtemp[index])){
+                acsii = 97;
+            }
+            if(!(Character.isUpperCase(wordtemp[index]) || Character.isLowerCase(wordtemp[index]))){
+                result += ' ';
+                continue;
+            }
+            abstmp = Math.abs(acsii - wordtemp[index]);
+            reverse = (acsii + 25 ) - abstmp;
+            result = result + String.valueOf((char) reverse);
+        }
+        return result;
     }
 }
