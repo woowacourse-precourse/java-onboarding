de2547e95d78b92eb1b15e0dbcbf882e0edb6835
kyliecamila <kyliecamila@naver.com>
2022-11-01 20:54:52 +0900

    [feat]problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,44 @@
 package onboarding;
 
 import java.util.List;
-
+import java.util.ArrayList;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        ArrayList<String> answer = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> emails = new ArrayList<>();
+
+        for(List<String> sets : forms){
+            for(String info : sets){
+                if (info.contains("@email.com")){
+                    emails.add(info);
+                }
+                else {
+                    names.add(info);
+                }
+            }
+        }
+
+        for(int i = 1; i < names.size(); i++){
+            String namep = names.get(i-1);
+            for (int j = 1; j < namep.length(); j++){
+                String compare = "" + namep.charAt(j-1) + namep.charAt(j);
+                for (int k = i; k < names.size(); k++){
+                    if (names.get(k).contains(compare)){
+                        System.out.println(compare + k);
+                        if(answer.contains(emails.get(i-1)) == false){
+                            answer.add(emails.get(k));
+                            answer.add(emails.get(k-1));
+                        }
+                        if(answer.contains(emails.get(k)) == false){
+                            answer.add(emails.get(k));
+                        }
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 }
