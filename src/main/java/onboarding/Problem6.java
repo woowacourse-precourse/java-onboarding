840fe73ce9bedf6f4723f282b9e84252e11547a3
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-01 15:36:50 +0900

    feat: 크루의 닉네임중 연속되는 글씨를 찾아내는 반복문 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        HashSet<String> emails = new HashSet<>();
+
+        final Map<String, String > crewList = new HashMap<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+
+            final String name = forms.get(i).get(1);
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                final String key = name.substring(j, j+2);
+            }
+        }
+        
         return answer;
     }
 }
