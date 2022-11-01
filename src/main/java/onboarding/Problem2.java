936278494a9d3355ca32d6055cde790b6e003820
JohnPrk <uo3641493@gmail.com>
2022-11-01 15:57:35 +0900

    feat : Problems2 클래스 프로덕션 코드 작성(연속되는 중복 문자를 제거하고 문자열 결합)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,13 @@ import java.util.stream.Collectors;
 public class Problem2 {
     public static String solution(String cryptogram) {
         Stack<Character> stack = getCharacterStack(cryptogram);
-        return null;
+        return getStringBuilder(stack);
+    }
+
+    private static String getStringBuilder(Stack<Character> stack) {
+        return stack.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 
     public static Stack<Character> getCharacterStack(String cryptogram) {
