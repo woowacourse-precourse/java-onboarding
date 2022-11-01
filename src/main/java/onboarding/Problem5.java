62f45da02dbcbaec8d6a9d0545a90dfe21270141
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-01 23:43:48 +0900

    Problem 5 : 메인 함수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -21,7 +21,15 @@ public class Problem5 {
         return num;
     }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        int[] money_unit = {50000,10000,5000,1000,500,100,50,10,1};
+
+        // 화폐 단위별 개수를 구하는 반복문
+        for(Integer unit:money_unit){
+            answer.add(numberOf(money,unit));
+            money = remainMoney(money,unit);
+        }
+
         return answer;
     }
 }
