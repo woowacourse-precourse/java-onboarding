8aee54ab294f31f97e589eb609d172fe7502f662
ghchoi <ghchoi0427@gmail.com>
2022-11-01 21:56:27 +0900

    fix: 점수를 기준으로 정렬

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,8 +5,21 @@ import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Problem7 p7 = new Problem7();
+        HashMap<String, Integer> recommendation = new HashMap<>();
+        List<String> usersFriends = p7.getOnesFriends(friends, user);
+
+        for (String friend : usersFriends) {
+            recommendation = p7.getAcquaintanceScore(recommendation, friends, friend, user);
+        }
+
+        recommendation = p7.getVisitScore(recommendation, p7.excludeFriend(visitors, usersFriends));
+
+        return recommendation.entrySet()
+                .stream()
+                .sorted((o1, o2) -> o2.getValue() - o1.getValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     private List<String> getOnesFriends(List<List<String>> friends, String user) {
@@ -15,9 +28,7 @@ public class Problem7 {
         return result.stream().filter(element -> !element.equals(user)).collect(Collectors.toList());
     }
 
-    private HashMap<String, Integer> getAcquaintanceScore(List<List<String>> friends, String userFriend, String user) {
-        HashMap<String, Integer> recommendation = new HashMap<>();
-
+    private HashMap<String, Integer> getAcquaintanceScore(HashMap<String, Integer> recommendation, List<List<String>> friends, String userFriend, String user) {
         for (List<String> relation : friends) {
             if (!relation.contains(user) && relation.contains(userFriend)) {
                 String stranger = relation.stream().filter(e -> !e.equals(userFriend)).findFirst().get();
@@ -35,17 +46,14 @@ public class Problem7 {
         return visitors.stream().filter(e -> !friends.contains(e)).collect(Collectors.toList());
     }
 
-    private HashMap<String, Integer> getVisitScore(List<String> visitors) {
-        HashMap<String, Integer> map = new HashMap<>();
+    private HashMap<String, Integer> getVisitScore(HashMap<String, Integer> recommendation, List<String> visitors) {
         for (String visitor : visitors) {
-            if (map.containsKey(visitor)) {
-                map.put(visitor, map.get(visitor) + 1);
+            if (recommendation.containsKey(visitor)) {
+                recommendation.put(visitor, recommendation.get(visitor) + 1);
             } else {
-                map.put(visitor, 1);
+                recommendation.put(visitor, 1);
             }
         }
-        return map;
+        return recommendation;
     }
-
-
 }
