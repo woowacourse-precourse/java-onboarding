e1dd3c328322e05b1fb634d5d1df4cd312914f91
ho2n <jungho2n@gmail.com>
2022-11-01 17:03:24 +0900

    매개변수로 전달된 인자의 페이지 번호가 예외사항에 해당되는지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,22 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!checkPages(pobi) || !checkPages(crong)) {
+            answer = -1;
+            return answer;
+        }
+
         return answer;
+    }
+
+    private static boolean checkPages(List<Integer> list) {
+        int leftPage = list.get(0);
+        int rightPage = list.get(1);
+        if ((leftPage + 1) != rightPage || (leftPage % 2 == 0) || (rightPage % 2 == 1) ||
+            (leftPage == 1) || (rightPage == 400)) {
+            return false;
+        }
+        return true;
     }
 }
