e5da66834b9661215e5ab75f6f487cae04c9585a
오승윤 <>
2022-11-01 16:44:56 +0900

    추가 : Problem7 모든 친구 정보를 Map에 저장하는 기능
    부가기능 - 입력받은 A 와 B를 친구로 만드는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static final int USER_IDX = 0;
+    private static final int FRIEND_IDX = 1;
+    Map<String, ArrayList<String>> friendInfoTable = new HashMap<>();
+    Map<String, Integer> scoreInfoTable = new HashMap<>();
+    ArrayList<String> RecommendedUsers = new ArrayList<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private void fillFriendInfoTable(List<List<String>> friends){
+        for(List<String> friendInfo : friends){
+            makeAandBFriend(friendInfo.get(USER_IDX), friendInfo.get(FRIEND_IDX));
+            makeAandBFriend(friendInfo.get(FRIEND_IDX), friendInfo.get(USER_IDX));
+        }
+    }
+
+    private void makeAandBFriend(String A,String B){
+        ArrayList<String> userFriends = friendInfoTable.getOrDefault(A,new ArrayList<>());
+        userFriends.add(B);
+        friendInfoTable.put(A,userFriends);
     }
 }
