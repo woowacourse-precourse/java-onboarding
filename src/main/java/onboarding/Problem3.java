4cc6de7c32d3f60729d2138b7df1d39e0cb83e70
we4sley <galaxy138742@gmail.com>
2022-11-01 23:40:53 +0900

    feat: problem3 solution 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,35 @@
 package onboarding;
 
 public class Problem3 {
+    private static final String MESSAGE = "잘못된 입력입니다.";
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        if (!isInputValid(number)) {
+            throw new IllegalArgumentException(MESSAGE);
+        }
+
+        return countClap(number);
+    }
+
+    private static boolean isInputValid(int number) {
+        return number >= 1 && number <= 10000;
+    }
+
+    private static int countClap(int number) {
+        int cnt = 0;
+        int checkNumber;
+
+        for (int i = 1; i <= number; i++) {
+            int order = i;
+            while (order != 0) {
+                checkNumber = order % 10;
+                if (checkNumber == 3 || checkNumber == 6 || checkNumber == 9) {
+                    cnt++;
+                }
+                order /= 10;
+            }
+        }
+        return cnt;
     }
 }
