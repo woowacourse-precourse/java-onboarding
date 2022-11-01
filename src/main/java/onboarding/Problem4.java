9547a6a299200ee1c27103a7874bdc1c6122c141
함궈 <heoja0123@naver.com>
2022-11-01 21:47:56 +0900

    [Problem4] 리팩토링 적용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,23 +1,35 @@
 package onboarding;
 
 public class Problem4 {
+    static final int MIN_WORD_LENGTH = 1;
+    static final int MAX_WORD_LENGTH = 1000;
+
     public static String solution(String word) {
         String answer = "";
+
+        validateWordLength(word);
 
         for(int i = 0; i < word.length(); i++){
             int ascii = word.charAt(i);
 
-            if(65 <= ascii && ascii <= 90){
-                answer += String.valueOf((char)((90 - ascii) + 65));
+            if(Character.isUpperCase(ascii)){
+                answer += String.valueOf((char)(('Z' - ascii) + 'A'));
 
-            }else if(97 <= ascii && ascii <= 122){
-                answer += String.valueOf((char)((122 - ascii) + 97));
+            }else if(Character.isLowerCase(ascii)){
+                answer += String.valueOf((char)(('z' - ascii) + 'a'));
 
             }else{
-                answer += String.valueOf(word.charAt(i));
+                answer += String.valueOf((char)ascii);
 
             }
         }
         return answer;
+    }
+
+    public static void validateWordLength(String word){
+        if(word.length() < MIN_WORD_LENGTH || word.length() > MAX_WORD_LENGTH){
+            throw new IllegalArgumentException("word는 길이가 " + MIN_WORD_LENGTH + "이상, " +
+                     MAX_WORD_LENGTH + "이하인 문자열이어야 한다.");
+        }
     }
 }
