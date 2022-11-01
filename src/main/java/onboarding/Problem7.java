7f0bc42273c10fa1f5038d4952da1eeb6adf6e46
masiljangajji <hjk172262@gmail.com>
2022-11-01 19:36:53 +0900

    feat: Problem7_사용자와 함께아는 친구 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        List<String> answer= new ArrayList<String>();
+        ArrayList<String> str=new ArrayList<String>();
+        str.add(user);
+        HashMap<String,Integer> map=new HashMap<String,Integer>();
+
+        for(int i=0;i< friends.size();i++)
+        {
+            for(int j=0;j<friends.get(i).size();j++)
+            {
+                map.put(friends.get(i).get(j),0);
+                if(friends.get(i).get(j)==user)
+                {
+                    if(j==0)
+                        str.add(friends.get(i).get(1));
+                    else
+                        str.add(friends.get(i).get(0));
+                }
+            }
+        }
+
         return answer;
     }
 }
