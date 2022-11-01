f4bec8efe7b6d2410997d9bf4236605c7c6a3bc3
Anthologia <pyoungjy@gmail.com>
2022-11-01 19:18:49 +0900

    feat: Problem 5 금액 검증

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int MIN_MONEY = 1;
+    private static final int MAX_MONEY = 1000000;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        validateMoney(money);
         return answer;
+    }
+
+    private static void validateMoney(int money) throws IllegalArgumentException {
+        if (!isValidMoneyRange(money)) {
+            throw new IllegalArgumentException(String.format("올바른 범위의 값은 %d - %d입니다.", MIN_MONEY, MAX_MONEY));
+        }
+    }
+
+    private static boolean isValidMoneyRange(int money) {
+        return (MIN_MONEY <= money) && (money <= MAX_MONEY);
     }
 }
