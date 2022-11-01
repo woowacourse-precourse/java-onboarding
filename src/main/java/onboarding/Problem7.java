9f870b58ab0622047b979a56bc392287ee5611de
chosunghyun18 <krismon@naver.com>
2022-11-01 11:41:56 +0900

    [7번] 요구 사항 리스트 3번 구현 완료,요구 사항 리스트 3번 표현 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,24 +8,21 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends_form, List<String> visitors) {
 
         List<String> answer = Collections.emptyList();
-        List<String> friends_list = new ArrayList<>();
-        List<List<String>> friends = new ArrayList<>(friends_form); // java.util.ArrayList와 java.util.arrays.ArrayList로 다르게 구현되어 있다.
+        List<String> friends_list = new ArrayList<>(); //get list of Already_freinds
+        List<List<String>> friends = new ArrayList<>(friends_form); // change ClassType java.util.ArrayList와 java.util.arrays.ArrayList로 다르게 구현되어 있다.
         HashMap<String,Integer> friends_score = new LinkedHashMap<>(); // 순서를 보장
 
-        friends = findAlreadyFreinds(friends_list, user, friends);
+        findAlreadyFreinds(friends_list, user, friends);
         sort(friends_list);
-        System.out.println("EndFindAlready");
-
-        System.out.println(friends_list);
-        System.out.println(friends);
-
-        calculateScore(friends_score,friends_list,friends);
-        System.out.println("after calculate");
+        calculateScoreTenPoints(friends_score,friends_list,friends);
+        System.out.println("after calculate 10 points");
         System.out.println(friends_score);
+        calculateScoreOnePoints(visitors,friends_list,friends_score);
+
 
         return answer;
     }
-    public static List<List<String>> findAlreadyFreinds(List<String> friends_list ,String user, List<List<String>> friends)
+    public static void  findAlreadyFreinds(List<String> friends_list ,String user, List<List<String>> friends)
     {
         for (int i =  0 ; i<friends.size();i++)
         {
@@ -41,9 +38,8 @@ public class Problem7 {
         }
         // 동일한 친구관계가 주어지지는 않으므로 중복 처리 없이 반환
         friends_list.remove(user);
-        return friends;
     }
-    public static void calculateScore(HashMap<String, Integer> friends_score, List<String> friends_list, List<List<String>> friends){
+    public static void calculateScoreTenPoints(HashMap<String, Integer> friends_score, List<String> friends_list, List<List<String>> friends){
 
         HashSet<String> already_set = new HashSet<>(friends_list);
 
@@ -65,6 +61,19 @@ public class Problem7 {
                 friends_score.put(friend.get(1), friends_score.get(friend.get(1)) + 10);
             } else if (!already_set.contains(friend.get(1))) {
                 friends_score.put(friend.get(0), friends_score.get(friend.get(0)) + 10);
+            }
+        }
+
+    }
+    public static void calculateScoreOnePoints(List<String> visitors_list,List<String> friends_list,HashMap<String,Integer> friends_score){
+        List<String> visitors = new ArrayList<>(visitors_list); //Type change
+        visitors.removeAll(friends_list); // left except current friend of user
+        for(String visitor:visitors)
+        {
+            if(!friends_score.containsKey(visitor)) {
+                friends_score.put(visitor,1);
+            }else{
+                friends_score.put(visitor, friends_score.get(visitor)+1);
             }
         }
 
