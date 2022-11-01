099d605403bf4a6b10e956b89eb39352150a175a
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:27:55 +0900

    [feat] Problem1 페이지가 잘못된 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,6 +16,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(pobi.get(0) + 1 != pobi.get(1) ||
+                crong.get(0) + 1 != crong.get(1) ||
+                pobi.get(0) % 2 != 1 ||
+                crong.get(0) % 2 != 1)
+            answer = -1;
         return answer;
     }
 }
