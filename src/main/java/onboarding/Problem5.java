bb538aa79de89e2a6f19ac086d3bb9aec1576ff3
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-01 19:40:17 +0900

    feat: [Prob 5] 큰 단위의 동전부터 순차적으로 필요한 동전의 개수를 세는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int countOfCoinUnit=9;
+    public static Integer[] coinValueByCoinUnit=new Integer[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    public static int[] countCoinByCoinUnit(int money){
+        int[] coinCountByCoinUnit=new int[]{0,0,0,0,0,0,0,0,0};
+        for(int coinUnitIndex=0;coinUnitIndex<countOfCoinUnit;coinUnitIndex++){
+            int coinCountByUnit=money/coinValueByCoinUnit[coinUnitIndex];
+            money-=coinCountByUnit*coinValueByCoinUnit[coinUnitIndex];
+            coinCountByCoinUnit[coinUnitIndex]=coinCountByUnit;
+        }
+        return coinCountByCoinUnit;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
