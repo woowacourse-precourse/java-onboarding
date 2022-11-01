9849a18169dd21b037751097e4c89eac5b57a328
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 20:29:21 +0900

    feat(PROBLEM7): 추천 점수별로 정렬 해주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +10,26 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static void sortList(List<Map.Entry<String, Integer>> entries) {
+        Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue() < o2.getValue()) {
+                    return 1;
+                }
+
+                if(o1.getValue() == o2.getValue()) {
+                    if (o1.getKey().charAt(0) > o2.getKey().charAt(0)) {
+                        return 1;
+                    }
+
+                    return -1;
+                }
+
+                return -1;
+            }
+        });
     }
 
     static void recommendFriend(List<Map.Entry<String, Integer>> entries, List<String> recommendedList) {
