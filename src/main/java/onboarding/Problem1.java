2f01b809c239f4dadbee881216d7efd1f6b929fc
yejin130 <jinis2023@gmail.com>
2022-11-01 18:34:59 +0900

    rename: problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,39 +6,38 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = -1;
 
-
         if(pobi.get(0) %2==1 && pobi.get(1) - pobi.get(0) ==1 && crong.get(0) %2==1 && crong.get(1) - crong.get(0) ==1 && pobi.get(0) !=1 && pobi.get(0) !=399 && crong.get(0) !=1 && crong.get(0) !=399) {
-            int p_max = 0;
+            int pobiMax = 0;
             for (int i = 0; i < 2; i++) {
                 String[] a = Integer.toString(pobi.get(i)).split("");
                 int sum = 0;
-                int prod = 1;
+                int multiple = 1;
                 for (int j = 0; j < a.length; j++) {
                     sum += Integer.parseInt(a[j]);
-                    prod *= Integer.parseInt(a[j]);
+                    multiple *= Integer.parseInt(a[j]);
                 }
-                p_max = Math.max(sum, prod);
+                pobiMax = Math.max(sum, multiple);
             }
 
-            int c_max = 0;
+            int crongMax = 0;
             for (int i = 0; i < 2; i++) {
                 String[] a = Integer.toString(crong.get(i)).split("");
                 int sum = 0;
-                int prod = 1;
+                int multiple = 1;
                 for (int j = 0; j < a.length; j++) {
                     sum += Integer.parseInt(a[j]);
-                    prod *= Integer.parseInt(a[j]);
+                    multiple *= Integer.parseInt(a[j]);
                 }
-                c_max = Math.max(sum, prod);
+                crongMax = Math.max(sum, multiple);
             }
 
-            if(p_max>c_max) {
+            if(pobiMax>crongMax) {
                 answer = 1;
             }
-            if(p_max<c_max) {
+            if(pobiMax<crongMax) {
                 answer = 2;
             }
-            if(p_max==c_max) {
+            if(pobiMax==crongMax) {
                 answer = 0;
             }
         }
