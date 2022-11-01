35726de03747a18f9dc84e52446056360426b5e0
song-wooseok <sws981202@gmail.com>
2022-11-01 18:39:57 +0900

    feat: [PROBLEM7 - 예외사항1] user 문자열 길이 검사

---
+++ src/main/java/onboarding/problem7/utils/validator/exception/UserLengthException.java
@@ -0,0 +1,7 @@
+package onboarding.problem7.utils.validator.exception;
+
+public class UserLengthException extends Exception{
+    public UserLengthException(String msg){
+        super(msg);
+    }
+}
