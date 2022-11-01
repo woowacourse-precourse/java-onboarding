9a965521fdad5e596b77563fbf59679e5133e98a
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 16:45:58 +0900

    comment(Problem5): 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 1. 알맞은 화폐로 변환한 수를 구하는 기능
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
