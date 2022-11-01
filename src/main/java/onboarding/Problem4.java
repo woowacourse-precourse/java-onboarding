f7d87e47e8505fe551f3d21640a115b445af48fa
은지 <midzy0228@gmail.com>
2022-11-01 23:26:16 +0900

    refactor[Problem4]: += 연산자 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,27 +8,27 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         Antonym antonym = new Antonym();
 
         for (int i=0; i<word.length(); i++) {
             if (65 <= word.charAt(i) && word.charAt(i) <= 90) {// 대문자인 경우
-                answer += antonym.antonym(word.charAt(i), 65);
+                answer.append(antonym.antonym(word.charAt(i), 65));
                 continue;
             }
             if (97 <= word.charAt(i) && word.charAt(i) <= 122){// 소문자인 경우
-                answer += antonym.antonym(word.charAt(i), 97);
+                answer.append(antonym.antonym(word.charAt(i), 97));
                 continue;
             }
-            answer += word.charAt(i); //대문자도 소문자도 아닌 경우
+            answer.append(word.charAt(i)); //대문자도 소문자도 아닌 경우
         }
-        return answer;
+        return answer.toString();
     }
 }
 
 class Antonym{
     public char antonym(char word, int firstNumber){
-        char result = ' ';
+        char result;
         int tmp = 25 - (word-firstNumber) * 2;
         result = (char) (word + tmp);
         return result;
