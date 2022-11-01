1bd58d64463c7753f730ee9459f78f8ab2ccb3da
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:31:55 +0900

    feat: Problem2 반복되는 문자열을 찾고 해당 인덱스를 리스트에 저장하는 기능 구현
    Problem2 반복되는 문자열을 찾고 해당 인덱스를 리스트에 저장하는 기능을 구현하고 반복되는 문자가 아예 없는 경우엔 처음 그대로 check 값이 false가 되도록 구현하였습니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        while (true){
+            List<Integer> temp = new ArrayList<>();
+            Boolean check = false;
+            for (int i=0; i<cryptogram.length()-1;i++){
+                if (check){
+                    if (cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                        temp.add(i);
+                    }
+                    else{
+                        temp.add(i);
+                        break;
+                    }
+                }else {
+                    if (cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                        temp.add(i);
+                        if (cryptogram.length()-1 == i+1)
+                            temp.add(i + 1);
+                        check=true;
+                    }
+                }
+            }
+
+        }
+        answer= cryptogram;
         return answer;
     }
+
 }
