e29b569d4b1eac6de56c3ac6fcf3cc7b6fa41ab1
yunjeong <uu0078@naver.com>
2022-11-01 23:04:57 +0900

    Problem7 1. 친구관계 그래프 Map 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /* 기능 목록
 1. 친구관계 그래프 Map 생성
@@ -14,6 +13,27 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // 1. 친구관계 그래프 Map 생성
+        Map<String, List<String>> connectMap = new HashMap<>();
+
+        for(List<String> connect : friends) {
+            if(connectMap.containsKey(connect.get(0)))
+                connectMap.get(connect.get(0)).add(connect.get(1));
+            else {
+                List<String> friendList = new ArrayList<>();
+                friendList.add(connect.get(1));
+                connectMap.put(connect.get(0), friendList);
+            }
+
+            if(connectMap.containsKey(connect.get(1)))
+                connectMap.get(connect.get(1)).add(connect.get(0));
+            else {
+                List<String> friendList = new ArrayList<>();
+                friendList.add(connect.get(0));
+                connectMap.put(connect.get(1), friendList);
+            }
+        }
         return answer;
     }
 }
