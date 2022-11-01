73bc45a233ca39849d905e3ca4e8d41b647599ff
오승윤 <>
2022-11-01 15:18:06 +0900

    추가 : Problem2 복호화할 구간이 있는지 찾아서 표시하는 기능, '복잡한 조건문 서술식 메서드로 캡슐화'

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,41 @@ public class Problem2 {
         return answer;
     }
 
+    private  boolean findDecodedSection(){
+        int cryptogramLength = cryptograph.length();
+        boolean existence = false;
+
+        for( int standardIdx=0 ; standardIdx<(cryptogramLength-1) ; standardIdx++){
+            for( int comparisonIdx=standardIdx+1 ; comparisonIdx<cryptogramLength ; comparisonIdx++){
+
+                if(isDifferentCharacters(standardIdx,comparisonIdx)){
+                    if(isExistSameCharactersBetween(standardIdx,comparisonIdx)){
+                        existence = true;
+                        checkXToBeDecodedSection(standardIdx,comparisonIdx);
+                        standardIdx = comparisonIdx-1;
+                    }
+                    break;
+                }
+
+            }
+        }
+
+        return existence;
+    }
+
+    private boolean isDifferentCharacters(int standardIdx,int comparisonIdx){
+        return cryptograph.charAt(standardIdx)!=cryptograph.charAt(comparisonIdx);
+    }
+    private boolean isExistSameCharactersBetween(int standardIdx,int comparisonIdx){
+        return (comparisonIdx-standardIdx)>1;
+    }
+
+    private void checkXToBeDecodedSection(int startPoin,int EndNextPoint){
+        for(int i=startPoin;i<EndNextPoint;i++){
+            cryptograph.setCharAt(i,'X');
+        }
+    }
+
     private void deleteFoundSection(){
         String decryptedCryptograph = cryptograph.toString().replace("X","");
         cryptograph = new StringBuffer(decryptedCryptograph);
