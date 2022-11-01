4e7d79a9cea2f1ca291ac5c1016e7ce16366c773
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 14:20:33 +0900

    feat: Problem7 해당 이름의 친구 관계를 key,value형태로 저장하는 기능 구현
    해당 이름의 친구 관계를 key,value형태로 저장하는 기능을 구현해서 friendList에 저장하였습니다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Map<String,List<String>> friendList = new HashMap<>();
+        Map<String,Integer> result = new HashMap<>();
+        for (List<String> friend : friends) {
+            if(friendList.containsKey(friend.get(0))){
+                List<String> lists = new ArrayList<>(friendList.get(friend.get(0)));
+                if (!lists.contains(friend.get(1))){
+                    lists.add(friend.get(1));
+                    friendList.put(friend.get(0), lists);
+                }
+            }
+            else{
+                String[] strings = {friend.get(1)};
+                friendList.put(friend.get(0),Arrays.asList(strings));
+            }
+
+            if (friendList.containsKey(friend.get(1))) {
+                List<String> lists = new ArrayList<>(friendList.get(friend.get(1)));
+                if (!lists.contains(friend.get(0))){
+                    lists.add(friend.get(0));
+                    friendList.put(friend.get(1), lists);
+                }
+            }
+            else{
+                String[] strings = {friend.get(0)};
+                friendList.put(friend.get(1),Arrays.asList(strings));
+            }
+        }
+
+
         return answer;
     }
+
 }
