bcb392ee3f08e67d1ddc446b43f533c3284d634b
sxxgxxnz <tmddms2292@gmail.com>
2022-11-01 12:39:21 +0900

    feat : Problem7[2] user와 친구가 아닌 리스트를 만든다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    public static void makeNonFriendList(String user,List<List<String>> friends,List<String> myFriend,List<String> nonFriend,List<String> visitors){
+        Map<String,Integer> scoreMap = new HashMap<>();
+        int score = 0;
+        for(int i=0;i<friends.size();i++){
+            List<String> friend = friends.get(i);
+            
+        }
+    }
     public static void makeFriendList(String user,List<List<String>> friends,List<String> myFriend){
 
         for(int i=0;i<friends.size();i++){
@@ -18,8 +26,10 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         List<String> myFriend = new ArrayList<>();
+        List<String> nonFriend = new ArrayList<>();
 
         makeFriendList(user,friends,myFriend);
+        makeNonFriendList(user,friends,myFriend,nonFriend,visitors);
         return answer;
     }
 }
