fdc374e58b287cacc27741d72305164eace0fca9
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:27:54 +0900

    문제5 : 화폐 단위 별 금액을 담은 배열 선언 후 반복문으로 반환값에 화폐 갯수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,24 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    /**
+     * 입력받은 문자열 2개를 비교해 2자 이상 중복되었는 판별함
+     * 
+     * @param name
+     * @param compareName
+     * @return {@code} boolean} 2자이상 중복 여부
+     */
+    private static boolean isDuplicated(String name, String compareName) {
+
+        for (int s = 0; s < name.length() - 1; s++) {
+            String slicedName = name.substring(s, s + 2);
+
+            if (compareName.indexOf(slicedName) > -1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
