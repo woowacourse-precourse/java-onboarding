7cc560d9ac278248b2e7ef43a81cbb2b5ff382d4
송민진 <m_6595@naver.com>
2022-11-01 11:49:31 +0900

    #6 feat: create checkList by deviding nicknames into 2 letters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -15,6 +15,22 @@ public class Problem6 {
             nicknames.add(form.get(1));
         }
 
+        List<String> checkList = createCheckList(nicknames);
+
         return answer;
+    }
+
+    private static List<String> createCheckList(List<String> nicknames) {
+        List<String> checkList = new ArrayList<>();
+
+        for (String nickname : nicknames) {
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String checkStr = nickname.substring(i, i + 1);
+                if (!checkList.contains(checkStr)) {
+                    checkList.add(checkStr);
+                }
+            }
+        }
+        return checkList;
     }
 }
