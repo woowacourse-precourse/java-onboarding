d2f4cde0dd0b7962fbbc6e18efaa8bd36b05740b
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-01 20:23:04 +0900

    [Problem6] 1) result로 오름차순 정렬되게 stream 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+
+
+
+        // result로 오름차순 정렬되게 stream 구현
+        return answer.stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
+
 }
