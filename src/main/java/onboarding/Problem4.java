1e1b4cabb7ecce2cb6a357ef8462ec8306e93be3
csb9427 <csb9427@naver.com>
2022-11-01 11:33:01 +0900

    Problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = reverseAlphabetic(word);
         return answer;
+    }
+
+    public static String reverseAlphabetic(String word) {
+        StringBuilder sb = new StringBuilder();
+        word.chars()
+                .mapToObj(i -> (char) i)
+                .forEach(c -> {
+                    if (isUppercase(c))
+                        sb.append((char) ('Z' + 'A' - c));
+                    else if (isLowercase(c))
+                        sb.append((char) ('z' + 'a' - c));
+                    else
+                        sb.append(c);
+                });
+        return sb.toString();
+    }
+
+    public static boolean isUppercase(char c) {
+        return 65 <= c && c <= 90;
+    }
+
+    public static boolean isLowercase(char c) {
+        return 97 <= c && c <= 122;
     }
 }
