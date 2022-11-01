fa9cc2a7005ab75c7b8ea252721a64d213f0715c
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 18:00:14 +0900

    add/ 문제 4) 알파벳 판별 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0;i<word.length();i++){
+            System.out.println("isAlpha(word) = " + isAlpha(word.charAt(i)));
+
+        }
         return answer;
+    }
+
+    //알파벳인지 그 외 문자인지 판별하는 기능
+    public static boolean isAlpha(char alpha){
+        return (65 <= (int)alpha && (int)alpha <= 90) || (97 <= (int)alpha && (int)alpha <= 122);
     }
 }
