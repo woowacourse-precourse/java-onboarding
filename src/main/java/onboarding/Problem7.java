0149c936951915960a79bf776352481065c24e3d
3juhwan <13selfesteem91@naver.com>
2022-11-01 19:43:16 +0900

    problem7: update solution function

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    HashMap<String, Integer> personScore = new HashMap<>();
+
+    private static void makeMapOfPersonScore(List<List<String>> friends, List<String> visitors) {
+
+    }
+
+    private static HashSet<String> getUserFriends(List<List<String>> friends) {
+        return new HashSet<>();
+    }
+
+    private static void scoreFriendsKnowWithUser(List<List<String>> friends) {
+        HashSet<String> userFriends = getUserFriends(friends);
+    }
+
+    private static void scoreVisitor(List<String> visitors) {
+
+    }
+
+    private static List<String> getMaxScoredPerson() {
+        return new ArrayList<>();
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        makeMapOfPersonScore(friends, visitors);
+        scoreFriendsKnowWithUser(friends);
+        scoreVisitor(visitors);
+        return getMaxScoredPerson();
     }
 }
