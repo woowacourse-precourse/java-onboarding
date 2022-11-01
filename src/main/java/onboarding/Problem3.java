b60a35452a57ef23c48ff23f939064f5c6edec16
lunchRamen <bloom6561@naver.com>
2022-11-01 18:37:49 +0900

    refactoring:Problem3 369갯수 구하는 로직 클래스 분리. 테스트케이스 추가.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,15 +1,9 @@
 package onboarding;
 
-public class Problem3 {
-    /*
-    * 기능 구현 목록
-    * 매개변수 숫자 -> 문자열로 바꾸는 기능
-    * 바꾼 문자열의 문자들을 탐색해서, 3,6,9가 있는지 확인하는 기능
-    * 시간복잡도는 O(5*N) = O(N)
-    * */
-    public static int solution(int number) {
-        int answer = 0;
 
+class Problem3Solve{
+    public static int get369Number(int number){
+        int answer = 0;
         for (int i=1; i<=number; i++){
             String s = Integer.toString(i);
 
@@ -19,6 +13,18 @@ public class Problem3 {
                 }
             }
         }
+        return answer;
+    }
+}
+public class Problem3 {
+    /*
+    * 기능 구현 목록
+    * 매개변수 숫자 -> 문자열로 바꾸는 기능
+    * 바꾼 문자열의 문자들을 탐색해서, 3,6,9가 있는지 확인하는 기능
+    * 시간복잡도는 O(5*N) = O(N)
+    * */
+    public static int solution(int number) {
+        int answer = Problem3Solve.get369Number(number);
 
         return answer;
     }
