ddb1c3cca1cd53cce0fc040fa9baa78280d91773
ckyeon <qsky8520@gmail.com>
2022-11-01 15:44:25 +0900

    Add getPlaceValues() to Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static List<Integer> getPlaceValues(int number) {
+        List<Integer> placeValues = new ArrayList<>();
+        while (number > 0) {
+            placeValues.add(number % 10);
+            number /= 10;
+        }
+        return placeValues;
     }
 }
