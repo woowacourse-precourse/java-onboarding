06c6d0e689b5c7457c9e27b1537ee523c0b95e3d
core <jinjs6@gmail.com>
2022-11-01 17:49:09 +0900

    중복되는 문자열의 시작지점(start)및 끝 지점(end) 변수 선언(-1로 초기화, 중복 지점이 없으면 둘 다 -1 취급)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        while (true) {
+            int start = -1;
+            int end = -1;
+            if (start == -1 && end == -1) {
+                break;
+            }
+        }
         return answer;
     }
 }
