84d5361b563f6ba13bf5b06bb65f258e3822e1a3
aran <aran0317@naver.com>
2022-11-01 20:56:46 +0900

    문제 1 점수 계산 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,7 +2,6 @@ package onboarding;
 
 import java.util.List;
 
-
 /*기능 요구 사항*/
 /* 1. 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수 번호이다.
 *  2. 페이지 번호의 각 숫자를 모두 더하거나 모두 곱해 큰 수를 구한다
@@ -20,6 +19,40 @@ class Problem1 {
         if(pobi.get(0) <= 1 || pobi.get(1) >=400 || crong.get(0) <= 1 || crong.get(1) >=400) return -1;
         if(pobi.get(0) != pobi.get(1)-1 || crong.get(0) !=crong.get(1) -1) return -1;
         if(pobi.get(0)%2 == 0 || pobi.get(1)%2 ==1 || crong.get(0)%2 == 0 || crong.get(1)%2 ==1) return -1;
+
+        /*포비의 점수 구하기*/
+        int pobiScore = 0;
+        for(int i=0; i<2; i++){
+            int add = pobi.get(i)%10;
+            int mul = pobi.get(i)%10;
+            int c=1;
+            do{
+                add += (pobi.get(i)/(int)Math.pow(10,c))%10;
+                mul *= (pobi.get(i)/(int)Math.pow(10,c))%10;
+            }while(pobi.get(i)/(int)Math.pow(10,++c)> 0);
+            if(add > pobiScore) pobiScore = add;
+            if(mul > pobiScore) pobiScore = mul;
+        }
+
+        /*크롱의 점수 구하기*/
+        int crongScore = 0;
+
+        for(int i=0; i<2; i++){
+            int add = crong.get(i)%10;
+            int mul = crong.get(i)%10;
+            int c=1;
+            do{
+                add += (crong.get(i)/(int)Math.pow(10,c))%10;
+                mul *= (crong.get(i)/(int)Math.pow(10,c))%10;
+            }while(crong.get(i)/(int)Math.pow(10,++c) >0);
+
+            if(add > crongScore) crongScore = add;
+            if(mul > crongScore) crongScore = mul;
+        }
+
+        if(pobiScore==crongScore) answer =0;
+        else if(pobiScore>crongScore) answer = 1;
+        else answer = 2;
 
         return answer;
     }
