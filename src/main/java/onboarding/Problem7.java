9ee0bb815e27503f08e26bf629981f867e875d7f
Moveuk <v_donguk@naver.com>
2022-11-01 22:04:07 +0900

    feat: Problem7 점수가 저장된 scoreBoard에 저장된 사람 중 추천 친구 5명을 뽑아오는 기능 구현
    추천 친구 목록에는 내 친구가 포함되면 안된다
    점수는 0점 이상인 친구가 출력되어야한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static final int DEFAULT_SCORE = 0;
     public static final int FRIEND_TOGETHER_SCORE = 10;
     public static final int FRIEND_VISITED_SCORE = 1;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Map<String, Integer> scoreBoard = new HashMap<>();
@@ -19,6 +21,9 @@ public class Problem7 {
 
         // 방문자들 점수주기
         giveVisitorsScore(visitors, scoreBoard);
+
+        // scoreBoard에서 user의 친구가 아니며 점수가 0점 이상인 사람만 5명 추리기
+        answer = getTop5RecommendedFriendList(friendList, scoreBoard);
 
         return answer;
     }
@@ -49,5 +54,14 @@ public class Problem7 {
 
     public static void giveVisitorsScore(List<String> visitors, Map<String, Integer> scoreBoard) {
         visitors.forEach(visitor -> scoreBoard.put(visitor, scoreBoard.getOrDefault(visitor, DEFAULT_SCORE) + FRIEND_VISITED_SCORE));
+    }
+
+    public static List<String> getTop5RecommendedFriendList(Set<String> friendList, Map<String, Integer> scoreBoard) {
+        return scoreBoard.entrySet().stream()
+                .filter(eachEntry -> eachEntry.getValue() > 0 && !friendList.contains(eachEntry.getKey()))
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
