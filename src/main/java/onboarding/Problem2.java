8515852f05abd52bc3e4c9418afcab08c1f10023
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-01 16:18:56 +0900

    feat : add function exception for cryptogram size

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,11 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        return removeDuplicated(cryptogram);
+
     }
 
     private static String removeDuplicated(String str){
+
+        // cryptogram은 길이가 1이상 1000이하인 문자열이다.
+        if (1 > str.length() || 1000 < str.length()) {
+            return "failed";
+        }
+
+        int str_num = str.length(); // cryptogram의 수 만큼 비교하기 위하여 따로 선언
+        for (int i = 0; i < str_num; i++) {
+            for (int j = 0; j < str.length()-1; j++) {
+                if (str.charAt(j) == str.charAt(j + 1)) {
+                    str = str.substring(0, j) + str.substring(j + 2);
+                }
+            }
+        }
         return str;
     }
 }
+
