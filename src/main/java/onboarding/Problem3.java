4df4080dc20f1cf27642a08d0b1cef22ec503930
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 14:52:43 +0900

    feat:static value

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    private static int count;
+    private static int sum;
     public static int solution(int number) {
         int answer = 0;
-        return answer;
+        return Howmany(number);
     }
 }
