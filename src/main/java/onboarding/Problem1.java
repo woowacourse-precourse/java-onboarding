40487a9513aea7169afe9eb05dad813f3938385e
broden-horizon <rlghd1698@naver.com>
2022-11-01 20:59:13 +0900

    문제1. 각 자릿수의 합을 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,13 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    //각 자리의 합계 구하는 메서드
+    private static int sumOfEachDigit(int num) {
+        int sum = 0;
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
 }
