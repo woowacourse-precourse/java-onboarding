55e63fe791ee8df05450fc05d0ea7cd451f4604c
Kim-AYoung <onionlily123@naver.com>
2022-11-01 20:59:46 +0900

    feat: Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (c >= 'A' && c <= 'Z') {
+                answer += (char) ('Z' - (c - 'A'));
+            } else if (c >= 'a' && c <= 'z') {
+                answer += (char) ('z' - (c - 'a'));
+            } else {
+                answer += c;
+            }
+        }
         return answer;
     }
 }
