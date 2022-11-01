524eabbf70d60e5867f93d38d7c93e28db8a35e4
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 15:27:59 +0900

    feat: 사용자 친구의 친구는 점수를 획득한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,12 +8,17 @@ public class Problem7 {
     private final static int TIMELINE_VISIT_SCORE = 1;
     private final static int KNOW_TOGETHER_SCORE = 10;
 
+    private static Map<String, Integer> friendsScore;
+
     private static Set<String> userFriends;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        friendsScore = new HashMap<>();
         List<String> answer = Collections.emptyList();
 
         userFriends = createUserFriendsSet(user, friends);
+        calculateFriendsRelationScore(user, friends);
+
 
         return answer;
     }
@@ -23,5 +28,24 @@ public class Problem7 {
                 .filter(pair -> pair.contains(user))
                 .flatMap(Collection::stream)
                 .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    private static void calculateFriendsRelationScore(String user, List<List<String>> friends) {
+        friends.stream()
+                .filter(pair -> !pair.contains(user))
+                .forEach(Problem7::addScoreEachFriend);
+    }
+
+    private static void addScoreEachFriend(List<String> friendsPair) {
+        final String p1 = friendsPair.get(0);
+        final String p2 = friendsPair.get(1);
+        addSecondScoreIfUserKnowFirst(p1, p2);
+        addSecondScoreIfUserKnowFirst(p2, p1);
+    }
+
+    private static void addSecondScoreIfUserKnowFirst(String p1, String p2) {
+        if (userFriends.contains(p1)) {
+            friendsScore.put(p2, friendsScore.getOrDefault(p2, 0) + KNOW_TOGETHER_SCORE);
+        }
     }
 }
