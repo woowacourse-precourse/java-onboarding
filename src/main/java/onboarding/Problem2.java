30f7d3c787bd39a0a7cfe05e04564bd87b4eb831
jsc1014 <wotlr5209@gmail.com>
2022-11-01 18:10:07 +0900

    main문 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,6 +4,33 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
+        int cnt = 0,i = 0;
+        StringBuffer sb = new StringBuffer(cryptogram);
+
+        while(i != sb.length()){
+            //지워야 하는 부분이 문자열의 끝에 있을때
+            if(i+2 > sb.length()){
+                if(cnt != 0) sb.delete(i - cnt, i+1);
+                break;
+            }
+
+            if(equalCheckNextLetter(sb.substring(i,i+1),(sb.substring(i+1,i+2)))){
+                cnt++;
+                i++;
+            }
+            else{
+                if(cnt == 0){
+                    i++;
+                    continue;
+                }
+                else{
+                    sb.delete(i - cnt, i+1);
+                    i -= (cnt+1);
+                    cnt = 0;
+                }
+            }
+        }
+        answer = sb.toString();
 
         return answer;
     }
