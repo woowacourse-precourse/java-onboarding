8547ca86538d64ebbff61eda0e5326553c52d0f4
kyliecamila <kyliecamila@naver.com>
2022-11-01 21:02:53 +0900

    [fix]problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,18 +2,10 @@ package onboarding;
 import java.util.ArrayList;
 public class Problem2 {
     public static String solution(String cryptogram) {
-//        String crypto = cryptogram;
-//        String answer = "answer";
-//        int[] index = new int[crypto.length()];
-//        char[] arr = crypto.toCharArray();
-//        String[] encrypt = new String[crypto.length()];
-//        String temp="temp";
-//        int count=1;
-
         ArrayList<Character> characters = new ArrayList<Character>();
 
-        for( char c : cryptogram.toCharArray()){
-            characters.add(c);
+        for( char cha : cryptogram.toCharArray()){
+            characters.add(cha);
         }
         int length = cryptogram.length();
         int n = 1;
@@ -31,37 +23,10 @@ public class Problem2 {
 
         String answer = "";
 
-        for(char c : characters){
-            answer += c;
+        for(char cha : characters){
+            answer += cha;
         }
-//
-//        while(count!=0){
-//            for (int i = 0; i < crypto.length(); i++) {
-//                index = new int[crypto.length()];
-//                index[i] = 0;
-//            }
-//            for (int i = 0; i < crypto.length()-1; i++) {
-//                if(arr[i]==arr[i+1]){
-//                    index[i]=1;
-//                    index[i+1]=1;
-//                }
-//            }
-//            count=0;
-//            int j=0;
-//            for (int i = crypto.length()-1; i >=0; i--) {
-//               if(index[i]==1){
-//                   count++;
-//                   encrypt[j]="";
-//               }else{
-//                   encrypt[j]=String.valueOf(arr[i]);
-//                           j++;
-//               }
-//            }
-//            temp=String.join(",", encrypt);
-//            if(count==0){
-//                return answer;
-//            }
-//        }
+
 
         return answer;
     }
