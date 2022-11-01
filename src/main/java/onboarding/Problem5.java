5b8b5d8136395edd73cd222ef584fbc041ed0607
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 15:23:22 +0900

    problem5 feat : add money divide func by order

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int startDiv = 50000;
+        int tempMoney = money;
+        for (int i=0; i< 8; i++){
+            if(i%2==0 && i!=0) startDiv = divideBy2(startDiv);
+            else if(i%2==1) startDiv = divideBy5(startDiv);
+        }
         return answer;
+    }
+    public static int divideBy5(int div){
+        return div/5;
+    }
+    public static int divideBy2(int div){
+        return div/2;
     }
 }
