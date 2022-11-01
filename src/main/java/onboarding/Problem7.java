61196b6ee13c111ec3823ec47b1a33c77de61078
이규승 <rpg8988@naver.com>
2022-11-01 22:06:14 +0900

    update: 커밋한 메소드들을 이용하여 리스트 변환 & 추천 점수(내림차순), 이름순(오름차순) 정렬, 후보 중 최대 5명 추리기 & 0점 제외 및 값 리턴

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        Map<String, Set<String>> relations = new HashMap<>();
+        Map<String, Integer> scores = new HashMap<>();
+        Set<String> newFriends = new HashSet<>();
+
+        friendsReset(user, friends,  relations, newFriends);
+
+        friendsScore(user, relations, newFriends, scores);
+
+        visitorScore(user, relations, scores, visitors);
+
+        List<Map.Entry<String, Integer>> scoreList = scores.entrySet().stream().collect(Collectors.toList());
+        scoreList.sort((a, b) -> {
+            if (a.getValue() == b.getValue()) {
+                return a.getKey().compareTo(b.getKey());
+            }
+            return b.getValue() - a.getValue();
+        });
+        
+        int cnt = 0;
+        for (Map.Entry<String, Integer> friend : scoreList) {
+            if (friend.getValue() == 0 || cnt == 5) break;
+            answer.add(friend.getKey());
+            cnt++;
+        }
+
         return answer;
     }
 
