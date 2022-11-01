cab094a19378b2cf0ab0a7549e294d50732d12c5
Konahyeon <koo@inha.edu>
2022-11-01 16:11:08 +0900

    단위 별 개수 구하는 moneyCount 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,7 +8,12 @@ public class Problem5 {
     private static final int[] UNIT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> moneyCount(int money) {
         List<Integer> answer = new ArrayList<>();
-        
+
+        for (int i = 0; i < UNIT.length; i++) {
+            answer.add(money/UNIT[i]);
+            money %= UNIT[i];
+        }
+
         return answer;
     }
 
