24a12f7ba88fbb5db35d9e51fc0b2ee14c5028e8
eom-tae-in <eti0728@daum.net>
2022-11-01 20:39:40 +0900

    feat: mergeConvertedCharacters() -> 청개구리 사진 정보를 바탕으로 변환한 문자들을 문자열로 바꿈

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -17,4 +17,16 @@ public class Problem4 {
     public static char lowerCaseConversion(char character) {
         return (char) (219 - character);
     }
+
+    public static String mergeConvertedCharacters(String word) {
+        sb.setLength(0);
+        char ch;
+        for (int i = 0; i < word.length(); i++) {
+            ch = (word.charAt(i) >= 65 && word.charAt(i) <= 90) ? upperCaseConversion(word.charAt(i)) :
+                    ((word.charAt(i) >= 97 && word.charAt(i) <= 122) ? lowerCaseConversion(word.charAt(i)) : word.charAt(i));
+            sb.append(ch);
+            sb.trimToSize();
+        }
+        return String.valueOf(sb);
+    }
 }
