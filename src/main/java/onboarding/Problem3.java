123e41c4cd8967362fbeb5921ffeec2193aeeed0
sun007021 <sun007021@gmail.com>
2022-11-01 11:12:55 +0900

    feat: 해당 수에 3,6,9 들어간 개수 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
-
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private int CountContainDigit369(int number) {// 숫자에 3,6,9 몇개 있는지 확인
+        int lastnum;
+        int temp;
+        int count=0;
+        temp = number;
+
+        while (temp > 0) {
+            lastnum = temp % 10;
+            temp = temp / 10;
+            if(lastnum == 3 || lastnum == 6 || lastnum ==9){
+                count++;
+            }
+        }
+        return count;
     }
 }
