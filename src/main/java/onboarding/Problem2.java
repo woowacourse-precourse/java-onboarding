56e342a6c83fa3331ec867597401825fd88db3fa
OO <OO@LGNOTE-HFB56BL>
2022-11-01 15:14:54 +0900

    프리코스 1주차

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,50 @@
 package onboarding;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Problem2 {
+
+    public static String charArraytoString(Iterable<Character> target) {
+        Iterator itr = target.iterator();
+        String temp = "";
+        while (itr.hasNext()) {
+            temp += itr.next().toString();
+        }
+        return temp;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<Character>();
+        
+        int beforeLen, afterLen;
+        
+        while (true) {
+            stack.clear();
+            beforeLen = cryptogram.length();
+            
+            for (char ch : cryptogram.toCharArray()) {
+                if (stack.isEmpty()){
+                    stack.push(ch);
+                }
+                else {
+                    if (stack.peek() != ch) {
+                        stack.push(ch);
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            cryptogram = charArraytoString(stack);
+            afterLen = cryptogram.length();
+            
+            if (beforeLen == afterLen) {
+                answer = cryptogram;
+                break;
+            }
+        }
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
-
-import java.util.List;
-
+import java.util.*;
 public class Problem6 {
+    public static HashMap<String, Set<Integer>> hashMap = new HashMap<>();
+    
+    public static Set<Integer> duplicateIdx = new HashSet<>();
+    
+    public static void addDuplicateCrews(){
+    	
+        for (String s : hashMap.keySet()) {
+            if (hashMap.get(s).size() >= 2){
+                for (Integer crewIdx : hashMap.get(s)) {
+                    duplicateIdx.add(crewIdx);
+                }
+            }
+        }
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++){
+            String name = forms.get(i).get(1);
+            if (name.length() == 1) continue;
+            for (int j = 0; j < name.length() - 1; j++){
+                String currPart = name.substring(j, j + 2);
+                hashMap.computeIfAbsent(currPart, s -> new HashSet<>());
+                hashMap.get(currPart).add(i);
+            }
+        }
+        addDuplicateCrews();
+
+        for (Integer idx : duplicateIdx) {
+            answer.add(forms.get(idx).get(0));
+        }
+        Collections.sort(answer);
+
         return answer;
     }
 }
