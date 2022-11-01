89b083105c77f19f4f82d236295da656b0690248
Namyunsuk <yunsuk0328@naver.com>
2022-11-01 11:17:49 +0900

    style: 파일 당 탑레벨 클래스 1개존재하도록 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,60 +2,60 @@ package onboarding;
 
 import java.util.List;
 
-class Calculation {
-	public static int sumOfPageNumbers(int pageNumber) {
-		int sum = 0;
-		while (pageNumber > 0) {
-			sum += pageNumber % 10;
-			pageNumber /= 10;
-		}
-		return sum;
-	}
-
-	public static int mulOfPageNumbers(int pageNumber) {
-		int mul = 1;
-		while (pageNumber > 0) {
-			mul *= pageNumber % 10;
-			pageNumber /= 10;
-		}
-		return mul;
-	}
-}
-
-class FindingMax {
-	public static int findMaxValue(int left, int right) {
-		int leftMaxValue =
-			Calculation.sumOfPageNumbers(left) > Calculation.mulOfPageNumbers(left)
-				? Calculation.sumOfPageNumbers(left) : Calculation.mulOfPageNumbers(left);
-		int rightMaxValue =
-			Calculation.sumOfPageNumbers(right) > Calculation.mulOfPageNumbers(right)
-				? Calculation.sumOfPageNumbers(right) : Calculation.mulOfPageNumbers(right);
-		return leftMaxValue > rightMaxValue ? leftMaxValue : rightMaxValue;
-	}
-}
-
-class FindingWinner {
-	public static int findWinner(int pobiMaxNumber, int crongMaxNumber) {
-		if (pobiMaxNumber == crongMaxNumber) {
-			return 0;
-		}
-		return pobiMaxNumber > crongMaxNumber ? 1 : 2;
-	}
-}
-
-class Exception {
-	public static boolean findInvalidPageNumber(int left, int right) {
-		if (left < 1 || left > 400 || right < 1 || right > 400) {
-			return true;
-		}
-		if ((right - left) != 1) {
-			return true;
-		}
-		return false;
-	}
-}
-
 public class Problem1 {
+	private static class Calculation {
+		public static int sumOfPageNumbers(int pageNumber) {
+			int sum = 0;
+			while (pageNumber > 0) {
+				sum += pageNumber % 10;
+				pageNumber /= 10;
+			}
+			return sum;
+		}
+
+		public static int mulOfPageNumbers(int pageNumber) {
+			int mul = 1;
+			while (pageNumber > 0) {
+				mul *= pageNumber % 10;
+				pageNumber /= 10;
+			}
+			return mul;
+		}
+	}
+
+	private static class FindingMax {
+		public static int findMaxValue(int left, int right) {
+			int leftMaxValue =
+				Calculation.sumOfPageNumbers(left) > Calculation.mulOfPageNumbers(left)
+					? Calculation.sumOfPageNumbers(left) : Calculation.mulOfPageNumbers(left);
+			int rightMaxValue =
+				Calculation.sumOfPageNumbers(right) > Calculation.mulOfPageNumbers(right)
+					? Calculation.sumOfPageNumbers(right) : Calculation.mulOfPageNumbers(right);
+			return leftMaxValue > rightMaxValue ? leftMaxValue : rightMaxValue;
+		}
+	}
+
+	private static class FindingWinner {
+		public static int findWinner(int pobiMaxNumber, int crongMaxNumber) {
+			if (pobiMaxNumber == crongMaxNumber) {
+				return 0;
+			}
+			return pobiMaxNumber > crongMaxNumber ? 1 : 2;
+		}
+	}
+
+	private static class Exception {
+		public static boolean findInvalidPageNumber(int left, int right) {
+			if (left < 1 || left > 400 || right < 1 || right > 400) {
+				return true;
+			}
+			if ((right - left) != 1) {
+				return true;
+			}
+			return false;
+		}
+	}
+
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int pobiLeftPageNumber = pobi.get(0);
 		int pobiRightPageNumber = pobi.get(1);
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,52 +4,52 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Money {
-	static final int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-	static final String invalidArgument = "InvalidArgument";
-}
-
-class ValidationMoney extends CalculationMoney {
-	public static boolean isDivide(int money) {
-		return (money / divideMoney) > 0;
-	}
-
-	public static boolean isValidAmount(int money) {
-		return (money >= 1) && (money <= 1_000_000);
-	}
-}
-
-class CalculationMoney {
-	static int divideMoney;
-
-	public static void setDivideMoney(int money) {
-		divideMoney = money;
-	}
-
-	public static int divide(int money) {
-		return money / divideMoney;
-	}
-
-	public static int countRemainder(int money) {
-		return money % divideMoney;
-	}
-}
-
 public class Problem5 {
+	private static class Money {
+		static final int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		static final String invalidArgument = "InvalidArgument";
+	}
+
+	private static class ValidationMoney extends CalculationMoney {
+		public static boolean isDivide(int money) {
+			return (money / divideMoney) > 0;
+		}
+
+		public static boolean isValidAmount(int money) {
+			return (money >= 1) && (money <= 1_000_000);
+		}
+	}
+
+	private static class CalculationMoney {
+		static int divideMoney;
+
+		public static void setDivideMoney(int money) {
+			divideMoney = money;
+		}
+
+		public static int divide(int money) {
+			return money / divideMoney;
+		}
+
+		public static int countRemainder(int money) {
+			return money % divideMoney;
+		}
+	}
+
 	public static List<Integer> solution(int money) {
-		int i;
+		int caseMoney;
 		List<Integer> answer = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
 		if (!ValidationMoney.isValidAmount(money)) {
 			System.out.println(Money.invalidArgument);
 		}
-		for (i = 0; i < Money.moneyList.length - 1; i++) {
-			CalculationMoney.setDivideMoney(Money.moneyList[i]);
+		for (caseMoney = 0; caseMoney < Money.moneyList.length - 1; caseMoney++) {
+			CalculationMoney.setDivideMoney(Money.moneyList[caseMoney]);
 			if (ValidationMoney.isDivide(money)) {
-				answer.set(i, CalculationMoney.divide(money));
+				answer.set(caseMoney, CalculationMoney.divide(money));
 				money = CalculationMoney.countRemainder(money);
 			}
 		}
-		answer.set(i, money);
+		answer.set(caseMoney, money);
 		return answer;
 	}
 }
