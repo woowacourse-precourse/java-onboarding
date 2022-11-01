2f021419af842fc6594024679f879ec9e5fb1992
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:49:04 +0900

    인접한 중복문자 판별 후 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,7 +10,13 @@ public class Problem2 {
         {
             if (i + 1 < cryptoLen) // 두개씩 검사하므로 뒤의 문자가 cryptogram의 길이보다 작게
             {
-
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) // 인접한 문자와 중복문자인지 검사
+                {
+                    duplicate.deleteCharAt(i); //연속된 중복문자 제거를 위해 두번 제거
+                    duplicate.deleteCharAt(i);
+                    String res = duplicate.toString(); //우선 제거된 문자열을 갖고 중복 문자열이 나오지 않을 때까지 재귀함수를 돌기 위해 for문 내부에서 선언
+                    return solution(res);
+                }
             }
         }
         return duplicate.toString();
