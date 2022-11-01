24ebcaf32f313beb48056d1ee804cfbde4c9f59b
1eejisoo <jisu991012@gmail.com>
2022-11-01 16:38:04 +0900

    [Problem 3] Style: 주석 삭제

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,13 +1,6 @@
 package onboarding;
 
 public class Problem3 {
-    /***
-     *
-     * 기능1. 1 ~ number까지의 수 나열을 문자열로 만드는 기능 -> numberSequenceToString()
-     * 기능2. 기능2의 합을 구하는 기능 -> getClapCount()
-     *
-     * 기능2 구현을 위한 함수 - 문자열에서 특정 문자의 횟수를 세는 함수 -> count()
-     */
 
     public static int solution(int number) {
         int answer = 0;
@@ -19,7 +12,6 @@ public class Problem3 {
         return answer;
     }
 
-    // 기능 1
     public static String numberSequenceToString(int number) {
         StringBuilder sb = new StringBuilder();
 
@@ -28,7 +20,6 @@ public class Problem3 {
         return sb.toString();
     }
 
-    // 기능 2
     public static int getClapCount(String routine) {
         return count(routine, '3') + count(routine, '6') + count(routine, '9');
     }
@@ -36,4 +27,5 @@ public class Problem3 {
     public static int count (String str, char ch) {
         return str.length() - str.replace(String.valueOf(ch), "").length();
     }
+
 }
