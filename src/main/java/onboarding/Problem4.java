f235c7052476054d2f476337d4b79ed4d0993597
hgo641 <hgo641@gmail.com>
2022-11-01 19:52:49 +0900

    feat: Problem4 문자를 청개구리 언어로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,23 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /**
+     * 문자를 청개구리 언어로 변환
+     * @param target 청개구리 언어로 변환할 문자
+     * @return 청개구리 언어로 변환된 문자
+     */
+    public static Character convertToFrogChar(Character target){
+        Character frogChar = null;
+        if(!Character.isAlphabetic(target)){
+            frogChar = target;
+        }
+        if(Character.isUpperCase(target)){
+            frogChar = (char)('Z' - target + 'A');
+        }
+        if(Character.isLowerCase(target)){
+            frogChar = (char)('z' - target + 'a');
+        }
+        return frogChar;
+    }
 }
