c7f088c9ea7f81ba6d2b6dde5ad82355fee6813f
Dasol Jung <greteljds0251@gmail.com>
2022-11-01 17:21:14 +0900

    feat: 5. Count for currency return

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,44 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] answer = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        while (money != 0) {
+            if (money >= 50000) {
+                money -= 50000;
+                answer[0]++;
+            } else if (money >= 10000) {
+                money -= 10000;
+                answer[1]++;
+            } else if (money >= 5000) {
+                money -= 5000;
+                answer[2]++;
+            } else if (money >= 1000) {
+                money -= 1000;
+                answer[3]++;
+            } else if (money >= 500) {
+                money -= 500;
+                answer[4]++;
+            } else if (money >= 100) {
+                money -= 100;
+                answer[5]++;
+            } else if (money >= 50) {
+                money -= 50;
+                answer[6]++;
+            } else if (money >= 10) {
+                money -= 10;
+                answer[7]++;
+            } else if (money >= 1) {
+                money -= 1;
+                answer[8]++;
+            }
+        }
+
+        List<Integer> count = Arrays.stream(answer).boxed().collect(Collectors.toList());
+        return count;
     }
 }
