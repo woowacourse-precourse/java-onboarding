3077b4075e96b26954079078ac3dd714604dfe13
JH22222 <plzgiveme45@gmail.com>
2022-11-01 23:19:09 +0900

    finish

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> notfriends = new ArrayList<>();            // user와 친구가 아닌 사람들
-        List<String> yesfriends = new ArrayList<>();            // user와 친구인 사람들
+        List<String> notfriends = new ArrayList<>();
+        List<String> yesfriends = new ArrayList<>();
         for(int i = 0; i < friends.size(); i++) {
             if(friends.get(i).get(1) == user) {
-                yesfriends.add(friends.get(i).get(0));          // 친구를 찾아 넣음
+                yesfriends.add(friends.get(i).get(0));
             }
             else {
                 notfriends.add(friends.get(i).get(0));
@@ -21,38 +21,42 @@ public class Problem7 {
             if(friends.get(i).get(1) != user)
                 notfriends.add(friends.get(i).get(1));
         }
-        for(int i = 0; i < visitors.size(); i++) {                  // visitors에서 친구가 아닌 사람들을 넣음
+        for(int i = 0; i < visitors.size(); i++) {
             if(!notfriends.contains(visitors.get(i)) && !yesfriends.contains(visitors.get(i)))
                 notfriends.add(visitors.get(i));
         }
-        List<String> f  = yesfriends.stream().distinct().collect(Collectors.toList());      // 친구 리스트 중복 제거
-        List<String> nf = notfriends.stream().distinct().collect(Collectors.toList());      // 친구가 아닌 사람들 중복 제거
-        f.remove(user);                         // user 제거
-        nf.remove(user);                        // user 제거
+        List<String> f  = yesfriends.stream().distinct().collect(Collectors.toList());
+        List<String> nf = notfriends.stream().distinct().collect(Collectors.toList());
+        f.remove(user);
+        nf.remove(user);
+
         for(int i = 0; i < f.size(); i++) {
             nf.remove(f.get(i));
         }
 
-        int[] score = new int[nf.size()];                           // not friends의 점수 배열. nf의 점수 배열과 순서 일치함
+        int[] score = new int[nf.size()];
         for(int i = 0; i < nf.size(); i++) {
             String p = nf.get(i);
             for(int j = 0; j < friends.size(); j++) {
-                if(p.equals(friends.get(j).get(0)) || p.equals(friends.get(j).get(1))){     // 친구의 친구일때
+                if(p.equals(friends.get(j).get(0)) || p.equals(friends.get(j).get(1))){
                     for(int k = 0; k < f.size(); k++) {
-                        if(friends.get(j).get(0).equals(f.get(i)) ||friends.get(j).get(1).equals(f.get(i)) )        // 친구의 친구일때 점수 부여
+                        if(friends.get(j).get(0).equals(f.get(i)) ||friends.get(j).get(1).equals(f.get(i)) )
                             score[i] += 10;
                     }
                 }
             }
         }
+
         for(int i = 0; i < f.size(); i++) {
-            visitors.remove(f.get(i));
-        }
-        for(int i = 0; i < visitors.size(); i++) {
-            score[nf.indexOf(visitors.get(i))]++;
+            nf.remove(f.get(i));
         }
 
-        for(int i = 0; i < score.length; i++) {                 // 버블 정렬을 사용해 점수대로 정렬
+        for(int i = 0; i < visitors.size(); i++) {
+            if(nf.indexOf(visitors.get(i)) >= 0)
+                score[nf.indexOf(visitors.get(i))]++;
+        }
+
+        for(int i = 0; i < score.length; i++) {
             for(int j = 0; j < score.length - 1; j++) {
                 if(score[j] < score[j + 1]) {
                     int tmp = score[j];
