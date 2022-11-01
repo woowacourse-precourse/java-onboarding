24e4e2f415b613fd127a3a7cb629942cd8c89747
yeada <yeadalm@gmail.com>
2022-11-01 22:23:40 +0900

    upload problem7 함께 아는 친구 점수 +10

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -33,4 +33,30 @@ public class Problem7 {
         return result;
     }
 
+    //friends 중 함께 아는 친구 구하고 +10 계산하기
+    public static HashMap<String, Integer> featuredFriends(String user,List<List<String>>friends, Set<String>trueFriends){
+        String[] trueList = trueFriends.toArray(new String[trueFriends.size()]);
+        List<String> friendsAndNot = new ArrayList<String>();
+
+        //user가 들어간 list 제외하고, 친구가 포함된 list만 저장
+        for(List<String>friend : friends){
+            if(user.equals(friend.get(0)) || user.equals(friend.get(1))) continue;
+            else for(int i=0; i<trueList.length; i++){
+                if(trueList[i].equals(friend.get(0))){
+                    friendsAndNot.add(friend.get(1));
+                } else if(trueList[i].equals(friend.get(1))){
+                    friendsAndNot.add(friend.get(0));
+                }
+            }
+        }
+
+        //점수 계산하기
+        List<String> trueFriendList = new ArrayList<>(trueFriends);
+        // 방문자 리스트 중 친구 제외
+        List<String> recommendList = new ArrayList<>();
+        recommendList.addAll(friendsAndNot);
+        recommendList.removeAll(trueFriendList);
+        HashMap<String, Integer> result = scoreCal(recommendList, 10);
+        return result;
+    }
 }
