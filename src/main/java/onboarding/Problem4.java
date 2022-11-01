7a90f4f4fa3a66c4e9b8120c30b1a37336a433c9
hyejuc <chjjudy@naver.com>
2022-11-01 15:43:12 +0900

    isAlphabet - 해당 문자가 알파벳인지 확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (!isAlphabet(word.charAt(i))){
+                answer += word.charAt(i);
+            }
+        }
         return answer;
     }
+    public static boolean isAlphabet(char letter){
+        if (letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z'){
+            return true;
+        }
+        return false;
+    }
+
 }
