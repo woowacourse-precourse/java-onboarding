9e068623571ada7ef2442b243cc1a149c4ce4d5c
Nugur1i <duckhee092@naver.com>
2022-11-01 16:13:46 +0900

    feat<problem7> : Add for loop to find USER INDEX

---
+++ src/main/java/Problem7Debugging.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem7Debugging {
+
+    public static void main(String[] args) {
+
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        List<String> result = List.of("andole", "jun", "bedi");
+
+        List<String> friendsOfUser = new ArrayList<>();
+
+
+        for (int i =0 ; i < friends.size() ; i++) {
+            int userIndex = friends.get(i).indexOf(user);
+
+            int friendIndex = 1-userIndex;
+
+
+
+
+        }
+
+    }
+}
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        for (int i =0 ; i < friends.size() ; i++) {
+            int userIndex = friends.get(i).indexOf(user);
+
+
+        }
+
+
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
