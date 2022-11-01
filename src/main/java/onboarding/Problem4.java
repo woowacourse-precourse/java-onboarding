3d5efe2c97a06f86cd529891f9e9fc8e1c04ffbe
이소영 <th42500@naver.com>
2022-11-01 21:10:31 +0900

    [Add] 청개구리 규칙에 맞게 알파벳 바꾸기 구현
    1. greenFlogRule (String word) 👉 입력받은 단어를 한 글자씩 뽑기
    2. changeAlphabet(char alphabet) 👉 매개변수로 넘어온 알파벳을 대문자 또는 소문자에 맞게 변환하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
+    public char changeAlphabet(char alphabet) {
+        if('A' <= alphabet &&  alphabet < 'a') {  // 대문자
+             alphabet = (char)('Z' + (char)('A' - alphabet));
+        } else {
+             alphabet = (char)('z' + (char)('a' - alphabet));
+        }
+
+        return alphabet;
+    }
+
+    private String greenFlogRule(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(changeAlphabet(word.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+
     public static String solution(String word) {
         String answer = "";
+
+        Problem4 main = new Problem4();
+        answer = main.greenFlogRule(word);
+
         return answer;
     }
 }
