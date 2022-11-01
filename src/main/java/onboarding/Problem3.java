5207365b635650f4d3cd78757301d536b98df742
lynne921 <dkfls921@gmail.com>
2022-11-01 18:41:44 +0900

    [feat][problem3] 3,6,9 포함 숫자의 3,6,9개수 찾는 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    private static int check369 (int number) {
+        int tmp = 0;
+        while (number > 0) {
+            int mod = number % 10;
+            if (mod == 3 || mod == 6 || mod == 9)
+                tmp++;
+            number /= 10;
+        }
+        return tmp;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += check369(i);
+        }
         return answer;
     }
 }
