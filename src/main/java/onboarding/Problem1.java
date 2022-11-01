595bdd1dfbc0707a35f0ae4036c608bc363d3dbe
오승윤 <>
2022-11-01 14:59:41 +0900

    추가 : Problem1 입력받은 페이지의 각 자릿수의 곱을 계산하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    
+    private  Integer calculateProductOfPlaceValue(Integer num){
+        String[] placeValuesOfNum = String.valueOf(num).split("");
+        Integer product = 1;
+
+        for(String placeValue : placeValuesOfNum){
+            product*=Integer.parseInt(placeValue);
+        }
+
+        return product;
+    }
 }
