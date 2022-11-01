4a61b47ba2eb5f5dae906badeafe54b6d9be75c1
si hwan <pum005@naver.com>
2022-11-01 22:38:27 +0900

    feat (problem6) : 입력 forms 을 예외처리 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import ExceptionValidation.P6ExceptionValidation;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Collection.*;
+import static java.util.Collections.sort;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+
+        for(List<String> form: forms){
+            String email = form.get(0);
+            String userName = form.get(1);
+            if(!(P6ExceptionValidation.p6ExceptionValidation(email, userName))){
+                return Collections.emptyList();
+            }
+        }
     }
+
 }
+
