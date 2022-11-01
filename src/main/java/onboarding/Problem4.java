71421e0797f38adb7a96c7a6c4b2eb8d305510af
Lee-SungMin <saevers26@gmail.com>
2022-11-01 20:35:27 +0900

    Add: 4번 문제 기능 구현 - 소문자인 경우 변환하는 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,10 @@ public class Problem4 {
     public static Integer changeUpper(Character word){
         return 'Z' - word + 'A';
     }
+    // 소문자일 경우
+    public static Integer changeLower(Character word){
+        return 'z' - word + 'a';
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
