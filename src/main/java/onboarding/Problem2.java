2e6d4936fcf07985c65e4e12790adf3f073341c1
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-01 23:19:21 +0900

    solve: Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,18 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static String findDuplicate(String cryptogram) {
+        String answer = cryptogram;
+        for(int i = 0; i < cryptogram.length() - 1; i++) {
+            if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                answer = cryptogram.substring(0, i) + cryptogram.substring(i+2);
+                break;
+            }
+        }
+        return answer;
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        char[] arr = new char[cryptogram.length()];
+        String answer = cryptogram;
 
-        for(int i = 0; i < cryptogram.length(); i++){
-            arr[i] = cryptogram.charAt(i);
+        while(!findDuplicate(answer).equals(answer)) {
+            answer = findDuplicate(answer);
         }
-
-        for(int i = 0; i < arr.length; i++){
-            
-        }
-
         return answer;
     }
 }
