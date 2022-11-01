6e059cc41fb132ec134289a3168942502d1c3e41
hyejuc <chjjudy@naver.com>
2022-11-01 14:58:15 +0900

    countThreeSixNine - 해당 숫자의 각 자릿수에 있는 3, 6, 9의 총 개수 계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 0; i < number + 1; i++) {
+            answer += countThreeSixNine(i);
+        }
         return answer;
+    }
+    public static int countThreeSixNine(int number){
+        int count = 0;
+        while (number != 0){
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9){
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
