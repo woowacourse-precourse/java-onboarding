d8afbc7f63342ebeff0fc6215de17fe18d03d059
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-01 14:54:44 +0900

    [Problem1] 가장 큰 수 반환 메서드 maxNum 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    // 가장 큰 수 반환 메서드
+    public static int maxNum(String numStr) {
+        int sumAdd = 0;
+        int sumProduct = 1;
+
+        for (String num : numStr.split("")) {
+            sumAdd += Integer.parseInt(num);
+        }
+
+        for (String num : numStr.split("")) {
+            sumProduct *= Integer.parseInt(num);
+        }
+        return Math.max(sumAdd, sumProduct);
+    }
 }
