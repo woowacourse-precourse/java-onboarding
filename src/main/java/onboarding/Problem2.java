11e3f9cbf0f28e90fddd2ff24aae1f2bc6f93005
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:36:54 +0900

    String -> StringBuilder

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,6 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) { // 중복된 문자를 찾아서 자신을 두번 제거하는 방식으로
-        String answer = "answer";
-        return answer;
+        StringBuilder duplicate = new StringBuilder(cryptogram); // 조작이 불가능한 String을 대신해서 사용 deleteCharAt 메서드 사용하기 위해서
     }
 }
