9c45affa51411f0e5894c4b1a8e5c990a51ec020
eunseo <qnrqjrm03@naver.com>
2022-11-01 21:32:43 +0900

    feat : problem2 - 기능2 메서드 추가, 기능1 메서드 수정, 기능목록 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,14 +2,18 @@ package onboarding;
 
 /**
  * 기능목록
- * 1. 중복된 문자 유,무를 찾는 기능
+ * 1. 중복된 문자의 인덱스를 기능
  * 2. 1에서 중복된 문자가 있을 시 해당 문자를 삭제하는 기능
  */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        while (findDuplication(cryptogram)) {
-
+        String answer = cryptogram;
+        while (true) {
+            int duplicationNum = findDuplication(answer);
+            if (duplicationNum == -1) {
+                break;
+            }
+            answer = removeDuplication(duplicationNum, answer);
         }
         return answer;
     }
@@ -17,14 +21,25 @@ public class Problem2 {
     /**
      * 기능 1
      * @param check
-     * @return 중복된 문자가 있으면 true, 없으면 false
+     * @return 중복된 문자가 있으면 중복시작 인덱스, 없으면 -1
      */
-    private static boolean findDuplication(String check) {
-        for (int i = 0; i < check.length(); i--) {
+    private static int findDuplication(String check) {
+        for (int i = 0; i < check.length()-1; i++) {
             if (check.charAt(i) == check.charAt(i + 1)) {
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    /**
+     * 기능 2
+     * @param duplicationNum
+     * @param check
+     * @return duplicationNum 번째와 duplictionNum+1 번째 문자를 제거한 문자열
+     */
+    private static String removeDuplication(int duplicationNum, String check) {
+        StringBuffer originalString = new StringBuffer(check);
+        return originalString.replace( duplicationNum ,duplicationNum+2 ,"").toString();
     }
 }
