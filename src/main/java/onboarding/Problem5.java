8ed00bb102688a95c29da86d88f52990ee325830
dingding-21 <phj8221@naver.com>
2022-11-01 22:56:29 +0900

    feat: Problem5 주어진 돈을 화폐로 변환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,21 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<Integer> getCoinNum(int money) {
+
+        int[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int quotient;
+
+        Integer[] number = new Integer[9];
+
+        for (int i = 0; i < coin.length; i++) {
+            quotient = money / coin[i];
+            number[i] = quotient;
+            money -= quotient * coin[i];
+        }
+
+        return Arrays.asList(number);
     }
 }
