b8c5a990de7d0a9718354e7efc48846a831454e6
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-01 11:43:44 +0900

    feat : P1 - 페이지 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import javax.xml.validation.Validator;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (validateNum(pobi) == -1 || validateNum(crong) == -1)
+            return -1;
+
         return answer;
+    }
+    // 페이지가 맞으면 1, 아니면 -1
+    private static int validateNum(List<Integer> lst) {
+        if (lst.get(0) < 1 || lst.get(1) > 400)
+            return -1;
+        if (lst.get(0) % 2 == 0 || lst.get(1) % 2 == 1)
+            return  -1;
+        if (lst.get(0) + 1 != lst.get(1))
+            return  -1;
+
+        return 1;
     }
 }
