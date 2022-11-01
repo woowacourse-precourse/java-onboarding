485f5473ca59dfba834adc364c307e07e779f3ce
Daeeun Ko <7273322@naver.com>
2022-11-01 13:42:10 +0900

    feat: validate if number is out of bounds

--- src/main/java/onboarding/problem3/Number.java
+++ src/main/java/onboarding/problem3/Number.java
@@ -1,3 +1,22 @@
 package onboarding.problem3;
+
 public class Number {
+
+    private final int MIN_NUMBER = 1;
+    private final int MAX_NUMBER = 10_000;
+
+    private int number;
+
+    public Number(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    private void validateNumber(int number) {
+        if (isOutOfBounds(number)) throw new IllegalArgumentException("number is out of bounds");
+    }
+
+    private boolean isOutOfBounds(int number) {
+        return number < MIN_NUMBER || number > MAX_NUMBER;
+    }
 }
