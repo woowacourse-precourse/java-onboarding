b88c7d2ef2be0e5e4677e43e2443cb9931b35018
brotherstone97 <brotherstone97@gmail.com>
2022-11-01 23:23:59 +0900

    Refactor: problem7 변수, 함수명 및 filterScore메서드 리팩토링

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,94 +3,93 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
-    static List<String> findFriends(String user, List<List<String>> friends) {
-        List<String> alreadyFriends = new ArrayList<>();
-        for (List<String> relationship : friends) {
+    static List<String> findFriends(String user, List<List<String>> relationships) {
+        List<String> currentFriends = new ArrayList<>();
+        for (List<String> relationship : relationships) {
             if (!relationship.get(0).equals(user) && !relationship.get(1).equals(user))
                 continue;
             if (relationship.get(0).equals(user)) {
-                alreadyFriends.add(relationship.get(1));
+                currentFriends.add(relationship.get(1));
                 continue;
             }
             if (relationship.get(1).equals(user)) {
-                alreadyFriends.add(relationship.get(0));
+                currentFriends.add(relationship.get(0));
             }
         }
-        return alreadyFriends;
+        return currentFriends;
     }
 
-    static HashMap<String, Integer> calcFriendsScore(String user, List<String> alreadyFriends, List<List<String>> friends) {
-        HashMap<String, Integer> score = new HashMap<>();
-        for (List<String> relationship : friends) {
-            for (String alreadyFriend : alreadyFriends) {
-                if (relationship.get(0).equals(alreadyFriend)) {
-                    if (!relationship.get(1).equals(user)) {
-                        if (score.containsKey(relationship.get(1))) {
-                            score.put(relationship.get(1), score.get(relationship.get(1)) + 10);
-                            continue;
-                        }
-                        score.put(relationship.get(1), 10);
+    static HashMap<String, Integer> calcFriendsScore(String user, List<String> currentFriends, List<List<String>> relationships) {
+        HashMap<String, Integer> recommendationScoreMap = new HashMap<>();
+        for (List<String> relationship : relationships) {
+            for (String currentFriend : currentFriends) {
+                if (relationship.get(0).equals(currentFriend) && !relationship.get(1).equals(user)) {
+                    if (recommendationScoreMap.containsKey(relationship.get(1))) {
+                        recommendationScoreMap.put(relationship.get(1), recommendationScoreMap.get(relationship.get(1)) + 10);
+                        continue;
                     }
+                    recommendationScoreMap.put(relationship.get(1), 10);
                 }
-                if (relationship.get(1).equals(alreadyFriend)) {
-                    if (!relationship.get(0).equals(user)) {
-                        if (score.containsKey(relationship.get(0))) {
-                            score.put(relationship.get(0), score.get(relationship.get(0)) + 10);
-                            continue;
-                        }
-                        score.put(relationship.get(0), 10);
+                if (relationship.get(1).equals(currentFriend) && !relationship.get(0).equals(user)) {
+                    if (recommendationScoreMap.containsKey(relationship.get(0))) {
+                        recommendationScoreMap.put(relationship.get(0), recommendationScoreMap.get(relationship.get(0)) + 10);
+                        continue;
                     }
+                    recommendationScoreMap.put(relationship.get(0), 10);
                 }
             }
         }
-        return score;
+        return recommendationScoreMap;
     }
 
-    static HashMap<String, Integer> calcVisitScore(List<String> alreadyFriends, List<String> visitors, HashMap<String, Integer> score) {
+    static HashMap<String, Integer> calcVisitScore(List<String> currentFriends, List<String> visitors, HashMap<String, Integer> scoreMap) {
         for (String visitor : visitors) {
-            if (!alreadyFriends.contains(visitor)) {
-                if (score.containsKey(visitor)) {
-                    score.put(visitor, score.get(visitor) + 1);
+            if (!currentFriends.contains(visitor)) {
+                if (scoreMap.containsKey(visitor)) {
+                    scoreMap.put(visitor, scoreMap.get(visitor) + 1);
                     continue;
                 }
-                score.put(visitor, 1);
+                scoreMap.put(visitor, 1);
             }
         }
-        return score;
+        return scoreMap;
     }
 
-    static List<String> filterScore(HashMap<String, Integer> score) {
-        List<String> keys = new ArrayList<String>();
-        List<Integer> values = new ArrayList<Integer>();
-        for (String k : score.keySet()) {
-            if (score.get(k) != 0) {
-                keys.add(k);
-                values.add(score.get(k));
+    static List<String> filterScore(HashMap<String, Integer> scoreMap) {
+        List<String> names = new ArrayList<String>();
+        List<Integer> scores = new ArrayList<Integer>();
+        for (String k : scoreMap.keySet()) {
+            if (scoreMap.get(k) != 0) {
+                names.add(k);
+                scores.add(scoreMap.get(k));
             }
         }
-        //점수순 정렬
-        for (int i = 0; i < keys.size(); i++) {
-            for (int j = i + 1; j < keys.size(); j++) {
-                if (values.get(i) < values.get(j)) {
-                    Collections.swap(values, i, j);
-                    Collections.swap(keys, i, j);
+        List<String> sortedNames = sortNames(names, scores);
+        if (sortedNames.size() > 5)
+            return (sortedNames.subList(0, 5));
+        return sortedNames;
+    }
+
+    static List<String> sortNames(List<String> names, List<Integer> scores){
+        for (int i = 0; i < names.size(); i++) {
+            for (int j = i + 1; j < names.size(); j++) {
+                if (scores.get(i) < scores.get(j)) {
+                    Collections.swap(scores, i, j);
+                    Collections.swap(names, i, j);
                 }
-                if (values.get(i) == values.get(j)) {
-                    //이름순 정렬
-                    if (keys.get(i).compareTo(keys.get(j))>0){
-                        Collections.swap(keys, i, j);
+                if (scores.get(i) == scores.get(j)) {
+                    if (names.get(i).compareTo(names.get(j)) > 0) {
+                        Collections.swap(names, i, j);
                     }
                 }
             }
         }
-        if (keys.size() > 5)
-            return (keys.subList(0, 5));
-        return keys;
+        return names;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> currentFriends = findFriends(user, friends);
-        HashMap<String, Integer> recommendationScore = calcVisitScore(currentFriends, visitors, calcFriendsScore(user, currentFriends, friends));
-        return filterScore(recommendationScore);
+        HashMap<String, Integer> recommendationScoreMap = calcVisitScore(currentFriends, visitors, calcFriendsScore(user, currentFriends, friends));
+        return filterScore(recommendationScoreMap);
     }
 }
