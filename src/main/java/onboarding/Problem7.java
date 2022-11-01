d4c837b609fb603930d696d93119cca4c7955636
DongWooE <ehddn977@naver.com>
2022-11-01 20:02:52 +0900

    [p7][add] friend들을 graph에 추가해주는 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -17,6 +17,19 @@ public class Problem7 {
         for(List<String> list : friends){
             String friend1 = list.get(0);
             String friend2 = list.get(1);
+            addFriendToGraph(friend1, friend2);
+            addFriendToGraph(friend2, friend1);
+        }
+    }
+
+    private void addFriendToGraph(String friend1, String friend2){
+        if(friendGraph.containsKey(friend1)){
+            friendGraph.get(friend1).add(friend2);
+        }
+        else{
+            ArrayList<String> list1 = new ArrayList<>();
+            list1.add(friend2);
+            friendGraph.put(friend1, list1);
         }
     }
 }
