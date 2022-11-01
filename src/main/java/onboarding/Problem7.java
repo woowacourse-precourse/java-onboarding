91866eae881c41e7c936552de893517d38c332b0
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-01 11:03:26 +0900

    feat : [problem7] calculateRecommendedFriendScore()메소드 작성 calculateRecommendedFriendScore() : 친구추천 알고리즘에 따라 유저별 점수를 계산해주는 함수 이미 친구인 경우는 추천하지 않는다. 나 자신인 경우 점수 계산하지 않는다. UserFriends에 있는 사람과 친구인 경우 10점 추가 visitors일 때 1점 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> userFriends = makeUserFriends(user, friends);
+        Map<String, Integer> recommendedFriendScore = calculateRecommendedFriendScore(userFriends, user, friends, visitors);
 
         List<String> answer = Collections.emptyList();
         return answer;
@@ -26,5 +27,63 @@ public class Problem7 {
         }
 
         return userFriends;
+    }
+
+    public static Map<String, Integer> calculateRecommendedFriendScore(List<String> userFriends, String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, Integer> recommendedFriendScore = new HashMap<>();
+
+        // 함께 하는 친구 10점 더하기
+        for (List<String> friend : friends) {
+            // user친구와 아는 사이일 때
+            // 그 친구가 나 이면 안된다.
+            // 이미 친구사이면 추천하지 않는다.
+            if (userFriends.contains(friend.get(0)) && friend.get(1).equals(user) == false && userFriends.contains(friend.get(1)) == false) {
+                //이미 있는 친구면 10점 더하기
+                if (recommendedFriendScore.containsKey(friend.get(1)) == true) {
+                    recommendedFriendScore.put(friend.get(1), recommendedFriendScore.get(friend.get(1)) + 10);
+                }
+
+                //없는 친구면 10점으로 저장
+                else {
+                    recommendedFriendScore.put(friend.get(1), 10);
+                }
+            } else if (userFriends.contains(friend.get(1)) && friend.get(0).equals(user) == false && userFriends.contains(friend.get(0)) == false) {
+                //이미 있는 친구면 10점 더하기
+                if (recommendedFriendScore.containsKey(friend.get(0)) == true) {
+                    recommendedFriendScore.put(friend.get(0), recommendedFriendScore.get(friend.get(0)) + 10);
+                }
+
+                //없는 친구면 10점으로 저장
+                else {
+                    recommendedFriendScore.put(friend.get(0), 10);
+                }
+            }
+        }
+
+        for (String visitor : visitors) {
+
+            // 이미 내 친구들인 경우 추천하지 않는다.
+            if (userFriends.contains(visitor)) {
+                continue;
+            }
+
+            // 내가 나를 방문한 경우에도 추천하지 않는다.
+            if (user.equals(visitor)) {
+                continue;
+            }
+
+            //이미 있는 친구면 1점 더하기
+            if (recommendedFriendScore.containsKey(visitor) == true) {
+                recommendedFriendScore.put(visitor, recommendedFriendScore.get(visitor) + 1);
+            }
+
+            //없는 친구면 1점으로 저장
+            else {
+                recommendedFriendScore.put(visitor, 1);
+            }
+
+        }
+
+        return recommendedFriendScore;
     }
 }
