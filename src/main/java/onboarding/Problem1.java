1a1b1bce037be7d4b4881331ea28d2b58fe4489a
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 20:54:04 +0900

    페이지 한자리 씩 나누는 기능 추가.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -22,8 +22,27 @@ class Problem1 {
         }
     }
 
+    public static ArrayList<Integer> convertPage(int page) {
+        String convertedpage = Integer.toString(page);
+        ArrayList<Integer> pagelist = new ArrayList<>();
+        for (int i = 0; i < convertedpage.length(); i++) {
+            pagelist.add(Integer.parseInt(String.valueOf(convertedpage.charAt(i))));
+        }
+        return pagelist;
+
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        ArrayList<ArrayList<Integer>> pobipagelistlist = new ArrayList<>();
+        pobipagelistlist.add(convertPage(pobi.get(0)));
+        pobipagelistlist.add(convertPage(pobi.get(1)));
+
+        ArrayList<ArrayList<Integer>> crongpagelistlist = new ArrayList<>();
+        crongpagelistlist.add(convertPage(crong.get(0)));
+        crongpagelistlist.add(convertPage(crong.get(1)));
+
         return answer;
 
     }
