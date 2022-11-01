25636acf536572f26049de9f71ab19c6556070ea
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-01 17:58:15 +0900

    feat(PROBLEM3): solution 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
+
 package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int totalClap = 0;
+        for (int i=1 ; i<=number ; i++){
+            totalClap += getClapCount(i);
+        }
+        return totalClap;
     }
 }
+
