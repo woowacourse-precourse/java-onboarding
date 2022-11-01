4d8edd1461d34de609b92bfd5c081281455d52c9
aran <aran0317@naver.com>
2022-11-01 21:01:32 +0900

    문제 2번 기능 요구사항

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/*기능 요구 사항*/
+/* 1. 연속하는 중복 문자들을 삭제한 결과를 return
+<solution>
+ * - 반복문을 통해 중복되는 문자가 없을 때까지 반복하여 return
+ * - 중복되는 문자가 있는지 확인하는 함수 사용 : check()
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
