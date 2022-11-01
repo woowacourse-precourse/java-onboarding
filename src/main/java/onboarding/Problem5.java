46df8fa82203078e69a5bed4cc9fabd15a6eaf4a
김형기 <khk211113@naver.com>
2022-11-01 23:26:08 +0900

    [FEAT] Problem5 - 결과 리스트 변환 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
@@ -8,8 +8,10 @@ public class Problem5 {
     private static final int[] MONEY_SIZE = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
 
     public static List<Integer> solution(int money) {
-
+        // 배열 초기화
         Integer[] result = new Integer[9];
+        for (int i = 0; i < result.length; i++)
+            result[i] = 0;
         int idx = 0;
 
         // idx가 더 이상 움직일 수 없거나, money가 0이 될 때까지 반복
@@ -23,5 +25,6 @@ public class Problem5 {
             }
         }
 
+        return Arrays.asList(result);
     }
 }
