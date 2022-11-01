9c0c5e156ebd165a9a84be049ef506c8ddd99d6f
소대훈 <theogns0681@naver.com>
2022-11-01 10:32:42 +0900

    [refactor] (문제6) 메소드 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 public class Problem6 {
     static boolean[] DuplicateName;
+    static List<List<String>> formsT;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
+        formsT = forms;
         DuplicateName = new boolean[forms.size()];
-        findDuplicate(forms);
-        AddEmail(forms, answer);
+        findDuplicate();
+        AddEmail(answer);
         Collections.sort(answer);
         return answer;
     }
@@ -20,37 +22,48 @@ public class Problem6 {
         문제의 조건에서 2개의 순차적인 문자만 같아도 해당되기때문에
         3,4개는 비교하지 않고 있습니다.
      */
-    private static void findDuplicate(List<List<String>> forms) {
-        StringBuilder sb;
-        for (int i = 0; i < forms.size() - 1 ; i++){
-            for (int j = 0; j < forms.get(i).get(1).length()-1 ; j++){
-                sb = new StringBuilder();
-                sb.append(forms.get(i).get(1).charAt(j)).
-                        append(forms.get(i).get(1).charAt(j+1));
-                compare(forms, sb, i);
-            }
+    private static void findDuplicate() {
+        for (int i = 0; i < formsT.size() - 1 ; i++){
+            pushWord(formsT.get(i),i);
         }
     }
+
+    private static void pushWord(List<String> form,int index) {
+        for (int j = 0; j < form.get(1).length()-1 ; j++){
+            String temp = form.get(1).substring(j,j+2);
+            compare(temp, index);
+        }
+    }
+
     /*
     넘겨받은 문자 2개가 다른 이름에 포함이 된다면
     넘겨준 이름과 비교된 이름이 해당되어 true처리합니다.
      */
-    private static void compare(List<List<String>> forms, StringBuilder sb, int j) {
-        for (int i = j + 1; i < forms.size() ; i++){
-            if (forms.get(i).get(1).contains(sb.toString())){
+    private static void compare(String temp, int j) {
+        for (int i = j + 1; i < formsT.size() ; i++){
+            if(processVisit(temp ,formsT.get(i))){
                 DuplicateName[i] = true;
-                DuplicateName[i] = true;
+                DuplicateName[j] = true;
             }
         }
     }
+
+    private static boolean processVisit(String temp, List<String> form) {
+        return form.get(1).contains(temp);
+    }
+
     /*
     넘겨받은 문제에서 조건을 만족하고 해당되는 이름을 answer리스트에 넣는 메소드
      */
-    private static void AddEmail(List<List<String>> forms, List<String> answer) {
+    private static void AddEmail(List<String> answer) {
         for (int i = 0; i < DuplicateName.length ; i++){
-            if (DuplicateName[i]){
-                answer.add(forms.get(i).get(0));
-            }
+            AddVisitEmail(answer, i);
+        }
+    }
+
+    private static void AddVisitEmail(List<String> answer, int i) {
+        if (DuplicateName[i]){
+            answer.add(formsT.get(i).get(0));
         }
     }
 }
