c90e8c16b665acf8eccef9ee4d520ee6ebad2dbc
BoyCho <iijokv@naver.com>
2022-11-01 12:42:15 +0900

    feat : 문제3. 손벽 횟수 카운팅 기능 및 풀이 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            int n = i;
+            while(isNotZero(n)){
+                if(Clap(n))
+                    answer++;
+                n /= 10;
+            }
+        }
         return answer;
     }
+    static boolean isNotZero (int n) { return n != 0; }
+    static boolean Clap (int n) { return n%10 == 3 || n%10 == 6 || n%10 == 9; }
 }
