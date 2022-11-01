999c18f5c72bf2117ae852ccfdb136146eb29d75
hwi215 <buc06137@naver.com>
2022-11-01 20:32:38 +0900

    docs: Problem6 기능 목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,12 +3,21 @@ package onboarding;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * [기능 목록]
+ * 1. 닉네임 중 같은 글자가 연속적으로 포함 되는 경우 찾는 기능
+ * 2. 결과 출력 기능
+ * - 신청받은 닉네임 중 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록 출력
+ * - 오름차순 정렬 및 중복 제거
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         HashSet<String> emails = new HashSet<>();
         Map<String, String> hash = new HashMap<>();
 
+        // 1. 닉네임 중 같은 글자가 연속적으로 포함 되는 경우 찾는 기능
         for (int i = 0; i < forms.size(); i++) {
             String name = forms.get(i).get(1);
             name = name.replaceAll("^\"|\"$", ""); // " 문자 제거
@@ -29,6 +38,8 @@ public class Problem6 {
             }
 
         }
+
+        //2. 결과 출력 기능
         answer = emails.stream().sorted().collect(Collectors.toList());
         //System.out.println("answer: " + answer);
         return answer;
