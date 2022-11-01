fc12d7b1f98c4f52b8b43e7b564c2dd3b7592b9b
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 18:33:36 +0900

    [Feat] convert 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+    int[] convert = {25,23,21,19,17,15,13,11,9,7,5,3,1,-1,-3,-5,-7,-9,-11,-13,-15,-17,-19,-21,-23,-25};
+    
     public static String solution(String word) {
-        String answer = "";
+        String answer;
+        char[] chars = word.toCharArray();
+
         return answer;
     }
 }
