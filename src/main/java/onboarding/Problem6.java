607f4f7468abfc901af408866e2f2c1b9b98ebd0
swa07016 <swa07016@khu.ac.kr>
2022-11-01 17:07:53 +0900

    Problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
+    public static List<String> updateDuplicateMember(int memberNum, String subStr, List<List<String>> forms, List<String> answer) {
+        for(int m = 0; m < forms.size(); m++) {
+            if(m == memberNum) continue;
+            String email = forms.get(m).get(0);
+            String nickName = forms.get(m).get(1);
+            if(nickName.contains(subStr) && !answer.contains(email)) {
+                answer.add(email);
+            }
+        }
+        return answer;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for(int m = 0; m < forms.size(); m++) {
+            String nickName = forms.get(m).get(1);
+            for(int i=2; i <= nickName.length(); i++) {
+                for(int j=0; j < nickName.length() - i; j++) {
+                    String subStr = nickName.substring(j, j + i);
+                    answer = updateDuplicateMember(m, subStr, forms, answer);
+                }
+            }
+        }
+        Collections.sort(answer);
         return answer;
     }
 }
