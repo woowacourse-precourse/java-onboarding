b4da7abddd44a3c62412ab9f315694620da11c19
신채원 <cw121@naver.com>
2022-11-01 15:02:25 +0900

    feat(problem4): 각 친구마다 친구목록을 만들어주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static final Map<String, List<String>> friendMap = new HashMap<String, List<String>>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void createFriendsList(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+            if (!friendMap.containsKey(user1)) {
+                friendMap.put(user1, new ArrayList<>());
+                friendMap.get(user1).add(user2);
+            }else{
+                friendMap.get(user1).add(user2);
+            }
+            if (!friendMap.containsKey(user2)) {
+                friendMap.put(user2, new ArrayList<>());
+                friendMap.get(user2).add(user1);
+            }else{
+                friendMap.get(user2).add(user1);
+            }
+        }
     }
 }
