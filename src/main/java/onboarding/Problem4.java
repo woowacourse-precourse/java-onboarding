dc54e9bb7c9da3860667f751d7c190fe329db79b
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2022-11-01 12:14:11 +0900

    to char[] && set ascii abs

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		String answer = "";
+		// String word -> char[] 형 변환 && ascii 번호와 반대 문자 전환을 위한 절댓값 변수 설정
+		char[] c = word.toCharArray();
+		int ascNum = 0;
+		int abs = 0;
+
+
+
+		return answer;
+	}
 }
