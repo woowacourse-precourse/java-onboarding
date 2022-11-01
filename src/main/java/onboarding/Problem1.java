a468ac3808133647b940fed25ca3aaa3ed0c50bd
shpark30 <seohyeonpark30.gmail.com>
2022-11-01 12:56:40 +0900

    refact: 점수 관리 객체
    페이지 점수 관리 객체 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class PageList {
     private List<Integer> pages;
@@ -36,6 +37,53 @@ class PageList {
     }
 }
 
+class ScoreList {
+    private final List<Integer> scores;
+
+    public ScoreList(PageList pages) {
+        this.scores = new ArrayList<>();
+        this.scores.addAll(mapToSum(pages.getPages())); // 자리수 숫자 합
+        this.scores.addAll(mapToMul(pages.getPages())); // 자리수 숫자 곱
+    }
+
+    public int maxScore() {
+        /* 최대 점수 반환 */
+        return Collections.max(this.scores);
+    }
+
+    private List<Integer> mapToSum(List<Integer> intList) {
+        return intList.stream()
+                .map(i -> sumNumberByDigit(i))
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> mapToMul(List<Integer> intList) {
+        return intList.stream()
+                .map(i -> mulNumberByDigit(i))
+                .collect(Collectors.toList());
+    }
+
+    private int sumNumberByDigit(int number) {
+        /* 정수 number 가 주어졌을 때 자리수(digit)별 숫자의 합을 return */
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;   // number 를 10으로 나눈 나머지(자리수 숫자)를 sum 에 더한다.
+            number /= 10;   // number 를 10으로 나눈 몫을 number 에 저장한다.
+        }
+        return sum;
+    }
+
+    private int mulNumberByDigit(int number) {
+        /* 숫자 number 가 주어졌을 때 자리수(digit)별 숫자의 곱을 return */
+        int mul = 1;
+        while (number != 0) {
+            mul *= number % 10;   // number 를 10으로 나눈 나머지(자리수 숫자)를 mul 에 곱한다.
+            number /= 10;   // number 를 10으로 나눈 볷을 number 에 저장한다.
+        }
+        return mul;
+    }
+}
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         /* 예외 사항 */
@@ -50,61 +98,16 @@ class Problem1 {
         }
 
         /* 점수 목록 */
-        List<Integer> scoresPobi = merge(mapToSum(pobiPages.getPages()), mapToMul(pobiPages.getPages()));
-        List<Integer> scoresCrong = merge(mapToSum(crongPages.getPages()), mapToMul(crongPages.getPages()));
-
-        /* 최대 점수 */
-        int maxPobi = Collections.max(scoresPobi);
-        int maxCrong = Collections.max(scoresCrong);
+        ScoreList scoresPobi = new ScoreList(pobiPages);
+        ScoreList scoresCrong = new ScoreList(crongPages);
 
         /* 결과 반환 */
-        if (maxPobi < maxCrong) {   // crong이 pobi보다 점수가 높으면
+        if (scoresPobi.maxScore() < scoresCrong.maxScore()) {   // crong이 pobi보다 점수가 높으면
             return 2;
         }
-        if (maxPobi > maxCrong) {   // pobi가 crong보다 점수가 높으면
+        if (scoresPobi.maxScore() > scoresCrong.maxScore()) {   // pobi가 crong보다 점수가 높으면
             return 1;
         }
         return 0;   // pobi와 crong의 점수가 같은 경우
     }
-
-    static int sumNumberByDigit(int number) {
-        /* 정수 number 가 주어졌을 때 자리수(digit)별 숫자의 합을 return */
-        int sum = 0;
-        while (number != 0) {
-            sum += number % 10;   // number 를 10으로 나눈 나머지(자리수 숫자)를 sum 에 더한다.
-            number /= 10;   // number 를 10으로 나눈 몫을 number 에 저장한다.
-        }
-        return sum;
-    }
-
-    static int mulNumberByDigit(int number) {
-        /* 숫자 number 가 주어졌을 때 자리수(digit)별 숫자의 곱을 return */
-        int mul = 1;
-        while (number != 0) {
-            mul *= number % 10;   // number 를 10으로 나눈 나머지(자리수 숫자)를 mul 에 곱한다.
-            number /= 10;   // number 를 10으로 나눈 볷을 number 에 저장한다.
-        }
-        return mul;
-    }
-
-    static List<Integer> mapToSum(List<Integer> intList) {
-        List<Integer> sumList = new ArrayList<>(intList);
-        sumList.replaceAll(i -> sumNumberByDigit(i));
-        return sumList;
-    }
-
-    static List<Integer> mapToMul(List<Integer> intList) {
-        List<Integer> mulList = new ArrayList<>(intList);
-        mulList.replaceAll(i -> mulNumberByDigit(i));
-        return mulList;
-    }
-
-    static List<Integer> merge(List<Integer> list1, List<Integer> list2) {
-        List<Integer> merged = new ArrayList<>();
-        merged.addAll(list1);
-        merged.addAll(list2);
-        return merged;
-    }
-
-    ;
 }
