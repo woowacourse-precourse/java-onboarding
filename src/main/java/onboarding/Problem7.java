be5e1792b30fe121d617cc5bd1d5392dbe0f31e0
ygreenb <nhj7911@gmail.com>
2022-11-01 09:57:41 +0900

    feat: Problem7.1 필요한 변수 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static HashMap<String,Integer> friendScoreMap;
+    static List<String> userFriends;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        friendScoreMap = new HashMap<>();
+        userFriends = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
 }
