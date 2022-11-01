95c9913a22d9f599efd76fa4ee50901c6d1700e2
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 18:09:03 +0900

    refactor : Problem3 클린코드를 위한 수정, 메소드 재분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,25 +2,26 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = countThreeSixNine(number);
-        return answer;
-    }
-
-    public static int countThreeSixNine(int number){
         int count = 0;
-        int tmpNumber;
-        int unitNumber;
+        int temp;
 
         for(int i=1; i<= number; i++){
-            tmpNumber = i;
-            while(tmpNumber > 0){
-                unitNumber = tmpNumber % 10;
-                if((unitNumber == 3) || (unitNumber == 6) || (unitNumber == 9))
-                    count++;
-                tmpNumber /= 10;
-            }
+            temp = i;
+            count += countThreeSixNine(temp);
         }
 
+        return count;
+    }
+
+    public static int countThreeSixNine(int num){
+        int unitNumber;
+        int count = 0;
+        while(num > 0){
+            unitNumber = num % 10;
+            if((unitNumber == 3) || (unitNumber == 6) || (unitNumber == 9))
+                count++;
+            num /= 10;
+        }
         return count;
     }
 }
