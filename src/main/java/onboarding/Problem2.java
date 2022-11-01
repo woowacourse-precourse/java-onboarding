488ca21f494ade63244e92b4ad2e4a5872887ca6
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:11:54 +0900

    feat: 주어진 문자열에서 연속한 문자 삭제하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> charStack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char nowChar = cryptogram.charAt(i);
+            if (charStack.empty() || !charStack.peek().equals(nowChar)) {
+                charStack.push(nowChar);
+            } else {
+                while(i + 1 < cryptogram.length() &&
+                        charStack.peek().equals(cryptogram.charAt(i + 1))){
+                    i++;
+                }
+                charStack.pop();
+            }
+        }
+
+        return charStack.stream()
+                .map(Object::toString)
+                .reduce("", (a, b) -> a + b);
     }
 }
