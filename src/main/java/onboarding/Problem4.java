fd08b842f043006e547402775380dc77688f5a81
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 15:00:36 +0900

    feat:reverseString()->영어 소문자를 소문자로 reverse 변환하고, 대문자를 대문자로 reverse 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,4 +7,14 @@ public class Problem4 {
         String answer = "";
         return reverseString(word);
     }
+
+    public static String reverseString(String string) {
+        if(!error(string)) {
+            for (int i = 0; i < string.length(); i++) {
+                sb = (string.charAt(i) < 91 && string.charAt(i) >64) ? sb.append((char) ('A' + 'Z' - string.charAt(i))) :
+                        ((string.charAt(i)<123 && string.charAt(i)>96)? sb.append((char) ('a' + 'z' - string.charAt(i))) : sb.append(string.charAt(i)));
+            }
+        }
+        return BuildertoString(sb);
+    }
 }
