a2a1928146902cfde6ab458d0567bea5314b89c2
JangHyojoon <fdasfaj9@gmail.com>
2022-11-01 18:47:47 +0900

    문제 3번 기능 1 예외 처리 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    private static boolean exception(int number) {
+        if(number < 1 || number > 1000){
+            return false;
+        }
+
+        return true;
+    }
+
 }
