1c9492b7de3fe72270dcf19c1683723ec61c69f9
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 14:32:27 +0900

    6번 문제 풀이

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,10 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+    public static List<String> solution(List<List<String>> forms) { // hashmap 돌면서 닉네임이 두글자 이상 중복인지 검사
+
     }
 }
