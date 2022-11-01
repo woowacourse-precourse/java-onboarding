15bce829499fc6e14c85cf6678fb6b4abafbd472
Basak <chgy2131@naver.com>
2022-11-01 11:42:25 +0900

    [feat] 1번 - 주이진 배열에 숫자들을 합하거나 곱하여 나오는 큰 수를 반환하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,4 +21,28 @@ class Problem1 {
 
         return result;
     }
+
+    // 주어진 배열에 숫자들을 합하거나 곱하여 나오는 큰 수를 반환하는 함수
+    private static int maxScore(List<Integer> numbers) {
+        int resultSum = sumNumbers(numbers);
+        int resultMulti = multiplyNumbers(numbers);
+
+        return Math.max(resultSum, resultMulti);
+    }
+
+    // 주어진 배열에 수를 모두 합하는 함수
+    private static int sumNumbers(List<Integer> numbers) {
+        int result = 0;
+        for(int n : numbers)
+            result += n;
+        return result;
+    }
+
+    // 주어진 배열에 수를 모두 곱하는 함수
+    private static int multiplyNumbers(List<Integer> numbers) {
+        int result = 1;
+        for(int n : numbers)
+            result *= n;
+        return result;
+    }
 }
