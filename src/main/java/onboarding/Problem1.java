f1fc9c52ad85e66fd5ae7a2bb9ea4d35848f5f6a
youngin <bluest12@naver.com>
2022-11-01 15:49:20 +0900

    예외처리 메서드 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static int solution(List<String> pobi, List<String> crong) {
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static int exception(List<String> pobi, List<String> crong) {
+        int exceptinoValue = 0;
+
+        if ((pobi.get(0) == "0" || pobi.get(pobi.size() - 1) == "400") || (crong.get(0) == "0" || crong.get(crong.size() - 1) == "400"))
+            exceptinoValue = -1;
+        else if (((Integer.parseInt(pobi.get(0)) % 2) == 0) || ((Integer.parseInt(crong.get(0)) % 2) == 0))
+            exceptinoValue = -1;
+        else if (((Integer.parseInt(pobi.get(0)) + 1) != (Integer.parseInt(pobi.get(1)))) || ((Integer.parseInt(crong.get(0)) + 1) != (Integer.parseInt(crong.get(1)))))
+            exceptinoValue = -1;
+        else exceptinoValue = 0;
+
+        return exceptinoValue;
     }
 }
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -12,24 +12,24 @@ class ApplicationTest {
     class Problem1Test {
         @Test
         void case1() {
-            List<Integer> pobi = List.of(97, 98);
-            List<Integer> crong = List.of(197, 198);
+            List<String> pobi = List.of("97", "98");
+            List<String> crong = List.of("197", "198");
             int result = 0;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
 
         @Test
         void case2() {
-            List<Integer> pobi = List.of(131, 132);
-            List<Integer> crong = List.of(211, 212);
+            List<String> pobi = List.of("131", "132");
+            List<String> crong = List.of("211", "212");
             int result = 1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
 
         @Test
         void case3() {
-            List<Integer> pobi = List.of(99, 102);
-            List<Integer> crong = List.of(211, 212);
+            List<String> pobi = List.of("99", "102");
+            List<String> crong = List.of("211", "212");
             int result = -1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
