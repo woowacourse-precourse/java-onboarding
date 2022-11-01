5a03626400a625201380b2a168f9151eb8e6e61b
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:54:20 +0900

    [feat] Problem5 큰 단위부터 돈 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,6 +12,12 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         List<Integer> units = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        int nowMoney = money;
+        for(int i : units){
+            int count = nowMoney / i;
+            nowMoney -= count * i;
+            answer.add( count);
+        }
         return answer;
     }
 }
