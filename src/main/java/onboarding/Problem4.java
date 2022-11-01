060bda90150093968ab44c649e047bd1bf857299
moonchaewon <codnjs8989@gmail.com>
2022-11-01 18:17:43 +0900

    feat(Problem4): add function to check word's lower and upper case

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+    /* 기능 단위
+     * 1. 소문자인지, 대문자인지 확인 (isLowerCase, isUpperCase)
+     * 2. 문자를 변환하는 기능
+     * */
+    private static boolean isLowerCase(int n) {
+        return n >= 65 && n <= 90;
+    }
+
+    private static boolean isUpperCase(int n) {
+        return n >= 97 && n <= 122;
+    }
     public static String solution(String word) {
         String answer = "";
+        String[] wordArray = word.split("");
+        for(int i=0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+        }
+        answer = String.join("", wordArray);
+        System.out.println("answer: " + answer);
         return answer;
     }
 }
