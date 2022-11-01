dad7c10a5629490824345f2191b3c39884bf76f8
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-01 10:45:01 +0900

    chore: Problem 4 주석 달기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,21 +2,22 @@ package onboarding;
 
 public class Problem4 {
     private static String answer;
+    // Uppercase 일때 Convert
     public static void convUpper(char c)
     {
         answer+=(char) ('A' + 'Z' -c);
     }
-
-    public static  void convLower(char c)
+    // Lowercase 일때 Convert.
+    public static void convLower(char c)
     {
         answer +=(char) ('a'+'z' - c);
     }
-
+    // Uppercase 인지 확인.
     public static boolean checkUpper (char c)
     {
         return c>= 'A' && c<='Z';
     }
-
+    // Lowercase 인지 확인.
     public static boolean checkLower (char c)
     {
         return c>= 'a' && c<='z';
@@ -26,13 +27,10 @@ public class Problem4 {
         answer= "";
         for (int i = 0 ; i< word.length(); i++)
         {
-            if (checkUpper(word.charAt(i))) {
+            if (checkUpper(word.charAt(i)))
                 convUpper(word.charAt(i));
-            }
             else if (checkLower(word.charAt(i)))
-            {
                 convLower(word.charAt(i));
-            }
             else
                 answer+=word.charAt(i);
         }
