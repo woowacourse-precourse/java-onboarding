d881fb81bfe33e4176f2988a6118f71e6380e6e7
0chil <0@chll.it>
2022-11-01 18:46:31 +0900

    refactor(test.problem7): 테스트케이스 보완

--- src/test/java/onboarding/problem7/NetworkTest.java
+++ src/test/java/onboarding/problem7/NetworkTest.java
@@ -9,30 +9,19 @@ import org.junit.jupiter.api.Test;
 
 public class NetworkTest {
 
-	// @Test
-	// @DisplayName("친구 추천 점수를 계산할 수 있다")
-	// void calculate_score() {
-	// 	List<List<String>> relationships = List.of(
-	// 		List.of("donut", "andole"),
-	// 		List.of("donut", "jun"),
-	// 		List.of("donut", "mrko"),
-	// 		List.of("shakevan", "andole"),
-	// 		List.of("shakevan", "jun"),
-	// 		List.of("shakevan", "mrko")
-	// 	);
-	// 	List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
-	// 	Network network = new Network(relationships, visitors);
-	//
-	// 	assertThat(network.calculateScoreOf("mrko", "jun")).isEqualTo(20);
-	// }
-
 	@Test
-	@DisplayName("친구를 추천한다")
+	@DisplayName("친구를 점수 순으로 최대 5명 추천한다")
 	void recommend_friends() {
 		List<List<String>> relationships = List.of(
 			List.of("donut", "andole"),
 			List.of("donut", "jun"),
 			List.of("donut", "mrko"),
+			List.of("donut", "aaa"),
+			List.of("donut", "bbb"),
+			List.of("donut", "ccc"),
+			List.of("shakevan", "ccc"),
+			List.of("shakevan", "bbb"),
+			List.of("shakevan", "aaa"),
 			List.of("shakevan", "andole"),
 			List.of("shakevan", "jun"),
 			List.of("shakevan", "mrko")
@@ -40,6 +29,6 @@ public class NetworkTest {
 		List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
 		Network network = new Network(relationships, visitors);
 
-		assertThat(network.recommendFriends("mrko")).contains("andole", "jun", "bedi");
+		assertThat(network.recommendFriendsFor("mrko")).containsExactly("andole", "jun", "bedi");
 	}
 }
