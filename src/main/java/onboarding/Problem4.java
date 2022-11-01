2283d247c4e1a087f82661e8db27825b9a5c3e77
mihea1206 <mihea12@gmail.com>
2022-11-01 09:22:25 +0900

    problem4(using HashMap)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    private static final HashMap<Character, Character> alphabetMap;
+
+    static {
+        alphabetMap = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            alphabetMap.put((char) ('a' + i), (char) ('z' - i));
+            alphabetMap.put((char) ('A' + i), (char) ('Z' - i));
+        }
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return word.chars()
+            .mapToObj(i -> (char)i).map(c-> alphabetMap.getOrDefault(c,c).toString())
+            .reduce("", String::concat);
     }
 }
