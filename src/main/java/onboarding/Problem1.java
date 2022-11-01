5a020d207a833ee88f9c9182f93ca28ad6e5f024
si hwan <pum005@naver.com>
2022-11-01 22:11:34 +0900

    feat (problem1) : 숫자의 각 자리수를 더하거나 곱한 값중 최대값을 선택하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,10 +7,35 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
+        final int LEFT = 0;
+        final int RIGHT = 1;
+
         if(!(P1ExceptionValidation.p1ExceptionValidation(pobi)
-        && P1ExceptionValidation.p1ExceptionValidation(crong))){
+                && P1ExceptionValidation.p1ExceptionValidation(crong))){
             return -1;
         }
+
+        int pobiLeftPage = MaxBetweenSumandMultiple(pobi.get(LEFT));
+        int pobiRightPage = MaxBetweenSumandMultiple(pobi.get(RIGHT));
+
+        int crongLeftPage = MaxBetweenSumandMultiple(crong.get(LEFT));
+        int crongRightPage = MaxBetweenSumandMultiple(crong.get(RIGHT));
+
+    }
+
+    public static int MaxBetweenSumandMultiple(int number) {
+
+        int sum = 0;
+        int multiple = 1;
+
+        String s = Integer.toString(number);
+
+        for (int i = 0; i < s.length(); i++) {
+            sum += Integer.parseInt(s.substring(i, i + 1));
+            multiple *= Integer.parseInt(s.substring(i, i + 1));
+        }
+
+        return Math.max(sum, multiple);
     }
 
 }
