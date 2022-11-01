0ba451ee454c96f349ba93fb0c040505b9fe5453
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 13:47:02 +0900

    [1번] feat: 합과 곱 중 최대값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import onboarding.Problem1Package.Calc;
+import onboarding.Problem1Package.Product;
+import onboarding.Problem1Package.Sum;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    // 합과 곱 중 최대값 구하기
+    private static int getMaxOfSumOrProduct(List<Integer> list) {
+        Calc sum = new Sum();
+        Calc product = new Product();
+
+        int sumResult = sum.getResult(list);
+        int productResult = product.getResult(list);
+
+        int max = Math.max(sumResult, productResult);
+        return max;
     }
 }
