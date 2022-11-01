b9b3b23a8c2f2e7de1d42eaff33cb4a967580941
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-01 19:56:40 +0900

    feat : P7 - 친구 목록 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,47 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        Map<String, List<String>> relation = makeFriendsRelation(user, friends, visitors);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map makeFriendsRelation(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, List<String>> temp = new HashMap<>();
+        temp.put(user, new ArrayList<>());
+
+        for (List info : friends) {
+            for (Object person : info) {
+                if (!temp.containsKey((String)person)) {
+                    List<String> defaultScore = new ArrayList<>();
+                    defaultScore.add("0");
+                    temp.put((String)person, defaultScore);
+                }
+            }
+
+            if (temp.containsKey(info.get(0))) {
+                List<String> friendsList = temp.get(info.get(0));
+                friendsList.add((String)info.get(1));
+            }
+
+            if (temp.containsKey(info.get(1))) {
+                List<String> friendsList = temp.get(info.get(1));
+                friendsList.add((String)info.get(0));
+            }
+
+            for (String visitor : visitors) {
+                if (!temp.containsKey(visitor)) {
+                    List<String> defaultScore = new ArrayList<>();
+                    defaultScore.add("0");
+                    temp.put(visitor, defaultScore);
+                }
+            }
+        }
+        return temp;
     }
 }
