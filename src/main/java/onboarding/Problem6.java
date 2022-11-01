66ec748f1af3115473e9bc06d4f3999eb8e6844c
hanuel <ezzeezz@naver.com>
2022-11-01 14:09:05 +0900

    feat: [problem6] Add solution

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = search(forms);
+
+        answer = answer.stream()
+                .distinct().sorted()
+                .collect(Collectors.toList());
+
         return answer;
     }
 }
