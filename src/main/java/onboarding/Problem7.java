fd28ee2aa3c50ed5c324cd25d2d24c699fd05d74
wonseok <tkddls23@konkuk.ac.kr>
2022-11-01 14:29:31 +0900

    feat: 7-1 각 친구 관계 정보 map으로 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, Integer> scoreMap = new HashMap<>();
+        List<String> friendList;
         return answer;
+    }
+    
+    private static Map<String,HashSet<String>> setFriend(List<List<String>> friends) {
+        Map<String, HashSet<String>> friendMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+
+            if(friendMap.containsKey(friend1))
+                friendMap.get(friend1).add(friend2);
+
+            if(!friendMap.containsKey(friend1))
+                friendMap.put(friend1, new HashSet<>(List.of(friend2)));
+
+            if(friendMap.containsKey(friend2))
+                friendMap.get(friend2).add(friend1);
+
+            if(!friendMap.containsKey(friend2))
+                friendMap.put(friend2, new HashSet<>(List.of(friend1)));
+
+        }
+        return friendMap;
     }
 }
