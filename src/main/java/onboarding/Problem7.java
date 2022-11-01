163d1bf6f8d551aca865bee4629e3dc3cac6f155
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 22:04:34 +0900

    feat(PROBLEM7): Friendship() 친구 관계 HashMap 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,25 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, List<String>> friendship = Friendship(friends);
+        System.out.println("friendship.keySet() = " + friendship.keySet());
+        System.out.println("friendship.values() = " + friendship.values());
         return answer;
     }
 
+    public static HashMap<String, List<String>> Friendship(List<List<String>> friends) {
+        HashMap<String, List<String>> friendship= new HashMap<>();
 
+        for(List<String> friend : friends){
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+
+            addHashMap(friendship,user1,user2);
+            addHashMap(friendship,user2,user1);
+        }
+
+        return friendship;
+    }
 
     public static void addHashMap(HashMap<String, List<String>> hashMap, String key, String value) {
         if(hashMap.containsKey(key))
