9a6bd24219d3849646bd1c14e56480ac84f2df71
hataerin <goil1113@likelion.org>
2022-11-01 20:09:13 +0900

    ADD: Problem5 화폐 개수 계산 기능 구현
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        List<Integer> answer = new ArrayList<>();
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         //화폐 개수 계산 기능
-        List<Integer> answer = Collections.emptyList();
+        for (int data : unit) {
+            answer.add(money / data);
+            money = money % data;
+        }
+
         return answer;
     }
 }
