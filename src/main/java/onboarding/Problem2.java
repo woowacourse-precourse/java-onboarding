232fbca6021f5298b8759ecda55b7d0865656b83
sangmins930 <sangmins930@gmail.com>
2022-11-01 21:13:40 +0900

    Problem2 solution upload

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        int cur = 1;
+        int prior = 0;
+
+        while (answer.length() > 0 && cur < answer.length()) {
+            if (answer.charAt(cur) == answer.charAt(prior)) {
+                char target = answer.charAt(prior);
+                for (cur = cur + 1; cur < answer.length(); cur++)
+                    if (target != answer.charAt(cur)) break;
+                answer = answer.substring(0, prior) + answer.substring(cur);
+                cur = 0;
+            }
+            prior = cur;
+            cur++;
+        }
         return answer;
     }
 }
