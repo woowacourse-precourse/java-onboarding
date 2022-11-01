8f397239cc53a2e45ece21fc41c5e70f5073f5bc
jfavy <guitiar1296@gmail.com>
2022-11-01 22:16:33 +0900

    1~3번 구현 및 커밋 테스트

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
+        for(int i=0;i<=1;i++) {
+        	if(pobi.get(i)%10 + pobi.get(i)/10 >= pobi.get(i)%10 * pobi.get(i)/10)
+            	pobi.add(2,pobi.get(i)%10 + pobi.get(i)/10);
+            else pobi.add(2,pobi.get(i)%10 * pobi.get(i)/10);
+        	
+        	if(crong.get(i)%10 + crong.get(i)/10 >= crong.get(i)%10 * crong.get(i)/10)
+        		crong.add(2,crong.get(i)%10 + crong.get(i)/10);
+            else crong.add(2,crong.get(i)%10 * crong.get(i)/10);
+        
+        }
         return answer;
     }
 }
