5a3f6830597dd065e6b1373375f99c33eae1882d
¤knj <krkoknj@gmail.com>
2022-11-01 16:36:16 +0900

    user와 친구인 사람들을 찾고 그 친구와 친구인 사람들을 찾는다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,13 +8,24 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         List<String> userFriendList = userFriends(user, friends);
-        friendAndFriends(userFriendList, friends);
+        List<String> friendAndFriends = friendAndFriends(userFriendList, friends, user);
         return answer;
     }
 
-    private static void friendAndFriends(List<String> userFriendList, List<List<String>> friends) {
+    private static List<String> friendAndFriends(List<String> userFriendList, List<List<String>> friends, String user) {
+        List<String> friendsAndFriends = new ArrayList<>();
         for (int i = 0; i < userFriendList.size(); i++) {
+            for (int j = 0; j < friends.size(); j++) {
+                if (friends.get(j).contains(userFriendList.get(i))) {
+                    if (friends.get(j).get(1).equals(user)) {
+                        continue;
+                    }
+                    System.out.println("friends.get(j) = " + friends.get(j));
+                    friendsAndFriends.add(friends.get(j).get(1));
+                }
+            }
         }
+        return friendsAndFriends;
     }
 
     private static List<String> userFriends(String user, List<List<String>> friends) {
