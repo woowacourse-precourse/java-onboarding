74c3fa3f3fdb0be1d4ddfa3e77344a874f679e6e
mihea1206 <mihea12@gmail.com>
2022-11-01 09:33:08 +0900

    problem6(create user map)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -15,14 +15,23 @@ public class Problem6 {
             .collect(Collectors.toList());
     }
 
-    private static void appendMapValue(String key,String appendValue,
-        HashMap<String, HashSet<String>> map){
-        HashSet<String> valueSet=map.getOrDefault(key, new HashSet<>());
+    private static void appendMapValue(String key, String appendValue,
+        HashMap<String, HashSet<String>> map) {
+        HashSet<String> valueSet = map.getOrDefault(key, new HashSet<>());
         valueSet.add(appendValue);
-        map.put(key,valueSet);
+        map.put(key, valueSet);
     }
 
     public static List<String> solution(List<List<String>> forms) {
+        //key: token / value: email
+        HashMap<String, HashSet<String>> userMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            String email = form.get(0);
+            List<String> tokens = tokenizer(nickname);
+            tokens.forEach(token -> appendMapValue(token, email, userMap));
+        }
         List<String> answer = List.of("answer");
         return answer;
     }
