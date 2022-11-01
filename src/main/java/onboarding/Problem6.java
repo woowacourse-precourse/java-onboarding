9cf4cb5c0303f993acb67079ed66bd2086ddbc1e
dener8 <8dener@gmail.com>
2022-11-01 16:16:44 +0900

    [Problem6] feat: 불가능한 닉네임의 이메일을 arraylist에 넣는 작업

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -16,7 +16,7 @@ public class Problem6 {
             nicknameElements = new HashSet<>();
             nickname = forms.get(i).get(1);
             for (int j = 0; j < nickname.length() - 1; j++) {
-                nicknameElements.add(nickname.substring(i, i+2));
+                nicknameElements.add(nickname.substring(j, j+2));
             }
             // hashmap에 이미 key값이 있으면 +=1, 없으면 추가하고 value값 1로 지정
             for (String nicknameElement : nicknameElements) {
@@ -26,9 +26,19 @@ public class Problem6 {
                     allNicknameElements.put(nicknameElement, 1);
                 }
             }
-
-
-
+        }
+        // 여기까지 오면, 닉네임의 부분 문자열은 모두 hashmap에 담긴 상태
+        // 그러면 각각의 닉네임이 1인 value값을 가지는지, 그렇지 않은지 확인해주면 됨
+        // 만약 1이라면 문제가 없지만, 2 이상이라면 문제가 있기 때문에 새로운 arrayList에 넣어줘서 출력해야 함
+        ArrayList<String> emails = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            nickname = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                if (allNicknameElements.get(nickname.substring(j, j+2)) != 1) {
+                    emails.add(email);
+                }
+            }
         }
     }
 }
