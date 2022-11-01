09fc7471bd00c14066102f67988f6177bddbafc8
This2sho <zx00018@naver.com>
2022-11-01 19:10:27 +0900

    feat: 중복 닉네임에 해당하는 이메일 반환 메소드 구현

--- src/main/java/onboarding/problem6/JoinFormUtils.java
+++ src/main/java/onboarding/problem6/JoinFormUtils.java
@@ -2,13 +2,29 @@ package onboarding.problem6;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class JoinFormUtils {
 
 	private final Map<String, String> joinNickNames = new HashMap<>();
 	private final Set<String> duplicateNickNames = new HashSet<>();
+	private Map<String, String> forms;
+
+	public void join(List<List<String>> inputForms) {
+		forms = mapToForms(inputForms);
+		for (String nickName : forms.keySet()) {
+			join(nickName);
+		}
+	}
+
+	Map<String, String> mapToForms(List<List<String>> forms) {
+		return forms.stream()
+			.map(Form::new)
+			.collect(Collectors.toMap(Form::getNickName, Form::getEmail));
+	}
 
 	public void join(String input) {
 		for (int startIndex = 0; input.length() - startIndex >= 2; startIndex++) {
@@ -28,7 +44,16 @@ public class JoinFormUtils {
 		}
 	}
 
-	public Set<String> getDuplicateNickNames() {
+	public List<String> findEmailsByDuplicateNickNames() {
+		return duplicateNickNames.stream()
+			.map(nickName -> this.forms.get(nickName))
+			.distinct()
+			.sorted()
+			.collect(Collectors.toList());
+	}
+
+	Set<String> getDuplicateNickNames() {
 		return duplicateNickNames;
 	}
+
 }
--- src/test/java/onboarding/problem6/JoinFormUtilsTest.java
+++ src/test/java/onboarding/problem6/JoinFormUtilsTest.java
@@ -2,6 +2,9 @@ package onboarding.problem6;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +28,19 @@ public class JoinFormUtilsTest {
 		Set<String> duplicateNickNames = JOIN_FORM_UTILS.getDuplicateNickNames();
 		assertThat(duplicateNickNames)
 			.containsExactlyInAnyOrder(input, input2, input3);
+	}
+
+	@Test
+	public void 중복_닉네임_이메일_찾기_테스트() {
+	    //given
+		List<List<String>> forms = Arrays.asList(
+			Arrays.asList("jm@email.com", "제이엠"), Arrays.asList("jason@email.com", "제이슨"), Arrays.asList("woniee@email.com", "워니"),
+			Arrays.asList("mj@email.com", "엠제이"), Arrays.asList("nowm@email.com", "이제엠"));
+		JOIN_FORM_UTILS.join(forms);
+	    //when
+		List<String> result = JOIN_FORM_UTILS.findEmailsByDuplicateNickNames();
+	    //then
+		assertThat(Arrays.asList("jason@email.com", "jm@email.com", "mj@email.com"))
+			.isEqualTo(result);
 	}
 }
