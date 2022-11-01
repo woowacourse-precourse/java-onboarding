de86bbd7e59bb465f80b3045afc14ab197d39f68
DongWooE <ehddn977@naver.com>
2022-11-01 20:20:18 +0900

    [p7][add] 친구 지인을 찾는 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,18 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
 
     HashMap<String, ArrayList<String>> friendGraph = new HashMap<>();
+    Map<String, Integer> knownList = new HashMap();
+    Set<String> friendList = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Problem7 p7 = new Problem7();
         p7.makeFriendGraph(friends);
+        p7.findKnownPeople(user);
+
         return null;
     }
 
@@ -30,6 +33,23 @@ public class Problem7 {
             ArrayList<String> list1 = new ArrayList<>();
             list1.add(friend2);
             friendGraph.put(friend1, list1);
+        }
+    }
+
+    private void findKnownPeople(String user){
+        ArrayList<String> friends = friendGraph.get(user);
+        friendList.addAll(friends);
+        for(String friend : friends){
+            for(String people : friendGraph.get(friend)){
+                if(people.equals(user) || friendList.contains(people)) continue;        // 만약 본인이고 친구이면 통과
+                if(knownList.containsKey(people)){
+                    Integer score = knownList.get(people);
+                    knownList.replace(people, score, score+10);
+                }
+            else{
+                    knownList.put(people, 10);
+                }
+            }
         }
     }
 }
