cc9fb7352a7f20ae1f83d3438094a2e3cdca26d5
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-01 22:34:22 +0900

    백엔드 6번문제
    미해결
    
    int Array를 하나 선언하여 for문을 통해 앞 두글자와 뒷 두글자를 contains로 체크, 해당 조건중 하나라도 만족하면 int Array에 해당하는 값을 넣고 answer에 이메일과 닉네임을 넣음. 이후 Comparator 클래스를 사용하여 오름차순 정렬로 바꾼뒤 return 예정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,26 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        /*
-        * 1. 닉네임을 한글자씩 비교하며 연속으로 같은 문자가 도출될경우 result에 값을 추가하는 기능
-        * 2. 그 중 중복되는 값들을 걸러내는 기능
-        * */
+        char[][] nickNames = new char[forms.size()][];
+
+        for(int i = 0; i < forms.size()-1;i++){
+            for(int j = 0; j < 18; j++) {
+                if (nickNames[i][j] == nickNames[i+1][j] && nickNames[i][j+1] == nickNames[i+1][j+1]){
+                    answer.add(String.valueOf(forms.get(i)));
+                    answer.add(String.valueOf(forms.get(i+1)));
+                }
+            }
+        }
+        //중복제거
         return answer;
-    }
-
-    public static void main(String[] args) {
-        List<List<String>> forms = List.of(
-                List.of("jm@email.com", "제이엠"),
-                List.of("jason@email.com", "제이슨"),
-                List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
-        );
-
-        System.out.println(solution(forms));
     }
 }
