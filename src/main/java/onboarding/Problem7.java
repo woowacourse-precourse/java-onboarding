c7158995b485a6f8210f3b0c032aa50e1d76e926
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 18:22:40 +0900

    FEAT mapScoreByFriendsList

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         HashMap<String, HashSet<String>> friendSetMap = mapFriendSet(friends);
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+
+        mapScoreByFriendsList(user, friendSetMap, scoreMap);
+        mapScoreByVisit(visitors, scoreMap);
 
         List<String> answer = Collections.emptyList();
 
@@ -23,6 +27,33 @@ public class Problem7 {
         }
 
         return ret;
+    }
+
+    private static void mapScoreByFriendsList(String user, HashMap<String, HashSet<String>> friendSetMap,
+                                              HashMap<String, Integer> scoreMap) {
+
+        if (!friendSetMap.containsKey(user)) {
+            return;
+        }
+
+        HashSet<String> userFriendSet = friendSetMap.get(user);
+
+        for (String userFriend : userFriendSet) {
+            HashSet<String> friendFriendSet = friendSetMap.get(userFriend);
+
+            for (String friendFriend : friendFriendSet) {
+                int score = scoreMap.getOrDefault(friendFriend, 0);
+                scoreMap.put(friendFriend, score + 10);
+            }
+        }
+
+    }
+
+    private static void mapScoreByVisit(List<String> visitors, HashMap<String, Integer> scoreMap) {
+        for (String visitor : visitors) {
+            int score = scoreMap.getOrDefault(visitor, 0);
+            scoreMap.put(visitor, score + 1);
+        }
     }
 
 }
