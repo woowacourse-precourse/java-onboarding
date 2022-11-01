bed85ff49788813a25566440afb2eaeaee7a22ec
3juhwan <13selfesteem91@naver.com>
2022-11-01 10:48:22 +0900

    problem6: update solution function

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+    static List<List<String>> divTwoChar = new ArrayList<>();
+    static HashMap<String, List<Integer>> twoCharMap = new HashMap<>();
+    static int numOfPeople = 0;
+
+    public static void makeDivTwoCharList(List<String> names) {
+
+    }
+
+    public static void makeTwoCharMap() {
+
+    }
+
+    public static List<String> getOverlapPersonEmail() {
+
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        numOfPeople = forms.size();
+        return getOverlapPersonEmail();
     }
 }
