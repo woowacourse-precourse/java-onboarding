33c6b99dcdddd296775682eeb3e173d3bd2acadf
si hwan <pum005@naver.com>
2022-11-01 22:23:03 +0900

    feat (problem3) : 0 ~ 입력된 수 까지 3,6,9의 개수를 세워주는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,5 +10,30 @@ public class Problem3 {
         if (!(P3ExceptionValidation.p3ExceptionValidation(number))){
             return 0;
         }
+
+        int clabNumber = 0;
+
+        for (int i = 1; i <= number; i++){
+            clabNumber += findNumberOfClab(i);
+        }
+
+        return clabNumber;
     }
+
+    public static int findNumberOfClab(int number){
+
+        int clabNumber = 0;
+        String s = Integer.toString(number);
+
+        for(int i = 0 ; i < s.length() ; i++) {
+            String subString = s.substring(i, i+1);
+
+            if (subString.equals("3") || subString.equals("6") || subString.equals("9")){
+                clabNumber += 1;
+            }
+        }
+
+        return clabNumber;
+    }
+
 }
