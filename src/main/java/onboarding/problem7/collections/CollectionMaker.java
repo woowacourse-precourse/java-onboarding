7b8ac3ee8c1665ae79ec31627601545c60d71e83
chanminmaxscaler <jcm04@naver.com>
2022-11-01 20:38:08 +0900

    feat(CollectionMaker): CollectionMaker 클래스의 멤버변수와 생성자, makeFriendList, makeVisitorList 메소드를 구현한다.
    CollectionMaker는 FriendList, VisitorList 객체를 멤버변수로 갖는다.
    생성자가 호출될 때 makeFriendList, makeVisitorList 메소드들을 호출해 멤버변수에 각각의 객체를 담는다.
    makeFriendList(List<List\<String>> friends) 메소드는 friends 리스트를 입력받아 FriendList를 생성해 반환한다.
    makeVisitorList(List\<String> visitors) visitors 리스트를 입력받아 VisitorList를 생성해 반환한다.

--- src/main/java/onboarding/problem7/collections/CollectionMaker.java
+++ src/main/java/onboarding/problem7/collections/CollectionMaker.java
@@ -3,12 +3,44 @@ package onboarding.problem7.collections;
 import onboarding.problem7.wrapper.Friend;
 import onboarding.problem7.wrapper.User;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CollectionMaker {
+
+    private final FriendList friendList;
+    private final VisitorList visitorList;
+
+    public CollectionMaker(List<List<String>> friendList, List<String> visitors) {
+        this.friendList = makeFriendList(friendList);
+        this.visitorList = makeVisitorList(visitors);
+    }
+
     public User makeUser(String userId) {
         return new User(userId);
     }
 
     public Friend makeFriendWithAAndB(User userA, User userB) {
         return new Friend(userA, userB);
+    }
+
+    public FriendList makeFriendList(List<List<String>> friends) {
+        List<Friend> friendList = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            User userA = makeUser(friend.get(0));
+            User userB = makeUser(friend.get(1));
+            Friend relationshipOfAAndB = makeFriendWithAAndB(userA, userB);
+            friendList.add(relationshipOfAAndB);
+        }
+        return new FriendList(friendList);
+    }
+
+    public VisitorList makeVisitorList(List<String> visitors) {
+        List<User> visitorList = visitors.stream()
+                .map(this::makeUser)
+                .collect(Collectors.toList());
+        return new VisitorList(visitorList);
     }
 }
