f1f76a1865c507bb3d2dee2a6de94a88be1985d5
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:42:16 +0900

    feat: 주어진 자연수를 큰 단위 부터 나누며 결과 리스트 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -9,8 +10,13 @@ public class Problem5 {
     private final static List<Integer> moneyUnitList = Arrays.asList(
             50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
     );
+    
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int unit : moneyUnitList) {
+            answer.add(money / unit);
+            money %= unit;
+        }
         return answer;
     }
 }
