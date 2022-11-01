783638c69192d206ac6d493d70ef2a89240df586
ckyeon <qsky8520@gmail.com>
2022-11-01 21:13:40 +0900

    Add getSubNickNames() to Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static HashSet<String> getSubNickNames(String nickName) {
+        HashSet<String> subNickNames = new HashSet<>();
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            subNickNames.add(nickName.substring(i, i + 2));
+        }
+        return subNickNames;
     }
 
     private enum Form {
