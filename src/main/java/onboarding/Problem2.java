f262f8f3223117d303dba9dc2e0a47b999446dbf
gksruf2848 <4455qqq@naver.com>
2022-11-01 18:35:19 +0900

    Problem2 기능4. 더 이상 제거할 문자가 없을 때까지 반복하는 while문 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
+
+        //4.더 이상 제거할 문자가 없을때까지 반복하는 while문
+        while(true) {
+            answer = deleteSameCharacter(cryptogram);
+            if(answer.equals(cryptogram)) {
+                break;
+            }
+            cryptogram = answer;
+        }
         return answer;
+    }
+
+    private static String deleteSameCharacter(String str) {
+
     }
 }
