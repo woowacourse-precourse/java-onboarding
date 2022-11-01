2f861693bca6a2c8b713ddb1018817730ae33cbe
pasly0920 <inwoo920621@gmail.com>
2022-11-01 15:05:11 +0900

    feat: 1 문자열 중복 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,38 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String crypted = cryptogram;
+
+        return crypted;
+    }
+
+    private static String deleteDuplicate(String origin){
+        String result = "";
+        for(int i=0; i<origin.length(); i++){
+            if(i == origin.length() - 1){
+                if(!origin.substring(i-1, i).equals(origin.substring(i, i+1))) {
+                    result += origin.substring(i, i + 1);
+                    continue;
+                }
+            }
+
+            String nowStr = origin.substring(i, i+1);
+            String nextStr = origin.substring(i+1, i+2);
+
+            if(!nowStr.equals(nextStr)){
+                result += nowStr;
+            }
+            else {
+                int sameStrEnd = i+1;
+                for(; sameStrEnd<origin.length() - 1; sameStrEnd++){
+                    nextStr = origin.substring(sameStrEnd+1, sameStrEnd+2);
+                    if(!nowStr.equals(nextStr))
+                        break;
+                }
+                i = sameStrEnd;
+            }
+        }
+
+        return result;
     }
 }
