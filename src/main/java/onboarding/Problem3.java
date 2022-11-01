4cecf06770f99d0c05acd8ba9ec01e5d5ccc8b02
yewon <yewon.ux@gmail.com>
2022-11-01 19:53:57 +0900

    기능 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        /*
+        기능 목록
+        1. 1부터 숫자 number 까지 for문 생성
+        2. 각각의 number에 3, 6, 9가 포함되어 있는 경우 해당 숫자 저장
+        3. 저장된 숫자중 3, 6, 9 개수 계산
+        */
         return answer;
     }
 }
