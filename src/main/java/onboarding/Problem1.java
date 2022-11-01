c571d9b2d482351bdb6ace7e33b5299a598f5450
dingding-21 <phj8221@naver.com>
2022-11-01 22:39:54 +0900

    feat: Problem1 각 자리 숫자의 합, 곱 계산 후 최대 값 구하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -23,4 +23,17 @@ class Problem1 {
 
         return true;
     }
+
+    private static int getMaxNum(Integer num) {
+        int sum = 0;
+        int mul = 1;
+
+        while (num != 0) {
+            sum += num % 10;
+            mul *= num % 10;
+            num /= 10;
+        }
+
+        return Math.max(sum, mul);
+    }
 }
