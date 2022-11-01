baf9d6000768aeb1796fc55979c8463fd33db2d0
임원정 <LWJ@MacBookPro.local>
2022-11-01 19:31:32 +0900

    feature : problem4 알파벳을 바꾸는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,10 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder sentence = new StringBuilder(word);
-        
+
         for (int i = 0; i < word.length(); i++) {
             sentence.setCharAt(i, changeAlphabet(word.charAt(i)));
         }
         return sentence.toString();
+    }
+
+    private static char changeAlphabet(char character) {
+        if ('a' <= character && character <= 'z')
+            return (char) (219 - character);
+        if ('A' <= character && character <= 'Z')
+            return (char) (155 - character);
+        return character;
     }
 }
