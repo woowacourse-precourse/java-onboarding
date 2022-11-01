bb85c3530599a5bd4c57771adf4d75848cd18196
mihea1206 <mihea12@gmail.com>
2022-11-01 09:31:19 +0900

    problem6(update HashMap function - append object to map value)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,6 +13,13 @@ public class Problem6 {
         return IntStream.rangeClosed(0, str.length() - 2)
             .mapToObj(i -> String.join("", Arrays.copyOfRange(str.split(""), i, i + 2)))
             .collect(Collectors.toList());
+    }
+
+    private static void appendMapValue(String key,String appendValue,
+        HashMap<String, HashSet<String>> map){
+        HashSet<String> valueSet=map.getOrDefault(key, new HashSet<>());
+        valueSet.add(appendValue);
+        map.put(key,valueSet);
     }
 
     public static List<String> solution(List<List<String>> forms) {
