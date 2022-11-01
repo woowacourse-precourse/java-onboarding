cb6af4c44053ae612b652e882a72641c2999c50d
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 15:15:58 +0900

    feat:static value

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static List<String> Member = new ArrayList<>();
+    private static List<String> Overlap =new ArrayList<>();
+    private static List<String> depulicate = new ArrayList<>();
+    private static final String NAME_REG = "^[가-힣]*$";
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        return answer;
+        return AllMemberCheck(forms);
     }
 }
