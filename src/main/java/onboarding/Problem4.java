3feb76e0484b52c8fd363808022ffa4c3bd16f3a
osy9757 <osy9757@gmail.com>
2022-11-01 23:06:01 +0900

    FEAT : solution4 함수 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = changeReverse(word);
         return answer;
+    }
+    public static String changeReverse(String word) {
+        char[] tempWordArray = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            int wordArrayNum = tempWordArray[i];
+            if (65 <= wordArrayNum && wordArrayNum <= 90) {
+                wordArrayNum = (90 - (wordArrayNum - 65));
+            } else if (97 <= wordArrayNum && wordArrayNum <= 122) {
+                wordArrayNum = (122 - (wordArrayNum - 97));
+            }
+            tempWordArray[i] = (char) wordArrayNum;
+        }
+        word = String.valueOf(tempWordArray);
+        return word;
     }
 }
