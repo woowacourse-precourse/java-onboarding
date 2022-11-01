eb4a665d6ad8b5f213feca8f078302ae81a34eb3
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 12:07:36 +0900

    feat: Problem4 add

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            char c = change(word.charAt(i));
+            answer+=c;
+        }
         return answer;
     }
+    public static char change(char c){
+        char result = c;
+        if (c>=65 && c<=90){
+            int temp = c - 65;
+            result = (char) (90-temp);
+        } else if (c >= 97 && c<=122) {
+            int temp = c - 97;
+            result = (char) (122-temp);
+        }
+        return result;
+    }
+
 }
