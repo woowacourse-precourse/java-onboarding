bb442166391fdf99a00055a965c4fa37f64a097a
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 19:43:06 +0900

    refactor: change variable name, access modifiers
    - public -> private
    - stack -> decryptionCharacters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -17,34 +17,34 @@ public class Problem2 {
         return decryption;
     }
 
-    public static String deleteDuplicate(String cryptogram, int size) {
-        Stack<Character> stack = new Stack<>();
+    private static String deleteDuplicate(String cryptogram, int size) {
+        Stack<Character> decryptionCharacters = new Stack<>();
         String decryption = "";
 
         for (int i = 0; i < size; i++) {
-            if (stack.isEmpty()) {
-                stack.push(cryptogram.charAt(i));
+            if (decryptionCharacters.isEmpty()) {
+                decryptionCharacters.push(cryptogram.charAt(i));
                 continue;
             }
 
-            if (stack.peek() == cryptogram.charAt(i)) {
+            if (decryptionCharacters.peek() == cryptogram.charAt(i)) {
                 // 마지막 중복값의 위치 구하기
-                i = findlastDuplicateIndex(cryptogram, i);
-                stack.pop();
+                i = findLastDuplicatedIndex(cryptogram, i);
+                decryptionCharacters.pop();
                 continue;
             }
 
-            stack.push(cryptogram.charAt(i));
+            decryptionCharacters.push(cryptogram.charAt(i));
         }
 
-        while (!stack.isEmpty()) {
-            decryption = stack.pop() + decryption;
+        while (!decryptionCharacters.isEmpty()) {
+            decryption = decryptionCharacters.pop() + decryption;
         }
 
         return decryption;
     }
 
-    public static int findlastDuplicateIndex(String cryptogram, int index) {
+    private static int findLastDuplicatedIndex(String cryptogram, int index) {
         char duplicatedCharacter = cryptogram.charAt(index);
         while (index < cryptogram.length()) {
             if (duplicatedCharacter != cryptogram.charAt(index)) {
