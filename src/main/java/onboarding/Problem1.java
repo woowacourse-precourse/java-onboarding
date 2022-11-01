44ac438c4472d9475f803a5e6fe693d781255045
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 19:39:46 +0900

    refactor: change variable name, access modifiers
    - public -> private
    - pobiMax, crongMax -> pobiMaxValue, crongMaxValue
    - left, right -> leftPage, rightPage
    - max -> maxValue
    - mul -> multiply

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,10 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static final int EXCEPTION = -1;
-    public static final int POBI_WIN = 1;
-    public static final int CRONG_WIN = 2;
-    public static final int DRAW = 0;
+    private static final int EXCEPTION = -1;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int DRAW = 0;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -15,10 +15,10 @@ class Problem1 {
             return EXCEPTION;
         }
 
-        int pobiMax = getMax(pobi);
-        int crongMax = getMax(crong);
+        int pobiMaxValue = getMax(pobi);
+        int crongMaxValue = getMax(crong);
 
-        answer = getAnswer(pobiMax, crongMax);
+        answer = getAnswer(pobiMaxValue, crongMaxValue);
         return answer;
     }
 
@@ -30,39 +30,39 @@ class Problem1 {
     }
 
     // 왼쪽 페이지, 오른쪽 페이지 중 최댓값 반환
-    public static int getMax(List<Integer> person) {
-        String left = String.valueOf(person.get(0));
-        String right = String.valueOf(person.get(1));
-        int max = Math.max(getLargeSumMul(left), getLargeSumMul(right));
+    private static int getMax(List<Integer> person) {
+        String leftPage = String.valueOf(person.get(0));
+        String rightPage = String.valueOf(person.get(1));
+        int maxValue = Math.max(getGreaterValueSumMultiply(leftPage), getGreaterValueSumMultiply(rightPage));
 
-        return max;
+        return maxValue;
     }
 
     // 해당 페이지에서 각 자리수 합 or 곱 중 큰값 반환
-    public static int getLargeSumMul(String page) {
-        int sum = 0, mul = 1;
+    private static int getGreaterValueSumMultiply(String page) {
+        int sum = 0, multiply = 1;
 
         for (int i = 0; i < page.length(); i++) {
             int n = page.charAt(i) - '0';
             sum += n;
-            mul *= n;
+            multiply *= n;
         }
 
-        return Math.max(sum, mul);
+        return Math.max(sum, multiply);
     }
 
     // 입력된 페이지들이 조건에 맞는지 확인
-    public static boolean isWrongInput(List<Integer> pobi, List<Integer> crong) {
+    private static boolean isWrongInput(List<Integer> pobi, List<Integer> crong) {
         return (isFirstOrLast(pobi, crong) || isNotConsecutive(pobi, crong));
     }
 
     // 연속된 페이지가 아닌경우
-    public static boolean isNotConsecutive(List<Integer> pobi, List<Integer> crong) {
+    private static boolean isNotConsecutive(List<Integer> pobi, List<Integer> crong) {
         return (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1));
     }
 
     // 시작면 or 마지막면이 나오는 경우
-    public static boolean isFirstOrLast(List<Integer> pobi, List<Integer> crong) {
+    private static boolean isFirstOrLast(List<Integer> pobi, List<Integer> crong) {
         return (pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400);
     }
 }
