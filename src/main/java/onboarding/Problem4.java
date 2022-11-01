8e27d715745f693e802c0515723200e0ca28649b
shpark30 <seohyeonpark30.gmail.com>
2022-11-01 21:16:46 +0900

    refact: List<Character> 일급 컬렉션 사용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,26 +3,46 @@ package onboarding;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Problem4 {
-    public static String solution(String word) {
-        /* ASCII 코드를 활용해 소문자, 대문자 알파벳을 반대로 변환*/
-        List<Character> characters = word.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
-        characters = reverseUpperCase(characters);  // 대문자 알파벳 반대로 변환
-        characters = reverseLowerCase(characters);  // 소문자 알파벳 반대로 변환
-        String answer = characters.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining());
-        ;  // 문자열 String으로 변환
-        return answer;
+class ReverseAlphabetList {
+    private List<Character> characters;
+
+    public ReverseAlphabetList(List<Character> characters) {
+        validateSize(characters);
+        this.characters = characters;
     }
 
-    static List<Character> reverseUpperCase(List<Character> characters) {
+    public List<Character> reverse() {
+        /* ASCII 코드를 활용해 소문자(65~90), 대문자(97~122) 알파벳을 반대로 변환*/
+        List<Character> characterList = this.characters;
+        characterList = reverseUpperCase(characterList);
+        characterList = reverseLowerCase(characterList);
+        return characterList;
+    }
+
+    private List<Character> reverseUpperCase(List<Character> characters) {
         characters.replaceAll(c -> (int) c >= 65 && (int) c <= 90 ? (char) (90 + 65 - (int) c) : c);
         return characters;
     }
 
-    static List<Character> reverseLowerCase(List<Character> characters) {
+    private List<Character> reverseLowerCase(List<Character> characters) {
         characters.replaceAll(c -> (int) c >= 97 && (int) c <= 122 ? (char) (122 + 97 - (int) c) : c);
         return characters;
+    }
+
+    private void validateSize(List<Character> characters) {
+        if (characters.size() == 0 || characters.size() > 1000) {
+            throw new IllegalArgumentException("characters must be between 1 and 1000");
+        }
+    }
+}
+
+public class Problem4 {
+    public static String solution(String word) {
+        List<Character> characters = word.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        ReverseAlphabetList alphabetList = new ReverseAlphabetList(characters);
+        String answer = alphabetList.reverse().stream() // 문자열 String으로 변환
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        return answer;
     }
 }
