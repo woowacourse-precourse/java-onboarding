10161ed879ab0f1a5f0c2d1e7c394164dff9c45d
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-01 15:15:26 +0900

    feat:A-Z순으로 변경되는 알파벳을 배열로 만듬

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        String[] array_word;
+        String[] change_word = {"Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"};
+
+        Map<String, String> settings = new HashMap<>();
+
+        char[] alphabet = new char[26];
+
+        for(int i = 0; i < alphabet.length; i++){
+            settings.put(Character.toString((char)('A' + i)), change_word[i]);
+        }
+
+        array_word = word.split("");
+
         return answer;
     }
 }
