c2c9ab79db176dccd87868d6ae695038e843ed6f
pyj <les85611@hanyang.ac.kr>
2022-11-01 21:09:12 +0900

    [p5] coin 리스트에 반환할 지폐 단위를 넣어 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,15 +9,7 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>(Collections.emptyList());
         int n = money;
         int[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
-        for (int i  = 0; i < coin.length; i++) {
-            if(n>=coin[i]) {
-                answer.add(n/coin[i]);
-                n = n % coin[i];
-            }
-            else answer.add(0);
-        }
-
+        
         return answer;
 
 
