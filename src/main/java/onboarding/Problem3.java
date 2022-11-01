6dcaeac22be9a433dd5c4144483a99e215550b36
JangHyojoon <fdasfaj9@gmail.com>
2022-11-01 18:48:12 +0900

    문제 3번 기능 2 해당 숫자는 몇번 박수를 치는지 세는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,5 +12,21 @@ public class Problem3 {
 
         return true;
     }
+    static int clap_count(int num){
+        // num의 각 자리를 알아보기 위해 String tmp 선언
+        String tmp = Integer.toString(num);
+        // 총 카운트 횟수
+        int cnt = 0;
 
+        // 각 자리의 숫자들을 보며 3, 6, 9이면 cnt를 1씩 증가시킨다.
+        for (int i = 0; i < tmp.length(); i++) {
+            if(tmp.charAt(i) == '3'
+                || tmp.charAt(i) == '6'
+                || tmp.charAt(i) == '9'){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
 }
