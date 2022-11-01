de5d29c6ce8761c8292e37e93a79a62198cd53ed
d0ri123 <lsh81550@gmail.com>
2022-11-01 14:34:07 +0900

    Problem2 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        StringBuilder sb = new StringBuilder(cryptogram);
+        boolean flag=true;
+        while(flag){
+            flag = false;
+            for(int i=0; i<sb.length()-1; i++) {
+                if(sb.charAt(i) == sb.charAt(i+1)){
+                    sb.deleteCharAt(i+1);
+                    sb.deleteCharAt(i);
+                    flag = true;
+                }
+            }
+            System.out.println(sb);
+        }
+        answer = sb.toString();
         return answer;
     }
 }
