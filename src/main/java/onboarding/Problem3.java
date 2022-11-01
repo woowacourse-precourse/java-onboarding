45df2ee18941d0ef5f3a2541f50433bddf61ee66
i960107 <i960107@naver.com>
2022-11-01 12:09:44 +0900

    feat(problem3): add constant and cache space and implement feature of get claps required at nth order

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    static int[] totalClapsCache = new int[10001];
+    static final int EMPTY = -1;
+
+    static {
+        Arrays.fill(totalClapsCache, EMPTY);
     }
+
+    static final List<Character> conditions = Arrays.asList('3', '6', '9');
+
+
+    public static int solution(int number) {
+        return 0;
+    }
+
+    public static int getClaps(int order) {
+
+        Stream<Character> characterStream = String.valueOf(order).
+                chars().
+                mapToObj(x -> (char) x);
+
+        return (int) (characterStream
+                .filter(ch -> conditions.contains(ch))
+                .count());
+    }
+
 }
+
+
