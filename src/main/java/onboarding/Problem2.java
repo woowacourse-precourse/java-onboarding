cb80c21508983d60d2a7985d8450a301a15ec61a
dlgosla <dlgoslaek1@gmail.com>
2022-11-01 11:23:22 +0900

    feature: [Problem2] 중복 제거 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,46 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
     }
+
+    public static String remove_duplication_from(String cryptogram) {
+        int length = cryptogram.length();
+
+        Deque<Character> dq = new ArrayDeque<>();
+        dq.addLast(cryptogram.charAt(0));
+
+        Character prev = cryptogram.charAt(0);
+
+        for (int i=1; i < length; i++) {
+            Character curr = cryptogram.charAt(i);
+
+            if (prev == curr){
+                if (dq.peekLast() == curr) {
+                    dq.removeLast();
+                }
+            } else {
+                dq.addLast(curr);
+            }
+
+            prev = curr;
+        }
+
+
+        String result = "";
+        while (!dq.isEmpty()){
+            result += dq.removeFirst();
+        }
+
+        return result;
+
+    }
+
+
 }
