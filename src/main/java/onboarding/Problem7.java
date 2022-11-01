77c9f2d1520dfb5404a65a71465d2e44d33e931f
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 15:26:08 +0900

    feat: 사용자의 친구 목록 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,16 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 
     private final static int TIMELINE_VISIT_SCORE = 1;
     private final static int KNOW_TOGETHER_SCORE = 10;
 
+    private static Set<String> userFriends;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        userFriends = createUserFriendsSet(user, friends);
+
         return answer;
+    }
+
+    private static Set<String> createUserFriendsSet(String user, List<List<String>> friends) {
+        return friends.stream()
+                .filter(pair -> pair.contains(user))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toCollection(HashSet::new));
     }
 }
