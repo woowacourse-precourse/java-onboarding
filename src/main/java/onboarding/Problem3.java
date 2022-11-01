b90537a42d3503b53535b7116b0c54e79d7ee5b7
pyj <les85611@hanyang.ac.kr>
2022-11-01 21:01:46 +0900

    [p3] while 문으로 answer의 카운트를 도와줄 수식 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,14 @@ public class Problem3 {
         for (int i = 1; i <= number; i++) {
             int current = i;
             int temp = answer;
-            
+            while (current != 0) {
+                //current가 0이 아니고 입력된 숫자가 3, 6, 9가 있다면 카운트한다.
+                if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9){
+                    answer++;
+                }
+                current /= 10;
+            }
+
         }
 
         return answer;
