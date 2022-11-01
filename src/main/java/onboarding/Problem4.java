bac4162fae123b107de98c68b8a647abd53c0858
JoonHyug <hyug0819@gmail.com>
2022-11-01 20:58:21 +0900

    4번 문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] words = word.toCharArray();
+
+        for(char c : words){
+            if('A' <= c && c <= 'Z'){
+                answer += (char)(('A'+'Z') - c);
+            } else if('a' <= c && c <= 'z'){
+                answer += (char)(('a'+'z') - c);
+            } else {
+                answer += c;
+            }
+        }
         return answer;
     }
 }
