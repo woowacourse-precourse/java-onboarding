adc99fd637c1e68984d17948d737d336bb7cb0bc
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 14:02:12 +0900

    FEAT decrypt

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,12 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decrypt(cryptogram);
 
         return answer;
     }
 
     public static String deduplicate(String s) {
         return s.replaceAll("(([a-zA-Z\\d])\\2+)", "");
+    }
+
+    public static String decrypt(String cryptogram) {
+        String deduplication = deduplicate(cryptogram);
+
+        if(deduplication.equals(cryptogram)){
+            return deduplication;
+        }
+
+        return decrypt(deduplication);
     }
 }
