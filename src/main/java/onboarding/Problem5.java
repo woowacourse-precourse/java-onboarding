a90e8ac8ed6c74d606940cad4759ff0dc91ef33b
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:58:41 +0900

    feat(problem5): money 유효성 검사

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        validate(money);
+        return new ArrayList<>();
+    }
+
+    private static void validate(int money) {
+        if (money < 1 || money > 1_000_000) {
+            throw new IllegalArgumentException("유효하지 않은 금액입니다.");
+        }
     }
 }
