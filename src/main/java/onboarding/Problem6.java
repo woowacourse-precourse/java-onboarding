ab15a0a9ba1d76b9690d13edbedfcc91abe11e78
minhee020 <minhee020@naver.com>
2022-11-01 12:53:56 +0900

    Problem6 | 기능별로 함수 나누기(리팩토링)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,10 @@ import java.util.List;
  * 2. result 문자열 오름차순 정렬
  */
 public class Problem6 {
-    static List<Boolean> Duplicate;
+    static List<Boolean> Duplicate = new ArrayList<>();
+    static List<String> answer = new ArrayList<>();
+    static List<String> nicknames = new ArrayList<>();
+
     private static boolean isDuplicate(String str1, String str2){
         for(int i = 0; i < str1.length() - 1 ; i++){
             for (int j = 0; j < str2.length() - 1; j++){
@@ -21,16 +24,7 @@ public class Problem6 {
         }
         return false;
     }
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
-        Duplicate = new ArrayList<>();
-        List<String> nicknames = new ArrayList<>();
-
-        for (int i = 0; i < forms.size(); i++){
-            Duplicate.add(false);
-            nicknames.add(forms.get(i).get(1));
-        }
-
+    private static void setBooleanValueDup(List<List<String>> forms){
         for (int i = 0; i < forms.size() - 1; i++){
             for (int j = i + 1; j < forms.size(); j++){
                 if (!Duplicate.get(j) && isDuplicate(nicknames.get(i), nicknames.get(j))){
@@ -39,6 +33,14 @@ public class Problem6 {
                 }
             }
         }
+    }
+    public static List<String> solution(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++){
+            Duplicate.add(false);
+            nicknames.add(forms.get(i).get(1));
+        }
+
+        setBooleanValueDup(forms);
 
         for (int i = 0; i < forms.size(); i++){
             if (Duplicate.get(i)){
