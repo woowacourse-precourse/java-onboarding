b0c87e4f114e8dbb381efc91ffe9610af343edad
lynne921 <dkfls921@gmail.com>
2022-11-01 22:11:17 +0900

    [feat][problem7] 친구 리스트 구하는 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,29 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem7 {
+
+    private static List<String> GetFriendShipFromName(List<List<String>> friendRelations, String user) {
+        List<String> friendList = new LinkedList<>();
+        friendList.add(user);
+        for (List<String> friendRelation : friendRelations) {
+            if (friendRelation.contains(user)) {
+                friendRelationAddUser(friendList, friendRelation, user);
+            }
+        }
+        return friendList;
+    }
+
+    private static void friendRelationAddUser(List<String> friendList, List<String> friendRelation, String user) {
+        for (String friendName : friendRelation) {
+            if (!friendName.equals(user))
+                friendList.add(friendName);
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
