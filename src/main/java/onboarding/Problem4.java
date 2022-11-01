0e1e8682e2e9a7d80c9a2e650adf525d75c5cc45
CJY-kor <vivo_reprise@naver.com>
2022-11-01 17:26:45 +0900

    DOCS(problem4) : 기능 목록 i), ii) iii) 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,3 +1,10 @@
+/*
+기능 목록
+i) getReversedAlphabet -> Ascii 코드를 이용하여 입력받은 char 을 변형하여 표에 맞춘 문자를 반환함
+ii) getReversedString -> i) 을 이용하여 문장 전체를 변형한 뒤 반환함.
+iii) solution -> ii) 을 이용하여 정답 반환
+ */
+
 package onboarding;
 
 public class Problem4 {
