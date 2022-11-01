f7ad8e522ddd52ffee9c07a9577337292e657b2c
Eungyu-code <Agyu625@gmail.com>
2022-11-01 12:55:17 +0900

    예외사항을 체크하는 함수 except() 함수와 예외사항 존재 시에는 -1을 리턴하는 로직 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        // 예외 사항에 대한 함수 except()
+        // 최대값을 구하는 함수 maxNumber()
+
+        if (! except(pobi) || ! except(crong)) return -1;
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static boolean except(List<Integer> list) {
+
+        return list.get(0).equals(list.get(1) - 1);
     }
 }
