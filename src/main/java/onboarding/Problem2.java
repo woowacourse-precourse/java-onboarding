3de062fa809047ebdcf55cd797dbc8d869816831
wwwndus <simock22@gmail.com>
2022-11-01 23:29:30 +0900

    feat:문자열을 char 배열로 자름

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,40 +1,20 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        char[] chars = cryptogram.toCharArray();
 
-        String[] str = cryptogram.split("");
-        List<String> strList = new ArrayList<>(Arrays.asList(str));
-        boolean boo = true;
+        List<String> charList = new LinkedList<>();
+        boolean isDuple = true;
 
-        while (boo) {
-            if (strList.size() == 0) {
-                break;
-            }
-            boo = isBoo(strList, boo);
-        }
 
-        answer = String.join("", strList);
 
         return answer;
     }
 
-    private static boolean isBoo(List<String> strList, boolean boo) {
-        for (int i = 0; i < strList.size()-1; i++) {
-            if (strList.get(i).equals(strList.get(i+1))) {
-                strList.remove(i);
-                strList.remove(i);
-                boo = true;
-                break;
-            } else {
-                boo = false;
-            }
-        }
-        return boo;
-    }
+
 }
