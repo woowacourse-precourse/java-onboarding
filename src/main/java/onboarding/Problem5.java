eee24eb92a5606fbe9fc31e4f1e04d8247ad2f82
ingpyo <juninpyo72@gmail.com>
2022-11-01 11:51:21 +0900

    [problem5]전체 코드

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,15 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         checkList(money);
 
+        List<Integer> answer = new ArrayList<>();
+        int[] won = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < won.length; i++) {
 
+            answer.add(money / won[i]);
+            money %= won[i];
+        }
+
+            return answer;
     }
 
     public static void checkList(int money) throws IllegalArgumentException{
