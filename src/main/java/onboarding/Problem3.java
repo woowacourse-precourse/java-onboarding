2a9eb49086524fd028d189719d66b1b6401478c4
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:03:14 +0900

    [3번] feat: 현재 숫자에서 ClapNumber 제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,16 @@ public class Problem3 {
         return answer;
     }
 
+    // 3. 현재 숫자에서 ClapNumber(3,6,9)만 제거하기
+    public static String removeClapNumber(String num)
+    {
+        String removedNum = num
+                .replace("3", "")
+                .replace("6", "")
+                .replace("9", "");
+        return removedNum;
+    }
+
     // 4. int -> String 타입 변환
     public static String changeIntToString(int num)
     {
