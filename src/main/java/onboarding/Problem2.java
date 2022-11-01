5db7515d266929ace47f3c08f1000ecb5bf0427a
si hwan <pum005@naver.com>
2022-11-01 22:19:08 +0900

    feat (problem2) : 중복문자 제거해 문자를 복원하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,5 +12,48 @@ public class Problem2 {
         if(!(P2ExceptionValidation.p2ExceptionValidation(cryptogram))){
             return "";
         }
+        String decrypedString = findDecryptedString(cryptogram);
 
+        return decrypedString;
+    }
+
+    public static String findDecryptedString(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        int duplicatedCount = 0;
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            String s = cryptogram.substring(i,i+1);
+
+            Character current = s.charAt(0);
+
+            if (stack.empty()) {
+                stack.push(current);
+            }
+            else {
+                if (stack.peek() != current) {
+                    for(int j = 0 ; j < duplicatedCount ; j++){
+                        if(!stack.isEmpty()){
+                            stack.pop();
+                        }
+                    }
+                    duplicatedCount = 0;
+                }
+                if (!stack.isEmpty() && stack.peek() == current) {
+                    if (duplicatedCount == 0) {
+                        duplicatedCount += 1;
+                    }
+                    duplicatedCount += 1;
+                }
+                stack.push(s.charAt(0));
+            }
+        }
+        for(int i = 0 ; i < duplicatedCount ; i++){
+            if(!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+
+
+        return stack.stream().map((v) -> v.toString()).collect(Collectors.joining(""));
+    }
 }
