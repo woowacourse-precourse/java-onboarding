7906720a312351dde5803dff78f5847bc0d71b4d
Geun <rlarjs220@naver.com>
2022-11-01 22:11:35 +0900

    6버부터 많이 어려워졌네

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,86 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        boolean page = exception(pobi, crong);
+        if (!page) {
+            return -1;
+        }
+
+        int pobi_sum = sum(pobi);
+        int pobi_mul = mul(pobi);
+        int pobi_max = max(pobi_sum, pobi_mul);
+
+        int crong_sum = sum(crong);
+        int crong_mul = mul(crong);
+        int crong_max = max(crong_sum, crong_mul);
+
+
+        if (pobi_max > crong_max) {
+            return 1;
+        } else if (pobi_max < crong_max) {
+            return 2;
+        } else if (pobi_max == crong_max) {
+            return 0;
+        } else {
+            return -1;
+        }
+
+
+    }
+    private static int sum(List<Integer> value) {
+
+        int val1 = 0;
+        int val2 = 0;
+
+        String[] array1 = String.valueOf(value.get(0)).split("");
+        for (String val : array1) {
+            val1 += Integer.valueOf(val);
+        }
+
+        String[] array2 = String.valueOf(value.get(1)).split("");
+        for (String val : array2) {
+            val2 += Integer.valueOf(val);
+        }
+        return max(val1, val2);
+    }
+
+    private static int mul(List<Integer> value) {
+
+        int val1 = 1;
+        int val2 = 1;
+
+        String[] array1 = String.valueOf(value.get(0)).split("");
+        for (String val : array1) {
+            val1 = val1 * Integer.valueOf(val);
+        }
+
+        String[] array2 = String.valueOf(value.get(1)).split("");
+        for (String val : array2) {
+            val2 = val2 * Integer.valueOf(val);
+        }
+        return max(val1, val2);
+    }
+
+    private static int max(int a, int b) {
+
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+
+    }
+
+    private static boolean exception(List<Integer> pobi, List<Integer> crong) {
+        boolean ex = true;
+
+        if (1 != Math.abs(pobi.get(0) - pobi.get(1))) {
+            return false;
+        } else if (1 != Math.abs(crong.get(0) - crong.get(1))) {
+            return false;
+        } else {
+            return ex;
+        }
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int result = 0;
+
+        for(int t = 1; t <= number; t++){
+
+            String str = String.valueOf(t);
+
+            for (int i = 0; i < str.length(); i++){
+
+                char check = str.charAt(i);
+
+                if(check == '3' || check == '6' || check == '9'){
+
+                    result = result + 1;
+
+                }
+            }
+        }
+        return result;
     }
 }
