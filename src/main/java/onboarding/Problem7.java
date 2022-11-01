d439d055dc4203fefedf11d23cf99164da10c795
kimdoha <kkhhjj3393@gmail.com>
2022-11-01 23:28:18 +0900

    chore: problem7 주석 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,6 +7,9 @@ public class Problem7 {
 
     private static HashMap<String, Integer> recommends = new HashMap<>();
 
+    /*
+    * friends 각 원소에서 친구 찾기 함수
+    */
     private static String findFriendInPairs(String user, List<String> pairs) {
 
         return pairs.get(0).equals(user) ?
@@ -14,6 +17,9 @@ public class Problem7 {
                pairs.get(0);
     }
 
+    /*
+     * 유저의 친구 리스트 얻는 함수
+     */
     private static List<String> getfriendListOfUser(String user, List<List<String>> friends) {
         ArrayList<String> friendList = new ArrayList<>();
 
@@ -27,12 +33,18 @@ public class Problem7 {
         return friendList;
     }
 
+    /*
+     * 사용자와 함께 아는 친구에게 점수를 부여하는 함수
+     */
     private static void setScoreToFriendOfUserFriend(List<String> friendsOfUserFriends) {
 
         for(String friend: friendsOfUserFriends)
             recommends.putIfAbsent(friend, 10);
     }
 
+    /*
+     * 친구 관계 정보에 따른 점수 부여 함수
+     */
     private static void getFriendsScore(String user, List<List<String>> friends) {
         List<String> userFriends = getfriendListOfUser(user, friends);
 
@@ -45,6 +57,10 @@ public class Problem7 {
 
         }
     }
+
+    /*
+     * 방문 기록에 따른 점수 부여 함수
+     */
     private static void getVisitorsScore(List<String> userFriends, List<String> visitors) {
 
         for(String visitor : visitors) {
@@ -57,6 +73,10 @@ public class Problem7 {
         }
     }
 
+    /*
+     * HashMap 정렬
+     * 점수 내림차순 , 점수 같을 시 이름 오름차순
+     */
     public static int compareByScoreAndName(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
         int comparison = (o1.getValue() - o2.getValue()) * -1;
         return comparison == 0 ? o1.getKey().compareTo(o2.getKey()) : comparison;
@@ -69,6 +89,9 @@ public class Problem7 {
         return entries;
     }
 
+    /*
+     * Entry -> List 로 변환하는 함수
+     */
     private static List<String> convertEntriesToList(List<Map.Entry<String, Integer>> entries) {
         List<String> result = new ArrayList<>(Collections.emptyList());
 
@@ -81,11 +104,9 @@ public class Problem7 {
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> userFriends = getfriendListOfUser(user, friends);
-        System.out.println(userFriends);
-
-
         getFriendsScore(user, friends);
         getVisitorsScore(userFriends, visitors);
+
         List<Map.Entry<String, Integer>> entries = sort(recommends);
         List<String> recommendResult = convertEntriesToList(entries);
 
