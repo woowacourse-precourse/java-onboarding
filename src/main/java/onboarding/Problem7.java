bbebf4b539d9dfcbce6f8c61734e8f5c46dc8a05
i960107 <i960107@naver.com>
2022-11-01 13:58:19 +0900

    feat(problem7): add constants

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Map;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+    public static final int MAX_RECOMMEND_SIZE = 5;
+    public static final int FRIEND_IN_COMMON_SCORE = 10;
+    public static final int VISITOR_SCORE = 1;
+
+    public static final Comparator<Map.Entry<String, Integer>> byValDesc = Map.Entry.comparingByValue(Comparator.reverseOrder());
+    public static final Comparator<Map.Entry<String, Integer>> byKeyAsc = Map.Entry.comparingByKey();
+    public static final Comparator<Map.Entry<String, Integer>> sortConditions = byValDesc.thenComparing(byKeyAsc);
+
+
 }
