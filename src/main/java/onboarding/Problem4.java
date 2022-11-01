536180e42f278bfceabfcfcac0b8c0f855777210
baexxbin <soo3886@gmail.com>
2022-11-01 18:23:55 +0900

    Refactor(PROBLEM4): "solution함수 if-else 구문 리펙토링"
    else문 삭제 및 후위연산자 사용으로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,13 +7,14 @@ public class Problem4 {
 
 		for (int ascii : wordList) {
 			if (65 <= ascii && ascii <= 90) {
-				wordList[idx] = (char)((char)25 - (ascii - 65) + 65);
-			} else if (97 <= ascii && ascii <= 122) {
-				wordList[idx] = (char)((char)25 - (ascii - 97) + 97);
-			} else {
-				wordList[idx] = (char)ascii;
+				wordList[idx++] = (char)((char)25 - (ascii - 65) + 65);
+				continue;
 			}
-			idx++;
+			if (97 <= ascii && ascii <= 122) {
+				wordList[idx++] = (char)((char)25 - (ascii - 97) + 97);
+				continue;
+			}
+			wordList[idx++] = (char)ascii;
 		}
 		return String.valueOf(wordList);
 	}
