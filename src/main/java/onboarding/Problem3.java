30223d19c9fdb0010b36c463412e36d10d01b208
mng990 <ryoo0504@pusan.ac.kr>
2022-11-01 14:32:45 +0900

    2022.11.01 14:32

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -16,14 +16,14 @@ public class Problem3 {
             if(head%3 == 0 && head != 0){
                 if(i<len -1) answer += Integer.parseInt(numStr.substring(i+1));
                 answer +=1;
-            }
+            } // 현재 자릿수로 인해 발생하는 박수 수
 
             int power = len - 1 -i;
 
-            if(head > 0) answer += (head - 1)/3 * Math.pow(10, power);
+            if(head > 0) answer += (head - 1)/3 * Math.pow(10, power); // 자릿수는 같지만 number보다 작은 수들의 박수 수
 
-            answer += 3*Math.pow(10, power)*prevhead;
-        }
+            answer += 3*Math.pow(10, power)*prevhead; // 상위 자릿수에서 발생하는 현재 자릿수의 박수 수
+        } // 각 자릿수 마다 반복
 
         return answer;
     }
