6ec8aa1b48c3e304162e14407d040016ad365d60
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-11-01 19:34:53 +0900

    comment: add comments for problem 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        // people = 등장하는 사람의 아이디를 모두 저장 (중복 X)
         HashSet<String> people = new HashSet<>();
         for (int i = 0; i < friends.size(); i++) {
             people.add(friends.get(i).get(0));
@@ -15,23 +16,27 @@ public class Problem7 {
 
         List<String> peopleList = new ArrayList<>(people);
 
+        // friend_map = {key: 아이디(string), value: 친구 목록(list<string>)}
         HashMap<String, List<String>> friend_map = new HashMap<>();
+        // score_map = {key: 아이디(string), value: 추천 점수(integer)}
         HashMap<String, Integer> score_map = new HashMap<>();
 
         for (List<String> friend : friends) {
             String first = friend.get(0);
             String second = friend.get(1);
 
+            // 친구 리스트의 첫번째 아이디가 이미 map의 key로 있으면, list에 두번째 아이디추가
             if (friend_map.containsKey(first)) {
                 List<String> list = friend_map.get(first);
                 list.add(second);
                 friend_map.put(first,list);
+                // 친구 리스트의 첫번째 아이디가 map의 key로 없으면, list 생성 후 list에 두번째 아이디추가
             }else{
                 List<String> list = new ArrayList<>();
                 list.add(second);
                 friend_map.put(first, list);
             }
-
+            // 첫번째와 두번째 아이디 순서를 바꿔서 위의 연산 재실행
             if (friend_map.containsKey(second)){
                 List<String> list = friend_map.get(second);
                 list.add(first);
@@ -42,18 +47,18 @@ public class Problem7 {
                 friend_map.put(second,list);
             }
         }
-
+        // score_map = {key: 아이디(string), value: 추천 점수(integer)}
         for(String pl : peopleList){
             String key = pl;
             Integer value=0;
             score_map.put(key,value);
         }
-
+        //userFriends = user의 친구를 저장한 리스트
         List<String> userFriends = friend_map.get(user);
-
         for(String userFriend : userFriends){
             List<String> scoreFriends = friend_map.get(userFriend);
             for(String scoreFriend : scoreFriends){
+                //scoreFriend가 user가 아니고, user와 친구가 아니면 추천 점수 10점 추가
                 if(!scoreFriend.equals(user) && !friend_map.get(user).contains(scoreFriend)) {
                     Integer score = score_map.get(scoreFriend);
                     score_map.put(scoreFriend, score + 10);
@@ -62,6 +67,7 @@ public class Problem7 {
         }
 
         for(String visitor: visitors){
+            //visitor가 user의 친구목록에 없으면 추천 점수 1점추가
             if (!friend_map.get(user).contains(visitor)) {
                 Integer score = score_map.get(visitor);
                 score_map.put(visitor, score + 1);
@@ -69,7 +75,7 @@ public class Problem7 {
         }
 
         List<Map.Entry<String,Integer>> entries = new ArrayList<Map.Entry<String,Integer>>(score_map.entrySet());
-
+        // 정렬
         Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
