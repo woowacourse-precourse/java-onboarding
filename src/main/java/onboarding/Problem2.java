14756cac86c37ef3d75d6b57a1801a5b307544f1
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:25:47 +0900

    feat(problem2): cryptogram 유효성 검사

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        validate(cryptogram);
+        return "";
+    }
+
+    private static void validate(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new IllegalArgumentException("길이가 1 이상 1000 이하인 문자열이어야 합니다.");
+        }
+        for (char character : cryptogram.toCharArray()) {
+            if (!(Character.isLowerCase(character) && Character.isAlphabetic(character))) {
+                throw new IllegalArgumentException("알파벳 소문자로만 이루어져 있어야 합니다.");
+            }
+        }
     }
 }
