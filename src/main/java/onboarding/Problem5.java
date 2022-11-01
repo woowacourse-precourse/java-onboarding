2b1844eaef2cf8f0f4ab63ce5e512632a9763382
kssumin <201566@jnu.ac.kr>
2022-11-01 17:56:17 +0900

    feat : 화폐가 몇장 필요한지 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,3 +9,30 @@ public class Problem5 {
         return answer;
     }
 }
+
+class Money {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 1000000;
+    private static final String NOT_ALLOWED_UNDER_MIN_RANGE = "money는 1 이상의 자연수입니다.";
+    private static final String NOT_ALLOWED_OVER_MAX_RANGE = "money는 1000000 이하의 자연수입니다.";
+    private final int money;
+
+    public Money(int money) {
+        validateMoneyRange(money);
+        this.money = money;
+    }
+
+    private void validateMoneyRange(int money) {
+        if (money < MIN_RANGE) {
+            throw new IllegalStateException(NOT_ALLOWED_UNDER_MIN_RANGE);
+        }
+        if (money > MAX_RANGE) {
+            throw new IllegalStateException(NOT_ALLOWED_OVER_MAX_RANGE);
+        }
+    }
+
+    private int changeMoney(Money moneyUnit) {
+        return this.money / moneyUnit.money;
+    }
+}
+
