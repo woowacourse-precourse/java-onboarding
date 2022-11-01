d7698b4b7e0e671c7dd357d8e7924f97d053cd05
hyejuc <chjjudy@naver.com>
2022-11-01 19:17:13 +0900

    findNotFriendInVisitors : visitors에 있는 아이디 중 user와 친구가 아닌 아이디를 탐색해 아이디를 key, 점수를 value로 hashmap에 추가, 중복일 경우 점수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,6 +11,7 @@ public class Problem7 {
         List<String> userFriends = findUserFriend(user, friends);
         HashMap<String, Integer> notUserFriends = new HashMap<>();
         notUserFriends = findNotFriendInFriends(user, friends, userFriends, notUserFriends);
+        notUserFriends = findNotFriendInVisitors(userFriends, visitors, notUserFriends);
         return answer;
     }
     public static List<String> findUserFriend(String user, List<List<String>> friends){
@@ -52,4 +53,18 @@ public class Problem7 {
         }
         return notUserFriends;
     }
+    public static HashMap<String,Integer> findNotFriendInVisitors(List<String> userFriends, List<String> visitors, HashMap<String, Integer> notUserFriends){
+        for (int i = 0; i < visitors.size(); i++) {
+            if (!userFriends.contains(visitors.get(i))){
+                if (!notUserFriends.containsKey(visitors.get(i))){
+                    notUserFriends.put(visitors.get(i), 1);
+                }
+                else {
+                    notUserFriends.put(visitors.get(i), notUserFriends.get(visitors.get(i))+1);
+                }
+            }
+        }
+        return notUserFriends;
+    }
+
 }
