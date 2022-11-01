d241e53f265156ac9b2a89742de03fc0479fdba6
Sehoon Kim <shggm2000@gmail.com>
2022-11-01 14:25:13 +0900

    HashMap을 사용해 청개구리 사전을 대문자와 소문자 각각 따로 담는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,10 +10,22 @@ public class Problem4 {
 
         char[] arr = word.toCharArray();
         ArrayList<Character> list = new ArrayList<Character>();
+        HashMap<Character, Character> upperAlphet = new HashMap<Character,Character>();
+        HashMap<Character, Character> lowerAlphet = new HashMap<Character,Character>();
 
         for (int k = 0; k < arr.length; k++) {
             list.add(arr[k]);
         }
+
+        for(int i=0; i<26; i++){
+            upperAlphet.put((char) ('A'+i), (char) ('Z'-i));
+        }
+
+        for(int k=0; k<26; k++){
+            lowerAlphet.put((char) ('a'+k), (char) ('z'-k));
+        }
+
+
 
 
 
