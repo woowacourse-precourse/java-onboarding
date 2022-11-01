0e6d35b1e5c07fd6e3a5a9647bf23b699e2bb684
Yebin <lyb2325@gmail.com>
2022-11-01 21:16:25 +0900

    feat: Problem6 이메일 도메인 확인 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, ArrayList<String>> consecutiveLettersMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            if (!email.endsWith("@email.com")) {  // 이메일의 도메인 확인
+                continue;
+            }
+        }
+
         return answer;
     }
 }
