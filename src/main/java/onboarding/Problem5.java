88acf3b350c403b8bd35056f5c6429f381ac163b
simjaeseo <delpho@naver.com>
2022-11-01 19:13:19 +0900

    feat: 문제 5 기능 목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. money를 큰 금액을 우선으로 변환하는 함수
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
