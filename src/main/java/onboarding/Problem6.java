32ef65bc2d24dda8dd15a064fd1b30a0e499514e
Suzie Lee <wnsuwls0707@gmail.com>
2022-11-01 23:00:21 +0900

    feat:Problem6, func repeatedEmails 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,15 +10,12 @@ public class Problem6 {
         HashMap<String , ArrayList<String>> dicNickname = new HashMap<>();
         for(int i=0;i<forms.size();i++){
             String nickname =forms.get(i).get(1);
-            System.out.println(nickname);
             for(int j = 1; j< nickname.length(); j++){
                 String tmp = nickname.charAt(j-1)+""+nickname.charAt(j);
                 if (dicNickname.containsKey(tmp)){
                     ArrayList<String> list = dicNickname.get(tmp);
                     list.add(forms.get(i).get(0));
                     dicNickname.put(tmp,list);
-                    System.out.println(dicNickname);
-
                 }
                 else{
                     ArrayList<String> list = new ArrayList<>();
@@ -28,9 +26,20 @@ public class Problem6 {
         }
         return dicNickname;
     }
+    public static List<String> repeatedEmails(HashMap<String , ArrayList<String>> dicNickname,ArrayList<String> answer){
+        for ( ArrayList emails:dicNickname.values()){
+            if(emails.size()>1){
+
+                answer.addAll(emails);
+            }
+        }
+        return answer;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        nicknameDic(forms);
+        ArrayList<String> answer = new ArrayList<>();
+
+        answer = (ArrayList<String>) repeatedEmails(nicknameDic(forms),answer);
+        Collections.sort(answer);
         return answer;
     }
 }
