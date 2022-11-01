f4811516225235be6d73d4b40a00b732599412a7
ONE <sodlapdlf13@gmail.com>
2022-11-01 18:26:26 +0900

    refactor: [Problem2] Stack을 사용하는 로직을 정규표현식을 사용하도록 변경하는 문을 사용하는 메소드로 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,44 +1,26 @@
 package onboarding;
 
-import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 1. removeDuplication(): 정규표현식을 반복해서 사용해 중복된 문자를 제거하고 문자열을 반환하는 메서드
- *   1-1. 중복된 문자를 재귀적으로 제거
+ *   1-1. 중복된 문자를 반복적으로 제거
  */
 
 public class Problem2 {
-    private static int index = 0;
     public static String solution(String cryptogram) {
-        Stack<Character> stack = new Stack<>();
+        return removeDuplication(cryptogram);
+    }
 
-        while (index < cryptogram.length()) {
-            if (isOverlap(cryptogram, stack)) {
-                removeOverlap(cryptogram, stack);
-                continue;
-            }
-            stack.push(cryptogram.charAt(index++));
+    private static String removeDuplication(String cryptogram) {
+        Pattern pattern = Pattern.compile("(\\w)\\1+");
+        Matcher matcher = pattern.matcher(cryptogram);
+
+        while (matcher.find()) {
+            cryptogram = matcher.replaceAll("");
+            matcher = pattern.matcher(cryptogram);
         }
-
-        return stackToString(stack);
-    }
-
-    private static boolean isOverlap(String cryptogram, Stack<Character> stack) {
-        return !stack.isEmpty() && index < cryptogram.length() && stack.peek().equals(cryptogram.charAt(index));
-    }
-
-    private static void removeOverlap(String cryptogram, Stack<Character> stack) {
-        while (isOverlap(cryptogram, stack)) {
-            index++;
-        }
-        stack.pop();
-    }
-
-    private static String stackToString(Stack<Character> stack) {
-        StringBuilder sb = new StringBuilder();
-
-        stack.forEach(sb::append);
-
-        return sb.toString();
+        return cryptogram;
     }
 }
