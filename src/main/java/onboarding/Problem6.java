0bcf45563d23969757ab1075cc4137b1d2608471
yeop0740 <ssucgoo@gmail.com>
2022-11-01 21:07:53 +0900

    make method checkSubstring(Problem6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,15 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static boolean checkSubString(List<String> name1, List<String> name2) {
+        boolean result = false;
+        for (int i = 0; i < name1.get(1).length() - 1; i++) {
+            if (name2.get(1).contains(name1.get(1).substring(i, i + 2))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
