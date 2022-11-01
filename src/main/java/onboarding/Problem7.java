ae4fcb613af74ea231512cff91404fb6678b3aba
Damgom <wisewish1@gmail.com>
2022-11-01 15:23:08 +0900

    Fix: member friends list return value

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -20,20 +20,25 @@ public class Problem7 {
     }
     private static Map<String, List<String>> getMembersFriendList(List<List<String>> friends) {
         Map<String, List<String>> memberFriendList = new HashMap<>();
-        List<String> friendsList = new ArrayList<>();
-        List<String> friendsList2 = new ArrayList<>();
+
         for(int i = 0; i < friends.size(); i++) {
             String member = friends.get(i).get(0);
             String member2 = friends.get(i).get(1);
 
-            if (friendsList.isEmpty()) {
-                friendsList.add(member2);
-            } else if(!friendsList.contains(member2)) friendsList.add(member2);
+            List<String> friendsList = new ArrayList<>();
+            List<String> friendsList2 = new ArrayList<>();
+
+            if (memberFriendList.get(member) != null) {
+                friendsList = memberFriendList.get(member);
+            }
+            friendsList.add(member2);
             memberFriendList.put(member, friendsList);
 
-            if (friendsList2.isEmpty()) {
-                friendsList2.add(member);
-            } else if(!friendsList2.contains(member)) friendsList2.add(member);
+
+            if (memberFriendList.get(member2) != null) {
+                friendsList2 = memberFriendList.get(member2);
+            }
+            friendsList2.add(member);
             memberFriendList.put(member2, friendsList2);
         }
         return memberFriendList;
