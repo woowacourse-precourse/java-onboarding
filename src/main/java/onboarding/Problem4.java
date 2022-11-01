3fa9f953d18190caed1a71e216c588baf6c6b32b
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:49:52 +0900

    feat(problem4): 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,7 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         validate(word);
-        return "";
+        return convertMessage(word);
     }
 
     private static void validate(String word) {
@@ -11,5 +11,23 @@ public class Problem4 {
         if (length < 1 || length > 1000) {
             throw new IllegalArgumentException("word는 길이가 1 이상 1,000 이하인 문자열이어야 합니다.");
         }
+    }
+
+    private static String convertMessage(String word) {
+        StringBuilder convertedMessage = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            convertedMessage.append(convertCharacter(word.charAt(i)));
+        }
+        return convertedMessage.toString();
+    }
+
+    private static char convertCharacter(char character) {
+        if (Character.isLowerCase(character)) {
+            character = (char) ('z' - (character - 'a'));
+        }
+        if (Character.isUpperCase(character)) {
+            character = (char) ('Z' - (character - 'A'));
+        }
+        return character;
     }
 }
