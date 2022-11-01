be50d8d334cb0523c8ba2644fe077a22561cfabe
ho2n <jungho2n@gmail.com>
2022-11-01 17:10:56 +0900

    반환해야 하는 문자열인지 체크하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (!isReturn(answer)) {
+            answer = removeRepetition(answer);
+        }
+
         return answer;
+    }
+
+    public static boolean isReturn(String cryptogram) {
+        boolean isReturn = true;
+        for (int i = 1; i < cryptogram.length(); ++i) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i - 1)) {
+                isReturn = false;
+                return isReturn;
+            }
+        }
+        if (cryptogram == "") {
+            isReturn = true;
+        }
+        return isReturn;
     }
 }
