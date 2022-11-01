91e626a5c89f1711b151783b993311f8d2c33eca
jamangstangs <jamangsong@gmail.com>
2022-11-01 10:31:51 +0900

    feat: problem2(진행), decrypt() -> 주어진 cryptogram을 문제의 정의에 따라 해독하는 메소드 선언

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        String lastVersionCryptogram = cryptogram;
+        String recentVersionCryptogram = decrypt(cryptogram);
         String answer = "answer";
         return answer;
     }
+
+    private static String decrypt(String cryptogram) {
+        List<Integer> continuousCharIndices = findContinuousChar(cryptogram);
+
+        return eraseCharByIndex(continuousCharIndices, cryptogram);
+
+    }
+
+    private static List<Integer> findContinuousChar(String cryptogram) {
+    }
+
+    private static String eraseCharByIndex(List<Integer> continuousCharIndices, String cryptogram) {
+    }
+
 }
