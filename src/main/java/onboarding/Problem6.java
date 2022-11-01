5bf784cca4b3d456023fe3c99a5a183009879ae3
KimJaeSung <behappy0916@naver.com>
2022-11-01 14:26:47 +0900

    feat java-onboarding6 닉네임을 두글자씩 잘라서 판별하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        Set<String> answerSet = new HashSet<>();
+        HashMap<String, String> emailNickName = new HashMap<>();
+
+        for(int i=0; i<forms.size(); i++){
+            String curNickName = forms.get(i).get(1);
+            String curEmail = forms.get(i).get(0);
+            for(int j=0; j<curNickName.length()-1; j++){
+                String splitNickName = curNickName.substring(j,j+2);
+                if(emailNickName.isEmpty()){
+                    emailNickName.put(splitNickName,curEmail);
+                }
+                else if(emailNickName.containsKey(splitNickName)&&!curEmail.equals(emailNickName.get(splitNickName))){
+                    answerSet.add(curEmail);
+                    answerSet.add(emailNickName.get(splitNickName));
+                }
+            }
+        }
+        List<String> answer = new ArrayList<>(answerSet);
         return answer;
     }
 }
