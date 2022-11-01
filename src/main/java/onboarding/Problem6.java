46e0ee0348de74d19b8fd96233bdda08a27838bd
si hwan <pum005@naver.com>
2022-11-01 22:39:19 +0900

    feat (problem6) : 1 Path 순회하면서, 중복 키 Set을 만드는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -21,5 +21,31 @@ public class Problem6 {
         }
     }
 
+    public static List<String> findDuplicatedEmailList(List<List<String>> forms) {
+
+        HashSet<String> validationSet = new HashSet<>();
+        HashSet<String> subSet = new HashSet<>();
+        HashSet<String> duplicatedUserSet = new HashSet<>();
+
+        String firstUserName = forms.get(0).get(1);
+
+        for (int i = 0; i < firstUserName.length() - 1; i++) {
+            subSet.add(firstUserName.substring(i, i + 2));
+        }
+
+        for (int i = 1; i < forms.size(); i++) {
+            String userName = forms.get(i).get(1);
+
+            for (int j = 0; j < userName.length() - 1; j++) {
+                String duplicatedKey = userName.substring(j, j + 2);
+                if (subSet.contains(duplicatedKey)) {
+                    validationSet.add(duplicatedKey);
+                }
+                subSet.add(duplicatedKey);
+
+            }
+        }
+
+    }
 }
 
