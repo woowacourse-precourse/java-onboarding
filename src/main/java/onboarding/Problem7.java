03490c6368d75f6c3ec961c73174a081743b7def
0417taehyun <0417taehyun@gmail.com>
2022-11-01 19:44:54 +0900

    feat: [문제7] 메서드 및 풀이 구조 구축

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,51 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    private static int friendScore = 10;
+    private static int visitScore = 1;
+
+    public static boolean isUserFriends(
+            HashMap<String, HashSet<String>> friendList, String user, String friend
+    ) {
+
+    }
+
+    public static HashMap<String, Integer> scoreVisitors(
+            HashMap<String, HashSet<String>> friendList,
+            HashMap<String, Integer> recommendedFriends,
+            List<String> visitors,
+            String user
+    ) {
+
+    }
+
+    public static HashMap<String, HashSet<String>> createFriendList(List<List<String>> friends) {
+        HashMap<String, HashSet<String>> friendListByUser = new HashMap<>();
+
+    }
+
+    public static HashMap<String, Integer> findRecommendedFriend(
+            HashMap<String, HashSet<String>> friendList, String user
+    ) {
+        HashMap<String, Integer> recommendedFriends = new HashMap<>();
+        HashSet<String> friends = friendList.get(user);
+
+    }
+
+    public static List<String> getRecommendedUsers(HashMap<String, Integer> recommendedFriends, int number) {
+        
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        HashMap<String, HashSet<String>> friendList = createFriendList(friends);
+        HashMap<String, Integer> recommendedFriends =  findRecommendedFriend(friendList, user);
+        List<String> answer = getRecommendedUsers(
+                scoreVisitors(friendList, recommendedFriends, visitors, user), 5
+        );
+
         return answer;
     }
 }
