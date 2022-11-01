93207dab1e1519597cd9e2820f6a21ceb7f36612
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-11-01 22:51:51 +0900

    Docs(Problem7): 추가 기능 목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+기능 목록
+1. 친구 구하기
+2. 친구 추천 후보 구하기
+3. 친구의 친구면 10점 추가
+4. 방문자면 1점 추가
+5. 정렬 - 점수 같으면 이름순, 아니면 점수순
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
