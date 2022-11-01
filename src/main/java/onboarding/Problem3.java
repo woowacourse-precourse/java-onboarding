c1ba32455b6b5ac8e18b935647b8f5d1953e585c
jsc1014 <wotlr5209@gmail.com>
2022-11-01 18:12:06 +0900

    3번: 숫자가 3의 배수인지 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+
+
         return answer;
     }
+
+    public static boolean checkMultipleOfThree(int num){
+        if(num != 0 && num % 3 == 0)
+            return true;
+        else
+            return false;
+    }
+
 }
