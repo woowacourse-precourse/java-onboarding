fe6474cf06a76a7954747e0ec72cace0d358db84
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 20:27:14 +0900

    feat(PROBLEM7): 점수별로 정렬한 친구를 추천해 주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,30 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static void recommendFriend(List<Map.Entry<String, Integer>> entries, List<String> recommendedList) {
+        for (Map.Entry<String, Integer> entry : entries) {
+            recommendFriendException(entry, recommendedList);
+        }
+    }
+
+    static void recommendFriendException(Map.Entry<String, Integer> entry, List<String> recommendedList) {
+        if (entry.getValue() == 0) {
+            return;
+        }
+
+        if (recommendedList.size() > 5) {
+            return;
+        }
+
+        recommendedList.add(entry.getKey());
     }
 }
