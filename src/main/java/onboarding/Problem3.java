2fd266f7967fcb55f1df1158cb95e2a0cd794c4f
youswim <youswim96@gmail.com>
2022-11-01 21:55:38 +0900

    Feat: solution 메서드 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int tot = 0;
+        for (int i = 3; i <= number; i++) {
+            int now = i;
+
+            while (now / 10 > 0) {
+                int tmp = now % 10;
+                tot = plusTot(tot, tmp);
+                now /= 10;
+            }
+            tot = plusTot(tot, now);
+        }
+        return tot;
     }
 
     private static int plusTot(int tot, int tmp) {
