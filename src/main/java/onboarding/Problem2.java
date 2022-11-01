ef23c50d19b7457bf97f66e2cb96f96efc0b3194
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:35:13 +0900

    2번 문제 풀이

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
+    public static String solution(String cryptogram) { // 중복된 문자를 찾아서 자신을 두번 제거하는 방식으로
         String answer = "answer";
         return answer;
     }
