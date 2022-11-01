aac8d3c416bf1c15240ece0042063cb6dc8105d0
ghchoi <ghchoi0427@gmail.com>
2022-11-01 14:30:06 +0900

    feat: 박수 치는 횟수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,35 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        Problem3 p3 = new Problem3();
+        for (int i = 1; i <= number; i++) {
+            answer += p3.getClapPerEachNum(i);
+        }
         return answer;
+    }
+
+
+    private boolean is369(int number) {  //TODO: 필요성 검토
+        while (number > 0) {
+            int temp = number % 10;
+            if (temp == 3 || temp == 6 || temp == 9) {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
+    }
+
+    private int getClapPerEachNum(int number) {
+        int count = 0;
+
+        while (number > 0) {
+            int temp = number % 10;
+            if (temp == 3 || temp == 6 || temp == 9) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
