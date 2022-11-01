212c47ad986411e6e59e395c3c70f15d4094f00f
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-11-01 21:09:04 +0900

    add function : take all cases of name

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-
+    public static List<String> getCase(String name) {
+        List<String> cases = new ArrayList<>();
+        for (int j = 2; j <= name.length(); j++) {
+            for (int i = 0; i <= name.length() - j; i++) {
+                cases.add(name.substring(i, j + i));
+            }
+        }
+        return cases;
+    }
     public static List<String> getName(List<List<String>> forms) {
         List<String> name = new ArrayList<>();
         for (List<String> form : forms) {
@@ -14,8 +22,11 @@ public class Problem6 {
     }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-        List<String> name = getName(forms);
-
+        List<String> names = getName(forms);
+        List<List<String>> nameCase = new ArrayList<>();
+        for (String name : names) {
+            nameCase.add(getCase(name));
+        }
         return answer;
     }
 }
