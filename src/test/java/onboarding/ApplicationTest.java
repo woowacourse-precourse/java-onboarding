532752553d9d78e91701dba6dce57a164d72a7d5
youngkyunIm <yee0230@naver.com>
2022-11-01 22:01:09 +0900

    test(Problem2) delete test

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -50,12 +50,6 @@ class ApplicationTest {
             String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
-        @Test
-        void case3(){
-            String cryptogram = "acccabba";
-            String result = "a";
-            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
-        }
     }
 
     @Nested
