af2e6f27a450b3098221a8b38262b50474098095
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 14:53:23 +0900

    feat:Howmany()-> 입력받은 값까지 checknumber()한 값

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,4 +7,14 @@ public class Problem3 {
         int answer = 0;
         return Howmany(number);
     }
+
+    public static int Howmany(int n){
+        sum=0;
+        if(!error(n)) {
+            for (int i = 0; i <= n; i++) {
+                sum += checknumber(i);
+            }
+        }
+        return sum;
+    }
 }
