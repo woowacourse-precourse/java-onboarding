cc4f155a1c3bb7b9797782081ebc7fb5dbcf941d
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-01 10:12:39 +0900

    Fix: fix Problem2, for testcase abcsss
    기존의 코드는 연속된 2개의 글자가 나타날 시에만 삭제하는 방식으로 구현되어있었는데,
    연속된 글자의 개수와 상관없이 모두 삭제하는 방식으로 수정하였음.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,16 +7,55 @@ public class Problem2 {
         int i=0;
         while (i != cryptogram.length()-1) {
             if (cryptogram.length() == 0) break;
+
             for (i = 0; i < cryptogram.length()-1; i++) {
-                if (cryptogram.charAt(i) == cryptogram.charAt(i+1)){
-                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
-                    i = 0;
-                    break;
+                char start = cryptogram.charAt(i);
+                int last = i;
+                while(start == cryptogram.charAt(last)){
+                    last += 1;
+                    if (last>=cryptogram.length()) break;
                 }
+                if (i+1==last) cryptogram = cryptogram.substring(0, i+1) + cryptogram.substring(last);
+                else {
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(last);
+                    i = 0;
+                }
+
+                /**
+                 *  abcsss 0,0 0
+                 * a bcsss 0,1 1
+                 * ab csss 0,2 2
+                 * abc sss 0,3
+                 */
+
+
+//                if (target == cryptogram.charAt(i+1)){
+//                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
+//                    i = 0;
+//                    break;
+//                }
             }
+        }
+
+        if (cryptogram.length()==2 && cryptogram.charAt(0)==cryptogram.charAt(1)) {
+            cryptogram = "";
         }
 
         return cryptogram;
     }
 
 }
+
+
+//    int i=0;
+//        while (i != cryptogram.length()-1) {
+//                if (cryptogram.length() == 0) break;
+//                for (i = 0; i < cryptogram.length()-1; i++) {
+//        char target = cryptogram.charAt(i);
+//        if (target == cryptogram.charAt(i+1)){
+//        cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
+//        i = 0;
+//        break;
+//        }
+//        }
+//        }
