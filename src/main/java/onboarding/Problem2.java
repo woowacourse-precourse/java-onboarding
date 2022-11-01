e5973b32283392db8a1bee99b0ef09a1287b2391
1eejisoo <jisu991012@gmail.com>
2022-11-01 13:53:05 +0900

    [Problem 2] Feat: 중복 문자를 제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,8 +5,34 @@ import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer = removeDuplicate(cryptogram);
+
+        while (isDuplicate(cryptogram)) {   // 중복 문자가 없을 때까지 반복
+            cryptogram = removeDuplicate(cryptogram);
+        }
+
+        return cryptogram;
+    }
+
+    //중복 문자를 제거하는 기능
+    public static String removeDuplicate(String cryptogram) {
+
+        List<String> duplicateCharacters = new ArrayList<>();
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                duplicateCharacters.add(cryptogram.substring(i, (getCutIndex(cryptogram.substring(i)) + i + 1)));
+                i = getCutIndex(cryptogram.substring(i)) + i;
+            }
+        }
+
+        for (String duplicateCharacter : duplicateCharacters) {
+            System.out.println(duplicateCharacter);
+            if (cryptogram.contains(duplicateCharacter))
+                cryptogram = cryptogram.replaceAll(duplicateCharacter, "");
+        }
+
+        return cryptogram;
     }
 
     // 중복 문자가 존재하는지 체크하는 기능
