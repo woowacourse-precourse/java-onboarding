47ca5b9c243f242171a10590cb294224bf15ffd1
junwon <todayhappymod365@gmail.com>
2022-11-01 09:23:07 +0900

    todayhappy0817

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,15 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem2 {
-    static Pattern pattern;
-    static Matcher matcher;
     public static String solution(String cryptogram) {
         return password(cryptogram,"(.)\\1+");
     }
     private static String password(String cryptogram,String regex){
         String answer = cryptogram;
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(answer);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(answer);
         while(matcher.find()){
             answer=answer.replaceAll(matcher.group(),"");
             matcher = pattern.matcher(answer);
