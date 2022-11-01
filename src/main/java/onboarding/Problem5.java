2e5baf2b96d1c91b07392b06699fa619b7ea7225
민병욱 <woook27@naver.com>
2022-11-01 13:27:22 +0900

    feat: exchange 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int exchange(int money, int unit) {
+        return money / unit;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
