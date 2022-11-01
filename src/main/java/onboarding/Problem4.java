e4df3c2e96520665b35a0048a7f384b9f8724441
choiejae <ejae8319@gmail.com>
2022-11-01 17:29:32 +0900

    feat (validityChecker): 예외 처리 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int UPPERCASE_NUMBER = 155;
+    private static final int LOWERCASE_NUMBER = 219;
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
     public static String solution(String word) {
+        validityChecker(word);
+
         String answer = "";
         return answer;
+    }
+
+    private static void validityChecker(String word) {
+        if (isNotValidWord(word)) {
+            throw new IllegalArgumentException("Not valid word");
+        }
+    }
+    private static boolean isNotValidWord(String word) {
+        return word.length() < MIN_LENGTH || MAX_LENGTH < word.length();
     }
 }
