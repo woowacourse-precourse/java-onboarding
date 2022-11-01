0f2fb22d670f8bbad31728d7f21ccb424ceaf14f
donghyun <9703l8@AL02038907.local>
2022-11-01 22:52:32 +0900

    four-zero

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        boolean bool = true;
+        while (bool){
+            bool = false;
+            for(int i=0;i<cryptogram.length()-1;i++){
+                if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                    if(i+2<cryptogram.length()){
+                        cryptogram = cryptogram.substring(0, i)+cryptogram.substring(i+2);
+                        bool = true;
+                    }else{
+                        cryptogram = cryptogram.substring(0,i);
+                    }
+                }
+            }
+            answer = cryptogram;
+        }
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,11 @@ import java.util.Stack;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            int c = word.charAt(i);
+        }
+
+
         return answer;
     }
 }
