7feea711880c6706e3690bde29aee8cbc581ad65
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-01 14:27:54 +0900

    Prob4 기능구현목록 작성 완료 및 Encoding 클래스 구현 완료.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,24 @@
 package onboarding;
 
+class Encoding {
+    StringBuilder result = new StringBuilder();
+    public StringBuilder encodeWord(String word) throws RestrictException {
+        if (word.length() < 1 || word.length() > 1000) {
+            throw new RestrictException("WordLengthOutOfRange");
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if ((int)word.charAt(i) <= 90 && (int)word.charAt(i) >= 65) {
+                result.append((char)(155 - (int) word.charAt(i)));
+            }
+            else if ((int)word.charAt(i) <= 122 && (int)word.charAt(i) >= 97) {
+                result.append((char)(219 - (int) word.charAt(i)));
+            } else {
+                result.append(word.charAt(i));
+            }
+        }
+        return result;
+    }
+}
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
