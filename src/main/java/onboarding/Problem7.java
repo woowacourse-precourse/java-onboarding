a6682c3e5c1e13e93fe3ddc132b3b7e7cf0075da
Lee-SungMin <saevers26@gmail.com>
2022-11-01 22:14:08 +0900

    Refactor: 7번 문제 리팩토링 - 간격 및 문법 조정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,54 +13,49 @@ public class Problem7 {
         Map<String, Integer> map = new TreeMap<>();
 
         // user 와 친구인 사람들 확인
-        for(int i = 0; i < friends.size(); i++){
+        for (int i = 0; i < friends.size(); i++) {
 
             List<String> temp = friends.get(i);
 
             String A = temp.get(0);
             String B = temp.get(1);
 
-            if(A.equals(user)){
+            if (A.equals(user)) {
                 user_friend_list.add(B);
-            }
-            else if(B.equals(user)){
+            } else if (B.equals(user)) {
                 user_friend_list.add(A);
             }
         }
 
         // user 와 친구인 사람들 중에서,
         // 사용자와 함께 아는 친구 수 점수 계산
-        for(int i = 0; i < user_friend_list.size(); i++){
+        for (int i = 0; i < user_friend_list.size(); i++) {
 
             String name = user_friend_list.get(i);
             // System.out.println(name);
 
             // 추천해 줄 친구가 있는 지 확인
-            for(int j = 0; j < friends.size(); j++){
+            for (int j = 0; j < friends.size(); j++) {
                 List<String> temp = friends.get(j);
 
                 String A = temp.get(0);
                 String B = temp.get(1);
 
                 // 단, user 및 이미 친구사이는 포함해서는 안된다.
-                if(A.equals(name) && !B.equals(user) && !user_friend_list.contains(B)){
+                if (A.equals(name) && !B.equals(user) && !user_friend_list.contains(B)) {
 
                     // 이미 map 에 있다면, 기존 점수에 + 10
-                    if(map.containsKey(B)){
+                    if (map.containsKey(B)) {
                         map.put(B, map.get(B) + 10);
-                    }
-
-                    else{
+                    } else {
                         map.put(B, 10);
                     }
 
-                }
-                else if(B.equals(name) && !A.equals(user) && !user_friend_list.contains(A)){
+                } else if (B.equals(name) && !A.equals(user) && !user_friend_list.contains(A)) {
 
-                    if(map.containsKey(A)){
+                    if (map.containsKey(A)) {
                         map.put(A, map.get(A) + 10);
-                    }
-                    else{
+                    } else {
                         map.put(A, 10);
                     }
                 }
@@ -69,17 +64,15 @@ public class Problem7 {
         }
 
         // 타임 라인 점수 계산
-        for(int i = 0; i < visitors.size(); i++){
+        for (int i = 0; i < visitors.size(); i++) {
 
             String name = visitors.get(i);
 
             // 이미 map 에 있다면, 기존 점수에 + 1
-            if(map.containsKey(name)){
+            if (map.containsKey(name)) {
                 map.put(name, map.get(name) + 1);
-            }
-
-            else{
-                if(!user_friend_list.contains(name)) {
+            } else {
+                if (!user_friend_list.contains(name)) {
                     map.put(name, 1);
                 }
             }
@@ -101,7 +94,7 @@ public class Problem7 {
             cnt++;
 
             // 최대 5명까지만 나오도록
-            if(cnt == 5){
+            if (cnt == 5) {
                 break;
             }
         }
