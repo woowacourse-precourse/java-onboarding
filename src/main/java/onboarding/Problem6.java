ff47cb21be5dd3263faacbe935e84890572c5e0e
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 14:12:55 +0900

    feat: Problem6 전체 닉네임을 두글자씩 끊어서 다른 닉네임에 포함되지 확인하는 기능 구현
    전체 닉네임을 두글자씩 끊어서 다른 닉네임에 포함되지 확인하는 기능을 구현했습니다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> result = new ArrayList<>();
+        for (List<String> form : forms) {
+            String s = form.get(1);
+            for (int i =0; i< s.length()-1;i++){
+                String temp = s.substring(i,i+2);
+                for (List<String> checkForm : forms){
+                    if (form.get(0)!=checkForm.get(0) && checkForm.get(1).contains(temp)){
+
+                    }
+                }
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        answer=result;
         return answer;
+
     }
+
 }
