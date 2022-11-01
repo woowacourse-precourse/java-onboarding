b4651a108c9c0146d378109601b0868f13d6a619
gitFILO <x1xgudwls@naver.com>
2022-11-01 23:06:44 +0900

    [Problem 6] 해시맵에 같은 연속된 2글자의 닉네임이 있는 지 key값을 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,26 +4,38 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
         HashSet<String> AnswerEmails = findAnswerEmailSet(forms);
         List<String> answer = new ArrayList<>(AnswerEmails);
         Collections.sort(answer);
 
         return answer;
     }
-
-    public static HashSet<String> findAnswerEmailSet(List<List<String>> forms) {
-        HashMap<String, String> allPartialNicknameWithEmail = new HashMap<>();
+    public static HashSet<String> findAnswerEmailSet(List<List<String>> forms){
+        HashMap<String,String> allPartialNicknameWithEmail = new HashMap<>();
         HashSet<String> answerEmails = new HashSet<>();
         String nickname;
         String email;
 
-        for (int i = 0; i < forms.size(); i++) {
+        for(int i=0;i < forms.size();i++){
             nickname = forms.get(i).get(1);
             email = forms.get(i).get(0);
-            putAllPartialNicknameWithEmail(allPartialNicknameWithEmail, answerEmails, nickname, email);
+            putAllPartialNicknameWithEmail(allPartialNicknameWithEmail,answerEmails,nickname,email);
         }
 
         return answerEmails;
+    }
+    public static void putAllPartialNicknameWithEmail(HashMap<String,String> allPartialNicknameWithEmail
+            , HashSet<String> answerEmails,String nickname,String email){
+        String partialNickname;
+        for(int i=0;i <= nickname.length()-2;i++) {
+            partialNickname = nickname.substring(i,i+2);
+            if(allPartialNicknameWithEmail.containsKey(partialNickname) == true) {
+                String newlyAddedEmail = allPartialNicknameWithEmail.get(partialNickname);
+                addEmailsToAnswerSet(answerEmails,email,newlyAddedEmail);
+            }
+            allPartialNicknameWithEmail.put(partialNickname,email);
+        }
     }
 }
 
