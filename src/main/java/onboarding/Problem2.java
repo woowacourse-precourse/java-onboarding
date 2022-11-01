63de095703ace936b8364e675c54535017632929
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-01 18:04:22 +0900

    refactor(problem2): split by function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,54 +5,52 @@ import java.util.Arrays;
 public class Problem2 {
 
 	static char[] chars;
-	static Boolean[] removeIndex;
+	static Boolean[] removedIndex;
 	static int lengthOfString;
+	static int nowRemoved;
 
 	public static String solution(String cryptogram) {
 		Boolean isChanged;
+		nowRemoved = 0;
 		chars = cryptogram.toCharArray();
 		lengthOfString = chars.length;
-		removeIndex = new Boolean[lengthOfString];
-		Arrays.fill(removeIndex, Boolean.FALSE);
-		int nowRemove = 0;
+		removedIndex = new Boolean[lengthOfString];
+		Arrays.fill(removedIndex, Boolean.FALSE);
 
 		do {
 			isChanged = false;
 			int before = -1;
 
 			for (int i = 0; i < lengthOfString; i++) {
-				if (removeIndex[i]) {
+				if (removedIndex[i]) {
 					continue;
 				}
 
-				if (before == -1) {
+				if (before == -1 || chars[before] != chars[i]) {
 					before = i;
 					continue;
 				}
 
-				if (chars[before] != chars[i]) {
-					before = i;
-				} else {
-					isChanged = true;
-					if (!removeIndex[before]) {
-						removeIndex[before] = true;
-						nowRemove += 1;
-					}
-					if (!removeIndex[i]) {
-						removeIndex[i] = true;
-						nowRemove += 1;
-					}
-				}
+				isChanged = true;
+				executeChange(before);
+				executeChange(i);
 			}
 		} while (isChanged);
 
 		return getCurrentString();
 	}
 
+	private static void executeChange(int i) {
+		if (!removedIndex[i]) {
+			removedIndex[i] = true;
+			nowRemoved += 1;
+		}
+	}
+
 	private static String getCurrentString() {
 		String answer = "";
 		for (int i = 0; i < lengthOfString; i++) {
-			if (!removeIndex[i]) {
+			if (!removedIndex[i]) {
 				answer += chars[i];
 			}
 		}
