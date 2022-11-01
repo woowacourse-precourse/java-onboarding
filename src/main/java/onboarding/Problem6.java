72f6e06ca419681174089d9c4d3615baa57331a0
lee gichan <>
2022-11-01 18:05:53 +0900

    Problem 6: Two Chars가 포함되어있는지 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
 import java.util.List;
-
+import java.util.ArrayList;
+import java.util.Collections;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+        int idxStr = 0;
+        int idxChar = 0;
+        int j = 0;
+
+        while (idxStr < forms.size())
+        {
+            idxChar = 0;
+            while (idxChar < forms.get(idxStr).get(1).length() - 1) {
+                String targetTwoChars = forms.get(idxStr).get(1).substring(idxChar, idxChar + 2);
+                j = 0;
+                while (j < forms.size()){
+                    if (j != idxStr && forms.get(j).get(1).contains(targetTwoChars) && !answer.contains(forms.get(j).get(0)))
+                        answer.add(forms.get(j).get(0));
+                    j++;
+                }
+                idxChar++;
+            }
+            idxStr++;
+        }
+        Collections.sort(answer);
+
         return answer;
     }
 }
