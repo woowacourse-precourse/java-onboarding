656d8dcd2659c80b3b62a23184820f229e8ec567
dvlpr0 <rladjswkd@naver.com>
2022-11-01 21:25:32 +0900

    added/Problem7: 친구 관계 정보를 담는 해시테이블을 초기화하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	private static Hashtable<String, List<String>> friendTable;
+	private static Hashtable<String, Integer> scoreTable;
+	private static void setFriendTable(List<List<String>> friends) {
+		String idA;
+		String idB;
+		List<String> listA;
+		List<String> listB;
+
+		for (List<String> relationship : friends) {
+			idA = relationship.get(0);
+			idB = relationship.get(1);
+			friendTable.putIfAbsent(idA, new ArrayList<>());
+			friendTable.putIfAbsent(idB, new ArrayList<>());
+			listA = friendTable.get(idA);
+			listA.add(idB);
+			listB = friendTable.get(idB);
+			listB.add(idA);
+		}
+	}
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		scoreTable = new Hashtable<>();
+		friendTable = new Hashtable<>();
+
+		setFriendTable(friends);
+//		scoreMutualFriends(user);
+//		scoreVisitors(visitors);
+//		removeUserAndFriends(user);
+//		removeZeros();
+//		return (getSortedList());
+	}
+
 }
