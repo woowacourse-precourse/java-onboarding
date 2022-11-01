a5ec1a5490387327c7beda94f123847860098db6
Yohan <sprae114@naver.com>
2022-11-01 14:40:04 +0900

    problem2 구현완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "a";
+
+        if(cryptogram.length() == 1){
+            return cryptogram;
+        }
+
+        answer = removeDupulication(cryptogram);
+
         return answer;
+    }
+
+    private static String removeDupulication(String cryptogram) {
+        List<String> stack = new ArrayList<>();
+        List<String> cryptogramList = new ArrayList<>(List.of(cryptogram.split("")));
+
+        for(int i = 0; i < cryptogramList.size(); i++){
+            if(!stack.isEmpty()){
+                if(cryptogramList.get(i).equals(stack.get(stack.size()-1))){
+                    stack.remove(stack.size()-1);
+                    continue;
+                }
+            }
+            stack.add(cryptogramList.get(i));
+        }
+
+        return String.join("",  stack);
     }
 }
