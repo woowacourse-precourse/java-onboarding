8f9c6f04632e40f0766ef60364d64a6fb21be12c
jay <jaewhan232@naver.com>
2022-11-01 21:38:04 +0900

    Implement : 친구 관계를 기반으로 점수를 계산하는 caculateRelationPoints() 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,15 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, List<String>> relationMap = getRelationMap(friends);
+        Map<String, Integer> userPoints = new HashMap<>();
+
+        caculateRelationPoints(user, relationMap, userPoints);
+
         return answer;
     }
 
@@ -25,5 +27,24 @@ public class Problem7 {
             relationMap.get(user2).add(user1);
         }
         return relationMap;
+    }
+
+    private static void caculateRelationPoints(
+            String user,
+            Map<String, List<String>> relationMap,
+            Map<String, Integer> userPoints
+    ) {
+        Queue<String> queue = new LinkedList<>();
+
+        for (String friend : relationMap.get(user)) {
+            for (String nextFriend: relationMap.get(friend)){
+                if (!nextFriend.equals(user)) queue.add(nextFriend);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            String recommendedFriend = queue.poll();
+            userPoints.put(recommendedFriend, userPoints.getOrDefault(recommendedFriend, 0)+10);
+        }
     }
 }
