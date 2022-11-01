efe7445327f0289c08fe27660b3f596b65e6b25e
dingding-21 <phj8221@naver.com>
2022-11-01 23:11:06 +0900

    feat: Problem7 모든 사용자의 친구 관계 정보를 Map으로 저장하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, List<String>> getFriendList(List<List<String>> friends) {
+        Map<String, List<String>> friendMap = new HashMap<>();
+
+        String friend1, friend2;
+        List<String> friend1Value;
+        List<String> friend2Value;
+
+        for (List<String> friend : friends) {
+            friend1 = friend.get(0);
+            friend2 = friend.get(1);
+
+            friend1Value = friendMap.getOrDefault(friend1, new ArrayList<>());
+            friend2Value = friendMap.getOrDefault(friend2, new ArrayList<>());
+
+            friend1Value.add(friend2);
+            friend2Value.add(friend1);
+
+            friendMap.put(friend1, friend1Value);
+            friendMap.put(friend2, friend2Value);
+        }
+
+        return friendMap;
     }
 }
