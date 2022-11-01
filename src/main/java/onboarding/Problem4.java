7db1e5d5040a3da37a81ba2b5b23c8f6b6e9b133
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 18:19:57 +0900

    문자 하나를 입력받고 변환한 결과를 리턴한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,15 @@ public class Problem4 {
     private static boolean isUpperCase(char letter) {
         return letter >= 'A' && letter <= 'Z';
     }
+    private static char transform(char letter) {
+        if (isLowerCase(letter)) {
+            return (char) ('a'+'z'-letter);
+        }
+        if (isUpperCase(letter)) {
+            return (char) ('A'+'Z'-letter);
+        }
+        return letter;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
