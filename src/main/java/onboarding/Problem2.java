f52f7f25cefae0533e8d439d63cb541d78f15c7b
이지영 <jioung93@naver.com>
2022-11-01 17:41:24 +0900

    [Problem 2] refactor: else문 제거 후 early return 사용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,15 +8,13 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
         String answer = "answer";
-        // 1. 문자열을 훑으면서 문자를 stack에 push
-        for (int i=0; i<cryptogram.length(); i++){
+        for (int i = 0; i < cryptogram.length(); i++) {
             pushOrNot(cryptogram.charAt(i));
         }
-        if(flag) stack.pop();
+        if (flag) stack.pop();
 
         StringBuffer sb = new StringBuffer();
-        // 2. 연속 문자가 제거된 채 쌓인 문자들을 뒤집어서 반환
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
         answer = sb.reverse().toString();
@@ -25,22 +23,17 @@ public class Problem2 {
 
     // 문자의 push 여부를 결정하는 함수 pushOrNot()
     private static void pushOrNot(char c) {
-        // 스택이 비어있다면 문자를 push
-        if(stack.isEmpty()){
+        if (stack.isEmpty()) {
             stack.push(c);
-        // 스택이 비어있지 않은 상황
-        } else {
-            // top의 문자가 연속 문자의 일부였다면 pop
-            if(flag) {
-                stack.pop();
-                flag = false;
-            }
-            // top의 문자가 넣으려는 문자와 같다면 flag를 true로 변경하고, 같지 않다면 문자를 push
-            if(stack.peek()==c) {
-                flag = true;
-            } else {
-                stack.push(c);
-            }
+            return;
         }
+        if (flag) {
+            stack.pop();
+            flag = false;
+        }
+        if (stack.peek() == c) {
+            flag = true;
+        }
+        stack.push(c);
     }
 }
