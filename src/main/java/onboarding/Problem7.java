82a2a0d52817d570c4dcc404a5110c91685213bb
Choi-JY <Choi-JY1107@github.com>
2022-11-01 19:45:51 +0900

    refactor: 기능 별 메소드 분리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,62 +1,98 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> userList = new ArrayList<>();
-        List<String> answer = new ArrayList<>();
-        boolean[][] friend = new boolean[35][35];
-        int[] score = new int[35];
+        List<String> answer;
+        List<String> friendsList;
+        Map<String, Integer> recommendScore;
 
-        userList.add(user);
-        int listSize = friends.size();
-        for(int i=0; i<listSize; ++i){
-            String friend1 = friends.get(i).get(0);
-            String friend2 = friends.get(i).get(1);
-            if(!userList.contains(friend1)) userList.add(friend1);
-            if(!userList.contains(friend2)) userList.add(friend2);
-            friend[userList.indexOf(friend1)][userList.indexOf(friend2)] =  true;
-            friend[userList.indexOf(friend2)][userList.indexOf(friend1)] =  true;
-        }
-
-        int userSize = userList.size();
-        for(int i=1; i<userSize; ++i)
-            if(!friend[0][i])
-                for(int j=1; j<userSize; ++j)
-                    if(friend[i][j] && i != j && friend[0][j])
-                        score[i] += 10;
-
-        int visitorSize = visitors.size();
-        for(int i=0; i<visitorSize; ++i){
-            String visitor = visitors.get(i);
-            if(!userList.contains(visitor)) userList.add(visitor);
-            if(!friend[0][userList.indexOf(visitor)])
-                score[userList.indexOf(visitor)] += 1;
-        }
-
-        userSize = userList.size();
-        while(true){
-            int maxScore = Integer.MIN_VALUE;
-            List<String> temp = new ArrayList<>();
-            for(int i=0; i<userSize; ++i) {
-                if(score[i] == 0) continue;
-                if(maxScore < score[i]){
-                    temp.clear();
-                    maxScore = score[i];
-                    temp.add(userList.get(i));
-                }
-                else if(maxScore == score[i]) temp.add(userList.get(i));
-            }
-            if(temp.isEmpty()) break;
-            for(int i=0; i<temp.size(); ++i)
-                score[userList.indexOf(temp.get(i))] = 0;
-
-            Collections.sort(temp);
-            if(answer.size() + temp.size() <= 5) answer.addAll(temp);
-            else answer.addAll(new ArrayList<>(temp.subList(0, 5-answer.size()-1)));
-        }
+        friendsList = findUserFriends(user, friends);
+        recommendScore = calculatedFriendsScore(user, friendsList, friends);
+        recommendScore = calculatedVisitorsScore(friendsList, visitors, recommendScore);
+        answer = calculatedRecommendation(recommendScore, friendsList);
 
         return answer;
+    }
+
+    private static List<String> findUserFriends(String user, List<List<String>> friends) {
+        List<String> friendsList = new ArrayList<>();
+
+        for (List<String> friend: friends) {
+            if (friend.get(0).compareTo(user) == 0) {
+                friendsList.add(friend.get(1));
+            }
+            if (friend.get(1).compareTo(user) == 0) {
+                friendsList.add(friend.get(0));
+            }
+        }
+
+        return friendsList;
+    }
+
+    private static Map<String, Integer> calculatedFriendsScore(String user, List<String> friendsList, List<List<String>> friends) {
+        Map<String, Integer> score = new HashMap<>();
+
+        for (List<String> friend: friends) {
+            if (friend.get(0).compareTo(user) == 0 || friend.get(1).compareTo(user) == 0) {
+                continue;
+            }
+            if (friendsList.contains(friend.get(0)) && !friendsList.contains(friend.get(1))) {
+                score = calculatedScore(score, friend.get(1), 10);
+            }
+            if (friendsList.contains(friend.get(1)) && !friendsList.contains(friend.get(0))) {
+                score = calculatedScore(score, friend.get(0), 10);
+            }
+        }
+        return score;
+    }
+
+    private static Map<String, Integer> calculatedVisitorsScore(List<String> friendsList, List<String> visitors, Map<String, Integer> recommendScore) {
+        for (String visitor: visitors) {
+            calculatedScore(recommendScore, visitor, 1);
+        }
+        return recommendScore;
+    }
+    private static Map<String, Integer> calculatedScore(Map<String, Integer> score, String str, int factor) {
+        if (score.containsKey(str)) {
+            score.put(str, score.get(str) + factor);
+            return score;
+        }
+        score.put(str, factor);
+        return score;
+    }
+
+    private static List<String> calculatedRecommendation(Map<String, Integer> recommendScore, List<String> friendsList) {
+        List<String> answer = new ArrayList<>();
+        List<Entry<String, Integer>> scoreList;
+        scoreList = sortRecommendation(recommendScore);
+
+        for(Entry<String, Integer> entry : scoreList) {
+            if(entry.getValue() == 0) break;
+            if(friendsList.contains(entry.getKey())) continue;
+            answer.add(entry.getKey());
+        }
+
+        return (answer.size() > 5) ? answer.subList(0, 4) : answer;
+    }
+
+    private static List<Entry<String, Integer>> sortRecommendation(Map<String, Integer> recommendScore) {
+        List<Entry<String, Integer>> scoreList = new ArrayList<Entry<String, Integer>>(recommendScore.entrySet());
+        Collections.sort(scoreList, new Comparator<Entry<String, Integer>>() {
+            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+                if (obj1.getValue().compareTo(obj2.getValue()) == 0) {
+                    return obj1.getKey().compareTo(obj2.getKey());
+                }
+                return obj2.getValue().compareTo(obj1.getValue());
+            }
+        });
+        return scoreList;
     }
 }
