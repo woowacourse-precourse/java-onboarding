a50bc20560d17a10fb5b8e586da69edb3a5f3569
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 17:14:13 +0900

    주어진 페이지가 범위 내에 있으면 true를 범위 밖에 있으면 false를 리턴한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static boolean isInRange(int page) {
+        return page >= 3 && page <= 398;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
