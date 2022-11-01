147f9da7b861d79e1f836bb128e3590df94c174e
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-01 18:55:37 +0900

    [P4] 문자가 대문자일 경우, 아스키코드값을 65로 설정.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int alphabet, ascii, reverseAlphabet;
+        alphabet = ascii = reverseAlphabet = 0;
+
+        for (int i=0; i < word.length(); i++) {
+            // 1. 문자가 대문자일 경우, 아스키코드값을 65로 설정.
+            if (Character.isUpperCase(word.charAt(i))) {
+                ascii = 65;
+            }
+        }
         return answer;
     }
 }
