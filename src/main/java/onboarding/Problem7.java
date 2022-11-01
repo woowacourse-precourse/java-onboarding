29df17e50b502bf5c34cbc7a2512c141622803b4
Donguk <95donguk.dev@gmail.com>
2022-11-01 23:54:25 +0900

    feat: 문제7의 user, friends, visitors 검증 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,11 +12,15 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends,
                                         List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Set<String> userFriends = new HashSet<>();
         Map<String, Integer> friendRelationScoreRepo = new HashMap<>();
+        UserValidator.validate(user);
+        FriendsValidator.validate(friends);
+        VisitorsValidator.validate(visitors);
         findUserFriends(user, friends, userFriends);
         calculateRelationScore(user, friends, userFriends, friendRelationScoreRepo);
         calculateVisitScore(userFriends, visitors, friendRelationScoreRepo);
@@ -29,12 +33,12 @@ public class Problem7 {
         List<Map.Entry<String, Integer>> entries =
             friendRelationScoreRepo.entrySet().stream().sorted(Map.Entry.comparingByKey())
                 .sorted(new Comparator<Map.Entry<String, Integer>>() {
-                        @Override
-                        public int compare(Map.Entry<String, Integer> o1,
-                                           Map.Entry<String, Integer> o2) {
-                            return o2.getValue() - o1.getValue();
-                        }
-                    })
+                    @Override
+                    public int compare(Map.Entry<String, Integer> o1,
+                                       Map.Entry<String, Integer> o2) {
+                        return o2.getValue() - o1.getValue();
+                    }
+                })
                 .collect(Collectors.toList());
         for (Map.Entry<String, Integer> entry : entries) {
             sortedResult.add(entry.getKey());
@@ -76,5 +80,81 @@ public class Problem7 {
         }
         userFriends.remove(user);
         return userFriends;
+    }
+
+    private static class UserValidator {
+        private static final int MIN_USER_LENGTH = 1;
+        private static final int MAX_USER_LENGTH = 30;
+
+        public static void validate(String user) {
+            char[] chars = user.toCharArray();
+            if (!isValidUserLength(user.length())) {
+                throw new IllegalArgumentException(
+                    "The user's length should be greater than or equal to 1, and less than or equal to 30.");
+            }
+
+            for (char aChar : chars) {
+                if(Character.isUpperCase(aChar)) {
+                    throw new IllegalArgumentException("The user ID should be in lowercase alphabetic characters.");
+                }
+            }
+        }
+
+        private static boolean isValidUserLength(int length) {
+            return length >= MIN_USER_LENGTH && length <= MAX_USER_LENGTH;
+        }
+    }
+
+    private static class FriendsValidator {
+        private static final int MIN_FRIENDS_LIST_SIZE = 1;
+        private static final int MAX_FRIENDS_LIST_SIZE = 10000;
+        private static final int FRIEND_LIST_SIZE = 2;
+        private static final int MIN_ID_LENGTH = 1;
+        private static final int MAX_ID_LENGTH = 30;
+
+        public static void validate(List<List<String>> friends) {
+            if (!isValidFriendsListSize(friends.size())) {
+                throw new IllegalArgumentException(
+                    "The size of friends list should be greater than or equal to 1, and less than or equal to 10000.");
+            }
+            for (List<String> friendList : friends) {
+                if (!isValidFriendListSize(friendList.size())) {
+                    throw new IllegalArgumentException("The size of friend list should be 2.");
+                }
+                for (String id : friendList) {
+                    if (!isValidIdLength(id.length())) {
+                        throw new IllegalArgumentException(
+                            "The id's Length should be greater than or equal to 1, and less than or equal to 30.");
+                    }
+                }
+            }
+        }
+
+        private static boolean isValidIdLength(int idLength) {
+            return idLength >= MIN_ID_LENGTH && idLength <= MAX_ID_LENGTH;
+        }
+
+        private static boolean isValidFriendListSize(int size) {
+            return size == FRIEND_LIST_SIZE;
+        }
+
+        private static boolean isValidFriendsListSize(int size) {
+            return size >= MIN_FRIENDS_LIST_SIZE && size <= MAX_FRIENDS_LIST_SIZE;
+        }
+    }
+
+    private static class VisitorsValidator {
+        private final static int MAX_VISITORS_LIST_SIZE = 10000;
+
+        public static void validate(List<String> visitors) {
+            if (!isValidVisitorsListSize(visitors.size())) {
+                throw new IllegalArgumentException(
+                    "The size of visitors list should be less than or equal to 10000.");
+            }
+        }
+
+        private static boolean isValidVisitorsListSize(int size) {
+            return size <= MAX_VISITORS_LIST_SIZE;
+        }
     }
 }
