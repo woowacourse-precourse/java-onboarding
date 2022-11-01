4fad987c90887a031c1d1596b29c35cebf4fa503
Heesu Jeong <huissppea@gmail.com>
2022-11-01 21:44:00 +0900

    style: (problem2) 코드 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,29 +1,28 @@
 package onboarding;
-import java.util.Arrays;
-import java.util.stream.IntStream;
+
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
         String tmp = "";
 
-        int check = 1;
+        boolean check = false;
         while (true){
             tmp = "";
             for (int i = 0; i < cryptogram.length()-1; i++){
-                String str = cryptogram.substring(i, i+1);
-                String strAfter = cryptogram.substring(i+1, i+2);
+                String strNow = cryptogram.substring(i, i+1);
+                String strNext = cryptogram.substring(i+1, i+2);
 
-                if (str.equals(strAfter)){     // 이후 글자와 같으면
-                    check = 0;
-                } else if (check == 0){
-                    check = 1;
+                if (strNow.equals(strNext)){      // 다음 글자와 같으면
+                    check = true;
+                } else if (check == true){  // 이전 글자와 같으면
+                    check = false;
                 } else {
-                    tmp += str;
+                    tmp += strNow;
                 }
             }
 
-            if (check == 1 && cryptogram.length() != 0){     // 전 글자와 같고 길이가 0이 아니면
+            if (check == false && cryptogram.length() != 0){     // 전 글자와 같고 길이가 0이 아니면
                 tmp += cryptogram.substring(cryptogram.length()-1, cryptogram.length());    // 마지막 글자
             }
 
@@ -32,14 +31,27 @@ public class Problem2 {
             } else{
                 cryptogram = tmp;
             }
+
         }
 
         answer = cryptogram;
-
         return answer;
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
