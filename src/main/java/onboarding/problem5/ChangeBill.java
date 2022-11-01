4a0865052c90ae30c2d50584787d4a47b5de6afc
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:21:46 +0900

    feat(문제5): 잔돈 교환 메소드 추가

--- src/main/java/onboarding/problem5/ChangeBill.java
+++ src/main/java/onboarding/problem5/ChangeBill.java
@@ -1,9 +1,23 @@
 package onboarding.problem5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChangeBill {
     private int money;
+    private int[] bills = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private List<Integer> krwWonCount = new ArrayList<>();
 
     public ChangeBill(int money){
         this.money = money;
+    }
+
+    public List<Integer> changeBills(){
+        for(int bill : bills){
+            krwWonCount.add(money / bill);
+            money %= bill;
+        }
+
+        return krwWonCount;
     }
 }
