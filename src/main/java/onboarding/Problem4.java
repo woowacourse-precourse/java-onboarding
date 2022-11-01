b69b9507aec5c531294fc08886048109170d4b67
devEdok13 <dev.edok13@gmail.com>
2022-11-01 16:18:23 +0900

    feat: P04-reverseCase

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -11,6 +11,13 @@ public class Problem4 {
         return answer;
     }
 
+    //  입력값의 대소문자 형변환 메소드
+    private static Character reverseCase(char input){
+        if (checkLowerCase(input))
+            return Character.toUpperCase(input);
+        return Character.toLowerCase(input);
+    }
+
     //  입력값이 소문자인지 확인하는 메소드
     private static Boolean checkLowerCase(char input){
         if(Character.toLowerCase(input) == input)
