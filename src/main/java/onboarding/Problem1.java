9dceda9f5a73f8bc12bfe8f56ad7e4fb794b4200
jay <jaewhan232@naver.com>
2022-11-01 15:44:20 +0900

    Implement : 정수의 각 자리수 합을 구하는 메소드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -25,4 +25,13 @@ class Problem1 {
         return true;
     }
 
+    private static int getSumOfDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number%10;
+            number /= 10;
+        }
+        return sum;
+    }
+
 }
