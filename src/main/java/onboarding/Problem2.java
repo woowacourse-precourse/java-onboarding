f90ed51176b50daf1c82145cac8d22bf4e180c39
neppiness <0414kjh@naver.com>
2022-11-01 11:29:51 +0900

    1st draft of onboarding mission

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        int i = 0;
+        while(i + 1 < answer.length()) {
+            if(answer.charAt(i) == answer.charAt(i + 1)) {
+                if(i == answer.length() - 2)
+                    answer = answer.substring(0, i);
+                else
+                    answer = answer.substring(0, i) + answer.substring(i + 2);
+                i = 0;
+            } else i++;
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Hashtable<String, Integer> ht = new Hashtable<>();
+        String s, subs;
+        int len, count;
+        for(List<String> f : forms) {
+            s = f.get(1);
+            len = s.length();
+            if(len < 2) continue;
+            for(int i = 0; i < len - 1; i++) {
+                subs = s.substring(i, i + 2);
+                count = ht.containsKey(subs) ? ht.get(subs) : 0;
+                ht.put(subs, count + 1);
+            }
+        }
+        for(List<String> f : forms) {
+            s = f.get(1);
+            len = s.length();
+            for(int i = 0; i < len - 1; i++) {
+                subs = s.substring(i, i + 2);
+                count = ht.get(subs);
+                if(count >= 2) {
+                    answer.add(f.get(0));
+                    break;
+                }
+            }
+        }
+        Collections.sort(answer);
         return answer;
     }
 }
