432cb8cd19d838667edadc095bf6e743819cf172
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 15:21:14 +0900

    feat : 상수 변수 및 전역 변수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+    private static final int FORM_EMAIL = 0;
+    private static final int FORM_NICKNAME = 1;
+    private static final int MIN_CREW = 1;
+    private static final int MAX_CREW = 10000;
+    private static final String DOMAIN = "email.com";
+    private static final int MIN_EMAIL = 11;
+    private static final int MAX_EMAIL = 19;
+    private static final int MIN_NICKNAME = 1;
+    private static final int MAX_NICKNAME = 19;
+
+    private static final String NAME = ".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.";
+
+
+    private static List<String> answer = new ArrayList<>();
+    private static List<Set<String>> partitionsList = new ArrayList<>();
+    private static Map<String, Integer> nicknameAndCount = new HashMap<>();
+
+
+
+
 }
