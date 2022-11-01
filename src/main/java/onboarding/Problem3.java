ddcf370fed46fa8d2238b93d2b39427692bed1ae
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 18:10:18 +0900

    [Feat] 숫자 n은 몇번의 박수를 쳐야되는지 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int GetClap(int number)
+    {
+        int ret = 0;
+        while (number != 0)
+        {
+            if ((number % 10 != 0) && (number % 10) % 3 == 0)
+                ret++;
+            number /= 10;
+        }
+        return ret;
+    }
     public static int solution(int number) {
         int answer = 0;
+        
         return answer;
     }
 }
