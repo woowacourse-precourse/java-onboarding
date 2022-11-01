dea475e2d7637b8f727e3769c3db8f50313f9a3e
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:26:05 +0900

    feat: Problem1 승패에 따르 결과 반환 및 올바르지 않은 페이지 처리 구현
    승패에 따르 결과를 반환 및 올바르지 않은 페이지 처리하도록 구현하였습니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-
+        if ((pobi.get(1) - pobi.get(0))!=1 || (crong.get(1) - crong.get(0))!=1){
+            answer=-1;
+            return answer;
+        }
+        int pobiVal= result(pobi);
+        int crongVal = result(crong);
+        if (pobiVal > crongVal) answer=1;
+        else if (pobiVal < crongVal) answer=2;
+        else if (pobiVal == crongVal) answer=0;
+        else
+            answer=-1;
         return answer;
     }
     public static int result(List<Integer> value){
