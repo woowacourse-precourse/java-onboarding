246d1b49a3c476cf3c5d2cbc61a8109a1563920f
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:31:57 +0900

    Feat: Change 솔루션 함수 로직

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = changeMoney(money);
+
         return answer;
     }
 
-    
+
     private static List<Integer> changeMoney(int money) {
         List<Integer> answer = new ArrayList<>();
         List<Integer> bills = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
