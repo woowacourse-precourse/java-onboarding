ee3ad74ca542295d1a5f20838bf21efa7c9ef638
osy9757 <osy9757@gmail.com>
2022-11-01 22:52:13 +0900

    FEAT : solution3 함수 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = count369(number);
         return answer;
+    }
+
+    public static int count369(int number) {
+        int countFreq369 = 0;
+        for (int i = number; i > 0; i--) {
+            number = i;
+            while (number > 0) {
+                if ((number % 10) == 3 || (number % 10) == 6 || (number % 10) == 9) {
+                    countFreq369 += 1;
+                }
+                number /= 10;
+            }
+        }
+        return countFreq369;
     }
 }
