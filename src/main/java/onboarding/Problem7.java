125a395d4cb673e75a06a3d2dec7ae96591f2b45
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-01 20:18:24 +0900

    feat: Add Problem7 Get FriendsMap
    - 문제 7번 친구 관계를 Map으로 가져오는 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,10 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -29,5 +32,28 @@ public class Problem7 {
 
     private static Boolean isVisitorsException(List<String> visitors) {
         return visitors.size() < 0 || visitors.size() > 10000;
+    }
+
+    private static Map<String, List<String>> getFriendsMap(List<List<String>> friends,
+                                                           List<String> userFriends,
+                                                           String user
+    ) {
+        Map<String, List<String>> friendsMap = new HashMap<>();
+        for (List<String> friendShip : friends) {
+            if (friendShip.contains(user)) {
+                userFriends.add(friendShip.get(0).equals(user)
+                        ? friendShip.get(0)
+                        : friendShip.get(1));
+                continue;
+            }
+            //아직 등록하지 않은 친구는 등록 후 저장
+            friendsMap.computeIfAbsent(friendShip.get(0), k -> new ArrayList<String>());
+            friendsMap.get(friendShip.get(0)).add(friendShip.get(1));
+
+            friendsMap.computeIfAbsent(friendShip.get(1), k -> new ArrayList<String>());
+            friendsMap.get(friendShip.get(1)).add(friendShip.get(0));
+        }
+
+        return friendsMap;
     }
 }
