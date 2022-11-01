557799801263fda04322e5abf469eee746ee5a8c
tdmdgh <tdmdgh@gmail.com>
2022-11-01 18:38:26 +0900

    problem2_1_연속된 중복 character를 제거하는 매소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+    private static String removeDuplicatedChar(String str){
+        String modifiedStr = "";
+        str+= " ";
+
+        boolean isDuplicate = false;
+        for(int i = 0 ; i < str.length() - 1; i++){
+            if (str.charAt(i) != str.charAt(i + 1)) {
+                if (!isDuplicate)
+                    modifiedStr += str.charAt(i);
+                isDuplicate = false;
+            } else {
+                isDuplicate = true;
+            }
+        }
+        return modifiedStr;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
