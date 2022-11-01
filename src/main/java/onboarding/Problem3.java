119c885821893ee87f24baa4d3016fd334c436e7
박세원 <tpdnjs1825@naver.com>
2022-11-01 22:56:53 +0900

    Feat : 숫자에 들어간 3,6,9의 개수를 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    private static int countNumber369(int number) {
+        int count369 = 0;
+        while (number > 0) {
+            if (is369(number % 10)) {
+                count369++;
+            }
+            number /= 10;
+        }
+        return count369;
+    }
+
+    private static boolean is369(int number) {
+        return (number == 3 || number == 6 || number == 9);
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
