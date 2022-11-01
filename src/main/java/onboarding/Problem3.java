dfdaeead943ad768861f62d59937ed3b187b57ea
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 19:08:28 +0900

    문제3 / feat : 유효성 검증 코드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,12 @@
 package onboarding;
 
+import onboarding.problem3.Validator;
+
 import static onboarding.problem3.Counter.count369;
 
 public class Problem3 {
     public static int solution(int number) {
+        if(!Validator.validate(number)){return -1;}
         return count369(number);
     }
 }
