5ed518e46f8b261f576be15c7e76f41f656a1d33
heohyeonjink <hyeonjin4870@naver.com>
2022-11-01 19:53:19 +0900

    feat(PROBLEM7) : 친구 관계 점수 추가하기 friendScore 해쉬맵에 친구 관계 점수를 구한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,13 +5,34 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String, Integer> friendsScore = new HashMap<>();
 
         // 친구 관계를 표시할 HashMap
         HashMap<String, ArrayList<String>> friendsMap = new HashMap<>();
 
+        // 친구 관계 만들기
         for(List<String> tmp : friends){
             makeFriendsMap(tmp, friendsMap);
         }
+
+        // 친구들의 Score 초기화
+        for(String friend : friendsMap.keySet()){
+            makeFriendsScore(friend,user,friendsScore);
+        }
+
+        // 함께 아는 친구 점수 추가
+        for(String friend : friendsScore.keySet()){
+            if(isUserFriend(user,friend,friendsMap)) continue;
+            addSameFriendScore(user,friend,friendsMap,friendsScore);
+        }
+
+        // 방문한 친구 점수 추가
+        for(String visitor : visitors){
+            if(isUserFriend(user,visitor,friendsMap)) continue;
+            addVisitFriendScore(visitor,friendsScore);
+        }
+
+        
 
         return answer;
     }
@@ -37,4 +58,29 @@ public class Problem7 {
 
     }
 
+    public static void makeFriendsScore(String friend, String user, HashMap<String, Integer> friendsScore){
+        if(friend.equals(user)) return;
+        friendsScore.put(friend,0);
+    }
+
+    public static boolean isUserFriend(String user, String friend, HashMap<String, ArrayList<String>> friendsMap){
+        for(String tmp : friendsMap.get(user)){
+            if(tmp.equals(friend)) return true;
+        }
+        return false;
+    }
+
+    public static void addSameFriendScore(String user, String friend, HashMap<String, ArrayList<String>>friendsMap, HashMap<String,Integer> friendsScore){
+        // 한 친구의 점수 매기기
+
+        int score = 0;
+        for(String tmp : friendsMap.get(user)){
+            if(friendsMap.get(tmp).contains(friend)) score+=10;
+        }
+        friendsScore.put(friend, score);
+    }
+
+    public static void addVisitFriendScore(String visitor, HashMap<String,Integer> friendsScore){
+        friendsScore.put(visitor, friendsScore.getOrDefault(visitor,0)+1);
+    }
 }
