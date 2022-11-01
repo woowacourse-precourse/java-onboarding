1832c44b6d9afdb4632c0950541970d925dd9715
BEMELON <hgdkim2@naver.com>
2022-11-01 21:45:23 +0900

    Solve Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static char getEncryptChar(char ch) {
+        if ('A' <= ch && ch <= 'Z') {
+            return (char) ('Z' - (ch - 'A'));
+        } else if ('a' <= ch && ch <= 'z') {
+            return (char) ('z' - (ch - 'a'));
+        } else {
+            return ch;
+        }
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for(int i = 0; i < word.length(); i++) {
+            answer.append(getEncryptChar(word.charAt(i)));
+        }
+        return answer.toString();
     }
 }
