cb3f875ef4966952bdebb04a76fa155a9a40abe4
yenawee <yaena0319@naver.com>
2022-11-01 18:38:14 +0900

    [FEAT] 7. 지금 친구인 목록 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,17 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> CurrentFriends(String usr, List<List<String>> friends){
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++){
+            if (friends.get(i).contains(usr)){
+                list.add(friends.get(i).get(0));
+                list.add(friends.get(i).get(1));
+            }
+        }
+        list.remove(usr);
+        return list;
     }
 }
