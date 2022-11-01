dc893670dbfea65ddfb56b7388698f062ab4da1d
ho2n <jungho2n@gmail.com>
2022-11-01 17:54:43 +0900

    최종 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,6 +8,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -17,17 +19,18 @@ public class Problem7 {
         List<String> result = new ArrayList<>();
 
         fillFriendAndScoreMap(friends, friendMap, scoreMap);
+
         setRecommendScoreByVisit(visitors, scoreMap);
         setRecommendBySameFriends(user, friendMap, scoreMap);
         removeNotRecommendFriendInScoreMap(user, friendMap, scoreMap);
         sortScore(scoreMap);
         setFinalResultList(scoreMap, result);
 
-        return answer;
+        return result;
     }
 
     private static void fillFriendAndScoreMap(List<List<String>> friends, HashMap<String, HashSet<String>> friendMap, HashMap<String, Integer> scoreMap) {
-        for (List<String> friend : friends) {
+        for(List<String> friend : friends) {
             if (friendMap.containsKey(friend.get(0))) {
                 HashSet<String> values = friendMap.get(friend.get(0));
                 values.add(friend.get(1));
@@ -52,16 +55,16 @@ public class Problem7 {
         }
     }
 
-        private static void setRecommendScoreByVisit(List<String> visitors, HashMap<String, Integer> scoreMap) {
-            for (String friend : visitors) {
-                if (scoreMap.containsKey(friend)) {
-                    int score = scoreMap.get(friend);
-                    scoreMap.put(friend, score + 1);
-                } else {
-                    scoreMap.put(friend, 0);
-                }
+    private static void setRecommendScoreByVisit(List<String> visitors, HashMap<String, Integer> scoreMap) {
+        for (String friend : visitors) {
+            if (scoreMap.containsKey(friend)) {
+                int score = scoreMap.get(friend);
+                scoreMap.put(friend, score + 1);
+            } else {
+                scoreMap.put(friend, 0);
             }
         }
+    }
 
     private static void setRecommendBySameFriends(String user, HashMap<String, HashSet<String>> friendMap, HashMap<String, Integer> scoreMap) {
         HashSet<String> userValues = friendMap.get(user);
@@ -76,19 +79,14 @@ public class Problem7 {
             }
         }
     }
-    private static void removeNotRecommendFriendInScoreMap(String user,
-        HashMap<String, HashSet<String>> friendMap, HashMap<String, Integer> scoreMap) {
+    private static void removeNotRecommendFriendInScoreMap(String user, HashMap<String, HashSet<String>> friendMap, HashMap<String, Integer> scoreMap) {
         removeUserInScoreMap(user, scoreMap);
         removeExistFriendInScoreMap(user, friendMap, scoreMap);
     }
-
     private static void removeUserInScoreMap(String user, HashMap<String, Integer> scoreMap) {
         scoreMap.remove(user);
     }
-
-    private static void removeExistFriendInScoreMap(String user,
-                HashMap<String, HashSet<String>> friendMap,
-                HashMap<String, Integer> scoreMap) {
+    private static void removeExistFriendInScoreMap(String user, HashMap<String, HashSet<String>> friendMap, HashMap<String, Integer> scoreMap) {
         HashSet<String> existFriends = friendMap.get(user);
         for (String friend : existFriends) {
             scoreMap.remove(friend);
@@ -125,4 +123,3 @@ public class Problem7 {
         }
     }
 }
-
