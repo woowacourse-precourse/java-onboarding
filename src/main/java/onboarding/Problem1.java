e5e73e9f795b17991cb5c183b9df5fc7df29925e
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:23:40 +0900

    feat: Problem1 왼쪽 페이지 오른쪽 페이지 중 최대합이나 최대곱을 반환하는 기능 구현
    파라미터로 받은 왼쪽과 오른쪽의 페이지 숫자를 모두 다 더하거나 곱한 값중 제일 큰 값을 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+    public static int result(List<Integer> value){
+        List<Integer> temp = new ArrayList<>();
+        for (int v : value){
+            String s = Integer.toString(v);
+            int sum=0;
+            for (int i =0; i<s.length(); i++) sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+            int mul=0;
+            if (s.length()>1){
+                mul=Integer.parseInt(String.valueOf(s.charAt(0)));
+                for (int i =1; i<s.length(); i++){
+                    mul*=Integer.parseInt(String.valueOf(s.charAt(i)));
+                }
+            }
+            temp.add(Math.max(sum,mul));
+        }
+        return Math.max(temp.get(0),temp.get(1));
     }
 }
