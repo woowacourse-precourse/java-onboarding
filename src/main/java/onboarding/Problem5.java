bc90f7358d33797bab8d6a8bce5063a9e81f2aaf
CJY-kor <vivo_reprise@naver.com>
2022-11-01 18:56:01 +0900

    DOC(problem5) : 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,18 @@
+/*
+기능 목록
+i) getExchange(int money) -> 돈을 입력받고 exchange 개수를 list 형태로 반환한다.
+ii) solution -> i) 를 이용하여 정답을 반환한다.
+ */
+
+
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
