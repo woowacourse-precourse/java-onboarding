9e0cc18a3f94a980b10b3bb0c334121857dfda68
sylee <sy>
2022-11-01 23:28:50 +0900

    [Feat] : Problem4 최종 변환 문자열 반환하는 함수 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem4 {
+
+    public final static int ASCII_ALPHABET_UPPER_A = 65;
+    public final static int ASCII_ALPHABET_UPPER_Z = 90;
+    public final static int ASCII_ALPHABET_LOWER_A = 97;
+    public final static int ASCII_ALPHABET_LOWER_Z = 122;
+
     public static String solution(String word) {
         String answer = "";
+
+        answer = getChangedWord(word);
+
         return answer;
+    }
+
+    public static String getChangedWord(String word) {
+        String result = "";
+        for(int i = 0 ; i < word.length(); i++) {
+            char changedChar = word.charAt(i);
+            int asciiOfChar = (int)changedChar;
+            if(asciiOfChar >= ASCII_ALPHABET_UPPER_A && asciiOfChar <= ASCII_ALPHABET_UPPER_Z) {
+                changedChar = (char)(ASCII_ALPHABET_UPPER_A + ASCII_ALPHABET_UPPER_Z - asciiOfChar);
+            }
+            else if(asciiOfChar >= ASCII_ALPHABET_LOWER_A && asciiOfChar <= ASCII_ALPHABET_LOWER_Z) {
+                changedChar = (char)(ASCII_ALPHABET_LOWER_A + ASCII_ALPHABET_LOWER_Z - asciiOfChar);
+            }
+            result += changedChar;
+        }
+        return result;
     }
 }
