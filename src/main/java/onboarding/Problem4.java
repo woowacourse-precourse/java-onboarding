7fd5be528970176a08abd7a4a0808cf5bc68ec12
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-11-01 22:57:16 +0900

    style: 코드 스타일 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,30 +1,40 @@
 package onboarding;
 
 public class Problem4 {
+
     public static int setSum(char alphabet) {
         int sum;
-        if (Character.isUpperCase(alphabet)) sum = 155;
-        else if (Character.isLowerCase(alphabet)) sum = 219;
-        else sum = 2 * alphabet;
+        if (Character.isUpperCase(alphabet)) {
+            sum = 155;
+        } else if (Character.isLowerCase(alphabet)) {
+            sum = 219;
+        } else {
+            sum = 2 * alphabet;
+        }
 
         return sum;
     }
+
     public static char convertChar(char alphabet) {
         char result;
         int sum = setSum(alphabet);
-        result = (char)(sum - (int)alphabet);
+        result = (char) (sum - (int) alphabet);
 
         return result;
     }
+
     public static String convertWord(String word) {
         char[] alphabetList = word.toCharArray();
-        for (int i=0; i < alphabetList.length; i++) {
-            alphabetList[i] = convertChar(alphabetList[i]);
+        for (int i = 0; i < alphabetList.length; i++) {
+            if (Character.isAlphabetic(alphabetList[i])) {
+                alphabetList[i] = convertChar(alphabetList[i]);
+            }
         }
         String result = new String(alphabetList);
 
         return result;
     }
+
     public static String solution(String word) {
         String answer = convertWord(word);
         return answer;
