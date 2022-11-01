7e0a7af89f17e7d8d1f1ad47df5d5b16629c0806
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 10:56:33 +0900

    feat(PROBLEM5): exchangeMoney() 화폐변환 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        int[] won = {50000,10000,5000,1000,500,100,50,10,1};
+    public static final int[] won = {50000,10000,5000,1000,500,100,50,10,1};
 
-        for(int i=0; i<9; i++){
-            answer.add(money/won[i]);
+    public static List<Integer> solution(int money) {
+        return exchangeMoney(money);
+    }
+
+    public static List<Integer> exchangeMoney(int money){
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i<won.length ; i++){
+            result.add(money/won[i]);
             money = money % won[i];
         }
-        return answer;
+        return result;
     }
 }
