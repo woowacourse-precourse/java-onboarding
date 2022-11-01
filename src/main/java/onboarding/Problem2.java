52eb087b59ae75a81a6f8f011dd7f96bd7cd58b8
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:26:12 +0900

    Problem2 solution method
    Iterate through string as character array to look for pairs

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,15 @@ public class Problem2 {
         char[] str = cryptogram.toCharArray();
         char[] pattern = new char[cryptogram.length() + 1];
         int index = 1;
-        
+        for (char c : str) {
+            if (pattern[index - 1] == c) {
+                pattern[index - 1] = '\u0000';
+                index--;
+            } else {
+                pattern[index] = c;
+                index++;
+            }
+        }
+        return String.valueOf(pattern).trim();
     }
 }
