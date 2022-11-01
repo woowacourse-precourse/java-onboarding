deb7d359e7c606e7ce44ab8ab3e5a71faaed39e6
김승규 <rlatmdrb1996@gmail.com>
2022-11-01 19:40:55 +0900

    feat(problem3): 주어진 숫자의 3, 6, 9 개수 반환하는 메소드 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,7 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuilder answer = new StringBuilder();
         Stack<Character> answerStack = new Stack<>();
-        
+
         for (int i = 0; i < cryptogram.length(); ++i) {
             if (!answerStack.isEmpty() && answerStack.peek() == cryptogram.charAt(i)) {
                 answerStack.pop();
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    /**
+     * 주어진 숫자의 3, 6, 9 개수 반환하는 메소드
+     */
+    private static int numOfClaps(int number) {
+        int count = 0;
+        while (number > 0) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                ++count;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
