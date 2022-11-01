f116a9b2b70a656719474cc4cd107b56c2f2e399
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-01 20:40:43 +0900

    conversionCharList function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,20 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char[] conversionCharList(String word) {
+        char[] chars = word.toCharArray();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (chars[i] == ' ') {
+                chars[i] = ' ';
+            } else if (Character.isUpperCase(chars[i])) {
+                chars[i] = (char)(155 - (int)chars[i]);
+            } else {
+                chars[i] = (char)(219 - (int)chars[i]);
+            }
+        }
+
+        return chars;
+    }
 }
