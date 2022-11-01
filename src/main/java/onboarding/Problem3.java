32b0d0de0a17509010029420ab48f4ca5904aaa4
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2022-11-01 11:03:16 +0900

    if 369_answer++_loop

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,16 @@ public class Problem3 {
 		// 손뼉 칠 때마다 올라가는 변수 answer
 		int answer = 0;
 
-
+		for (int i = 1; i <= number; i++) {
+			// 현재 숫자가 3 6 9를 포함 시 answer++
+			int cur = i;
+			while (cur > 0) {
+				if (cur % 10 == 3 || cur % 10 == 6 || cur % 10 == 9) {
+					answer++;
+				}
+				cur /= 10;
+			}
+		}
 
 		return answer;
 	}
