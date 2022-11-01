9ae8928a458d56ea77d119550c5db8d2a0f09b5f
1eejisoo <jisu991012@gmail.com>
2022-11-01 16:30:24 +0900

    [Problem 1] Style: 코드 순서 변경, 주석 삭제

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     private static final int EXCEPTION = -1;
     private static final int DRAW = 0;
     private static final int POBI_WIN = 1;
@@ -18,26 +19,23 @@ class Problem1 {
         return EXCEPTION;
     }
 
-    // 가장 큰 수를 구하는 기능을 하는 함수
+    public static int getWinner(Integer pobiMaxNum, Integer crongMaxNum) {
+        if (pobiMaxNum.equals(crongMaxNum)) {
+            return DRAW;
+        }
+        if (pobiMaxNum > crongMaxNum) {
+            return POBI_WIN;
+        }
+        return CRONG_WIN;
+    }
+
     public static int getMaxNumber (Integer leftPageNum, Integer rightPageNum) {
         int leftPageMaxNum = calcMax(getSumPageNum(leftPageNum), getMultiplyPageNum(leftPageNum));
-        int rightPageMaxNum = calcMax(getSumPageNum(leftPageNum), getMultiplyPageNum(leftPageNum));
+        int rightPageMaxNum = calcMax(getSumPageNum(rightPageNum), getMultiplyPageNum(rightPageNum));
 
         return calcMax(leftPageMaxNum, rightPageMaxNum);
     }
 
-    // 승부 체크 기능을 하는 함수
-    public static int getWinner(Integer pobiMaxNum, Integer crongMaxNum) {
-        if (pobiMaxNum.equals(crongMaxNum)) { // pobi, crong DRAW
-            return DRAW;
-        }
-        if (pobiMaxNum > crongMaxNum) {   // pobi WIN
-            return POBI_WIN;
-        }
-        return CRONG_WIN;   // crong WIN
-    }
-
-    // 페이지 번호의 각 자리 숫자를 모두 더하는 기능을 하는 함수
     public static int getSumPageNum(Integer pageNum){
         int sum = 0;
         while (pageNum > 0) {
@@ -47,7 +45,6 @@ class Problem1 {
         return sum;
     }
 
-    // 페이지 번호의 각 자리 숫자를 모두 곱하는 기능을 하는 함수
     public static int getMultiplyPageNum(Integer pageNum) {
         int mul = 1;
         while (pageNum > 0) {
@@ -57,19 +54,9 @@ class Problem1 {
         return mul;
     }
 
-    // 두 수 중 큰 수를 반환하는 기능을 하는 함수
     public static int calcMax(Integer num1, Integer num2) {
         return Math.max(num1, num2);
     }
-
-    /**
-     *  예외사항 체크
-     * 0. 전체 예외사항들을 체크 - > isException()
-     * 1. 각 페이지 번호가 1 ~ 400 범위 안에 들어가있는지 확인 -> isPageRange()
-     * 2. 왼쪽, 오른쪽 페이지 번호가 연속된 수인지 확인 -> isContinuous()
-     * 3. 왼쪽 페이지 번호는 홀수, 오른쪽 페이지 번호는 짝수인지 확인 -> isEvenOrOdd()
-     * 4. 시작 면과 마지막 면이 아닌지 확인 -> isNotNull()
-     */
 
     public static boolean isException(List<Integer> page) {
         return isPageRange(page) && isContinuous(page) && isEvenOrOdd(page);
