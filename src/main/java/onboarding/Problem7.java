22db7eaf521a5cbe9586d5b7bcedbb5545fe40bc
ghchoi <ghchoi0427@gmail.com>
2022-11-01 19:15:13 +0900

    feat: 지인 점수 반환 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem7 {
@@ -17,4 +14,38 @@ public class Problem7 {
         friends.stream().filter(e -> e.contains(user)).forEach(result::addAll);
         return result.stream().filter(element -> !element.equals(user)).collect(Collectors.toList());
     }
+
+    private HashMap<String, Integer> getAcquaintanceScore(List<List<String>> friends, String userFriend, String user) {
+        HashMap<String, Integer> recommendation = new HashMap<>();
+
+        for (List<String> relation : friends) {
+            if (!relation.contains(user) && relation.contains(userFriend)) {
+                String stranger = relation.stream().filter(e -> !e.equals(userFriend)).findFirst().get();
+                if (recommendation.containsKey(stranger)) {
+                    recommendation.put(stranger, recommendation.get(stranger) + 10);
+                } else {
+                    recommendation.put(stranger, 10);
+                }
+            }
+        }
+        return recommendation;
+    }
+
+    private List<String> excludeFriend(List<String> visitors, List<String> friends) {
+        return visitors.stream().filter(e -> !friends.contains(e)).collect(Collectors.toList());
+    }
+
+    private HashMap<String, Integer> getVisitScore(List<String> visitors) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String visitor : visitors) {
+            if (map.containsKey(visitor)) {
+                map.put(visitor, map.get(visitor) + 1);
+            } else {
+                map.put(visitor, 1);
+            }
+        }
+        return map;
+    }
+
+
 }
