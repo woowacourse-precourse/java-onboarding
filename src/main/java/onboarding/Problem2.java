47f0379870ea87b202773facbbd5ca4f28fbc7dc
jay <jaewhan232@naver.com>
2022-11-01 16:08:43 +0900

    Implement : 중복을 제거한 문자열 반환하는 메소드 구현.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = getTrimedString(cryptogram);
+        return answer;
+    }
+
+    private static String getTrimedString(String cryptogram) {
+        String answer = "";
+        for (char ch : cryptogram.toCharArray()) {
+            if (answer.endsWith(String.valueOf(ch))) {
+                answer = answer.substring(0, answer.length()-1);
+            } else {
+                answer += ch;
+            }
+        }
         return answer;
     }
 }
