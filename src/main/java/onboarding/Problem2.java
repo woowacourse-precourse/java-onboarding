89375319454fb113fa4c951b0e0e63501f792765
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-01 20:26:31 +0900

    overlapNum function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static List<Integer> overlapNum(String inputString) {
+        List<Integer> overlapList = new ArrayList<Integer>();
+
+        for (int i = 0; i < inputString.length() - 1; i++) {
+            if (inputString.charAt(i) == inputString.charAt(i + 1)) {
+                overlapList.add(i);
+            }
+        }
+
+        return overlapList;
     }
 }
