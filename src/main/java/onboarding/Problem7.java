dd2215abccbe26cdc24f395e913632ae032fdaee
민병욱 <woook27@naver.com>
2022-11-01 20:47:49 +0900

    feat: setFriendList 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
+
+    public static HashMap<String, HashSet<String>> friendList = new HashMap<>();
+    public static HashMap<String, Integer> scoreboard = new HashMap<>();
+
+    private static List<String> getRecommendedFriends(String user) {
+
+    }
+
+    private static void setFriendList(List<List<String>> friends) {
+        for (int i = 0; i < friends.size(); i++) {
+            String user1 = friends.get(i).get(0);
+            String user2 = friends.get(i).get(1);
+            HashSet<String> user1List = friendList.get(user1);
+            HashSet<String> user2List = friendList.get(user2);
+            user1List.add(user2);
+            user2List.add(user1);
+            friendList.put(user1, user1List);
+            friendList.put(user2, user2List);
+        }
+    }
+
+    private static void setVistors(String user, List<String> visitors) {
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        setFriendList(friends);
+        setVistors(user, visitors);
+        List<String> answer = getRecommendedFriends(user);
         return answer;
     }
 }
