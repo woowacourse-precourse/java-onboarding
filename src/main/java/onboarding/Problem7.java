ddfc7a12bd2a89b7e00636acd81e5faf1b13293b
RadSparkle <wwwartg1@naver.com>
2022-11-01 23:46:05 +0900

    최종 제출.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,18 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        HashMap hashMap = new HashMap<>();
+
+        for(int i=0; i<friends.size();i++){
+            hashMap.put(friends.get(i).get(0),friends.get(i).get(1));
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
