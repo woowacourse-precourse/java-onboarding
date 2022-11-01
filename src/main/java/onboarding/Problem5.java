19c9b84ce5955dcc0583bc173e091cc097aa610c
SeungEon <js112099@gmail.com>
2022-11-01 21:05:29 +0900

    문제5 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,21 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        List<Integer> answer = new ArrayList<>();
         int[] price = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int temp = money;
 
         for(int i = 0; i < 9; i++) {
-            answer.set(i, money/price[i]);
+            answer.add(money/price[i]);
 
             money = money % price[i];
         }
 
         System.out.println("money\tresult");
 
-        System.out.println(money + "\t" + answer);
+        System.out.println(temp + "\t\t" + answer);
 
         return answer;
     }
