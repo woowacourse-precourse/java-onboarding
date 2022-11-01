3dd80c36d6a24aa4c68d8fa87a3ada1ca411b374
devEdok13 <dev.edok13@gmail.com>
2022-11-01 16:14:18 +0900

    feat: P04-staticVariable

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    //  Static-Variable
+    private static final int ASCII_CODE_a = 97;
+    private static final int ASCII_CODE_A = 65;
+    private static final int ASCII_CODE_ZtoA = 25;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
