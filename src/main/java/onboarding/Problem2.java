c28d4e6d8ef357e0c96a44d79f1d01444fdaed04
JoonHyug <hyug0819@gmail.com>
2022-11-01 20:57:59 +0900

    2번 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        List<Integer> index = new ArrayList<>();
+        List<String> array2 = new ArrayList<>();
+        String[] array = cryptogram.split("");
+        boolean check = false;
+
+        while(!check){
+            check = true;
+            for(int i=0;i< array.length-1;i++){
+                if(array[i].equals(array[i+1])){
+                    index.add(i);
+                    index.add(i+1);
+                    i++;
+                    check = false;
+                }
+            }
+            for(int i=0;i< array.length;i++){
+                if(!index.contains(i)){
+                    array2.add(array[i]);
+                }
+            }
+            array = array2.toArray(new String[array2.size()]);
+            index.clear();
+            array2.clear();
+        }
+
+        for(String data : array){
+            answer += data;
+        }
         return answer;
     }
 }
