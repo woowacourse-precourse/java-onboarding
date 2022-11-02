b703a536961411c8a718c6cd2df9ad48f845c4bc
Cho-El <syzzang21c@gmail.com>
2022-11-02 16:50:24 +0900

    refactor:코딩컨벤션

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,20 +4,21 @@ public class Problem4 {
     // 단어 거꾸로 하는 함수
     static String changeWord(String word){
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             char alpha = word.charAt(i);
-            if ((alpha >= 'a' && alpha <= 'z')){// 소문자 일때
-                char changeAlpha = (char)('z' - (alpha - 'a'));
+            if ((alpha >= 'a' && alpha <= 'z')) {// 소문자 일때
+                char changeAlpha = (char) ('z' - (alpha - 'a'));
                 result.append(changeAlpha);
-            }else if( (alpha >= 'A' && alpha <= 'Z')){// 대문자 일때
-                char changeAlpha = (char)('Z' - (alpha - 'A'));
+            } else if ((alpha >= 'A' && alpha <= 'Z')) {// 대문자 일때
+                char changeAlpha = (char) ('Z' - (alpha - 'A'));
                 result.append(changeAlpha);
-            }else{// 그 밖의 문자일 때
+            } else {// 그 밖의 문자일 때
                 result.append(alpha);
             }
         }
         return result.toString();
     }
+
     public static String solution(String word) {
         return changeWord(word);
     }
