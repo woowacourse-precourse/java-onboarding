86b9a2e5eb96aff5daa2f410c6a7c8a55ba55b5a
bnbnac <bnbnac@naver.com>
2022-11-02 12:59:10 +0900

    feat: (P6)닉네임이 2글자 이상인 경우, 닉네임을 순회하며 2글자씩 HashMap에 입력하는 메서드(키:2글자, 값:이메일)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
+
+    static HashSet<String> mailSet = new HashSet<String>();
+    static HashMap<String, String> mailMappedBySubNickname = new HashMap<String, String>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for (List<String> form : forms) {
+            checkMapped(form);
+        }
         return answer;
+    }
+
+    private static void checkMapped(List<String> form) {
+        String email = form.get(0);
+        String nickname = form.get(1);
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String subNickname = nickname.substring(i, i + 2);
+            mapDuplication(email, subNickname);
+        }
+    }
+
+    private static void mapDuplication(String email, String subNickname) {
+        mailMappedBySubNickname.put(subNickname, email);
     }
 }
