81509e6805aaaf13b8f9c0f90450dfa7d3c51b64
bnbnac <bnbnac@naver.com>
2022-11-02 09:52:02 +0900

    docs: (P5)List를 순회하며 List의 원소로 입력값을 나눈 몫을 구하여 원소의 값을 갱신하고 입력값을 갱신

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
+        int quotient;
+        int bill;
         answer.add(50000);
         answer.add(10000);
         answer.add(5000);
@@ -16,6 +18,12 @@ public class Problem5 {
         answer.add(10);
         answer.add(1);
 
+        for (int i = 0; i < 9; i++) {
+            bill = answer.get(i);
+            quotient = money / bill;
+            answer.set(i, quotient);
+            money -= bill * quotient;
+        }
 
         return answer;
     }
