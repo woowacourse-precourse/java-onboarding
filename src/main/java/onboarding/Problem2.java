5e601787c0f838166f61db1ee2d16a43d1c5be09
seungmin-park <tmddudals369@naver.com>
2022-11-02 17:03:38 +0900

    중복 문자 제거 메서드 추출, 중복 체크 변수명 flag -> isExistDuplication 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,23 +4,29 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         StringBuilder sb = new StringBuilder(cryptogram);
-        boolean flag = false;
-        while (!flag){
-            flag = true;
-            for (int i =0; i < sb.length()-1; i++){
-                if (sb.charAt(i) == sb.charAt(i+1)){
-                    int duplicateCnt = 1;
-                    for (int j = i+1; j < sb.length(); j++){
-                        if (sb.charAt(i) == sb.charAt(j)) duplicateCnt++;
-                        else break;
-                    }
-                    sb.delete(i,i + duplicateCnt);
-                    i--;
-                    flag = false;
-                }
-            }
+        boolean isExistDuplication;
+        do {
+            isExistDuplication = removeDuplicationWords(sb);
         }
+        while (isExistDuplication);
         answer = sb.toString();
         return answer;
+    }
+
+    private static boolean removeDuplicationWords(StringBuilder sb) {
+        boolean isExistDuplication = false;
+        for (int i = 0; i < sb.length()-1; i++){
+            if (sb.charAt(i) == sb.charAt(i+1)){
+                int duplicateCnt = 1;
+                for (int j = i+1; j < sb.length(); j++){
+                    if (sb.charAt(i) == sb.charAt(j)) duplicateCnt++;
+                    else break;
+                }
+                sb.delete(i,i + duplicateCnt);
+                isExistDuplication = true;
+                i--;
+            }
+        }
+        return isExistDuplication;
     }
 }
