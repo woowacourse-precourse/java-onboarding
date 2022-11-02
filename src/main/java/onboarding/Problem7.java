00d230cd7177207bdfe9dbe8b0e7904f599e33d4
dclxxi <dclxxi@kakao.com>
2022-11-02 20:49:53 +0900

    refactor(문제7): 변수 제거 후 값 바로 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,17 +5,17 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Problem7 {
-    private static final int USER = 0;
-    private static final int FRIEND = 1;
+    private static final int USER_INDEX = 0;
+    private static final int FRIEND_INDEX = 1;
     private static final int FRIEND_SCORE = 10;
     private static final int VISIT_SCORE = 1;
     private static final int MIN_SCORE = 1;
     private static final int MAX_COUNT = 5;
 
     public static class User {
-        private String id;
+        private final String id;
         private int score;
-        private List<String> friends;
+        private final List<String> friends;
 
         public User(String id) {
             this.id = id;
@@ -57,8 +57,8 @@ public class Problem7 {
         users.put(user, new User(user));
 
         for (List<String> friend : friends) {
-            String userId = friend.get(USER);
-            String friendId = friend.get(FRIEND);
+            String userId = friend.get(USER_INDEX);
+            String friendId = friend.get(FRIEND_INDEX);
 
             addFriendOfUser(users, userId, friendId);
             addFriendOfUser(users, friendId, userId);
@@ -106,23 +106,19 @@ public class Problem7 {
     private static List<String> getRecommendedFriends(Map<String, User> users, String user) {
         List<User> recommendCandidates = getRecommendCandidates(users, user);
 
-        List<String> recommendFriends = recommendCandidates.stream()
+        return recommendCandidates.stream()
                 .sorted(sortRecommendFriend())
-                .map(u -> u.getId())
+                .map(User::getId)
                 .limit(MAX_COUNT)
                 .collect(Collectors.toList());
-
-        return recommendFriends;
     }
 
     private static List<User> getRecommendCandidates(Map<String, User> users, String user) {
         List<String> friendIds = findFriendIdsById(users, user);
 
-        List<User> recommendCandidates = users.values().stream()
+        return users.values().stream()
                 .filter(canRecommend(user, new HashSet<>(friendIds)))
                 .collect(Collectors.toList());
-
-        return recommendCandidates;
     }
 
     private static Predicate<User> canRecommend(String user, Set<String> friendIds) {
