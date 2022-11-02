3bfafe0fde870c84f78142c58c15a4e0b458b4c2
bnbnac <bnbnac@naver.com>
2022-11-02 09:44:52 +0900

    docs: (P5)화폐 금액들을 크기순으로 담은 List 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        answer.add(50000);
+        answer.add(10000);
+        answer.add(5000);
+        answer.add(1000);
+        answer.add(500);
+        answer.add(100);
+        answer.add(50);
+        answer.add(10);
+        answer.add(1);
+
+
         return answer;
     }
 }
