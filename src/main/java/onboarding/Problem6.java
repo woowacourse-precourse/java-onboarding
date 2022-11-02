c65cdc389ef8d09dcdbaa81f9ccc90119d53ac13
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-02 21:24:50 +0900

    [feat] Problem6: 이메일 및 닉네임 예외처리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
+        if(isEmailFormatArgumentException(forms)){
+            throw new IllegalArgumentException("이메일 양식을 다시 확인하여 주십시오.");
+        }
+        if(isNicNameFormatArgumentException(forms)){
+            throw new IllegalArgumentException("닉네임 양식을 다시 확인하여 주십시오.");
+        }
+
         Set<String> sendEmailList = new HashSet<>();
         int size = forms.size();
         for (int i = 0; i < size - 1; i++) {
@@ -23,6 +33,46 @@ public class Problem6 {
         return sendEmailList.stream()
                 .sorted()
                 .collect(Collectors.toList());
+    }
+
+    private static boolean isNicNameFormatArgumentException(List<List<String>> forms) {
+        String nicNameRegex = "^[가-힣]*$"; // 한글만 허용
+        Pattern pattern = Pattern.compile(nicNameRegex);
+        for (List<String> form : forms) {
+            String nicName = form.get(1);
+            Matcher checkNicName = pattern.matcher(nicName);
+            if(isNotNicNameSize(nicName)){
+                return true;
+            }
+            if(!checkNicName.matches()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNotNicNameSize(String nicName) {
+        return nicName.length() < 1 || nicName.length() >= 20;
+    }
+
+    private static boolean isEmailFormatArgumentException(List<List<String>> forms) {
+        String emailRegex = "^[A-Za-z0-9_.-]+@email.com"; //영문 대소문자 숫자 _.-까지 허용
+        Pattern pattern = Pattern.compile(emailRegex);
+        for(List<String> form : forms){
+            String email = form.get(0);
+            Matcher checkEmail = pattern.matcher(email);
+            if(isNotEmailSize(email)){
+                return true;
+            }
+            if(!checkEmail.matches()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNotEmailSize(String email) {
+        return email.length() < 11 || email.length() >= 20;
     }
 
     private static boolean isDuplicateNicName(String name, String compare) {
