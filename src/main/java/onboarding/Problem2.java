0d1cf9d2730d09f6b17f55c8a46a552fbaaa5d15
backcoder <backcoder2@gmail.com>
2022-11-08 17:41:12 +0900

    problem1,2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static StringBuffer remover(String answer) {
+        StringBuffer sb = new StringBuffer(answer);
+        for (int i = 0; i < sb.length() - 1; i++) {
+            if (sb.charAt(i) == sb.charAt(i + 1)) {
+                sb = sb.deleteCharAt(i);
+                sb = sb.deleteCharAt(i);
+                sb = remover(String.valueOf(sb));
+            }
+        }
+        return sb;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return String.valueOf(remover(cryptogram));
     }
 }
