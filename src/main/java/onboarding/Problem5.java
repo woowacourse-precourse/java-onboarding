5f81ce1ee078def2317b39be277ec10ebdfe3a9a
Hyejin Eom <hjeom.dev@gmail.com>
2022-11-08 21:58:23 +0900

    add: problem 5 (test passed)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] digit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < digit.length; i++) {
+            if (money >= digit[i]) {
+                int count = money / digit[i];
+                answer.add(count);
+                money %= digit[i];
+            } else {
+                answer.add(0);
+            }
+        }
+
         return answer;
     }
 }
