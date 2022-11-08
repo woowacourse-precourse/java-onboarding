3619a8372cfd1746ee5b88dec505ddc85e09b109
Hyejin Eom <hjeom.dev@gmail.com>
2022-11-08 15:30:58 +0900

    add: problem 2 (test passed)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Stack<String> s = new Stack<>();
+
+        s.push(cryptogram.substring(0, 1));
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            String current = cryptogram.substring(i, i + 1);
+            if (s.peek().equals(current)) {
+                s.pop();
+            } else {
+                s.push(current);
+            }
+        }
+
+        answer = Arrays.stream(s.toArray())
+                .map(Object::toString)
+                .collect(Collectors.joining());
+
         return answer;
     }
 }
