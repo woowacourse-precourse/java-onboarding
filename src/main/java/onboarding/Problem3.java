f74af067c2bfb927d22509ac92556f113c5dd6b4
kdkdhoho <hkim4410@naver.com>
2022-11-15 11:55:51 +0900

    refactor(Problem3): 코드 컨벤션을 위한 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,13 +2,13 @@ package onboarding;
 
 public class Problem3 {
 
-    private static final int START_NUM = 1;
+    private static final int START_NUMBER = 1;
 
     public static int solution(int number) {
         int answer = 0;
 
-        for(int i = START_NUM; i <= number; i++) {
-            answer += count369(i);
+        for (int currentNumber = START_NUMBER; currentNumber <= number; currentNumber++) {
+            answer += count369(currentNumber);
         }
 
         return answer;
@@ -16,20 +16,23 @@ public class Problem3 {
 
     public static int count369(int number) {
         int result = 0;
-        String numStr = String.valueOf(number);
+        String data = String.valueOf(number);
 
-        for(char ch : numStr.toCharArray()) {
-            int checkNum = Integer.parseInt(String.valueOf(ch));
-            if(is369(checkNum))
+        for (char word : data.toCharArray()) {
+            String tmpData = String.valueOf(word);
+            int checkNum = Integer.parseInt(tmpData);
+            if(is369(checkNum)) {
                 result++;
+            }
         }
 
         return result;
     }
 
-    public static boolean is369(int checkNum) {
-        if(checkNum == 3 || checkNum == 6 || checkNum == 9)
+    public static boolean is369(int checkNumber) {
+        if(checkNumber == 3 || checkNumber == 6 || checkNumber == 9) {
             return true;
+        }
         return false;
     }
 }
