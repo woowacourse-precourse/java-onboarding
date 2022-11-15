debcdb6f016de498829606fcd9948d17b7ec72a4
kdkdhoho <hkim4410@naver.com>
2022-11-15 11:58:19 +0900

    refactor(Problem5): 변수명 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,17 +1,16 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
-    private static int[] amountUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static int[] amountUnits = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
-        for(int unit : amountUnit) {
+        for(int unit : amountUnits) {
             answer.add(money / unit);
             money %= unit;
         }
