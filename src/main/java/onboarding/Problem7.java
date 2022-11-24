fbc7075dfce78de06943ac229e99b2dfc0cfed5d
Kwon-sang <kwonsg514@gmail.com>
2022-11-24 23:36:17 +0900

    [리팩토링] Problem7.java

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,60 +1,66 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 
-    private static Map<String, Set<String>> friendsRelationRepository;
-    private static Map<String, Integer> friendsScoreRepository;
+    private static final Map<String, Set<String>> friendsRelationRepository = new HashMap<>();
+    private static final Map<String, Integer> friendsScoreRepository = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        friendsRelationRepository = new HashMap<>();
-        friendsScoreRepository = new HashMap<>();
-
         friends.forEach(x-> saveFriendRelation(x.get(0), x.get(1)));
-
         assign_10score(user);
         assign_1score(user, visitors);
-
-        List<Map.Entry<String, Integer>> scoreList = new ArrayList<>(friendsScoreRepository.entrySet());
-        scoreList.sort((o1, o2)->{
-            if (o1.getValue()==o2.getValue()) o1.getKey().compareTo(o2.getKey());
-            return o2.getValue()- o1.getValue();
-        });
-
-        List<String> recommendedList = new ArrayList<>();
-        for(Map.Entry<String, Integer> i : scoreList) {
-            if (i.getValue()!=0) recommendedList.add(i.getKey());
-            if (recommendedList.size() > 5) break;
+        Map<String, Integer> sortedFriendsScoreRepository = sortedMapByScore();
+        List<String> recommendedFriends = new ArrayList<>(sortedFriendsScoreRepository.keySet());
+        if (sortedFriendsScoreRepository.size() <= 5) {
+            return recommendedFriends;
         }
-
-        return recommendedList;
+        return recommendedFriends.subList(0,5);
     }
 
-    static void saveFriendRelation(String friends1, String friends2) {
-        if (!friendsRelationRepository.containsKey(friends1)) friendsRelationRepository.put(friends1, new HashSet<>());
-        if (!friendsRelationRepository.containsKey(friends2)) friendsRelationRepository.put(friends2, new HashSet<>());
+    private static Map<String, Integer> sortedMapByScore() {
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(friendsScoreRepository.entrySet());
+        Collections.sort(entries, (Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) -> {
+            if (o1.getValue() == o2.getValue()) {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+            return o2.getValue() - o1.getValue();
+        });
+
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        entries.stream().forEach(x -> sortedMap.put(x.getKey(), x.getValue()));
+        return sortedMap;
+    }
+
+    private static void saveFriendRelation(String friends1, String friends2) {
+        if (!friendsRelationRepository.containsKey(friends1)) {
+            friendsRelationRepository.put(friends1, new HashSet<>());
+        }
+        if (!friendsRelationRepository.containsKey(friends2)) {
+            friendsRelationRepository.put(friends2, new HashSet<>());
+        }
         friendsRelationRepository.get(friends1).add(friends2);
         friendsRelationRepository.get(friends2).add(friends1);
     }
 
-    static void assign_10score(String user){
-        for(String i : friendsRelationRepository.keySet()){
-            if(i.equals(user)) continue;
-            if(!friendsRelationRepository.get(user).contains(i)) {
-                int count = (int)friendsRelationRepository.get(i).stream().
-                        filter(x -> friendsRelationRepository.get(user).contains(x)).count();
-                friendsScoreRepository.put(i, 10*count);
-            }
-        }
+    private static void assign_10score(String user){
+        Set<String> friendsOfUser = friendsRelationRepository.get(user);
+        List<String> friendsOfFriends = friendsOfUser.stream().
+                map(friendsRelationRepository::get).
+                flatMap(Collection::stream).
+                filter(x-> !x.equals(user)).
+                collect(Collectors.toList());
+        friendsOfFriends.stream().
+                filter(x -> !friendsOfUser.contains(x)).
+                forEach(x -> friendsScoreRepository.put(x, friendsScoreRepository.getOrDefault(x, 0) + 10));
     }
 
     static void assign_1score(String user, List<String> visitor) {
-        for(String i : visitor){
-            if (i.equals(user)) continue;
-            if(!friendsRelationRepository.get(user).contains(i)){
-                friendsScoreRepository.put(i, friendsScoreRepository.getOrDefault(i,0)+1);
-            }
-        }
+        visitor.stream().
+                filter(x -> !x.equals(user)).
+                filter(x -> !friendsRelationRepository.get(user).contains(x)).
+                forEach(x -> friendsScoreRepository.put(x, friendsScoreRepository.getOrDefault(x, 0)+1));
     }
 }
