26837c6b257b7e66b30a33fedfd3c50994a1c3fa
Kwon-sang <kwonsg514@gmail.com>
2022-11-24 15:33:29 +0900

    [리팩토링] Problem1.java

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,39 +5,52 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (!isCorrect(pobi) || !isCorrect(crong)) return -1;
+        if (!validate(pobi) || !validate(crong)) {
+            return -1;
+        }
 
-        int pobiScore = score(pobi);
-        int crongScore = score(crong);
+        int pobiScore = calculateScore(pobi);
+        int crongScore = calculateScore(crong);
 
-        if(pobiScore == crongScore) return 0;
-        return pobiScore > crongScore ? 1 : 2;
+        if (pobiScore == crongScore) return 0;
+        if (pobiScore > crongScore) return 1;
+        return 2;
     }
 
-    private static boolean isCorrect(List<Integer> player) {
-        Integer leftPage = player.get(0);
-        Integer rightPage = player.get(1);
-        if(rightPage-leftPage!=1) return false;
-        if(leftPage < 1 || rightPage > 400) return false;
+
+    private static boolean validate(List<Integer> openedPages) {
+        return validatePage(openedPages) && (openedPages.get(0) + 1 == openedPages.get(1));
+    }
+    private static boolean validatePage(List<Integer> openedPages) {
+        Integer leftPage = openedPages.get(0);
+        Integer rightPage = openedPages.get(1);
+        if (leftPage < 1 || leftPage > 400 || leftPage % 2 == 0) {
+            return false;
+        }
+        if (rightPage < 1 || rightPage > 400 || rightPage % 2 == 1) {
+            return false;
+        }
         return true;
     }
 
-    static int score(List<Integer> player) {
+
+    private static int calculateScore(List<Integer> player) {
         int leftPage = player.get(0);
         int rightPage = player.get(1);
-        int maxDigitSum = Math.max(sumDigits(leftPage), sumDigits(rightPage));
-        int maxDigitProduct = Math.max(productDigits(leftPage), productDigits(rightPage));
-
-        return Math.max(maxDigitSum, maxDigitProduct);
+        return Math.max(maxDigitsSum(leftPage, rightPage), maxDigitsProduct(leftPage, rightPage));
     }
-
-    static int sumDigits(int page){
-        String[] digits = String.valueOf(page).split("");
-        return Arrays.stream(digits).mapToInt(Integer::parseInt).sum();
+    private static int maxDigitsSum(int leftPage, int rightPage){
+        int leftDigitsSum = Arrays.stream(String.valueOf(leftPage).split("")).
+                mapToInt(Integer::parseInt).sum();
+        int rightDigitsSum = Arrays.stream(String.valueOf(rightPage).split("")).
+                mapToInt(Integer::parseInt).sum();
+        return Math.max(leftDigitsSum, rightDigitsSum);
     }
-
-    static int productDigits(int page){
-        String[] digits = String.valueOf(page).split("");
-        return Arrays.stream(digits).mapToInt(Integer::parseInt).reduce(1, (x,y)->x*y);
+    private static int maxDigitsProduct(int leftPage, int rightPage){
+        int leftDigitsProduct = Arrays.stream(String.valueOf(leftPage).split("")).
+                mapToInt(Integer::parseInt).reduce(1, (x,y)->x*y);
+        int rightDigitsProduct = Arrays.stream(String.valueOf(rightPage).split("")).
+                mapToInt(Integer::parseInt).reduce(1, (x,y)->x*y);
+        return Math.max(leftDigitsProduct, rightDigitsProduct);
     }
 }
