b4958842c01de9ad85a805a8943459a064b69635
Kwon-sang <kwonsg514@gmail.com>
2022-11-24 17:36:10 +0900

    [리팩토링] Problem5.java

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,22 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
+
+    private static final List<Integer> MONETARY_UNITS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        return monetaryClassifier(money);
+    }
 
-        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
+    private static List<Integer> monetaryClassifier(int money) {
+        Map<Integer, Integer> classifier = new LinkedHashMap<>();
         int temp = money;
-        for(int i=0; i<units.length; i++) {
-            answer.add(temp/units[i]);
-            temp %= units[i];
+        for (int i : MONETARY_UNITS) {
+            classifier.put(i, temp / i);
+            temp %= i;
         }
-
-        return answer;
+        return new ArrayList(classifier.values());
     }
 }
