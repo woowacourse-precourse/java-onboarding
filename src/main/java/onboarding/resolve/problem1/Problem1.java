9c7c5e2bed21685f167c40d621fb412ad7c1754f
yoojin Jang <yoojinjangjang@gmail.com>
2022-11-26 17:57:00 +0900

    [feat] Problem1 resolve - pobi와 crong 점수 구하기

--- src/main/java/onboarding/resolve/problem1/Problem1.java
+++ src/main/java/onboarding/resolve/problem1/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding.resolve.problem1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem1 {
@@ -9,7 +10,45 @@ public class Problem1 {
         if (!ExceptionHandler.validateInput(pobi) || !ExceptionHandler.validateInput(crong)) {
             return Constants.EXCEPTION;
         }
+        Integer pobiScore = getScore(pobi);
+        Integer crongScore = getScore(crong);
 
         return answer;
     }
+
+    private static Integer getScore(List<Integer> pobi) {
+        List<Integer> left = splitInt(pobi.get(0));
+        List<Integer> right = splitInt(pobi.get(1));
+        int sumMax = Integer.max(getSum(left),getSum(right));
+        int mulMax = Integer.max(getMul(left),getMul(right));
+
+        return Integer.max(sumMax,mulMax);
+    }
+
+    private static int getMul(List<Integer> splitInts) {
+        int mulInt = 1;
+        for (Integer splitInt : splitInts) {
+            mulInt *= splitInt;
+        }
+        return mulInt;
+    }
+
+    private static int getSum(List<Integer> splitInts) {
+        int sumInt = 0;
+        for (Integer splitInt : splitInts) {
+            sumInt += splitInt;
+        }
+        return sumInt;
+    }
+
+    private static List<Integer> splitInt(Integer num) {
+        List<Integer> splitInts = new ArrayList<>();
+        while(num > 0) {
+            splitInts.add(num%10);
+            num /= 10;
+        }
+        return splitInts;
+    }
+
+
 }
---
+++ src/test/java/onboarding/resolve/problem1/Problem1Test.java
@@ -0,0 +1,11 @@
+package onboarding.resolve.problem1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem1Test {
+
+
+
+}
