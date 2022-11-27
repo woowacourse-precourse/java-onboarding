956f499a03323578d81b96e1cfb483296d590390
Eunkeee <eunkeeeee@gmail.com>
2022-11-28 00:41:28 +0900

    [P3] refactor: 메서드 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.List;
 
 public class Problem3 {
+
+    public static final String COUNT_THREE = "3";
+    public static final String COUNT_SIX = "6";
+    public static final String COUNT_NINE = "9";
+
     public static int solution(int number) {
         return calculateTotalClaps(number);
     }
@@ -10,15 +15,26 @@ public class Problem3 {
     private static int calculateTotalClaps(int number) {
         int totalClaps = 0;
         for (int currentNumber = 1; currentNumber <= number; currentNumber++) {
-            int claps = 0;
-            List<String> digits = List.of(String.valueOf(currentNumber).split(""));
-            for (String digit : digits) {
-                if (digit.equals("3") || digit.equals("6") || digit.equals("9")) {
-                    claps++;
-                }
-            }
-            totalClaps += claps;
+            totalClaps += calculateCurrentNumberClaps(currentNumber);
         }
         return totalClaps;
+    }
+
+    private static int calculateCurrentNumberClaps(int currentNumber) {
+        int claps = 0;
+        for (String digit : splitNumberToDigits(currentNumber)) {
+            if (hasToClap(digit)) {
+                claps++;
+            }
+        }
+        return claps;
+    }
+
+    private static boolean hasToClap(String digit) {
+        return digit.equals(COUNT_THREE) || digit.equals(COUNT_SIX) || digit.equals(COUNT_NINE);
+    }
+
+    private static List<String> splitNumberToDigits(int currentNumber) {
+        return List.of(String.valueOf(currentNumber).split(""));
     }
 }
