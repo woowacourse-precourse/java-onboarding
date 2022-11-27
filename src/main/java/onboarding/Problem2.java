d2e61aff4d184daacde40f69c01d038c9834b6c4
Eunkeee <eunkeeeee@gmail.com>
2022-11-28 00:09:28 +0900

    [P2] feat: 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,29 @@
 package onboarding;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        for (int index = 0; index < cryptogram.length(); index++) {
+            char alphabet = cryptogram.charAt(index);
+            if (stack.isEmpty()) {
+                stack.push(alphabet);
+                continue;
+            }
+            char top = stack.pop();
+            if (top != alphabet) {
+                stack.push(top);
+                stack.push(alphabet);
+            }
+        }
 
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Iterator<Character> iterator = stack.iterator();
+        while (iterator.hasNext()) {
+            answer.append(iterator.next());
+        }
+        return answer.toString();
     }
 }
