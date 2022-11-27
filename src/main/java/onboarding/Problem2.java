d14a82cbc89b46f755b8d9e8ed43d03a4ca522a9
Eunkeee <eunkeeeee@gmail.com>
2022-11-28 00:13:15 +0900

    [P2] refactor: 메서드 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,12 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        return formatAnswer(decrypt(cryptogram));
+    }
+
+    private static Stack<Character> decrypt(String cryptogram) {
         Stack<Character> stack = new Stack<>();
-        for (int index = 0; index < cryptogram.length(); index++) {
-            char alphabet = cryptogram.charAt(index);
+        for (char alphabet : cryptogram.toCharArray()) {
             if (stack.isEmpty()) {
                 stack.push(alphabet);
                 continue;
@@ -18,7 +21,10 @@ public class Problem2 {
                 stack.push(alphabet);
             }
         }
+        return stack;
+    }
 
+    private static String formatAnswer(Stack<Character> stack) {
         StringBuilder answer = new StringBuilder();
         Iterator<Character> iterator = stack.iterator();
         while (iterator.hasNext()) {
