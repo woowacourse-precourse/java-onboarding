e51ec7e69e8196faf0af82dc9bb189abcc45a710
Eunkeee <eunkeeeee@gmail.com>
2022-11-28 00:44:09 +0900

    [P3] refactor: 구현 로직 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,13 +1,6 @@
 package onboarding;
 
-import java.util.List;
-
 public class Problem3 {
-
-    public static final String COUNT_THREE = "3";
-    public static final String COUNT_SIX = "6";
-    public static final String COUNT_NINE = "9";
-
     public static int solution(int number) {
         return calculateTotalClaps(number);
     }
@@ -21,20 +14,8 @@ public class Problem3 {
     }
 
     private static int calculateCurrentNumberClaps(int currentNumber) {
-        int claps = 0;
-        for (String digit : splitNumberToDigits(currentNumber)) {
-            if (hasToClap(digit)) {
-                claps++;
-            }
-        }
-        return claps;
+        int originalLength = String.valueOf(currentNumber).length();
+        return originalLength - String.valueOf(currentNumber).replaceAll("[369]", "").length();
     }
 
-    private static boolean hasToClap(String digit) {
-        return digit.equals(COUNT_THREE) || digit.equals(COUNT_SIX) || digit.equals(COUNT_NINE);
-    }
-
-    private static List<String> splitNumberToDigits(int currentNumber) {
-        return List.of(String.valueOf(currentNumber).split(""));
-    }
 }
