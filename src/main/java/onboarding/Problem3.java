200a5a93dee05e4b3ef1c988cd649ab722b5f295
Eunkeee <eunkeeeee@gmail.com>
2022-11-28 00:34:29 +0900

    [P3] feat: 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return calculateTotalClaps(number);
+    }
+
+    private static int calculateTotalClaps(int number) {
+        int totalClaps = 0;
+        for (int currentNumber = 1; currentNumber <= number; currentNumber++) {
+            int claps = 0;
+            List<String> digits = List.of(String.valueOf(currentNumber).split(""));
+            for (String digit : digits) {
+                if (digit.equals("3") || digit.equals("6") || digit.equals("9")) {
+                    claps++;
+                }
+            }
+            totalClaps += claps;
+        }
+        return totalClaps;
     }
 }
