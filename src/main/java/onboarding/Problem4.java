2532007a0a2b49cd5179ec1e8f5b2b37514a8a5d
Eunkeee <eunkeeeee@gmail.com>
2022-11-28 15:21:32 +0900

    [P4] refactor: 메서드 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,5 @@
 package onboarding;
 
-import java.security.spec.RSAOtherPrimeInfo;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Problem4 {
 
     public static final int VALUE_UPPER_A = 65;
@@ -12,16 +8,33 @@ public class Problem4 {
     public static final int VALUE_LOWER_Z = 122;
 
     public static String solution(String word) {
+
         StringBuilder answer = new StringBuilder();
-        for (int value : word.toCharArray()) {
-            if (value >= VALUE_UPPER_A && value <= VALUE_UPPER_Z) {
-                value = VALUE_UPPER_A + VALUE_UPPER_Z - value;
-            }
-            if (value >= VALUE_LOWER_A &&value <= VALUE_LOWER_Z) {
-                value = VALUE_LOWER_A + VALUE_LOWER_Z - value;
-            }
-            answer.append((char)value);
+
+        for (char value : word.toCharArray()) {
+            answer.append(getNewValue(value));
         }
+
         return answer.toString();
+    }
+
+    private static char getNewValue(char value) {
+        if (Character.isAlphabetic(value)) {
+            if (Character.isUpperCase(value)) {
+                return getNewUpperCaseValue(value);
+            }
+            if (Character.isLowerCase(value)) {
+                return getNewLowerCaseValue(value);
+            }
+        }
+        return value;
+    }
+
+    private static char getNewLowerCaseValue(char value) {
+        return (char) (VALUE_LOWER_A + VALUE_LOWER_Z - value);
+    }
+
+    private static char getNewUpperCaseValue(char value) {
+        return (char) (VALUE_UPPER_A + VALUE_UPPER_Z - value);
     }
 }
