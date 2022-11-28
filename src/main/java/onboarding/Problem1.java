bbf1dabe8b9b18bce2c8b289bad01ea3053eb1f3
glass <glass7312@gmail.com>
2022-11-28 14:57:01 +0900

    refactor:코드 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,7 +2,6 @@ package onboarding;
 
 import java.util.*;
 
-
 class Problem1 {
     public static int get_sum(int num){
         return get_digitList(num).stream().mapToInt(Integer::intValue).sum();
@@ -10,14 +9,12 @@ class Problem1 {
     public static int get_prod(int num) {
         return get_digitList(num).stream().reduce(1,(a,b) -> a*b);
     }
-    public static int get_max(int num1, int num2) {
-        return num1 > num2 ? num1 : num2;
-    }
+ 
     public static int compare_max(List<Integer> digit_ls) {
-
-        int num=0,max=0;
+        int num;
+        int max=0;
         for(int i=0; i<2; i++){
-            num=get_max(get_sum(digit_ls.get(i)),get_prod(digit_ls.get(i)));
+            num=Math.max(get_sum(digit_ls.get(i)),get_prod(digit_ls.get(i)));
             if(num>max){
                 max=num;
             }
@@ -39,11 +36,12 @@ class Problem1 {
 
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int Max_p=0,Max_c = 0;
+        int Max_p,Max_c;
         if(1!=(pobi.get(1)-pobi.get(0))) return -1; if(1!=(crong.get(1))-crong.get(0)) return -1;
         Max_p=compare_max(pobi);
         Max_c = compare_max(crong);
-        int answer = (Max_p > Max_c) ? 1 : 0;
-        return answer;
+        if (Max_p > Max_c) return 1;
+        return  0;
+       // return answer;
     }
 }
