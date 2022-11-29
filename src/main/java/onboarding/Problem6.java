8db52c60b55619a8947bbbc3c945270b60a1d0e5
soyunim <togksk123@naver.com>
2022-11-29 15:16:58 +0900

    refactor: 리스트명 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> nameList = new ArrayList<>();
-        List<String> sameList = new ArrayList<>();
+        List<String> name = new ArrayList<>();
+        List<String> same = new ArrayList<>();
         List<String> isOverlap = new ArrayList<>();
         List<String> resultName = new ArrayList<>();
         List<String> result = new ArrayList<>();
@@ -15,37 +15,37 @@ public class Problem6 {
         int cnt = 0;
 
         for(int i=0; i<forms.size(); i++){
-            nameList.add(forms.get(i).get(1));
+            name.add(forms.get(i).get(1));
         }
 
         // sameList value's len is more than 2
-        for (int i = 0; i < nameList.size(); i++) {
-            for (int j = 0; j < nameList.get(i).length()-1; j++) {
-                for (int k = j+1; k < nameList.get(i).length(); k++) {
-                    temp=nameList.get(i).substring(j,k+1);
-                    if(!sameList.contains(temp)){
-                        sameList.add(temp);
+        for (int i = 0; i < name.size(); i++) {
+            for (int j = 0; j < name.get(i).length()-1; j++) {
+                for (int k = j+1; k < name.get(i).length(); k++) {
+                    temp=name.get(i).substring(j,k+1);
+                    if(!same.contains(temp)){
+                        same.add(temp);
                     }
                 }
             }
         }
 
         // remove the full nickname
-        for (int i = 0; i < sameList.size(); i++) {
-            for(int j = 0; j < nameList.size(); j++){
-                if(sameList.get(i).equals(nameList.get(j))){
-                    sameList.remove(i);
+        for (int i = 0; i < same.size(); i++) {
+            for(int j = 0; j < name.size(); j++){
+                if(same.get(i).equals(name.get(j))){
+                    same.remove(i);
                 }
             }
         }
 
         // add the name if there is duplication about samelist value
-        for(int i = 0; i < sameList.size(); i++){
+        for(int i = 0; i < same.size(); i++){
             cnt=0;
-            for(int j = 0; j < nameList.size(); j++){
-                if(nameList.get(j).contains(sameList.get(i))){
+            for(int j = 0; j < name.size(); j++){
+                if(name.get(j).contains(same.get(i))){
                     cnt++;
-                    isOverlap.add(nameList.get(j));
+                    isOverlap.add(name.get(j));
                 }
             }
             // cnt==1 means there is no duplication
