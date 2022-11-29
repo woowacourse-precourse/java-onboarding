923881510eb78dec2ebfe571092a9256831e4921
buddy <jang_taehwan@naver.com>
2022-11-29 12:09:31 +0900

    refactor(PROBLEM6): Crew 매직넘버 상수화

--- src/main/java/onboarding/problem6/Crew.java
+++ src/main/java/onboarding/problem6/Crew.java
@@ -2,6 +2,13 @@ package onboarding.problem6;
 
 public class Crew {
 
+    private static final String EMAIL_REGEX = ".*(@email.com)$";
+    private static final String NICKNAME_REGEX = "^([ㄱ-ㅎㅏ-ㅣ가-힣]).*";
+    private static final int EMAIL_MAX_LENGTH = 19;
+    private static final int EMAIL_MIN_LENGTH = 11;
+    private static final int NICKNAME_MAX_LENGTH = 19;
+    private static final int NICKNAME_MIN_LENGTH = 1;
+
     private final String email;
     private final String nickname;
 
@@ -11,18 +18,20 @@ public class Crew {
     }
 
     private String validateEmail(String email) {
-        if (!email.matches(".*(@email.com)$")) {
+        if (!email.matches(EMAIL_REGEX)) {
             throw new RuntimeException("Email 형식이 올바르지 않습니다.");
-        } else if (email.length() < 11 || email.length() >= 20) {
+        }
+        if (email.length() < EMAIL_MIN_LENGTH || email.length() > EMAIL_MAX_LENGTH) {
             throw new RuntimeException("Email 은 11자 이상 20자 미만의 길이만 사용 가능합니다.");
         }
         return email;
     }
 
     private String validateNickname(String nickname) {
-        if (!nickname.matches("^([ㄱ-ㅎㅏ-ㅣ가-힣]).*")) {
+        if (!nickname.matches(NICKNAME_REGEX)) {
             throw new RuntimeException("Nickname 형식이 올바르지 않습니다.");
-        } else if (nickname.length() < 1 || nickname.length() >= 20) {
+        }
+        if (nickname.length() < NICKNAME_MIN_LENGTH || nickname.length() > NICKNAME_MAX_LENGTH) {
             throw new RuntimeException("Nickname 은 1자 이상 20자 미만의 길이만 사용 가능합니다.");
         }
         return nickname;
