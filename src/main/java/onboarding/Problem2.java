58dd1a6c701d95d36121ff54328305dbda0de35d
buddy <jang_taehwan@naver.com>
2022-11-29 11:48:17 +0900

    refactor(PROBLEM2): 정적 메서드를 사용하도록 로직 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,7 +5,6 @@ import onboarding.problem2.Decoder;
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        Decoder decoder = new Decoder();
-        return decoder.decode(cryptogram);
+        return Decoder.decode(cryptogram);
     }
 }
--- src/main/java/onboarding/problem2/Decoder.java
+++ src/main/java/onboarding/problem2/Decoder.java
@@ -5,15 +5,13 @@ import java.util.regex.Pattern;
 
 public class Decoder {
 
-    private static final String REGEX = "(([a-z])\\2+)";
+    private static final String REGEX_STRING = "(([a-z])\\2+)";
+    private static final Pattern REGEX_PATTERN = Pattern.compile("(([a-z])\\2+)");
 
-    private final Pattern pattern;
-
-    public Decoder() {
-        this.pattern = Pattern.compile(REGEX);
+    private Decoder() {
     }
 
-    public String decode(String cryptogram) {
+    public static String decode(String cryptogram) {
         validateCryptogram(cryptogram);
 
         while (hasDuplicates(cryptogram)) {
@@ -22,19 +20,20 @@ public class Decoder {
         return cryptogram;
     }
 
-    private boolean hasDuplicates(String cryptogram) {
-        Matcher matcher = pattern.matcher(cryptogram);
+    private static boolean hasDuplicates(String cryptogram) {
+        Matcher matcher = REGEX_PATTERN.matcher(cryptogram);
         return matcher.find();
     }
 
-    private String deleteDuplicates(String cryptogram) {
-        return cryptogram.replaceAll(REGEX, "");
+    private static String deleteDuplicates(String cryptogram) {
+        return cryptogram.replaceAll(REGEX_STRING, "");
     }
 
-    private void validateCryptogram(String cryptogram) {
+    private static void validateCryptogram(String cryptogram) {
         if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
             throw new RuntimeException("cryptogram 은 1 이상 1000 이하의 길이만 사용 가능합니다.");
-        } else if (!cryptogram.matches("^[a-z]*$")) {
+        }
+        if (!cryptogram.matches("^[a-z]*$")) {
             throw new RuntimeException("cryptogram 은 소문자만 사용 가능합니다.");
         }
     }
