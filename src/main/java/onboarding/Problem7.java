bdc6ef2b0925c94cd5721bdb52b465df510b1234
soyunim <togksk123@naver.com>
2022-11-29 15:26:31 +0900

    refactor: problem7 리스트명 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> myFriend = new ArrayList<>();
-        List<List<String>> theirFriend = new ArrayList<>();
-        List<String> listName = new ArrayList<>();
-        List<Integer> listScore = new ArrayList<>();
+        List<String> myFriends = new ArrayList<>();
+        List<List<String>> theirFriends = new ArrayList<>();
+        List<String> names = new ArrayList<>();
+        List<Integer> scores = new ArrayList<>();
 
         int idx;
         int temp;
@@ -16,43 +16,43 @@ public class Problem7 {
         // Who is my friend?
         for(int i=0; i<friends.size(); i++){
             if(friends.get(i).get(0).equals(user)){
-                myFriend.add(friends.get(i).get(1));
-                listName.add(friends.get(i).get(1));
-                listScore.add(0);
+                myFriends.add(friends.get(i).get(1));
+                names.add(friends.get(i).get(1));
+                scores.add(0);
             }
             else if(friends.get(i).get(1).equals(user)){
-                myFriend.add(friends.get(i).get(0));
-                listName.add(friends.get(i).get(0));
-                listScore.add(0);
+                myFriends.add(friends.get(i).get(0));
+                names.add(friends.get(i).get(0));
+                scores.add(0);
             }
             else{
-                theirFriend.add(friends.get(i));
+                theirFriends.add(friends.get(i));
             }
         }
 
         // if my friend's friend, add score
-        for(int i=0; i<theirFriend.size()-1; i++){
-            for(int j=0; j<myFriend.size(); j++){
-                if(theirFriend.get(i).get(0).equals(myFriend.get(j))){
-                    if(listName.contains(theirFriend.get(i).get(1))){
-                        idx=listName.indexOf(theirFriend.get(i).get(1));
-                        temp=listScore.get(i)+10;
-                        listScore.set(idx,temp);
+        for(int i=0; i<theirFriends.size()-1; i++){
+            for(int j=0; j<myFriends.size(); j++){
+                if(theirFriends.get(i).get(0).equals(myFriends.get(j))){
+                    if(names.contains(theirFriends.get(i).get(1))){
+                        idx=names.indexOf(theirFriends.get(i).get(1));
+                        temp=scores.get(i)+10;
+                        scores.set(idx,temp);
                     }
                     else{
-                        listName.add(theirFriend.get(i).get(1));
-                        listScore.add(10);
+                        names.add(theirFriends.get(i).get(1));
+                        scores.add(10);
                     }
                 }
-                else if(theirFriend.get(i).get(1).equals(myFriend.get(j))){
-                    if(listName.contains(theirFriend.get(i).get(0))){
-                        idx=listName.indexOf(theirFriend.get(i).get(0));
-                        temp=listScore.get(i)+10;
-                        listScore.set(idx,temp);
+                else if(theirFriends.get(i).get(1).equals(myFriends.get(j))){
+                    if(names.contains(theirFriends.get(i).get(0))){
+                        idx=names.indexOf(theirFriends.get(i).get(0));
+                        temp=scores.get(i)+10;
+                        scores.set(idx,temp);
                     }
                     else{
-                        listName.add(theirFriend.get(i).get(0));
-                        listScore.add(10);
+                        names.add(theirFriends.get(i).get(0));
+                        scores.add(10);
                     }
                 }
             }
@@ -60,47 +60,46 @@ public class Problem7 {
 
         // add visitors, add score
         for (int i = 0; i < visitors.size(); i++) {
-            if(listName.contains(visitors.get(i))){
-                idx=listName.indexOf(visitors.get(i));
-                temp=listScore.get(idx)+3;
-                listScore.set(idx,temp);
+            if(names.contains(visitors.get(i))){
+                idx=names.indexOf(visitors.get(i));
+                temp=scores.get(idx)+3;
+                scores.set(idx,temp);
             }
             else{
-                listName.add(visitors.get(i));
-                listScore.add(3);
+                names.add(visitors.get(i));
+                scores.add(3);
             }
         }
 
         // remove my friend in listName
-        for (int j = 0; j < myFriend.size(); j++) {
-            if(listName.contains(myFriend.get(j))){
-                idx=listName.indexOf(myFriend.get(j));
-                listName.remove(idx);
-                listScore.remove(idx);
+        for (int j = 0; j < myFriends.size(); j++) {
+            if(names.contains(myFriends.get(j))){
+                idx=names.indexOf(myFriends.get(j));
+                names.remove(idx);
+                scores.remove(idx);
             }
         }
 
         // sort order
         String change;
-        for (int i = 0; i < listScore.size()-1; i++) {
-            for (int j = i+1; j < listScore.size(); j++) {
-                if(listScore.get(i)<listScore.get(j)){
-                    temp=listScore.get(i);
-                    listScore.set(i,listScore.get(j));
-                    listScore.set(j,temp);
-                    change=listName.get(i);
-                    listName.set(i,listName.get(j));
-                    listName.set(j,change);
+        for (int i = 0; i < scores.size()-1; i++) {
+            for (int j = i+1; j < scores.size(); j++) {
+                if(scores.get(i)<scores.get(j)){
+                    temp=scores.get(i);
+                    scores.set(i,scores.get(j));
+                    scores.set(j,temp);
+                    change=names.get(i);
+                    names.set(i,names.get(j));
+                    names.set(j,change);
                 }
-                if(listScore.get(i).equals(listScore.get(j)) && listName.get(i).compareTo(listName.get(j))>0){
-                    change=listName.get(i);
-                    listName.set(i,listName.get(j));
-                    listName.set(j,change);
+                if(scores.get(i).equals(scores.get(j)) && names.get(i).compareTo(names.get(j))>0){
+                    change=names.get(i);
+                    names.set(i,names.get(j));
+                    names.set(j,change);
                 }
             }
         }
 
-        return listName;
+        return names;
     }
 }
-//test
