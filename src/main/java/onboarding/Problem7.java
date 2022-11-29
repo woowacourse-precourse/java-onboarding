151adc20760bfdf9c16e352ddff953e831ef9656
Eunkeee <eunkeeeee@gmail.com>
2022-11-29 13:10:55 +0900

    [P7] feat: 점수 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,9 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Problem7 {
@@ -12,32 +11,27 @@ public class Problem7 {
     public static final int INITIAL_SCORE = 0;
     public static final int SCORE_MUTUAL_FRIEND = 10;
     public static final int SCORE_VISITOR = 1;
-    private static HashMap<String, Integer> score;
+    private static HashMap<String, Integer> score = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
         List<String> answer = Collections.emptyList();
 
-        score = initializeScore(friends, visitors);
+        initializeScore(friends, visitors);
 
-        for(String visitor: visitors) {
+        for (String visitor : visitors) {
             score.put(visitor, score.get(visitor) + SCORE_VISITOR);
         }
 
-        Set<String> directFriendsOfUser = getDirectFriends(user, friends);
-        Set<String> mutualFriendsOfUser = getMutualFriends(friends, directFriendsOfUser);
+        List<String> directFriendsOfUser = getDirectFriends(user, friends);
+        handleMutualFriends(friends, directFriendsOfUser);
 
-        for(String mutualFriend: mutualFriendsOfUser) {
-            score.put(mutualFriend, score.get(mutualFriend) + SCORE_MUTUAL_FRIEND);
-        }
-
-
+        System.out.println(score);
 
         return answer;
     }
 
-    private static HashMap<String, Integer> initializeScore(List<List<String>> friends, List<String> visitors) {
-        HashMap<String, Integer> score = new HashMap<>();
+    private static void initializeScore(List<List<String>> friends, List<String> visitors) {
         for (List<String> pairedFriends : friends) {
             for (String friend : pairedFriends) {
                 score.put(friend, INITIAL_SCORE);
@@ -46,19 +40,19 @@ public class Problem7 {
         for (String visitor : visitors) {
             score.put(visitor, INITIAL_SCORE);
         }
-        return score;
     }
 
-    private static Set<String> getMutualFriends(List<List<String>> friends, Set<String> directFriendsOfUser) {
-        Set<String> mutualFriendsOfUser = new HashSet<>();
-        for (String directFriend : directFriendsOfUser) {
-            mutualFriendsOfUser.addAll(getDirectFriends(directFriend, friends));
+    private static void handleMutualFriends(List<List<String>> friends, List<String> directFriends) {
+        for (String directFriend : directFriends) {
+            List<String> mutualFriends = getDirectFriends(directFriend, friends);
+            for (String mutualFriend : mutualFriends) {
+                score.put(mutualFriend, score.get(mutualFriend) + SCORE_MUTUAL_FRIEND);
+            }
         }
-        return mutualFriendsOfUser;
     }
 
-    private static Set<String> getDirectFriends(String target, List<List<String>> friends) {
-        Set<String> directFriends = new HashSet<>();
+    private static List<String> getDirectFriends(String target, List<List<String>> friends) {
+        List<String> directFriends = new ArrayList<>();
         for (List<String> friendPair : getFriendPairsWithTarget(target, friends)) {
             for (String directFriend : friendPair) {
                 if (!directFriend.equals(target)) {
