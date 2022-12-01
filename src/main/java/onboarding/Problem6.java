4edc6661ee2e24f9f97cedbbee5de7dea3431963
eunbii0213 <eunbii0213@naver.com>
2022-12-01 14:34:00 +0900

    refactor: (Problem6) Refactoring

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,29 +1,26 @@
 package onboarding;
 
+import static onboarding.enums.problem6.Index.*;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Problem6 {
-    public static final int START_INDEX = 0;
-    public static final int EMAIL_INDEX = 0;
-    public static final int NAME_INDEX = 1;
     public static final String REGEX = "";
     public static List<String> targetList;
     public static List<String> compareList;
     public static String[] targetNicknameArr;
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>(findAnswer(forms));
-
-        return answer;
+        return new ArrayList<>(findAnswer(forms));
     }
 
     public static Set<String> findAnswer(List<List<String>> forms) {
         Set<String> answer = new HashSet<>();
 
-        for (int targetIndex = START_INDEX; targetIndex < forms.size() - 1; targetIndex++) {
+        for (int targetIndex = START_INDEX.getIndex(); targetIndex < forms.size() - 1; targetIndex++) {
             chooseTargetNickName(forms, targetIndex);
             compareWithTarget(forms, answer, targetIndex);
         }
@@ -43,11 +40,11 @@ public class Problem6 {
 
     public static void chooseTargetNickName(List<List<String>> forms, int targetIndex) {
         targetList = forms.get(targetIndex);
-        targetNicknameArr = targetList.get(NAME_INDEX).split(REGEX);
+        targetNicknameArr = targetList.get(NAME_INDEX.getIndex()).split(REGEX);
     }
 
     public static void findTwoSameLetter(Set<String> answer) {
-        for (int letterIndex = START_INDEX; letterIndex < targetNicknameArr.length - 1; letterIndex++) {
+        for (int letterIndex = START_INDEX.getIndex(); letterIndex < targetNicknameArr.length - 1; letterIndex++) {
             String targetTwoLetter = chooseTargetTwoLetter(letterIndex);
 
             if (isSameTwoLetter(targetTwoLetter)) {
@@ -58,12 +55,12 @@ public class Problem6 {
     }
 
     public static void addEmailAddress(Set<String> answer) {
-        answer.add(targetList.get(EMAIL_INDEX));
-        answer.add(compareList.get(EMAIL_INDEX));
+        answer.add(targetList.get(EMAIL_INDEX.getIndex()));
+        answer.add(compareList.get(EMAIL_INDEX.getIndex()));
     }
 
     public static boolean isSameTwoLetter(String targetTwoLetter) {
-        return compareList.get(NAME_INDEX).contains(targetTwoLetter);
+        return compareList.get(NAME_INDEX.getIndex()).contains(targetTwoLetter);
     }
 
     public static String chooseTargetTwoLetter(int letterIndex) {
---
+++ src/main/java/onboarding/enums/problem6/Index.java
@@ -0,0 +1,17 @@
+package onboarding.enums.problem6;
+
+public enum Index {
+	START_INDEX(0),
+	EMAIL_INDEX(0),
+	NAME_INDEX(1);
+
+	private int index;
+
+	Index(int index) {
+		this.index = index;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+}
