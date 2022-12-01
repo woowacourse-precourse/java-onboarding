89d4e204968e60542cb9d6c10218b32bc4087ea5
eunbii0213 <eunbii0213@naver.com>
2022-12-01 11:29:31 +0900

    refactor: (Problem3) Refactoring

--- src/test/java/onboarding/Problem3Test.java
+++ src/test/java/onboarding/Problem3Test.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 
 public class Problem3Test {
 	@Test
-	void case1() {
+	void 기능_테스트1() {
 		int number = 22;
 		int result = 6;
 		assertThat(Problem3.solution(number)).isEqualTo(result);
 	}
 
 	@Test
-	void case2() {
+	void 기능_테스트2() {
 		int number = 39;
 		int result = 22;
 		assertThat(Problem3.solution(number)).isEqualTo(result);
