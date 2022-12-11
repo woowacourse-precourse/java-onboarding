ab3caf687990f20db2e486b1fde381d643ebe5fc
Eunkeee <eunkeeeee@gmail.com>
2022-12-11 18:00:43 +0900

    [P3] refactor: 반복문 개선

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,21 +1,23 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
     public static int solution(int number) {
         return calculateTotalClaps(number);
     }
 
     private static int calculateTotalClaps(int number) {
-        int totalClaps = 0;
-        for (int currentNumber = 1; currentNumber <= number; currentNumber++) {
-            totalClaps += calculateCurrentNumberClaps(currentNumber);
-        }
-        return totalClaps;
+        return IntStream.rangeClosed(1,number).map(Problem3::calculateCurrentNumberClaps).sum();
     }
 
     private static int calculateCurrentNumberClaps(int currentNumber) {
         int originalLength = String.valueOf(currentNumber).length();
-        return originalLength - String.valueOf(currentNumber).replaceAll("[369]", "").length();
+        return originalLength - countTargets(currentNumber).length();
+    }
+
+    private static String countTargets(int currentNumber) {
+        return String.valueOf(currentNumber).replaceAll("[369]", "");
     }
 
 }
